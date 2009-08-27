b1f6f9a5ff1d24d85400f6274dc179cf8a59b724
dmitry pervushin <dpervushin@nvidia.com>
2009-08-28 00:20:57 +0100

    pca953x: add power management support
    Bug 1425616
    
    Change-Id: I445efaed2454a5e82fe6caad986e09058feaa58c
    Signed-off-by: dmitry pervushin <dpervushin@nvidia.com>
    Reviewed-on: http://git-master/r/345575
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Rajesh Hosakote <rhosakote@nvidia.com>
    Tested-by: Rajesh Hosakote <rhosakote@nvidia.com>
    Reviewed-by: Sumeet Gupta <sumeetg@nvidia.com>
    Reviewed-by: Sandeep Trasi <strasi@nvidia.com>

--- drivers/gpio/gpio-pca953x.c
+++ drivers/gpio/gpio-pca953x.c
@@ -3,6 +3,7 @@
  *
  *  Copyright (C) 2005 Ben Gardner <bgardner@wabtec.com>
  *  Copyright (C) 2007 Marvell International Ltd.
+ *  Copyright (C) 2014 NVIDIA CORPORATION.  All rights reserved.
  *
  *  Derived from drivers/i2c/chips/pca9539.c
  *
@@ -842,10 +843,53 @@ static const struct of_device_id pca953x_dt_ids[] = {
 
 MODULE_DEVICE_TABLE(of, pca953x_dt_ids);
 
+#ifdef CONFIG_PM
+static int pca953x_suspend(struct device *dev)
+{
+	/* we do know our state already, so nothing to save */
+	return 0;
+}
+
+static int pca953x_resume(struct device *dev)
+{
+	struct pca953x_chip *chip = i2c_get_clientdata(to_i2c_client(dev));
+	int i;
+
+	for (i = 0; i < NBANK(chip); i++) {
+		switch (chip->chip_type) {
+		case PCA953X_TYPE:
+			pca953x_write_single(chip, PCA953X_OUTPUT,
+					chip->reg_output[i], i);
+			pca953x_write_single(chip, PCA953X_DIRECTION,
+					chip->reg_direction[i], i);
+			break;
+		case PCA957X_TYPE:
+			pca953x_write_single(chip, PCA957X_OUT,
+					chip->reg_output[i], i);
+			pca953x_write_single(chip, PCA957X_CFG,
+					chip->reg_direction[i], i);
+			break;
+		default:
+			/* unknown chip type? */
+			return 0;
+		}
+	}
+	return 0;
+}
+
+static const struct dev_pm_ops pca953x_pm = {
+	.suspend_late = pca953x_suspend,
+	.resume_early = pca953x_resume,
+};
+#endif
+
 static struct i2c_driver pca953x_driver = {
 	.driver = {
 		.name	= "pca953x",
 		.of_match_table = pca953x_dt_ids,
+#ifdef CONFIG_PM
+		.pm	= &pca953x_pm,
+#endif
 	},
 	.probe		= pca953x_probe,
 	.remove		= pca953x_remove,
