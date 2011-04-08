abc73fd42b5f19c096caa5724370e6f91430418b
Steve Lin <stlin@nvidia.com>
2011-04-08 14:12:07 -0700

    ARM: tegra: usb: using proper usb phy types
    Using proper usb phy types instead of hard coded usb instance.
    Bug 800301
    
    Original-Change-Id: I24a66ce062b937a947ac6cb34e829d6b42ecf0b4
    Reviewed-on: http://git-master/r/24371
    Reviewed-by: Niket Sirsi <nsirsi@nvidia.com>
    Tested-by: Niket Sirsi <nsirsi@nvidia.com>
    Original-Change-Id: I5a4bd4d25ec8d4d4335f17d0794c4440a470a099
    
    Rebase-Id: Rc67f327a6bc401f2643d9315ab81a706755d2401

--- include/linux/usb/tegra_usb_phy.h
+++ include/linux/usb/tegra_usb_phy.h
@@ -19,6 +19,7 @@
 #include <linux/clk.h>
 #include <linux/regulator/consumer.h>
 #include <linux/usb/otg.h>
+#include <linux/platform_data/tegra_usb.h>
 
 struct tegra_utmip_config {
 	u8 hssync_start_delay;
@@ -30,12 +31,6 @@ struct tegra_utmip_config {
 	u8 xcvr_lsrslew;
 };
 
-enum tegra_ulpi_inf_type {
-	TEGRA_USB_LINK_ULPI = 0,
-	TEGRA_USB_NULL_ULPI,
-	TEGRA_USB_UHSIC,
-};
-
 struct tegra_ulpi_trimmer {
 	u8 shadow_clk_delay;	/* 0 ~ 31 */
 	u8 clock_out_delay;	/* 0 ~ 31 */
@@ -44,7 +39,6 @@ struct tegra_ulpi_trimmer {
 };
 
 struct tegra_ulpi_config {
-	enum tegra_ulpi_inf_type inf_type;
 	int reset_gpio;
 	const char *clk;
 	const struct tegra_ulpi_trimmer *trimmer;
@@ -69,13 +63,6 @@ enum tegra_usb_phy_port_speed {
 enum tegra_usb_phy_mode {
 	TEGRA_USB_PHY_MODE_DEVICE,
 	TEGRA_USB_PHY_MODE_HOST,
-};
-
-enum tegra_usb_phy_type {
-	TEGRA_USB_PHY_TYPE_UTMIP = 1,
-	TEGRA_USB_PHY_TYPE_ULPI = 2,
-	TEGRA_USB_PHY_TYPE_HSIC = 3,
-	TEGRA_USB_PHY_TYPE_ICUSB = 4,
 };
 
 struct usb_phy_plat_data {
@@ -107,6 +94,8 @@ struct tegra_usb_phy {
 	int initialized;
 };
 
+typedef void (*tegra_phy_fp)(struct tegra_usb_phy *phy);
+
 struct tegra_usb_phy *tegra_usb_phy_open(struct device *dev, int instance,
 	void __iomem *regs, void *config, enum tegra_usb_phy_mode phy_mode,
 	enum tegra_usb_phy_type usb_phy_type);
@@ -118,6 +107,8 @@ void tegra_usb_phy_clk_enable(struct tegra_usb_phy *phy);
 void tegra_usb_phy_preresume(struct tegra_usb_phy *phy);
 
 void tegra_usb_phy_postresume(struct tegra_usb_phy *phy);
+
+void tegra_ehci_post_reset(struct tegra_usb_phy *phy);
 
 void tegra_ehci_phy_restore_start(struct tegra_usb_phy *phy,
 				 enum tegra_usb_phy_port_speed port_speed);
