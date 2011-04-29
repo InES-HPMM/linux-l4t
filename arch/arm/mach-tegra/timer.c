41ce7f069c27e97400ff5a0aefd1431089a97b1f
Colin Cross <ccross@android.com>
2011-04-29 16:59:29 -0700

    ARM: tegra: timer: Add suspend and resume support
    Change-Id: I4d431037e83742e123ad81e4614354b14e32d453
    Signed-off-by: Colin Cross <ccross@android.com>
    
    Rebase-Id: R5e6252fbc10f4ae9f966e30b085b366cfadd14cb

--- arch/arm/mach-tegra/timer.c
+++ arch/arm/mach-tegra/timer.c
@@ -59,6 +59,8 @@ static void __iomem *rtc_base = IO_ADDRESS(TEGRA_RTC_BASE);
 
 static struct timespec persistent_ts;
 static u64 persistent_ms, last_persistent_ms;
+static u32 usec_offset;
+static bool usec_suspended;
 
 #define timer_writel(value, reg) \
 	__raw_writel(value, timer_reg_base + (reg))
@@ -107,7 +109,10 @@ static struct clock_event_device tegra_clockevent = {
 
 static u32 notrace tegra_read_sched_clock(void)
 {
-	return timer_readl(TIMERUS_CNTR_1US);
+	u32 cyc = usec_offset;
+	if (!usec_suspended)
+		cyc += timer_readl(TIMERUS_CNTR_1US);
+	return cyc;
 }
 
 /*
@@ -177,6 +182,31 @@ static void __init tegra_twd_init(void)
 #define tegra_twd_init()	do {} while(0)
 #endif
 
+static u32 usec_config;
+
+static int tegra_timer_suspend(void)
+{
+	usec_config = timer_readl(TIMERUS_USEC_CFG);
+
+	usec_offset += timer_readl(TIMERUS_CNTR_1US);
+	usec_suspended = true;
+
+	return 0;
+}
+
+static void tegra_timer_resume(void)
+{
+	timer_writel(usec_config, TIMERUS_USEC_CFG);
+
+	usec_offset -= timer_readl(TIMERUS_CNTR_1US);
+	usec_suspended = false;
+}
+
+static struct syscore_ops tegra_timer_syscore_ops = {
+	.suspend = tegra_timer_suspend,
+	.resume = tegra_timer_resume,
+};
+
 void __init tegra_init_timer(void)
 {
 	struct clk *clk;
@@ -242,34 +272,8 @@ void __init tegra_init_timer(void)
 	tegra_clockevent.irq = tegra_timer_irq.irq;
 	clockevents_register_device(&tegra_clockevent);
 	tegra_twd_init();
-	register_persistent_clock(NULL, tegra_read_persistent_clock);
-}
 
-#ifdef CONFIG_PM
-static u32 usec_config;
-
-static int tegra_timer_suspend(void)
-{
-	usec_config = timer_readl(TIMERUS_USEC_CFG);
-
-	return 0;
-}
-
-static void tegra_timer_resume(void)
-{
-	timer_writel(usec_config, TIMERUS_USEC_CFG);
-}
-
-static struct syscore_ops tegra_timer_syscore_ops = {
-	.suspend = tegra_timer_suspend,
-	.resume = tegra_timer_resume,
-};
-
-static int tegra_timer_syscore_init(void)
-{
 	register_syscore_ops(&tegra_timer_syscore_ops);
 
-	return 0;
+	register_persistent_clock(NULL, tegra_read_persistent_clock);
 }
-subsys_initcall(tegra_timer_syscore_init);
-#endif
