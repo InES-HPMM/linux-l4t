529103e5303d02e5cf1d9d848ef05650693b7b0f
Diwakar Tundlam <dtundlam@nvidia.com>
2011-05-27 16:09:38 -0700

    ARM: tegra: dvfs: Update DVFS tables with data for T30S
    Change-Id: I38b9752adc9e927935fe7ffe5590c41577a45809
    Reviewed-on: http://git-master/r/34381
    Reviewed-by: Niket Sirsi <nsirsi@nvidia.com>
    Tested-by: Niket Sirsi <nsirsi@nvidia.com>
    
    Rebase-Id: R318f6916f8213c25092110a8800eb506d1718b38

--- arch/arm/mach-tegra/fuse.c
+++ arch/arm/mach-tegra/fuse.c
@@ -210,11 +210,9 @@ void tegra_init_fuse(void)
 	tegra_init_speedo_data();
 #endif
 
-	pr_info("Tegra Revision: %s SKU: %d CPU Process: %d Core Process: %d "
-		"Speedo ID: %d\n",
-		tegra_revision_name[tegra_revision],
-		tegra_sku_id, tegra_cpu_process_id,
-		tegra_core_process_id, tegra_soc_speedo_id);
+	pr_info("Tegra Revision: %s SKU: %d CPU Process: %d Core Process: %d\n"
+		tegra_revision_name[tegra_revision], tegra_sku_id,
+		tegra_cpu_process_id, tegra_core_process_id);
 }
 
 unsigned long long tegra_chip_uid(void)
