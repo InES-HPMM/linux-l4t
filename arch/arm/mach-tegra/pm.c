05d11b17a39b1cf150a500ef56ba5f39784b4097
Scott Williams <scwilliams@nvidia.com>
2011-07-22 14:55:36 -0700

    ARM: tegra: Don't die if L2X0 cache is turned off
    Change-Id: I1e065ae03da74d1ebf3327f8e29df7f81aa512a8
    Signed-off-by: Scott Williams <scwilliams@nvidia.com>
    DW: Split into logical changes
    Signed-off-by: Dan Willemsen <dwillemsen@nvidia.com>
    
    Rebase-Id: Rd5df55e4c8afeea439ef6003fdf2b89064753bd2

--- arch/arm/mach-tegra/pm.c
+++ arch/arm/mach-tegra/pm.c
@@ -454,7 +454,9 @@ unsigned int tegra_idle_lp2_last(unsigned int sleep_time, unsigned int flags)
 
 	tegra_sleep_cpu(PHYS_OFFSET - PAGE_OFFSET);
 
+#ifdef CONFIG_CACHE_L2X0
 	l2x0_enable();
+#endif
 	tegra_cluster_switch_time(flags, tegra_cluster_switch_time_id_switch);
 	restore_cpu_complex();
 	cpu_cluster_pm_exit();
