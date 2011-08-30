502fcd1b88f7a138ca4fbd283d5f869ca44a049b
Sumit Bhattacharya <sumitb@nvidia.com>
2011-08-30 22:01:46 +0530

    ASoC: tegra: WM8753: Add WM8753 machine driver
    Bug 862023
    
    Change-Id: I98842fef1c4617724159414132c772ae20495062
    Reviewed-on: http://git-master/r/49873
    Tested-by: Sumit Bhattacharya <sumitb@nvidia.com>
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>
    
    Rebase-Id: Raf35ea34cc87b64d96782a03fd296451b43c2646

--- sound/soc/tegra/tegra_wm8753.c
+++ sound/soc/tegra/tegra_wm8753.c
@@ -48,8 +48,15 @@
 
 #define DRV_NAME "tegra-snd-wm8753"
 
+#define GPIO_SPKR_EN    BIT(0)
+#define GPIO_HP_MUTE    BIT(1)
+#define GPIO_INT_MIC_EN BIT(2)
+#define GPIO_EXT_MIC_EN BIT(3)
+
 struct tegra_wm8753 {
 	struct tegra_asoc_utils_data util_data;
+	struct tegra_wm8753_platform_data *pdata;
+	int gpio_requested;
 };
 
 static int tegra_wm8753_hw_params(struct snd_pcm_substream *substream,
@@ -57,6 +64,7 @@ static int tegra_wm8753_hw_params(struct snd_pcm_substream *substream,
 {
 	struct snd_soc_pcm_runtime *rtd = substream->private_data;
 	struct snd_soc_dai *codec_dai = rtd->codec_dai;
+	struct snd_soc_dai *cpu_dai = rtd->cpu_dai;
 	struct snd_soc_codec *codec = codec_dai->codec;
 	struct snd_soc_card *card = codec->card;
 	struct tegra_wm8753 *machine = snd_soc_card_get_drvdata(card);
@@ -65,6 +73,15 @@ static int tegra_wm8753_hw_params(struct snd_pcm_substream *substream,
 
 	srate = params_rate(params);
 	switch (srate) {
+	case 8000:
+	case 16000:
+	case 24000:
+	case 32000:
+	case 48000:
+	case 64000:
+	case 96000:
+		mclk = 12288000;
+		break;
 	case 11025:
 	case 22050:
 	case 44100:
@@ -72,7 +89,7 @@ static int tegra_wm8753_hw_params(struct snd_pcm_substream *substream,
 		mclk = 11289600;
 		break;
 	default:
-		mclk = 12288000;
+		mclk = 12000000;
 		break;
 	}
 
@@ -82,7 +99,25 @@ static int tegra_wm8753_hw_params(struct snd_pcm_substream *substream,
 		return err;
 	}
 
-	err = snd_soc_dai_set_sysclk(codec_dai, WM8753_MCLK, mclk,
+	err = snd_soc_dai_set_fmt(codec_dai,
+					SND_SOC_DAIFMT_I2S |
+					SND_SOC_DAIFMT_NB_NF |
+					SND_SOC_DAIFMT_CBS_CFS);
+	if (err < 0) {
+		dev_err(card->dev, "codec_dai fmt not set\n");
+		return err;
+	}
+
+	err = snd_soc_dai_set_fmt(cpu_dai,
+					SND_SOC_DAIFMT_I2S |
+					SND_SOC_DAIFMT_NB_NF |
+					SND_SOC_DAIFMT_CBS_CFS);
+	if (err < 0) {
+		dev_err(card->dev, "cpu_dai fmt not set\n");
+		return err;
+	}
+
+	err = snd_soc_dai_set_sysclk(codec_dai, 0, mclk,
 					SND_SOC_CLOCK_IN);
 	if (err < 0) {
 		dev_err(card->dev, "codec_dai clock not set\n");
@@ -96,19 +131,191 @@ static struct snd_soc_ops tegra_wm8753_ops = {
 	.hw_params = tegra_wm8753_hw_params,
 };
 
-static const struct snd_soc_dapm_widget tegra_wm8753_dapm_widgets[] = {
-	SND_SOC_DAPM_HP("Headphone Jack", NULL),
-	SND_SOC_DAPM_MIC("Mic Jack", NULL),
+static struct snd_soc_jack tegra_wm8753_hp_jack;
+
+static struct snd_soc_jack_pin tegra_wm8753_hp_jack_pins[] = {
+	{
+		.pin = "Headphone Jack",
+		.mask = SND_JACK_HEADPHONE,
+	},
 };
+
+static struct snd_soc_jack_gpio tegra_wm8753_hp_jack_gpio = {
+	.name = "headphone detect",
+	.report = SND_JACK_HEADPHONE,
+	.debounce_time = 150,
+	.invert = 0,
+};
+
+static int tegra_wm8753_event_int_spk(struct snd_soc_dapm_widget *w,
+					struct snd_kcontrol *k, int event)
+{
+	struct snd_soc_dapm_context *dapm = w->dapm;
+	struct snd_soc_card *card = dapm->card;
+	struct tegra_wm8753 *machine = snd_soc_card_get_drvdata(card);
+	struct tegra_wm8753_platform_data *pdata = machine->pdata;
+
+	if (!(machine->gpio_requested & GPIO_SPKR_EN))
+		return 0;
+
+	gpio_set_value_cansleep(pdata->gpio_spkr_en,
+				SND_SOC_DAPM_EVENT_ON(event));
+
+	return 0;
+}
+
+static int tegra_wm8753_event_hp(struct snd_soc_dapm_widget *w,
+					struct snd_kcontrol *k, int event)
+{
+	struct snd_soc_dapm_context *dapm = w->dapm;
+	struct snd_soc_card *card = dapm->card;
+	struct tegra_wm8753 *machine = snd_soc_card_get_drvdata(card);
+	struct tegra_wm8753_platform_data *pdata = machine->pdata;
+
+	if (!(machine->gpio_requested & GPIO_HP_MUTE))
+		return 0;
+
+	gpio_set_value_cansleep(pdata->gpio_hp_mute,
+				!SND_SOC_DAPM_EVENT_ON(event));
+
+	return 0;
+}
+
+static const struct snd_soc_dapm_widget tegra_wm8753_dapm_widgets[] = {
+	SND_SOC_DAPM_SPK("Int Spk", tegra_wm8753_event_int_spk),
+	SND_SOC_DAPM_OUTPUT("Earpiece"),
+	SND_SOC_DAPM_OUTPUT("Mono Out"),
+	SND_SOC_DAPM_HP("Headphone Jack", tegra_wm8753_event_hp),
+	SND_SOC_DAPM_MIC("Mic Jack", NULL),
+	SND_SOC_DAPM_INPUT("Int Mic"),
+	SND_SOC_DAPM_LINE("LineIn Jack", NULL),
+};
+
+static const struct snd_soc_dapm_route whistler_audio_map[] = {
+	{"Int Spk", NULL, "ROUT1"},
+	{"Int Spk", NULL, "LOUT1"},
+	{"Earpiece", NULL, "ROUT2"},
+	{"Earpiece", NULL, "LOUT2"},
+	{"Mono Out", NULL, "MONO1"},
+	{"Mono Out", NULL, "MONO2"},
+	{"Headphone Jack", NULL, "ROUT1"},
+	{"Headphone Jack", NULL, "LOUT1"},
+	{"Mic Bias", NULL, "Mic Jack"},
+	{"MIC1", NULL, "Mic Bias"},
+	{"Mic Bias", NULL, "Int Mic"},
+	{"MIC2", NULL, "Mic Bias"},
+	{"MIC2N", NULL, "Mic Bias"},
+	{"LINE1", NULL, "LineIn Jack"},
+	{"LINE2", NULL, "LineIn Jack"},
+};
+
+static const struct snd_kcontrol_new tegra_wm8753_controls[] = {
+	SOC_DAPM_PIN_SWITCH("Int Spk"),
+	SOC_DAPM_PIN_SWITCH("Earpiece"),
+	SOC_DAPM_PIN_SWITCH("Mono Out"),
+	SOC_DAPM_PIN_SWITCH("Headphone Jack"),
+	SOC_DAPM_PIN_SWITCH("Mic Jack"),
+	SOC_DAPM_PIN_SWITCH("Int Mic"),
+	SOC_DAPM_PIN_SWITCH("LineIn Jack"),
+};
+
+static int tegra_wm8753_init(struct snd_soc_pcm_runtime *rtd)
+{
+	struct snd_soc_codec *codec = rtd->codec;
+	struct snd_soc_dapm_context *dapm = &codec->dapm;
+	struct snd_soc_card *card = codec->card;
+	struct tegra_wm8753 *machine = snd_soc_card_get_drvdata(card);
+	struct tegra_wm8753_platform_data *pdata = machine->pdata;
+	int ret;
+
+	if (gpio_is_valid(pdata->gpio_spkr_en)) {
+		ret = gpio_request(pdata->gpio_spkr_en, "spkr_en");
+		if (ret) {
+			dev_err(card->dev, "cannot get spkr_en gpio\n");
+			return ret;
+		}
+		machine->gpio_requested |= GPIO_SPKR_EN;
+
+		gpio_direction_output(pdata->gpio_spkr_en, 0);
+	}
+
+	if (gpio_is_valid(pdata->gpio_hp_mute)) {
+		ret = gpio_request(pdata->gpio_hp_mute, "hp_mute");
+		if (ret) {
+			dev_err(card->dev, "cannot get hp_mute gpio\n");
+			return ret;
+		}
+		machine->gpio_requested |= GPIO_HP_MUTE;
+
+		gpio_direction_output(pdata->gpio_hp_mute, 0);
+	}
+
+	if (gpio_is_valid(pdata->gpio_int_mic_en)) {
+		ret = gpio_request(pdata->gpio_int_mic_en, "int_mic_en");
+		if (ret) {
+			dev_err(card->dev, "cannot get int_mic_en gpio\n");
+			return ret;
+		}
+		machine->gpio_requested |= GPIO_INT_MIC_EN;
+
+		/* Disable int mic; enable signal is active-high */
+		gpio_direction_output(pdata->gpio_int_mic_en, 0);
+	}
+
+	if (gpio_is_valid(pdata->gpio_ext_mic_en)) {
+		ret = gpio_request(pdata->gpio_ext_mic_en, "ext_mic_en");
+		if (ret) {
+			dev_err(card->dev, "cannot get ext_mic_en gpio\n");
+			return ret;
+		}
+		machine->gpio_requested |= GPIO_EXT_MIC_EN;
+
+		/* Enable ext mic; enable signal is active-low */
+		gpio_direction_output(pdata->gpio_ext_mic_en, 0);
+	}
+
+	ret = snd_soc_add_codec_controls(codec, tegra_wm8753_controls,
+				   ARRAY_SIZE(tegra_wm8753_controls));
+	if (ret < 0)
+		return ret;
+
+	snd_soc_dapm_new_controls(dapm, tegra_wm8753_dapm_widgets,
+					ARRAY_SIZE(tegra_wm8753_dapm_widgets));
+
+	snd_soc_dapm_add_routes(dapm, whistler_audio_map,
+					ARRAY_SIZE(whistler_audio_map));
+
+	if (gpio_is_valid(pdata->gpio_hp_det)) {
+		tegra_wm8753_hp_jack_gpio.gpio = pdata->gpio_hp_det;
+		snd_soc_jack_new(codec, "Headphone Jack", SND_JACK_HEADPHONE,
+				&tegra_wm8753_hp_jack);
+		snd_soc_jack_add_pins(&tegra_wm8753_hp_jack,
+					ARRAY_SIZE(tegra_wm8753_hp_jack_pins),
+					tegra_wm8753_hp_jack_pins);
+		snd_soc_jack_add_gpios(&tegra_wm8753_hp_jack,
+					1,
+					&tegra_wm8753_hp_jack_gpio);
+	}
+
+	snd_soc_dapm_nc_pin(dapm, "ACIN");
+	snd_soc_dapm_nc_pin(dapm, "ACOP");
+	snd_soc_dapm_nc_pin(dapm, "OUT3");
+	snd_soc_dapm_nc_pin(dapm, "OUT4");
+
+	snd_soc_dapm_sync(dapm);
+
+	return 0;
+}
 
 static struct snd_soc_dai_link tegra_wm8753_dai = {
 	.name = "WM8753",
-	.stream_name = "WM8753 PCM",
+	.stream_name = "WM8753 PCM HIFI",
+	.codec_name = "wm8753-codec.4-001a",
+	.platform_name = "tegra-pcm-audio",
+	.cpu_dai_name = "tegra20-i2s.0",
 	.codec_dai_name = "wm8753-hifi",
+	.init = tegra_wm8753_init,
 	.ops = &tegra_wm8753_ops,
-	.dai_fmt = SND_SOC_DAIFMT_I2S |
-			SND_SOC_DAIFMT_NB_NF |
-			SND_SOC_DAIFMT_CBS_CFS,
 };
 
 static struct snd_soc_card snd_soc_tegra_wm8753 = {
@@ -126,7 +333,14 @@ static int tegra_wm8753_driver_probe(struct platform_device *pdev)
 {
 	struct snd_soc_card *card = &snd_soc_tegra_wm8753;
 	struct tegra_wm8753 *machine;
+	struct tegra_wm8753_platform_data *pdata;
 	int ret;
+
+	pdata = pdev->dev.platform_data;
+	if (!pdata) {
+		dev_err(&pdev->dev, "No platform data supplied\n");
+		return -EINVAL;
+	}
 
 	machine = devm_kzalloc(&pdev->dev, sizeof(struct tegra_wm8753),
 			       GFP_KERNEL);
@@ -135,6 +349,8 @@ static int tegra_wm8753_driver_probe(struct platform_device *pdev)
 		ret = -ENOMEM;
 		goto err;
 	}
+
+	machine->pdata = pdata;
 
 	card->dev = &pdev->dev;
 	platform_set_drvdata(pdev, card);
@@ -192,10 +408,20 @@ static int tegra_wm8753_driver_remove(struct platform_device *pdev)
 {
 	struct snd_soc_card *card = platform_get_drvdata(pdev);
 	struct tegra_wm8753 *machine = snd_soc_card_get_drvdata(card);
+	struct tegra_wm8753_platform_data *pdata = machine->pdata;
 
 	snd_soc_unregister_card(card);
 
 	tegra_asoc_utils_fini(&machine->util_data);
+
+	if (machine->gpio_requested & GPIO_EXT_MIC_EN)
+		gpio_free(pdata->gpio_ext_mic_en);
+	if (machine->gpio_requested & GPIO_INT_MIC_EN)
+		gpio_free(pdata->gpio_int_mic_en);
+	if (machine->gpio_requested & GPIO_HP_MUTE)
+		gpio_free(pdata->gpio_hp_mute);
+	if (machine->gpio_requested & GPIO_SPKR_EN)
+		gpio_free(pdata->gpio_spkr_en);
 
 	return 0;
 }
