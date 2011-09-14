5f9ecae1d1d15585dc05ebbf9ce80f8a3a0fce2f
Jin Qian <jqian@nvidia.com>
2011-09-14 11:53:35 -0700

    ARM: tegra: reduce LP0 resume CPU power on time
    cherry-picked from adf08ef4030598a6bf9036f45584be8acc008fea
    
    Bug 862504
    
    Change-Id: I79460aa4abdccc4f2ca17867197bb12668d59dea
    Reviewed-on: http://git-master/r/52420
    Tested-by: Jin Qian <jqian@nvidia.com>
    Reviewed-by: Scott Williams <scwilliams@nvidia.com>
    Reviewed-by: Aleksandr Frid <afrid@nvidia.com>
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>
    
    Rebase-Id: Rffc2d7314a61f04274e0db116c5a1cb7004dc77d

--- arch/arm/mach-tegra/platsmp.c
+++ arch/arm/mach-tegra/platsmp.c
@@ -132,7 +132,7 @@ static int tegra30_power_up_cpu(unsigned int cpu)
 	 * On first boot entry do not wait - go to direct ungate.
 	 */
 	if (cpu_isset(cpu, tegra_cpu_init_map)) {
-		timeout = jiffies + HZ;
+		timeout = jiffies + 5;
 		do {
 			if (tegra_powergate_is_powered(pwrgateid))
 				goto remove_clamps;
