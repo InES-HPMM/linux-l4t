0773bc4cb0cc153d07786b8000a8425023c306a1
Artiste Hsu <chhsu@nvidia.com>
2011-09-27 09:47:32 +0800

    usb: host: tegra: Configurable USB hotplug
    Hotplug on usb is supported when hotplug flag is set in platform data
    
    Bug 869745
    
    (cherry picked from commit I57ba0b18a2d4232b2df2074c91aff97c6e639e7d)
    Reviewed-on: http://git-master/r/54588
    
    Change-Id: I24e5ddf49cead14d3eebe30da0f92402015c95c7
    Reviewed-on: http://git-master/r/57725
    Tested-by: Artiste Hsu <chhsu@nvidia.com>
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>
    
    Rebase-Id: R63a77b717f60a42441f3ae7794a2f56573efb250

--- drivers/usb/host/ehci-tegra.c
+++ drivers/usb/host/ehci-tegra.c
@@ -71,6 +71,7 @@ struct tegra_ehci_hcd {
 	struct usb_phy *transceiver;
 	int host_resumed;
 	int port_resuming;
+	int hotplug;
 	struct delayed_work work;
 	enum tegra_usb_phy_port_speed port_speed;
 	struct work_struct clk_timer_work;
@@ -183,7 +184,7 @@ static irqreturn_t tegra_ehci_irq (struct usb_hcd *hcd)
 			spin_unlock (&ehci->lock);
 		}
 	}
-	if (tegra->phy->instance == 2) {
+	if (tegra->hotplug) {
 		spin_lock(&ehci->lock);
 		val = readl(hcd->regs + TEGRA_USB_SUSP_CTRL_OFFSET);
 		if ((val  & TEGRA_USB_PHY_CLK_VALID_INT_STS)) {
@@ -1195,6 +1196,7 @@ static int tegra_ehci_probe(struct platform_device *pdev)
 	}
 
 	tegra->host_resumed = 1;
+	tegra->hotplug = pdata->hotplug;
 	tegra->ehci = hcd_to_ehci(hcd);
 
 	irq = platform_get_irq(pdev, 0);
