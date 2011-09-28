fc7895559166da4ecab93997b63213365c621e3f
Sumit Bhattacharya <sumitb@nvidia.com>
2011-09-28 20:31:23 +0530

    ASoC: Tegra: Enable SPDIF DAI for Tegra30
    Bug 872652
    
    Change-Id: Ia35ba728859bcba1bff1ce73c622203d027bc8f9
    Signed-off-by: Sumit Bhattacharya <sumitb@nvidia.com>
    Reviewed-on: http://git-master/r/54989
    Reviewed-by: Scott Peterson <speterson@nvidia.com>
    Reviewed-by: Stephen Warren <swarren@nvidia.com>
    Tested-by: Gerrit_Virtual_Submit
    
    Rebase-Id: Rd23d694c056cefb76669c510b1367128255f3835

--- sound/soc/tegra/tegra_wm8903.c
+++ sound/soc/tegra/tegra_wm8903.c
@@ -239,7 +239,6 @@ static struct snd_soc_dai_link tegra_wm8903_dai[] = {
 			   SND_SOC_DAIFMT_NB_NF |
 			   SND_SOC_DAIFMT_CBS_CFS,
 	},
-#if defined(CONFIG_ARCH_TEGRA_2x_SOC)
 	{
 		.name = "SPDIF",
 		.stream_name = "SPDIF PCM",
@@ -252,7 +251,6 @@ static struct snd_soc_dai_link tegra_wm8903_dai[] = {
 			   SND_SOC_DAIFMT_NB_NF |
 			   SND_SOC_DAIFMT_CBS_CFS,
 	}
-#endif
 };
 
 static struct snd_soc_card snd_soc_tegra_wm8903 = {
@@ -295,6 +293,8 @@ static int tegra_wm8903_driver_probe(struct platform_device *pdev)
 	if (machine_is_cardhu()) {
 		tegra_wm8903_dai[0].codec_name = "wm8903.4-001a",
 		tegra_wm8903_dai[0].cpu_dai_name = "tegra30-i2s.1";
+
+		tegra_wm8903_dai[1].cpu_dai_name = "tegra30-spdif";
 	}
 
 	card->dev = &pdev->dev;
