d5901a4e0ec638d4eaa21fc494a85ba6f5edb6c1
Manoj Chourasia <mchourasia@nvidia.com>
2011-11-22 23:41:37 +0530

    tegra: pcie: Remove unnecessary clock operations
    All these clock operations should be handled by
    powergate operations.
    
    bug 840051
    
    Reviewed-on: http://git-master/r/66177
    (cherry picked from commit 1ad8fe3e184db04063275c837e240827bda009e9)
    
    Change-Id: I0159c6c1f64932b22b25d31d4bb1ff9d41385879
    Reviewed-on: http://git-master/r/86126
    Tested-by: Jay Agarwal <jagarwal@nvidia.com>
    Reviewed-by: Manoj Chourasia <mchourasia@nvidia.com>
    Reviewed-by: Krishna Thota <kthota@nvidia.com>
    Reviewed-by: Mayuresh Kulkarni <mkulkarni@nvidia.com>
    Reviewed-by: Varun Wadekar <vwadekar@nvidia.com>
    
    Rebase-Id: R51086a12e0d30b6aa707d18072676762fa6fd127

--- arch/arm/mach-tegra/powergate.c
+++ arch/arm/mach-tegra/powergate.c
@@ -134,6 +134,9 @@ static struct powergate_partition powergate_partition_info[] = {
 						{MC_CLIENT_AFI, MC_CLIENT_LAST},
 						{{"afi", CLK_AND_RST},
 						{"pcie", CLK_AND_RST},
+#ifndef CONFIG_ARCH_TEGRA_2x_SOC
+						{"cml0", CLK_ONLY},
+#endif
 						{"pciex", RST_ONLY} }, },
 	[TEGRA_POWERGATE_VDEC]	= { "vde",
 						{MC_CLIENT_VDE, MC_CLIENT_LAST},
