ab90ce38e49477ca66eebc3704df9ae626fb52cf
Alex Frid <afrid@nvidia.com>
2012-01-12 17:39:04 -0800

    ARM: tegra: clock: Auto-detect PLLP rate in uart init
    Tegra3 platform may boot with one of the predefined fixed PLLP
    (peripheral PLL) output rates: 216MHz, 408MHz, or 204MHz. This
    commit implements auto-detection of PLLP rate, and debug uart
    configuration during kernel uart initialization.
    
    Bug 928260
    
    Change-Id: I3fac4c462f28ac3dc1c72c0cc0f8f87fa0a809c4
    Reviewed-on: http://git-master/r/75849
    Reviewed-by: Krishna Reddy <vdumpa@nvidia.com>
    Signed-off-by: Alex Frid <afrid@nvidia.com>
    Signed-off-by: Varun Wadekar <vwadekar@nvidia.com>
    Reviewed-on: http://git-master/r/77294
    Reviewed-by: Automatic_Commit_Validation_User
    
    Rebase-Id: R852359d6b2696a8080239f7a8df303cb24907b68

--- arch/arm/mach-tegra/devices.c
+++ arch/arm/mach-tegra/devices.c
@@ -26,6 +26,7 @@
 #include <linux/i2c-tegra.h>
 #include <linux/tegra_avp.h>
 #include <linux/nvhost.h>
+#include <linux/clk.h>
 #include <mach/irqs.h>
 #include <linux/usb/tegra_usb_phy.h>
 #include <mach/tegra_smmu.h>
@@ -1785,3 +1786,20 @@ struct platform_device tegra_nvmap_device = {
 	.name	= "tegra-nvmap",
 	.id	= -1,
 };
+
+void tegra_init_debug_uart_rate(void)
+{
+	unsigned int uartclk;
+	struct clk *debug_uart_parent = clk_get_sys(NULL, "pll_p");
+
+	BUG_ON(IS_ERR(debug_uart_parent));
+	uartclk = clk_get_rate(debug_uart_parent);
+
+	debug_uarta_platform_data[0].uartclk = uartclk;
+	debug_uartb_platform_data[0].uartclk = uartclk;
+	debug_uartc_platform_data[0].uartclk = uartclk;
+	debug_uartd_platform_data[0].uartclk = uartclk;
+#if !defined(CONFIG_ARCH_TEGRA_2x_SOC)
+	debug_uarte_platform_data[0].uartclk = uartclk;
+#endif
+}
