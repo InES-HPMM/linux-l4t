d38a9625fd2d8ed00f225545b30bea83961dbc4d
Venu Byravarasu <vbyravarasu@nvidia.com>
2012-01-20 14:25:53 +0530

    arm: tegra: pci: fix section mismatch warnings.
    Section mismatch warning messages were due to declaring tegra_pcie_hw using
     __initdata & tegra_pcie_scan_bus using __init.
    Hence removed them.
    
    bug 929358
    
    Reviewed-on: http://git-master/r/76495
    
    Change-Id: I1894c1c360e0fc0f3da18bbe840e88afa4de6ffc
    Signed-off-by: Venu Byravarasu <vbyravarasu@nvidia.com>
    Signed-off-by: Varun Wadekar <vwadekar@nvidia.com>
    Reviewed-on: http://git-master/r/77308
    Reviewed-by: Automatic_Commit_Validation_User
    
    Rebase-Id: R76dfb3e70f6aec535255c8bb6372e6403bd5d94a

--- arch/arm/mach-tegra/pcie.c
+++ arch/arm/mach-tegra/pcie.c
@@ -545,7 +545,7 @@ static int tegra_pcie_map_irq(const struct pci_dev *dev, u8 slot, u8 pin)
 	return INT_PCIE_INTR;
 }
 
-static struct pci_bus __init *tegra_pcie_scan_bus(int nr,
+static struct pci_bus *tegra_pcie_scan_bus(int nr,
 						  struct pci_sys_data *sys)
 {
 	struct tegra_pcie_port *pp;
@@ -560,7 +560,7 @@ static struct pci_bus __init *tegra_pcie_scan_bus(int nr,
 				 &sys->resources);
 }
 
-static struct hw_pci tegra_pcie_hw __initdata = {
+static struct hw_pci tegra_pcie_hw = {
 	.nr_controllers	= MAX_PCIE_SUPPORTED_PORTS,
 	.setup		= tegra_pcie_setup,
 	.scan		= tegra_pcie_scan_bus,
