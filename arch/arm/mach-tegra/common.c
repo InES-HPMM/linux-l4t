4b7d9fa677f221445eada07252dd590c1e578f86
Hiro Sugawara <hsugawara@nvidia.com>
2012-02-17 17:05:02 -0800

    ARM: tegra: common: Fix checkpatch errors and warnings
    Change-Id: I5ce31a7dd41e5fc699a2f23179085de0cbbd385c
    Signed-off-by: Hiro Sugawara <hsugawara@nvidia.com>
    Reviewed-on: http://git-master/r/86661
    Reviewed-by: Scott Williams <scwilliams@nvidia.com>
    
    Rebase-Id: Recd77185e07c81db1fe5064663d021218ba615ef

--- arch/arm/mach-tegra/common.c
+++ arch/arm/mach-tegra/common.c
@@ -141,7 +141,7 @@ void __init tegra_dt_init_irq(void)
 void tegra_assert_system_reset(char mode, const char *cmd)
 {
 #if defined(CONFIG_TEGRA_FPGA_PLATFORM) || NEVER_RESET
-	printk("tegra_assert_system_reset() ignored.....");
+	pr_info("tegra_assert_system_reset() ignored.....");
 	do { } while (1);
 #else
 	void __iomem *reset = IO_ADDRESS(TEGRA_PMC_BASE + 0);
@@ -331,7 +331,7 @@ void tegra_init_cache(bool init)
 static void __init tegra_init_power(void)
 {
 #ifdef CONFIG_ARCH_TEGRA_HAS_SATA
-        tegra_powergate_partition_with_clk_off(TEGRA_POWERGATE_SATA);
+	tegra_powergate_partition_with_clk_off(TEGRA_POWERGATE_SATA);
 #endif
 #ifdef CONFIG_ARCH_TEGRA_HAS_PCIE
 	tegra_powergate_partition_with_clk_off(TEGRA_POWERGATE_PCIE);
@@ -375,22 +375,26 @@ static void __init tegra_init_ahb_gizmo_settings(void)
 
 	val = gizmo_readl(AHB_MEM_PREFETCH_CFG1);
 	val &= ~MST_ID(~0);
-	val |= PREFETCH_ENB | AHBDMA_MST_ID | ADDR_BNDRY(0xc) | INACTIVITY_TIMEOUT(0x1000);
+	val |= PREFETCH_ENB | AHBDMA_MST_ID |
+		ADDR_BNDRY(0xc) | INACTIVITY_TIMEOUT(0x1000);
 	gizmo_writel(val, AHB_MEM_PREFETCH_CFG1);
 
 	val = gizmo_readl(AHB_MEM_PREFETCH_CFG2);
 	val &= ~MST_ID(~0);
-	val |= PREFETCH_ENB | USB_MST_ID | ADDR_BNDRY(0xc) | INACTIVITY_TIMEOUT(0x1000);
+	val |= PREFETCH_ENB | USB_MST_ID | ADDR_BNDRY(0xc) |
+		INACTIVITY_TIMEOUT(0x1000);
 	gizmo_writel(val, AHB_MEM_PREFETCH_CFG2);
 
 	val = gizmo_readl(AHB_MEM_PREFETCH_CFG3);
 	val &= ~MST_ID(~0);
-	val |= PREFETCH_ENB | USB3_MST_ID | ADDR_BNDRY(0xc) | INACTIVITY_TIMEOUT(0x1000);
+	val |= PREFETCH_ENB | USB3_MST_ID | ADDR_BNDRY(0xc) |
+		INACTIVITY_TIMEOUT(0x1000);
 	gizmo_writel(val, AHB_MEM_PREFETCH_CFG3);
 
 	val = gizmo_readl(AHB_MEM_PREFETCH_CFG4);
 	val &= ~MST_ID(~0);
-	val |= PREFETCH_ENB | USB2_MST_ID | ADDR_BNDRY(0xc) | INACTIVITY_TIMEOUT(0x1000);
+	val |= PREFETCH_ENB | USB2_MST_ID | ADDR_BNDRY(0xc) |
+		INACTIVITY_TIMEOUT(0x1000);
 	gizmo_writel(val, AHB_MEM_PREFETCH_CFG4);
 }
 
@@ -855,7 +859,8 @@ void __init tegra_reserve(unsigned long carveout_size, unsigned long fb_size,
 			tegra_lp0_vec_start + tegra_lp0_vec_size - 1 : 0,
 		tegra_bootloader_fb_start,
 		tegra_bootloader_fb_size ?
-			tegra_bootloader_fb_start + tegra_bootloader_fb_size - 1 : 0,
+			tegra_bootloader_fb_start + tegra_bootloader_fb_size - 1
+			: 0,
 		tegra_fb_start,
 		tegra_fb_size ?
 			tegra_fb_start + tegra_fb_size - 1 : 0,
@@ -866,8 +871,7 @@ void __init tegra_reserve(unsigned long carveout_size, unsigned long fb_size,
 		tegra_carveout_size ?
 			tegra_carveout_start + tegra_carveout_size - 1 : 0,
 		tegra_vpr_start,
-		tegra_vpr_size ?
-			tegra_vpr_start + tegra_vpr_size - 1 : 0);
+		tegra_vpr_size ? tegra_vpr_start + tegra_vpr_size - 1 : 0);
 }
 
 static struct resource ram_console_resources[] = {
@@ -877,8 +881,8 @@ static struct resource ram_console_resources[] = {
 };
 
 static struct platform_device ram_console_device = {
-	.name 		= "ram_console",
-	.id 		= -1,
+	.name		= "ram_console",
+	.id		= -1,
 	.num_resources	= ARRAY_SIZE(ram_console_resources),
 	.resource	= ram_console_resources,
 };
@@ -910,9 +914,9 @@ void __init tegra_ram_console_debug_init(void)
 	int err;
 
 	err = platform_device_register(&ram_console_device);
-	if (err) {
-		pr_err("%s: ram console registration failed (%d)!\n", __func__, err);
-	}
+	if (err)
+		pr_err("%s: ram console registration failed (%d)!\n",
+			__func__, err);
 }
 
 void __init tegra_release_bootloader_fb(void)
