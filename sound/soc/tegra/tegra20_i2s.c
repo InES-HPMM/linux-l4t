58f3d0dcce5ad4d59a371b4a94dc11fa2bb44e1d
Sumit Bhattacharya <sumitb@nvidia.com>
2012-02-20 18:53:23 +0530

    ASOC: Tegra: Fix Tegra20 BT SCO playback/record
    Set I2s FIFO attention level based on sample size and channel count.
    Also set playback DMA destination bus width and capture DMA source bus
    width based on sample size. These changes are needed to have proper
    BT SCO playback and record which uses 16bit-mono format.
    
    Bug 934101
    Bug 874428
    Bug 927978
    
    Signed-off-by: Sumit Bhattacharya <sumitb@nvidia.com>
    Reviewed-on: http://git-master/r/84817
    (cherry picked from commit 3ca2eb665af450d7e8f3bf6f2471e31203052641)
    
    Change-Id: I95c10716eaa990adb8b6ae535ce6acfca122a609
    Reviewed-on: http://git-master/r/87192
    Tested-by: Sumit Bhattacharya <sumitb@nvidia.com>
    Reviewed-by: Scott Peterson <speterson@nvidia.com>
    
    Rebase-Id: R6ff05dd621c5b1288e64d7a2edacf39568c8afe4

--- sound/soc/tegra/tegra20_i2s.c
+++ sound/soc/tegra/tegra20_i2s.c
@@ -194,9 +194,14 @@ static int tegra20_i2s_hw_params(struct snd_pcm_substream *substream,
 
 	regmap_write(i2s->regmap, TEGRA20_I2S_TIMING, val);
 
-	regmap_write(i2s->regmap, TEGRA20_I2S_FIFO_SCR,
-		     TEGRA20_I2S_FIFO_SCR_FIFO2_ATN_LVL_FOUR_SLOTS |
-		     TEGRA20_I2S_FIFO_SCR_FIFO1_ATN_LVL_FOUR_SLOTS);
+	if (sample_size * params_channels(params) >= 32)
+		regmap_write(i2s->regmap, TEGRA20_I2S_FIFO_SCR,
+			     TEGRA20_I2S_FIFO_SCR_FIFO2_ATN_LVL_FOUR_SLOTS |
+			     TEGRA20_I2S_FIFO_SCR_FIFO1_ATN_LVL_FOUR_SLOTS);
+	else
+		regmap_write(i2s->regmap, TEGRA20_I2S_FIFO_SCR,
+			     TEGRA20_I2S_FIFO_SCR_FIFO2_ATN_LVL_EIGHT_SLOTS |
+			     TEGRA20_I2S_FIFO_SCR_FIFO1_ATN_LVL_EIGHT_SLOTS);
 
 	i2s->reg_ctrl &= ~TEGRA20_I2S_CTRL_FIFO_FORMAT_MASK;
 	reg = tegra20_i2s_read(i2s, TEGRA20_I2S_PCM_CTRL);
@@ -208,12 +213,17 @@ static int tegra20_i2s_hw_params(struct snd_pcm_substream *substream,
 		else
 			i2s->reg_ctrl |= TEGRA20_I2S_CTRL_FIFO_FORMAT_32;
 
+		i2s->capture_dma_data.width = sample_size;
+		i2s->playback_dma_data.width = sample_size;
+
 		if (substream->stream == SNDRV_PCM_STREAM_PLAYBACK)
 			reg |= TEGRA20_I2S_PCM_CTRL_TRM_MODE_EN;
 		else
 			reg |= TEGRA20_I2S_PCM_CTRL_RCV_MODE_EN;
 	} else {
 		i2s->reg_ctrl |= TEGRA20_I2S_CTRL_FIFO_FORMAT_PACKED;
+		i2s->capture_dma_data.width = 32;
+		i2s->playback_dma_data.width = 32;
 		if (substream->stream == SNDRV_PCM_STREAM_PLAYBACK)
 			reg &= ~TEGRA20_I2S_PCM_CTRL_TRM_MODE_EN;
 		else
