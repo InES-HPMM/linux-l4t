526779fdc6c58b50c5c0577a393c56779511cd6e
Krupal Divvelak <kdivvela@nvidia.com>
2012-02-28 10:38:20 +0530

    ARM: tegra: curacao: Enable camera sensor
    Bug 946945
    
    Change-Id: I0418b51f3348edf89ccc24b9a35f5efbe244b9b3
    Signed-off-by: Krupal Divvela <kdivvela@nvidia.com>
    Reviewed-on: http://git-master/r/85665
    Reviewed-by: Scott Williams <scwilliams@nvidia.com>
    
    Rebase-Id: R1996100c7b26f8e76b60eeae02e2b046548de469

--- arch/arm/mach-tegra/board-curacao.c
+++ arch/arm/mach-tegra/board-curacao.c
@@ -521,6 +521,7 @@ static void __init tegra_curacao_init(void)
 	curacao_panel_init();
 	curacao_hs_uart_init();
 	curacao_bt_rfkill();
+	curacao_sensors_init();
 }
 
 static void __init tegra_curacao_reserve(void)
