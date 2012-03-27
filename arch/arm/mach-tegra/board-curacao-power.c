abf9135095423df800f3d4a7115cfa7d1df3b549
Alex Frid <afrid@nvidia.com>
2012-03-26 22:42:13 -0700

    ARM: tegra11: curacao: Emulate CL-DVFS paltform data
    Change-Id: I16695edded92c825bfbb4417d38adebfe7dae478
    Signed-off-by: Alex Frid <afrid@nvidia.com>
    Reviewed-on: http://git-master/r/92446
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Bo Yan <byan@nvidia.com>
    Reviewed-by: Scott Williams <scwilliams@nvidia.com>
    
    Rebase-Id: R5e51bf9593f1348b74fa82e12e4542934207b5ed

--- arch/arm/mach-tegra/board-curacao-power.c
+++ arch/arm/mach-tegra/board-curacao-power.c
@@ -28,6 +28,7 @@
 
 #include "pm.h"
 #include "board.h"
+#include "tegra_cl_dvfs.h"
 
 static int ac_online(void)
 {
@@ -64,9 +65,55 @@ static struct tegra_suspend_platform_data curacao_suspend_data = {
 	.sysclkreq_high	= true,
 };
 
+/* FIXME: the cl_dvfs data below is bogus, just to enable s/w
+   debugging on FPGA */
+
+/* Board characterization parameters */
+static struct tegra_cl_dvfs_cfg_param curacao_cl_dvfs_param = {
+	.sample_rate = 12500,
+
+	.force_mode = TEGRA_CL_DVFS_FORCE_FIXED,
+	.cf = 3,
+	.ci = 1,
+	.cg = 0xfa,
+
+	.droop_cut_value = 0x8,
+	.droop_restore_ramp = 0x10,
+	.scale_out_ramp = 0x2,
+};
+
+/* PMU data : assume fixed 12.5mV steps from 600mV to 1400mV starting
+   with reg value 3 */
+#define PMU_CPU_VDD_MAP_SIZE ((1400000 - 600000) / 12500 + 1)
+static struct voltage_reg_map pmu_cpu_vdd_map[PMU_CPU_VDD_MAP_SIZE];
+static void fill_reg_map(void)
+{
+	int i;
+	for (i = 0; i < PMU_CPU_VDD_MAP_SIZE; i++) {
+		pmu_cpu_vdd_map[i].reg_value = i + 3;
+		pmu_cpu_vdd_map[i].reg_uV = 600000 + 12500 * i;
+	}
+}
+
+static struct tegra_cl_dvfs_platform_data curacao_cl_dvfs_data = {
+	.pmu_if = TEGRA_CL_DVFS_PMU_I2C,
+	.u.pmu_i2c = {
+		.fs_rate = 50000,
+		.hs_master_code = 0, /* no hs mode support */
+		.slave_addr = 0x2D,
+		.reg = 0x27,
+	},
+	.vdd_map = pmu_cpu_vdd_map,
+	.vdd_map_size = PMU_CPU_VDD_MAP_SIZE,
+
+	.cfg_param = &curacao_cl_dvfs_param,
+};
+
 int __init curacao_regulator_init(void)
 {
 	platform_device_register(&curacao_pda_power_device);
+	fill_reg_map();
+	tegra_cl_dvfs_set_plarform_data(&curacao_cl_dvfs_data);
 	return 0;
 }
 
