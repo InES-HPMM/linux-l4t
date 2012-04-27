83c4c5c80f50a5a94ab3094cd1cf930075da85e2
Animesh Kishore <ankishore@nvidia.com>
2012-04-27 16:50:02 +0530

    video: tegra: dsi: Implement ganged mode
    Change comprises of:
    1. ganged mode framework
    2. ganged mode left-right symmetric
    
    Bug 944115
    
    Change-Id: Iaaeb5a843dfcfefc0d07e1d13a1684f540138ccb
    Signed-off-by: Animesh Kishore <ankishore@nvidia.com>
    Reviewed-on: http://git-master/r/99396
    Reviewed-by: Varun Colbert <vcolbert@nvidia.com>
    Tested-by: Varun Colbert <vcolbert@nvidia.com>
    
    Rebase-Id: R6098c974d39d64849ea7791fce3c40750fa3e2f1

--- arch/arm/mach-tegra/include/mach/dc.h
+++ arch/arm/mach-tegra/include/mach/dc.h
@@ -73,6 +73,11 @@ enum {
 };
 
 enum {
+	TEGRA_DSI_GANGED_SYMMETRIC_LEFT_RIGHT,
+	TEGRA_DSI_GANGED_SYMMETRIC_EVEN_ODD,
+};
+
+enum {
 	TEGRA_DSI_PACKET_CMD,
 	TEGRA_DSI_DELAY_MS,
 };
@@ -134,6 +139,11 @@ enum {
 	DSI_VS_1 = 0x1,
 };
 
+enum {
+	DSI_INSTANCE_0,
+	DSI_INSTANCE_1,
+};
+
 struct tegra_dsi_out {
 	u8		n_data_lanes;			/* required */
 	u8		pixel_format;			/* required */
@@ -163,7 +173,7 @@ struct tegra_dsi_out {
 	u8		video_data_type;		/* required */
 	u8		video_clock_mode;
 	u8		video_burst_mode;
-	u8		ganged_mode_type;
+	u8		ganged_type;
 
 	u16		panel_buffer_size_byte;
 	u16		panel_reset_timeout_msec;
@@ -176,9 +186,6 @@ struct tegra_dsi_out {
 					 * most panels. */
 	bool		te_polarity_low;
 	bool		power_saving_suspend;
-	bool		source_invert; /* Invert the source of DSI. By
-					* defaut DCa is connected to
-					* DSIa. */
 
 	u32		max_panel_freq_khz;
 	u32		lp_cmd_mode_freq_khz;
