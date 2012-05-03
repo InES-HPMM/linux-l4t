d01f3905929c3448041e2162d8fb26e79e1a7440
Ankit Gupta <ankitgupta@nvidia.com>
2012-05-03 15:06:27 +0530

    asoc: tegra: pcm: Add support for setting bias level
    Allow setting bias level to turn off clock extern1 when codec
    is idle for Enterprise (Maxim 98088 codec).
    
    Bug 984678
    Signed-off-by: Ankit Gupta <ankitgupta@nvidia.com>
    
    Change-Id: I09538dafe6c6f01547ff989de3c23933c9745db0
    Reviewed-on: http://git-master/r/100286
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Scott Peterson <speterson@nvidia.com>
    Reviewed-by: Manoj Gangwal <mgangwal@nvidia.com>
    Tested-by: Ankit Gupta (Engrg-SW) <ankitgupta@nvidia.com>
    
    Rebase-Id: R4b54c2219c89320156c522c24397200582bc7894

--- sound/soc/tegra/tegra_pcm.c
+++ sound/soc/tegra/tegra_pcm.c
@@ -263,7 +263,7 @@ static void tegra_pcm_free(struct snd_pcm *pcm)
 
 static int tegra_pcm_probe(struct snd_soc_platform *platform)
 {
-	if(machine_is_kai())
+	if(machine_is_kai() || machine_is_tegra_enterprise())
 		platform->dapm.idle_bias_off = 1;
 
 	return 0;
