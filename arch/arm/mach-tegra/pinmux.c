ce4d59dc2a6e5e2f582e78ca8384770697056c42
Varun Wadekar <vwadekar@nvidia.com>
2012-05-07 17:35:14 +0530

    ARM: tegra: pinmux: fix dbg_pinmux_show routine
    e4b9a093889311aac9a54b6bd55f255fd482ffda added support to consider reg_base
    of zero as valid.
    
    Add the same support in debugfs' _show routine so that all the pinmux
    registers are displayed when we do "cat /d/tegra_pinmux"
    
    Change-Id: Ia4eeaf808c5f853db96a2d118d308e072dbc38f4
    Signed-off-by: Varun Wadekar <vwadekar@nvidia.com>
    Reviewed-on: http://git-master/r/100926
    Reviewed-by: Pritesh Raithatha <praithatha@nvidia.com>
    Reviewed-by: Nitin Kumbhar <nkumbhar@nvidia.com>
    Reviewed-by: Mayuresh Kulkarni <mkulkarni@nvidia.com>
    
    Rebase-Id: Rd43e93a31c618cdd0aec4119d1158a2a4bd040f6

--- arch/arm/mach-tegra/pinmux.c
+++ arch/arm/mach-tegra/pinmux.c
@@ -1019,7 +1019,7 @@ static int dbg_pinmux_show(struct seq_file *s, void *unused)
 		len = strlen(pingroups[i].name);
 		dbg_pad_field(s, 15 - len);
 
-		if (pingroups[i].mux_reg <= 0) {
+		if (pingroups[i].mux_reg < 0) {
 			seq_printf(s, "TEGRA_MUX_NONE");
 			len = strlen("NONE");
 		} else {
@@ -1031,7 +1031,7 @@ static int dbg_pinmux_show(struct seq_file *s, void *unused)
 				seq_printf(s, "TEGRA_MUX_INVALID");
 				len = 7;
 			} else if (pingroups[i].funcs[mux] & TEGRA_MUX_RSVD) {
-				seq_printf(s, "TEGRA_MUX_RSVD%1lu", mux+1);
+				seq_printf(s, "TEGRA_MUX_RSVD%1lu", mux);
 				len = 5;
 			} else {
 				BUG_ON(!tegra_mux_names[pingroups[i].funcs[mux]]);
@@ -1052,7 +1052,7 @@ static int dbg_pinmux_show(struct seq_file *s, void *unused)
 			dbg_pad_field(s, 6 - len);
 		}
 #endif
-		if (pingroups[i].pupd_reg <= 0) {
+		if (pingroups[i].pupd_reg < 0) {
 			seq_printf(s, "TEGRA_PUPD_NORMAL");
 			len = strlen("NORMAL");
 		} else {
@@ -1064,7 +1064,7 @@ static int dbg_pinmux_show(struct seq_file *s, void *unused)
 		}
 		dbg_pad_field(s, 9 - len);
 
-		if (pingroups[i].tri_reg <= 0) {
+		if (pingroups[i].tri_reg < 0) {
 			seq_printf(s, "TEGRA_TRI_NORMAL");
 		} else {
 			reg = pg_readl(pingroups[i].tri_bank,
