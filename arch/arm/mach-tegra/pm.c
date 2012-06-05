b4463222b9aba75946b3b11921cc32d68d4ba38a
Ashwini Ghuge <aghuge@nvidia.com>
2012-06-05 14:20:11 +0530

    ARM: tegra: t114: Pinmux LP0 entry/exit sequence
    Updating pinmux LP0 entry/exit sequence
    to prevent pad glitches
    
    Bug 988086
    
    Change-Id: I68040ee8c744ae878f9aa96c2a2745fcdb94b650
    Signed-off-by: Ashwini Ghuge <aghuge@nvidia.com>
    Reviewed-on: http://git-master/r/105172
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Venkat Moganty <vmoganty@nvidia.com>
    
    Rebase-Id: R2413e61ac8acb71b4f352a89d527ae687a43833c

--- arch/arm/mach-tegra/pm.c
+++ arch/arm/mach-tegra/pm.c
@@ -132,6 +132,8 @@ struct suspend_context tegra_sctx;
 #define PMC_DPAD_ORIDE		0x1C
 #define PMC_WAKE_DELAY		0xe0
 #define PMC_DPD_SAMPLE		0x20
+#define PMC_IO_DPD_REQ_0	0x1b8
+#define PMC_IO_DPD2_REQ_0	0X1C0
 
 #define PMC_WAKE_STATUS		0x14
 #define PMC_SW_WAKE_STATUS	0x18
@@ -717,7 +719,10 @@ static void tegra_pm_set(enum tegra_suspend_mode mode)
 		/* Enable DPD sample to trigger sampling pads data and direction
 		 * in which pad will be driven during lp0 mode*/
 		writel(0x1, pmc + PMC_DPD_SAMPLE);
-
+#if !defined(CONFIG_ARCH_TEGRA_3x_SOC) && !defined(CONFIG_ARCH_TEGRA_2x_SOC)
+		writel(0x800fffff, pmc + PMC_IO_DPD_REQ_0);
+		writel(0x80001fff, pmc + PMC_IO_DPD2_REQ_0);
+#endif
 		/* Set warmboot flag */
 		boot_flag = readl(pmc + PMC_SCRATCH0);
 		pmc_32kwritel(boot_flag | 1, PMC_SCRATCH0);
