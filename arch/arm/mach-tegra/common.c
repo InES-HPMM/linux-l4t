4815406d187d9c9c8367bf0cc9fe7c6e770490e4
Alex Frid <afrid@nvidia.com>
2012-06-05 20:19:39 -0700

    ARM: tegra11: clock: Update common clock table
    - removed duplicated sbc5 and sbc6 entries (merge artifact)
    - consolidated non-tegra3 settings
    - don't check for tegra2 in emulation (not supported)
    
    Change-Id: Ic23c6d07dd46a8a14d3fcfef6d10d60a15237071
    Signed-off-by: Alex Frid <afrid@nvidia.com>
    Reviewed-on: http://git-master/r/106933
    Reviewed-by: Alexander Van Brunt <avanbrunt@nvidia.com>
    Reviewed-by: Bo Yan <byan@nvidia.com>
    
    Rebase-Id: R6bc23acf96e137690d07c396212d35093c2a0090

--- arch/arm/mach-tegra/common.c
+++ arch/arm/mach-tegra/common.c
@@ -187,11 +187,15 @@ static __initdata struct tegra_clk_init_table tegra11_clk_init_table[] = {
 	{ "sclk",	 "pll_p_out2",	102000000,	true },
 	{ "pll_p_out4",	 "pll_p",	204000000,	true },
 	{ "host1x",	"pll_p",	102000000,	false },
+	{ "cl_dvfs_ref", "pll_p",       54000000,       false },
+	{ "cl_dvfs_soc", "pll_p",       54000000,       false },
 #else
 	{ "pll_p_out2",	"pll_p",	108000000,	false },
 	{ "sclk",	"pll_p_out2",	108000000,	true },
 	{ "pll_p_out4",	"pll_p",	216000000,	true },
 	{ "host1x",	"pll_p",	108000000,	false },
+	{ "hclk",	"sclk",		108000000,	true },
+	{ "pclk",	"hclk",		54000000,	true },
 #endif
 	{ "wake.sclk",	 NULL,		250000000	true },
 	{ "cl_dvfs_ref", "pll_p",	54000000,	false },
