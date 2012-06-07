74b3fbf378f5d64e807529deccaf485194e2c13f
Sanjay Singh Rawat <srawat@nvidia.com>
2012-06-07 15:03:14 +0530

    arm: tegra: resolve compilation time warnings
    Bug 949219
    
    Change-Id: I91a67d30869e9800c483f112d58b9f76e2dbe361
    Signed-off-by: Sanjay Singh Rawat <srawat@nvidia.com>
    Reviewed-on: http://git-master/r/103534
    Reviewed-by: Simone Willett <swillett@nvidia.com>
    Tested-by: Simone Willett <swillett@nvidia.com>
    
    Conflicts:
    
    	arch/arm/mach-tegra/pcie.c
    
    Rebase-Id: Ra2f3484ab72c9477ec2cd646a86f4042e3826d48

--- arch/arm/mach-tegra/cpu-tegra3.c
+++ arch/arm/mach-tegra/cpu-tegra3.c
@@ -302,7 +302,7 @@ static int min_cpus_notify(struct notifier_block *nb, unsigned long n, void *p)
 
 	if ((n >= 1) && is_lp_cluster()) {
 		/* make sure cpu rate is within g-mode range before switching */
-		unsigned int speed = max((unsigned long)tegra_getspeed(0), 
+		unsigned int speed = max((unsigned long)tegra_getspeed(0),
 			clk_get_min_rate(cpu_g_clk) / 1000);
 		tegra_update_cpu_speed(speed);
 
