17aa21a527693b6645373634523a4b401fb4de8f
Alex Frid <afrid@nvidia.com>
2012-06-11 20:31:49 -0700

    ARM: tegra11: curacao: Update cl-dvfs platform data
    Updated cl-dvfs platform data to match curacao PMU; made sure
    regulators are initialized before cl-dvfs.
    
    Change-Id: I47d2bdac8aa2d263ccc89623aaa408c8c5565493
    Signed-off-by: Alex Frid <afrid@nvidia.com>
    Reviewed-on: http://git-master/r/108129
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Bo Yan <byan@nvidia.com>
    
    Rebase-Id: Re100f728fb12f7c4a95e42adf062deeeefd05bec

--- arch/arm/mach-tegra/board-curacao-power.c
+++ arch/arm/mach-tegra/board-curacao-power.c
@@ -429,18 +429,32 @@ static struct tegra_cl_dvfs_cfg_param curacao_cl_dvfs_param = {
 	.scale_out_ramp = 0x2,
 };
 
-/* PMU data : assume fixed 12.5mV steps from 600mV to 1400mV starting
-   with reg value 3 */
+/* PMU data : fixed 12.5mV steps from 600mV to 1400mV, no offset */
 #define PMU_CPU_VDD_MAP_SIZE ((1400000 - 600000) / 12500 + 1)
 static struct voltage_reg_map pmu_cpu_vdd_map[PMU_CPU_VDD_MAP_SIZE];
 static void fill_reg_map(void)
 {
 	int i;
 	for (i = 0; i < PMU_CPU_VDD_MAP_SIZE; i++) {
-		pmu_cpu_vdd_map[i].reg_value = i + 3;
+		pmu_cpu_vdd_map[i].reg_value = i;
 		pmu_cpu_vdd_map[i].reg_uV = 600000 + 12500 * i;
 	}
 }
+
+static struct tegra_cl_dvfs_platform_data curacao_cl_dvfs_data = {
+	.dfll_clk_name = "dfll_cpu",
+	.pmu_if = TEGRA_CL_DVFS_PMU_I2C,
+	.u.pmu_i2c = {
+		.fs_rate = 50000,
+		.hs_master_code = 0, /* no hs mode support */
+		.slave_addr = 0x78,
+		.reg = 0x16,
+	},
+	.vdd_map = pmu_cpu_vdd_map,
+	.vdd_map_size = PMU_CPU_VDD_MAP_SIZE,
+
+	.cfg_param = &curacao_cl_dvfs_param,
+};
 
 static int __init curacao_gpio_regulator_init(void)
 {
@@ -457,32 +471,17 @@ static int __init curacao_gpio_regulator_init(void)
 			ARRAY_SIZE(gpio_regs_devices));
 }
 
-static struct tegra_cl_dvfs_platform_data curacao_cl_dvfs_data = {
-	.dfll_clk_name = "dfll_cpu",
-	.pmu_if = TEGRA_CL_DVFS_PMU_I2C,
-	.u.pmu_i2c = {
-		.fs_rate = 50000,
-		.hs_master_code = 0, /* no hs mode support */
-		.slave_addr = 0x2D,
-		.reg = 0x27,
-	},
-	.vdd_map = pmu_cpu_vdd_map,
-	.vdd_map_size = PMU_CPU_VDD_MAP_SIZE,
-
-	.cfg_param = &curacao_cl_dvfs_param,
-};
-
 int __init curacao_regulator_init(void)
 {
 	int ret;
 
 	platform_device_register(&curacao_pda_power_device);
-	fill_reg_map();
-	tegra_cl_dvfs_set_plarform_data(&curacao_cl_dvfs_data);
 	ret = curacao_max77663_regulator_init();
 	if (ret < 0)
 		return ret;
 
+	fill_reg_map();
+	tegra_cl_dvfs_set_plarform_data(&curacao_cl_dvfs_data);
 	return curacao_gpio_regulator_init();
 }
 
