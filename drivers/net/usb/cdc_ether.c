a86a42aea0f5d1705fecd41b8fdd3437734cd644
Mohan T <mohant@nvidia.com>
2012-06-25 11:06:17 +0530

    usb: cdc_ether: Add rmnet device info to PH450 and Tango
    Modified device info element for PH450 and Tango to
    sync with framework.
    
    Change-Id: I325571358f8c1875484254a9f497c606adb03180
    Signed-off-by: Mohan T <mohant@nvidia.com>
    Reviewed-on: http://git-master/r/110765
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Matthieu Vincenot <mvincenot@nvidia.com>
    Reviewed-by: Steve Lin <stlin@nvidia.com>
    
    Rebase-Id: R343518c99670c6aeead333fcd9a3b16b8c0933f0

--- drivers/net/usb/cdc_ether.c
+++ drivers/net/usb/cdc_ether.c
@@ -473,6 +473,15 @@ static const struct driver_info wwan_info = {
 	.manage_power =	usbnet_manage_power,
 };
 
+static const struct driver_info rmnet_info = {
+	.description =	"Mobile Broadband Network Device",
+	.flags =	FLAG_RMNET,
+	.bind =		usbnet_cdc_bind,
+	.unbind =	usbnet_cdc_unbind,
+	.status =	usbnet_cdc_status,
+	.manage_power =	usbnet_manage_power,
+};
+
 /*-------------------------------------------------------------------------*/
 
 #define HUAWEI_VENDOR_ID	0x12D1
@@ -647,17 +656,17 @@ static const struct usb_device_id	products [] = {
 	.match_flags = USB_DEVICE_ID_MATCH_INT_INFO
 		| USB_DEVICE_ID_MATCH_DEVICE,
 	USB_DEVICE(0x1983, 0x0310),
-	.driver_info = (unsigned long)&wwan_info,
+	.driver_info = (unsigned long)&rmnet_info,
 }, {
 	.match_flags = USB_DEVICE_ID_MATCH_INT_INFO
 		| USB_DEVICE_ID_MATCH_DEVICE,
 	USB_DEVICE(0x1983, 0x0321),
-	.driver_info = (unsigned long)&wwan_info,
+	.driver_info = (unsigned long)&rmnet_info,
 }, {
 	.match_flags = USB_DEVICE_ID_MATCH_INT_INFO
 		| USB_DEVICE_ID_MATCH_DEVICE,
 	USB_DEVICE(0x1983, 0x0327),	/* 5AE */
-	.driver_info = (unsigned long)&wwan_info,
+	.driver_info = (unsigned long)&rmnet_info,
 },
 
 /* Tango module */
@@ -665,7 +674,7 @@ static const struct usb_device_id	products [] = {
 	.match_flags = USB_DEVICE_ID_MATCH_INT_INFO
 		 | USB_DEVICE_ID_MATCH_DEVICE,
 	USB_DEVICE(0x0489,0xE03A),
-	.driver_info = (unsigned long)&wwan_info,
+	.driver_info = (unsigned long)&rmnet_info,
 },
 /*
  * WHITELIST!!!
