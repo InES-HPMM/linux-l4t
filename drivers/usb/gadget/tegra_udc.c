2faf07913c0c9eebf55a2b6a6cb5581fd005145c
Rakesh Bodla <rbodla@nvidia.com>
2012-07-04 10:52:56 +0530

    ARM: tegra: usb: Add config option to increase cpu freq
    Adding an option to increase the cpu frequency to
    improve usb gadget transfer rate performance.
    
    Bug 923594
    
    Reviewed-on: http://git-master/r/80050
    (cherry picked from commit d67b47bbb8322d65f745dcb2226af3f3d582d69a)
    
    Conflicts:
    
    	arch/arm/mach-tegra/Kconfig
    
    Change-Id: I4b23fc9948aec37ec58891205ec7dd02f0597af2
    Reviewed-on: http://git-master/r/113437
    Reviewed-by: Rakesh Bodla <rbodla@nvidia.com>
    Tested-by: Rakesh Bodla <rbodla@nvidia.com>
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Venkat Moganty <vmoganty@nvidia.com>
    
    Rebase-Id: Ra2ea1650109a141610775585a6c83e19366500bd

--- drivers/usb/gadget/tegra_udc.c
+++ drivers/usb/gadget/tegra_udc.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012 NVIDIA Corporation
+ * Copyright (c) 2012, NVIDIA CORPORATION.  All rights reserved.
  *
  * Description:
  * High-speed USB device controller driver.
@@ -37,6 +37,7 @@
 #include <linux/workqueue.h>
 #include <linux/err.h>
 #include <linux/io.h>
+#include <linux/pm_qos.h>
 
 #include <asm/byteorder.h>
 #include <asm/io.h>
@@ -96,7 +97,7 @@ static const u8 tegra_udc_test_packet[53] = {
 static struct tegra_udc *the_udc;
 
 #ifdef CONFIG_TEGRA_GADGET_BOOST_CPU_FREQ
-	static struct pm_qos_request_list boost_cpu_freq_req;
+	static struct pm_qos_request boost_cpu_freq_req;
 	static u32 ep_queue_request_count;
 	static u8 boost_cpufreq_work_flag;
 #endif
