a65d62231305fbbd24d6cdfbbcec9e917d254a05
Varun Wadekar <vwadekar@nvidia.com>
2012-07-18 18:01:29 +0530

    Revert "ARM: tegra: make tegra_cpu_reset_handler_enable() __init"
    This reverts commit c1389ce1e02c1e1f617d7cf70852ad5d9f9ef355 as it breaks
    LP0 resume. Clearly, this was not tested before pushing upstream.
    
    Change-Id: Iaf0a40f5c290c6f73c84b5d5bbba33d70c373a49
    Signed-off-by: Varun Wadekar <vwadekar@nvidia.com>
    
    Rebase-Id: R6aed1954d5d59ae5ff903b691ec0cf8784bbe5d3

--- arch/arm/mach-tegra/reset.c
+++ arch/arm/mach-tegra/reset.c
@@ -33,7 +33,7 @@
 
 static bool is_enabled;
 
-static void __init tegra_cpu_reset_handler_enable(void)
+static void tegra_cpu_reset_handler_enable(void)
 {
 	void __iomem *iram_base = IO_ADDRESS(TEGRA_IRAM_BASE);
 #ifndef CONFIG_TRUSTED_FOUNDATIONS
