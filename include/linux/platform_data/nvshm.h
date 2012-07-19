399f3401018239c47b48452fa884826fa5429868
Martin Chabot <mchabot@nvidia.com>
2012-07-19 13:47:27 +0200

    include: linux: platform_data: nvshm driver platform data definition
    This header contain platform data structure for Tegra NVSHM driver
    Apply only on Tegra device which include integrated baseband
    
    Bug 957942
    
    Change-Id: Icae4674ccfe78d103dfda274a43f2c928f9fd8df
    Signed-off-by: Martin Chabot <mchabot@nvidia.com>
    Reviewed-on: http://git-master/r/120941
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Alexandre Berdery <aberdery@nvidia.com>
    Reviewed-by: Bo Yan <byan@nvidia.com>

---
+++ include/linux/platform_data/nvshm.h
@@ -0,0 +1,29 @@
+/*
+ * include/linux/platform_data/nvshm.h
+ *
+ * Copyright (C) 2012 NVIDIA Corporation.
+ *
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef __PLATFORM_DATA_NVSHM_H
+#define __PLATFORM_DATA_NVSHM_H
+
+struct nvshm_platform_data {
+	void *ipc_base_virt;
+	size_t ipc_size;
+	void *mb_base_virt;
+	size_t mb_size;
+	struct platform_device *tegra_bb;
+};
+
+#endif
