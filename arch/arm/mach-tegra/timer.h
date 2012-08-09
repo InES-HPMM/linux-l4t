6775e23103cf32538538e81d9675489506bcf07c
Varun Wadekar <vwadekar@nvidia.com>
2012-08-09 16:24:31 +0530

    ARM: Tegra: remove tegra_get_linear_age() from under CONFIG_HAVE_ARM_TWD
    Change-Id: Ib106db20f587e53c95410fcc3d834c257b112a19
    Signed-off-by: Varun Wadekar <vwadekar@nvidia.com>
    
    Rebase-Id: R13ae409121430392638065bab0494325e21912b5

--- arch/arm/mach-tegra/timer.h
+++ arch/arm/mach-tegra/timer.h
@@ -48,6 +48,7 @@ struct tegra_twd_context {
 };
 
 void __init tegra_cpu_timer_init(void);
+int tegra_get_linear_age(void);
 
 #ifdef CONFIG_HAVE_ARM_TWD
 int tegra_twd_get_state(struct tegra_twd_context *context);
