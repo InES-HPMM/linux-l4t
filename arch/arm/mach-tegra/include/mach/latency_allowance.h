201ca2d7429a2088544bdf7f00ba8ab9ae7ff929
Krishna Reddy <vdumpa@nvidia.com>
2012-08-23 13:57:47 -0700

    arm: tegra: la: Add config option for latency allowance.
    This allows enable/disable latency allowance.
    
    Change-Id: Iee2cb320f40de902e0c2792c516d7ac108451224
    Signed-off-by: Krishna Reddy <vdumpa@nvidia.com>
    Reviewed-on: http://git-master/r/126984
    GVS: Gerrit_Virtual_Submit
    
    Rebase-Id: R59d215bff70a652b9277282dd7756067e00774ef

--- arch/arm/mach-tegra/include/mach/latency_allowance.h
+++ arch/arm/mach-tegra/include/mach/latency_allowance.h
@@ -88,7 +88,7 @@ enum tegra_la_id {
 	TEGRA_LA_MAX_ID
 };
 
-#if defined(CONFIG_ARCH_TEGRA_2x_SOC) || !defined(CONFIG_TEGRA_SILICON_PLATFORM)
+#if !defined(CONFIG_TEGRA_LATENCY_ALLOWANCE)
 static inline int tegra_set_latency_allowance(enum tegra_la_id id,
 						unsigned int bandwidth_in_mbps)
 {
