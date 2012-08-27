149b8320ad975555b329209cc5bba38e93d05c02
Laxman Dewangan <ldewangan@nvidia.com>
2012-08-27 18:26:05 +0530

    regulator: max77663: remove non-required platform data
    Some of platform data line init_apply, init_voltage,
    init_state is nto require as the same functionality
    can be found through the regulator init data.
    Hence removing this paramter to configure through
    platform data.
    
    Change-Id: I8d7c4e3238cae9f377a3f0683c1875a8d136deda
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Reviewed-on: http://git-master/r/127774
    
    Rebase-Id: Rc7f891c2b6fc62e24fdf1a50282d83b60dd13358

--- arch/arm/mach-tegra/board-curacao-power.c
+++ arch/arm/mach-tegra/board-curacao-power.c
@@ -115,7 +115,6 @@ static struct max77663_regulator_fps_cfg max77663_fps_cfgs[] = {
 
 #define MAX77663_PDATA_INIT(_rid, _id, _min_uV, _max_uV, _supply_reg,	\
 		_always_on, _boot_on, _apply_uV,			\
-		_init_apply, _init_enable, _init_uV,			\
 		_fps_src, _fps_pu_period, _fps_pd_period, _flags)	\
 	static struct regulator_init_data max77663_regulator_idata_##_id = {  \
 		.supply_regulator = _supply_reg,			\
@@ -140,9 +139,6 @@ static struct max77663_regulator_fps_cfg max77663_fps_cfgs[] = {
 	{								\
 		.reg_init_data = &max77663_regulator_idata_##_id,	\
 		.id = MAX77663_REGULATOR_ID_##_rid,			\
-		.init_apply = _init_apply,				\
-		.init_enable = _init_enable,				\
-		.init_uV = _init_uV,					\
 		.fps_src = _fps_src,					\
 		.fps_pu_period = _fps_pu_period,			\
 		.fps_pd_period = _fps_pd_period,			\
@@ -151,43 +147,43 @@ static struct max77663_regulator_fps_cfg max77663_fps_cfgs[] = {
 	}
 
 MAX77663_PDATA_INIT(SD0, sd0,  600000, 3387500, NULL, 1, 0, 0,
-		    0, 0, -1, FPS_SRC_NONE, -1, -1, EN2_CTRL_SD0 | SD_FSRADE_DISABLE);
+		    FPS_SRC_NONE, -1, -1, EN2_CTRL_SD0 | SD_FSRADE_DISABLE);
 
-MAX77663_PDATA_INIT(SD1, sd1,  800000, 1587500, NULL, 1, 0, 0,
-		    1, 1, -1, FPS_SRC_1, -1, -1, SD_FSRADE_DISABLE);
+MAX77663_PDATA_INIT(SD1, sd1,  800000, 1587500, NULL, 1, 1, 0,
+		    FPS_SRC_1, -1, -1, SD_FSRADE_DISABLE);
 
-MAX77663_PDATA_INIT(SD2, sd2,  1800000, 1800000, NULL, 1, 0, 0,
-		    1, 1, -1, FPS_SRC_NONE, -1, -1, 0);
+MAX77663_PDATA_INIT(SD2, sd2,  1800000, 1800000, NULL, 1, 1, 0,
+		    FPS_SRC_NONE, -1, -1, 0);
 
-MAX77663_PDATA_INIT(SD3, sd3,  600000, 3387500, NULL, 1, 0, 0,
-		    1, 1, -1, FPS_SRC_NONE, -1, -1, 0);
+MAX77663_PDATA_INIT(SD3, sd3,  600000, 3387500, NULL, 1, 1, 0,
+		    FPS_SRC_NONE, -1, -1, 0);
 
-MAX77663_PDATA_INIT(LDO0, ldo0, 800000, 2350000, max77663_rails(sd3), 1, 0, 0,
-		    1, 1, -1, FPS_SRC_1, -1, -1, 0);
+MAX77663_PDATA_INIT(LDO0, ldo0, 800000, 2350000, max77663_rails(sd3), 1, 1, 0,
+		    FPS_SRC_1, -1, -1, 0);
 
 MAX77663_PDATA_INIT(LDO1, ldo1, 800000, 2350000, max77663_rails(sd3), 0, 0, 0,
-		    0, 0, -1, FPS_SRC_NONE, -1, -1, 0);
+		    FPS_SRC_NONE, -1, -1, 0);
 
-MAX77663_PDATA_INIT(LDO2, ldo2, 800000, 3950000, NULL, 1, 0, 0,
-		    1, 1, -1, FPS_SRC_1, -1, -1, 0);
+MAX77663_PDATA_INIT(LDO2, ldo2, 800000, 3950000, NULL, 1, 1, 0,
+		    FPS_SRC_1, -1, -1, 0);
 
-MAX77663_PDATA_INIT(LDO3, ldo3, 800000, 3950000, NULL, 1, 0, 0,
-		    1, 1, -1, FPS_SRC_NONE, -1, -1, 0);
+MAX77663_PDATA_INIT(LDO3, ldo3, 800000, 3950000, NULL, 1, 1, 0,
+		    FPS_SRC_NONE, -1, -1, 0);
 
-MAX77663_PDATA_INIT(LDO4, ldo4, 800000, 1587500, NULL, 0, 0, 0,
-		    1, 1, 1000000, FPS_SRC_NONE, -1, -1, LDO4_EN_TRACKING);
+MAX77663_PDATA_INIT(LDO4, ldo4, 1000000, 1000000, NULL, 0, 1, 0,
+		    FPS_SRC_NONE, -1, -1, LDO4_EN_TRACKING);
 
-MAX77663_PDATA_INIT(LDO5, ldo5, 800000, 2800000, NULL, 0, 0, 0,
-		    1, 1, -1, FPS_SRC_NONE, -1, -1, 0);
+MAX77663_PDATA_INIT(LDO5, ldo5, 800000, 2800000, NULL, 0, 1, 0,
+		    FPS_SRC_NONE, -1, -1, 0);
 
 MAX77663_PDATA_INIT(LDO6, ldo6, 800000, 3950000, NULL, 0, 0, 0,
-		    0, 0, -1, FPS_SRC_NONE, -1, -1, 0);
+		    FPS_SRC_NONE, -1, -1, 0);
 
 MAX77663_PDATA_INIT(LDO7, ldo7, 800000, 3950000, max77663_rails(sd3), 0, 0, 0,
-		    0, 0, -1, FPS_SRC_NONE, -1, -1, 0);
+		    FPS_SRC_NONE, -1, -1, 0);
 
-MAX77663_PDATA_INIT(LDO8, ldo8, 800000, 3950000, max77663_rails(sd3), 0, 0, 0,
-		    1, 1, -1, FPS_SRC_1, -1, -1, 0);
+MAX77663_PDATA_INIT(LDO8, ldo8, 800000, 3950000, max77663_rails(sd3), 0, 1, 0,
+		    FPS_SRC_1, -1, -1, 0);
 
 #define MAX77663_REG(_id, _data) &max77663_regulator_pdata_##_data
 
