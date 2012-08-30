4d2af19fc4e9b3a559b844574972d55aca87d2f0
Laxman Dewangan <ldewangan@nvidia.com>
2012-08-30 18:01:51 +0530

    ARM: tegra: introduce board-id header
    In place of defining the board-id on different boardfile,
    introducing the header file for all board-id.
    This file will contains all Tegra board definition and
    will be used across platform.
    
    Change-Id: Ifd48c7668093c17e037d736b14f5d47bf17db6db
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Reviewed-on: http://git-master/r/130180
    Reviewed-by: Automatic_Commit_Validation_User
    
    Rebase-Id: R2f444c14f3277bcf874c84a973c4effdd1d787a2

---
+++ arch/arm/mach-tegra/tegra-board-id.h
@@ -0,0 +1,40 @@
+/*
+ * tegra-board-id.h: Defines all boardid of Tegra.
+ *
+ * Copyright (c) 2012, NVIDIA Corporation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef _MACH_TEGRA_BOARD_ID_H
+#define _MACH_TEGRA_BOARD_ID_H
+
+/* Processor Board  ID */
+#define BOARD_E1611   0x064B
+#define BOARD_E1612   0x064C
+#define BOARD_E1613   0x064D
+#define BOARD_E1580   0x062C
+
+/* Board Fab version */
+#define BOARD_FAB_A00			0x0
+#define BOARD_FAB_A01			0x1
+#define BOARD_FAB_A02			0x2
+#define BOARD_FAB_A03			0x3
+#define BOARD_FAB_A04			0x4
+#define BOARD_FAB_A05			0x5
+#define BOARD_FAB_A06			0x6
+#define BOARD_FAB_A07			0x7
+
+#endif
