e200d5487e51e64fc9c6b293618949ffc1060e15
Alex Frid <afrid@nvidia.com>
2012-08-31 12:12:36 -0700

    regulator: Pass voltage level to change notification
    Passed voltage level to notification chain for events:
    REGULATOR_EVENT_OUT_PRECHANGE and REGULATOR_EVENT_OUT_POSTCHANGE.
    
    Change-Id: Ia94495c2237f56ebba2d456af4cd2fc7e98d3e20
    Signed-off-by: Alex Frid <afrid@nvidia.com>
    Reviewed-on: http://git-master/r/130354
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>
    
    Rebase-Id: R9b67729c67d3318aef41a23602115c9636dc7086

--- drivers/regulator/core.c
+++ drivers/regulator/core.c
@@ -2488,11 +2488,11 @@ static int _regulator_do_set_voltage(struct regulator_dev *rdev,
 			return old_selector;
 	}
 
-	if (_regulator_is_enabled(rdev))
-		_notifier_call_chain(rdev, REGULATOR_EVENT_OUT_PRECHANGE,
-				     NULL);
-
 	if (rdev->desc->ops->set_voltage) {
+		if (_regulator_is_enabled(rdev))
+			_notifier_call_chain(rdev,
+			REGULATOR_EVENT_OUT_PRECHANGE, (void *)min_uV);
+
 		ret = rdev->desc->ops->set_voltage(rdev, min_uV, max_uV,
 						   &selector);
 
@@ -2521,6 +2521,9 @@ static int _regulator_do_set_voltage(struct regulator_dev *rdev,
 		if (ret >= 0) {
 			best_val = rdev->desc->ops->list_voltage(rdev, ret);
 			if (min_uV <= best_val && max_uV >= best_val) {
+				if (_regulator_is_enabled(rdev))
+					_notifier_call_chain(rdev,
+					REGULATOR_EVENT_OUT_PRECHANGE, (void *)ret);
 				selector = ret;
 				if (old_selector == selector)
 					ret = 0;
@@ -2563,9 +2566,12 @@ static int _regulator_do_set_voltage(struct regulator_dev *rdev,
 				     (void *)data);
 	}
 
-	if (_regulator_is_enabled(rdev))
+	if (_regulator_is_enabled(rdev)) {
+		if (selector != -1)
+			min_uV = selector;
 		_notifier_call_chain(rdev, REGULATOR_EVENT_OUT_POSTCHANGE,
-				     NULL);
+				     (void *)min_uV);
+	}
 
 	trace_regulator_set_voltage_complete(rdev_get_name(rdev), best_val);
 
