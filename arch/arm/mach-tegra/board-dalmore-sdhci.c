18e7bf70757f4b2eae1e1e2ac6f730c7a71e1e85
naveenk <naveenk@nvidia.com>
2012-09-03 16:25:03 +0530

    ARM: tegra: dalmore/pluto: Remove unused code
    Remove T30 specific code, because Same instances are used
    for T30 and T114 on Dakmore/Pluto
    
    Bug 1017708
    
    Change-Id: I73f2e30d7ea0d355ee943c73013a1df9008940cf
    Signed-off-by: naveen kumar arepalli <naveenk@nvidia.com>
    Reviewed-on: http://git-master/r/130035
    Reviewed-by: Pradeep Goudagunta <pgoudagunta@nvidia.com>
    Reviewed-by: Sachin Nikam <snikam@nvidia.com>
    Reviewed-by: Automatic_Commit_Validation_User
    
    Rebase-Id: R3eb5c4b1e7a6819835472222a8ee0a099b087bef

--- arch/arm/mach-tegra/board-dalmore-sdhci.c
+++ arch/arm/mach-tegra/board-dalmore-sdhci.c
@@ -36,8 +36,6 @@
 #include "board.h"
 #include "board-dalmore.h"
 
-
-#if defined(CONFIG_ARCH_TEGRA_11x_SOC)
 #define DALMORE_WLAN_PWR	TEGRA_GPIO_PCC5
 #define DALMORE_WLAN_RST	TEGRA_GPIO_PX7
 #define DALMORE_WLAN_WOW	TEGRA_GPIO_PU5
@@ -309,96 +307,3 @@ int __init dalmore_sdhci_init(void)
 	dalmore_wifi_init();
 	return 0;
 }
-
-#else
-#define DALMORE_SD_CD	TEGRA_GPIO_PI5
-#define DALMORE_SD_WP	TEGRA_GPIO_PT3
-static struct resource sdhci_resource0[] = {
-	[0] = {
-		.start	= INT_SDMMC1,
-		.end	= INT_SDMMC1,
-		.flags	= IORESOURCE_IRQ,
-	},
-	[1] = {
-		.start	= TEGRA_SDMMC1_BASE,
-		.end	= TEGRA_SDMMC1_BASE + TEGRA_SDMMC1_SIZE-1,
-		.flags	= IORESOURCE_MEM,
-	},
-};
-
-static struct resource sdhci_resource3[] = {
-	[0] = {
-		.start	= INT_SDMMC4,
-		.end	= INT_SDMMC4,
-		.flags	= IORESOURCE_IRQ,
-	},
-	[1] = {
-		.start	= TEGRA_SDMMC4_BASE,
-		.end	= TEGRA_SDMMC4_BASE + TEGRA_SDMMC4_SIZE-1,
-		.flags	= IORESOURCE_MEM,
-	},
-};
-
-static struct tegra_sdhci_platform_data tegra_sdhci_platform_data0 = {
-	.cd_gpio = DALMORE_SD_CD,
-	.wp_gpio = -1,
-	.power_gpio = -1,
-	.tap_delay = 0x0F,
-	.ddr_clk_limit = 41000000,
-/*	.is_voltage_switch_supported = true,
-	.vdd_rail_name = "vddio_sdmmc1",
-	.slot_rail_name = "vddio_sd_slot",
-	.vdd_max_uv = 3320000,
-	.vdd_min_uv = 3280000,
-	.max_clk = 208000000,
-	.is_8bit_supported = false, */
-};
-
-static struct tegra_sdhci_platform_data tegra_sdhci_platform_data3 = {
-	.cd_gpio = -1,
-	.wp_gpio = -1,
-	.power_gpio = -1,
-	.is_8bit = 1,
-	.tap_delay = 0x0F,
-	.ddr_clk_limit = 41000000,
-	.mmc_data = {
-		.built_in = 1,
-	}
-/*	.is_voltage_switch_supported = false,
-	.vdd_rail_name = NULL,
-	.slot_rail_name = NULL,
-	.vdd_max_uv = -1,
-	.vdd_min_uv = -1,
-	.max_clk = 48000000,
-	.is_8bit_supported = true, */
-};
-
-static struct platform_device tegra_sdhci_device0 = {
-	.name		= "sdhci-tegra",
-	.id		= 0,
-	.resource	= sdhci_resource0,
-	.num_resources	= ARRAY_SIZE(sdhci_resource0),
-	.dev = {
-		.platform_data = &tegra_sdhci_platform_data0,
-	},
-};
-
-
-static struct platform_device tegra_sdhci_device3 = {
-	.name		= "sdhci-tegra",
-	.id		= 3,
-	.resource	= sdhci_resource3,
-	.num_resources	= ARRAY_SIZE(sdhci_resource3),
-	.dev = {
-		.platform_data = &tegra_sdhci_platform_data3,
-	},
-};
-
-
-int __init dalmore_sdhci_init(void)
-{
-	platform_device_register(&tegra_sdhci_device3);
-	platform_device_register(&tegra_sdhci_device0);
-	return 0;
-}
-#endif
