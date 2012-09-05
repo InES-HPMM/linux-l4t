e4c8413f866f2358d45055950b963d90cd7240a5
naveenk <naveenk@nvidia.com>
2012-09-05 14:32:01 +0530

    sdhci: tegra: Disable HS200, Packed Command Support
    Disable HS200, Packed Command Support for eMMC 4.5
    
    Bug 1017708
    
    Change-Id: Ia0d18d7cc257915c2fcf689964e5570106c31ceb
    Signed-off-by: naveen kumar arepalli <naveenk@nvidia.com>
    Reviewed-on: http://git-master/r/130022
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Pradeep Goudagunta <pgoudagunta@nvidia.com>
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>
    
    Rebase-Id: R27ab9780dfd0fe1b618ae5f520f6a221a1c73853

--- drivers/mmc/host/sdhci-tegra.c
+++ drivers/mmc/host/sdhci-tegra.c
@@ -1348,8 +1348,6 @@ static int sdhci_tegra_probe(struct platform_device *pdev)
 #else
 	tegra_host->hw_ops = &tegra_11x_sdhci_ops;
 	tegra_sdhost_std_freq = TEGRA3_SDHOST_STD_FREQ;
-	/* enable HS200 capable */
-	host->mmc->caps2 |= MMC_CAP2_HS200;
 #endif
 	rc = sdhci_add_host(host);
 	if (rc)
