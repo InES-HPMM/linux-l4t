d980146ce786e94a4dc0ded42a87bcf3a7e26970
Graziano Misuraca <gmisuraca@nvidia.com>
2012-09-05 15:16:00 -0700

    ARM: tegra: dalmore: initialize 3rd usb port
    Change-Id: I74ff45003bc64eec12967c96f5a83c4f7fefb441
    Signed-off-by: Ray Poudrier <rapoudrier@nvidia.com>
    Signed-off-by: Graziano Misuraca <gmisuraca@nvidia.com>
    Reviewed-on: http://git-master/r/129850
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Lokesh Pathak <lpathak@nvidia.com>
    Tested-by: Thomas Cherry <tcherry@nvidia.com>
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>
    
    Rebase-Id: R3792ffab9a8b1e79096ba62b840fc13778b471eb

--- arch/arm/mach-tegra/board-dalmore.c
+++ arch/arm/mach-tegra/board-dalmore.c
@@ -439,6 +439,31 @@ static struct tegra_usb_platform_data tegra_ehci1_utmi_pdata = {
 	},
 };
 
+static struct tegra_usb_platform_data tegra_ehci3_utmi_pdata = {
+	.port_otg = false,
+	.has_hostpc = true,
+	.phy_intf = TEGRA_USB_PHY_INTF_UTMI,
+	.op_mode = TEGRA_USB_OPMODE_HOST,
+	.u_data.host = {
+		.vbus_gpio = -1,
+		.vbus_reg = "vdd_vbus_typea_usb",
+		.hot_plug = true,
+		.remote_wakeup_supported = true,
+		.power_off_on_suspend = true,
+	},
+	.u_cfg.utmi = {
+	.hssync_start_delay = 0,
+		.elastic_limit = 16,
+		.idle_wait_delay = 17,
+		.term_range_adj = 6,
+		.xcvr_setup = 8,
+		.xcvr_lsfslew = 2,
+		.xcvr_lsrslew = 2,
+		.xcvr_setup_offset = 0,
+		.xcvr_use_fuses = 1,
+	},
+};
+
 static struct tegra_usb_otg_data tegra_otg_pdata = {
 	.ehci_device = &tegra_ehci1_device,
 	.ehci_pdata = &tegra_ehci1_utmi_pdata,
@@ -456,6 +481,9 @@ static void dalmore_usb_init(void)
 	tegra_ehci2_device.dev.platform_data =
 		&tegra_ehci2_hsic_smsc_hub_pdata;
 	platform_device_register(&tegra_ehci2_device);
+
+	tegra_ehci3_device.dev.platform_data = &tegra_ehci3_utmi_pdata;
+	platform_device_register(&tegra_ehci3_device);
 }
 
 static void dalmore_modem_init(void)
