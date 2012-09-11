9e932d264471b3788480ef734378815acc64c8f8
Prashant Malani <pmalani@nvidia.com>
2012-09-10 17:08:36 -0700

    drivers: rtc: tegra: Remove rtc register reset
    RTC registers are accessed before
    tegra_rtc_probe(). As such, resetting them here
    would lead to inconsistent state, and possible
    problems with time subtraction.
    
    Bug 1010972
    
    Change-Id: I342a88411b5fcde86078537c313d857e3cd9a4f2
    Signed-off-by: Prashant Malani <pmalani@nvidia.com>
    Reviewed-on: http://git-master/r/131263
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Bo Yan <byan@nvidia.com>
    Reviewed-by: Varun Wadekar <vwadekar@nvidia.com>
    
    Rebase-Id: Rdfd5f6598c9cc9ec86fd2faae7af3e035fa81d29

--- drivers/rtc/rtc-tegra.c
+++ drivers/rtc/rtc-tegra.c
@@ -366,36 +366,6 @@ static int __init tegra_rtc_probe(struct platform_device *pdev)
 
 	dev_notice(&pdev->dev, "Tegra internal Real Time Clock\n");
 
-#ifndef CONFIG_TEGRA_SILICON_PLATFORM
-	{
-		struct rtc_time tm;
-
-		/* Get the current time from the RTC. */
-		ret = tegra_rtc_read_time(&pdev->dev, &tm);
-		if (ret) {
-			/* Report but ignore this error. */
-			dev_err(&pdev->dev,
-				"Failed to get FPGA internal RTC time (err=%d)\n",
-				ret);
-		} else if (tm.tm_year < 2010) {
-			/* The RTC's default reset time is soooo last century. */
-			tm.tm_year = 2010-1900;
-			tm.tm_mon  = 0;
-			tm.tm_mday = 1;
-			tm.tm_hour = 0;
-			tm.tm_min  = 0;
-			tm.tm_sec  = 0;
-			ret = tegra_rtc_set_time(&pdev->dev, &tm);
-			if (ret) {
-				/* Report but ignore this error. */
-				dev_err(&pdev->dev,
-					"Failed to set FPGA internal RTC time (err=%d)\n",
-					ret);
-			}
-		}
-	}
-#endif
-
 	return 0;
 }
 
