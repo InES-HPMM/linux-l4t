70936e6e8eb7c2a4e56d675390124b07bb907559
Robert Collins <rcollins@nvidia.com>
2012-09-16 11:10:45 -0700

    touch: Radyium: Move panel config structures to board files.
    Reviewed-on: http://git-master/r/133110
    
    Change-Id: I93343d2d67982fd05cb2c72b2d983c0f0cff5bf7
    Signed-off-by: Robert Collins <rcollins@nvidia.com>
    Signed-off-by: Deepak Nibade <dnibade@nvidia.com>
    Reviewed-on: http://git-master/r/143115
    Reviewed-by: Rohan Somvanshi <rsomvanshi@nvidia.com>
    Tested-by: Rohan Somvanshi <rsomvanshi@nvidia.com>
    
    Rebase-Id: R8e0c81e45ec3f10cb3e470419191acd3bf52f68c

--- arch/arm/mach-tegra/board-touch-raydium.h
+++ arch/arm/mach-tegra/board-touch-raydium.h
@@ -1,5 +1,5 @@
 /*
- * arch/arm/mach-tegra/board-touch.h
+ * arch/arm/mach-tegra/board-touch-raydium.h
  *
  * Copyright (c) 2012, NVIDIA CORPORATION.  All rights reserved.
  *
@@ -16,7 +16,9 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef _MACH_TEGRA_BOARD_TOUCH_H
-#define _MACH_TEGRA_BOARD_TOUCH_H
+#ifndef _MACH_TEGRA_BOARD_TOUCH_RAYDIUM_H
+#define _MACH_TEGRA_BOARD_TOUCH_RAYDIUM_H
+
+int __init touch_init_raydium(int irq_gpio, int reset_gpio, struct rm_spi_ts_platform_data *rm31080ts_data, struct spi_board_info *rm31080a_spi_board, int size);
 
 #endif
