6fd7d60e9aa504b2d29d4a7c8a9193cf4b99ab9d
naveenk <naveenk@nvidia.com>
2012-09-20 01:57:44 +0530

    ARM: tegra: keep clock always on for sdmmc3
    do not dosable sdmmc3 clock to enable sd card
    hotplug support
    
    Reviewed-on: http://git-master/r/133875
    (cherry picked from commit 001da1dc050d7ce641228bab3d67e5a17899ef21)
    
    Change-Id: I8e12dd19bc7c2b4c620c056a97718ffc488616ab
    Signed-off-by: naveenk <naveenk@nvidia.com>
    Signed-off-by: Deepak Nibade <dnibade@nvidia.com>
    Reviewed-on: http://git-master/r/143100
    Reviewed-by: Simone Willett <swillett@nvidia.com>
    Tested-by: Simone Willett <swillett@nvidia.com>
    
    Rebase-Id: Rddfe3ab25d1920740f385b0dac3ab8afe48550ef

--- arch/arm/mach-tegra/common.c
+++ arch/arm/mach-tegra/common.c
@@ -233,7 +233,7 @@ static __initdata struct tegra_clk_init_table tegra11x_clk_init_table[] = {
 	{ "pll_u_60M",	NULL,		0,		true },
 	{ "pll_u_480M",	NULL,		0,		true },
 	{ "sdmmc1",	"pll_p",	48000000,	false},
-	{ "sdmmc3",	"pll_p",	48000000,	false},
+	{ "sdmmc3",	"pll_p",	48000000,	true},
 	{ "sdmmc4",	"pll_p",	48000000,	false},
 	{ "sbc1.sclk",	NULL,		40000000,	false},
 	{ "sbc2.sclk",	NULL,		40000000,	false},
