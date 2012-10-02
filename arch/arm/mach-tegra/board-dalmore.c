152318805442a5146390aba371b53addc2480326
David Jung <djung@nvidia.com>
2012-10-02 13:18:55 -0700

    touch: raydium: change SPI bus to 18MHz board file
    Update Dalmore to 18 MHz SPI bus.
    
    Bug 1054642
    
    Change-Id: I4a3d9baf6f324271f0b4444176e1152596749a5b
    Signed-off-by: David Jung <djung@nvidia.com>
    Signed-off-by: Xiaohui Tao <xtao@nvidia.com>
    Reviewed-on: http://git-master/r/147646
    Reviewed-by: Simone Willett <swillett@nvidia.com>
    Tested-by: Simone Willett <swillett@nvidia.com>
    
    Rebase-Id: R31fe8db1ed410da0cd4e1f538835cd766ce630f3

--- arch/arm/mach-tegra/board-dalmore.c
+++ arch/arm/mach-tegra/board-dalmore.c
@@ -631,7 +631,7 @@ struct rm_spi_ts_platform_data rm31080ts_dalmore_data = {
 
 static struct tegra_spi_device_controller_data dev_cdata = {
 	.rx_clk_tap_delay = 0,
-	.tx_clk_tap_delay = 0,
+	.tx_clk_tap_delay = 13,
 };
 
 struct spi_board_info rm31080a_dalmore_spi_board[1] = {
@@ -639,7 +639,7 @@ struct spi_board_info rm31080a_dalmore_spi_board[1] = {
 	 .modalias = "rm_ts_spidev",
 	 .bus_num = 3,
 	 .chip_select = 2,
-	 .max_speed_hz = 12 * 1000 * 1000,
+	 .max_speed_hz = 18 * 1000 * 1000,
 	 .mode = SPI_MODE_0,
 	 .controller_data = &dev_cdata,
 	 .platform_data = &rm31080ts_dalmore_data,
