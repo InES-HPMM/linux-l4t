8b8837234b1a0a7b4042896c05696179d81fb72f
Sayak Ghosh Choudhury <sayakc@nvidia.com>
2012-10-05 21:24:24 +0530

    alsa: hda: update max channel info from eld buffer
    the max pcm channel info was not coming proper and was not matching
    eld buffer information. This fix is made to solve that issue.
    
    Bug 1046366
    
    Change-Id: I8f3abe06cca5a3d7047c46df9e35cb635ceee5d9
    Signed-off-by: Sayak Ghosh Choudhury <sayakc@nvidia.com>
    Reviewed-on: http://git-master/r/141995
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Nikesh Oswal <noswal@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Scott Peterson <speterson@nvidia.com>
    
    Rebase-Id: Ra651a61cd14621090fcc72e3fe61093ecc6ab10b

--- sound/pci/hda/hda_eld.c
+++ sound/pci/hda/hda_eld.c
@@ -432,6 +432,7 @@ int snd_hdmi_get_eld(struct hda_codec *codec, hda_nid_t nid,
 		hdmi_update_lpcm_sad_eld(codec, nid, eld, size);
 
 	codec->recv_dec_cap = 0;
+	codec->max_pcm_channels = 0;
 	for (i = 0; i < eld->sad_count; i++) {
 		if (eld->sad[i].format == AUDIO_CODING_TYPE_AC3) {
 			codec->recv_dec_cap |= (1 << AUDIO_CODING_TYPE_AC3);
