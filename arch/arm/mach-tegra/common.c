5f19ba55673ab9ed04b731d426cd4fe1e755bf97
Alex Frid <afrid@nvidia.com>
2012-10-05 17:21:28 -0700

    ARM: tegra11: clock: Initialize pll_re_vco to 312 MHz
    Signed-off-by: Alex Frid <afrid@nvidia.com>
    Reviewed-on: http://git-master/r/142128
    (cherry picked from commit 218e7e5f5fc097198d56980818b0c510b588be3b)
    
    Change-Id: Ia152b61a9f3df7662a9be67bc13afe0b1d7f978e
    Signed-off-by: Bharat Nihalani <bnihalani@nvidia.com>
    Reviewed-on: http://git-master/r/146278
    Reviewed-by: Automatic_Commit_Validation_User
    
    Rebase-Id: R159a24516e4d10fe9651d1b804db651b75321784

--- arch/arm/mach-tegra/common.c
+++ arch/arm/mach-tegra/common.c
@@ -228,6 +228,7 @@ static __initdata struct tegra_clk_init_table tegra11x_clk_init_table[] = {
 	{ "csite",      NULL,           0,              true },
 #endif
 	{ "pll_u",	NULL,		480000000,	true },
+	{ "pll_re_vco",	NULL,		312000000,	true },
 	{ "sdmmc1",	"pll_p",	48000000,	false},
 	{ "sdmmc3",	"pll_p",	48000000,	true},
 	{ "sdmmc4",	"pll_p",	48000000,	false},
