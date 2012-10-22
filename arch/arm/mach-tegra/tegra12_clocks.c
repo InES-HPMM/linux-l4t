e2a638375f875a2a399e221e4685b6896e4e2a82
Bhanu Chetlapalli <bchetlapalli@nvidia.com>
2012-10-22 13:47:59 -0400

    ARM: tegra12: clock: fix curacao_sim build break
    Picked from tegra11 & applied to tegra12
    Original Commit: 2597328141764ab70cd703a10303d246bd810715
    
    Fixes unused warning-as-error problems with curacao_sim
    build target.
    
    Signed-off-by: Bhanu Chetlapalli <bchetlapalli@nvidia.com>
    Change-Id: Ibcf040e66e67ceba641e7c81fc0d4cff5ab02abd
    Reviewed-on: http://git-master/r/193133
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Jeff Smith <jsmith@nvidia.com>
    Reviewed-by: Chao Xu <cxu@nvidia.com>

--- arch/arm/mach-tegra/tegra12_clocks.c
+++ arch/arm/mach-tegra/tegra12_clocks.c
@@ -3875,6 +3875,7 @@ static struct clk_ops tegra_periph_clk_ops = {
 };
 
 
+#if !defined(CONFIG_TEGRA_SIMULATION_PLATFORM)
 /* msenc clock propagation WAR for bug 1005168 */
 static int tegra12_msenc_clk_enable(struct clk *c)
 {
@@ -3898,7 +3899,7 @@ static struct clk_ops tegra_msenc_clk_ops = {
 	.round_rate		= &tegra12_periph_clk_round_rate,
 	.reset			= &tegra12_periph_clk_reset,
 };
-
+#endif
 /* Periph extended clock configuration ops */
 static int
 tegra12_vi_clk_cfg_ex(struct clk *c, enum tegra_clk_ex_param p, u32 setting)
