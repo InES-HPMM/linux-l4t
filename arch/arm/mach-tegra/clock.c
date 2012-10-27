b353ea72a3c623c9092512aeec2a46229d26e554
Alex Frid <afrid@nvidia.com>
2012-10-27 00:46:38 -0700

    ARM: tegra: clock: Mark BW users in the clock tree
    Marked bandwidth shared users with "+" sign in the clock tree.
    
    Change-Id: I9cbc766a6407d6075b2f39638a9f83f22d98e1f7
    Signed-off-by: Alex Frid <afrid@nvidia.com>
    Reviewed-on: http://git-master/r/159430
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Yu-Huan Hsu <yhsu@nvidia.com>
    
    Rebase-Id: R88bbd1241677767ae3f4353666caa3b91821c61a

--- arch/arm/mach-tegra/clock.c
+++ arch/arm/mach-tegra/clock.c
@@ -1138,7 +1138,8 @@ static void clock_tree_show_one(struct seq_file *s, struct clk *c, int level)
 		c->cansleep ? '$' : ' ',
 		state, c->refcnt, div, rate);
 	if (c->parent && !list_empty(&c->parent->shared_bus_list))
-		seq_printf(s, " (%lu)", c->u.shared_bus_user.rate);
+		seq_printf(s, " (%lu%s)", c->u.shared_bus_user.rate,
+			   c->u.shared_bus_user.mode == SHARED_BW ? "+" : "");
 	seq_printf(s, "\n");
 
 	if (c->dvfs)
