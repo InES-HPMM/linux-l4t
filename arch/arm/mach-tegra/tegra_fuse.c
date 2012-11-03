ba8449155de9712970ec262544783486bec6949b
Krishna Reddy <vdumpa@nvidia.com>
2012-11-02 17:53:19 -0700

    arm: tegra: fuse: fix the minor number for T11x A01 version.
    The minor number is set to 0 instead of 1 for A01.
    
    Change-Id: I5ffa98f740d4cb6d88ac96d787fe6b463feaf7f0
    Signed-off-by: Krishna Reddy <vdumpa@nvidia.com>
    Reviewed-on: http://git-master/r/161039
    Reviewed-by: Aleksandr Frid <afrid@nvidia.com>
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Kaz Fukuoka <kfukuoka@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>

--- arch/arm/mach-tegra/tegra_fuse.c
+++ arch/arm/mach-tegra/tegra_fuse.c
@@ -425,7 +425,7 @@ static struct chip_revision tegra_chip_revisions[] = {
 	CHIP_REVISION(TEGRA3,  1, 1, 0,   A01),
 	CHIP_REVISION(TEGRA3,  1, 2, 0,   A02),
 	CHIP_REVISION(TEGRA3,  1, 3, 0,   A03),
-	CHIP_REVISION(TEGRA11, 1, 0, 0,   A01),
+	CHIP_REVISION(TEGRA11, 1, 1, 0,   A01),
 };
 #endif
 
