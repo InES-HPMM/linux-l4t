8b412ff08295f472eace9f6699aa262967d20eba
Matt Wagner <mwagner@nvidia.com>
2012-11-02 18:35:44 -0700

    ARM: Tegra: Roth: Remove Incorrect Calibration Data
    bl_output contains only 128 entries while we access 256 of them.
    This causes the display to go black on certain brightnesses. bl_output
    is removed until the backlight is properly calibrated.
    
    Bug 1170236
    
    Change-Id: I5715c72d3bc974c43ace4774d7d8739aa9847733
    Signed-off-by: Matt Wagner <mwagner@nvidia.com>
    Reviewed-on: http://git-master/r/161043
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Thomas Cherry <tcherry@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- arch/arm/mach-tegra/board-roth-panel.c
+++ arch/arm/mach-tegra/board-roth-panel.c
@@ -170,26 +170,6 @@ static u8 panel_ce10[] = {
 static u8 panel_ce11[] = {0x7a, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0};
 static u8 panel_ce12[] = {0x7b, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0};
 static u8 panel_ce13[] = {0x7c, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0};
-static tegra_dc_bl_output roth_bl_output_measured = {
-	0, 1, 3, 5, 7, 9, 11, 12,
-	14, 15, 16, 18, 19, 21, 22, 24,
-	25, 26, 27, 28, 29, 30, 31, 32,
-	33, 34, 35, 36, 38, 39, 40, 41,
-	42, 43, 44, 45, 46, 47, 48, 49,
-	50, 51, 51, 52, 52, 53, 54, 55,
-	56, 56, 57, 58, 59, 60, 61, 62,
-	63, 64, 65, 66, 67, 68, 69, 70,
-	71, 72, 73, 74, 75, 76, 76, 77,
-	78, 79, 80, 81, 81, 82, 83, 83,
-	84, 85, 85, 86, 87, 88, 89, 90,
-	91, 92, 93, 94, 95, 96, 96, 97,
-	98, 99, 100, 101, 102, 103, 103, 104,
-	104, 105, 106, 107, 108, 109, 110, 111,
-	112, 113, 114, 115, 116, 117, 118, 119,
-	120, 121, 122, 123, 124, 124, 125, 127
-};
-
-static p_tegra_dc_bl_output bl_output = roth_bl_output_measured;
 
 static struct tegra_dsi_cmd dsi_init_cmd[] = {
 	DSI_CMD_LONG(DSI_GENERIC_LONG_WRITE, panel_dsi_config),
@@ -626,8 +606,6 @@ static int roth_disp1_bl_notify(struct device *unused, int brightness)
 	/* Apply any backlight response curve */
 	if (brightness > 255)
 		pr_info("Error: Brightness > 255!\n");
-	else
-		brightness = bl_output[brightness];
 
 	return brightness;
 }
