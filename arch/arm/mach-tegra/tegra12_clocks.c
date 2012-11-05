9d254292b1f182bc6b32bbd68c07169b836363cc
Bhanu Chetlapalli <bchetlapalli@nvidia.com>
2012-11-05 10:56:06 -0800

    ARM: tegra12: clock: Update PLLC2/C3 configuration
    e58689bd9190388e164ececab9d7b4c765c5e4db Update PLLC2/C3 configuration
    
    Change-Id: Icc2c635d32c235d21b94d91aee024e1ce41d7c27
    Signed-off-by: Bhanu Chetlapalli <bchetlapalli@nvidia.com>
    Reviewed-on: http://git-master/r/161366
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Chao Xu <cxu@nvidia.com>

--- arch/arm/mach-tegra/tegra12_clocks.c
+++ arch/arm/mach-tegra/tegra12_clocks.c
@@ -255,7 +255,7 @@ enum tegra_revision tegra_get_revision(void); /* !!!FIXME!!! eliminate */
 					(0x2 << PLLCX_MISC_FILT_DIV_SHIFT) | \
 					PLLCX_MISC_RESET)
 #define PLLCX_MISC1_DEFAULT_VALUE	0x000d2308
-#define PLLCX_MISC2_DEFAULT_VALUE	0x11111200
+#define PLLCX_MISC2_DEFAULT_VALUE	0x31211200
 #define PLLCX_MISC3_DEFAULT_VALUE	0x0
 
 /* PLLX and PLLC (PLLXC)*/
@@ -2118,6 +2118,10 @@ static u32 pllcx_round_p_to_pdiv(u32 p, u32 *pdiv)
 
 	if (p) {
 		for (i = 0; i <= PLLCX_PDIV_MAX; i++) {
+			/* Do not use DIV3 p values - mapped to even PDIV */
+			if (i && ((i & 0x1) == 0))
+				continue;
+
 			if (p <= pllcx_p[i]) {
 				if (pdiv)
 					*pdiv = i;
@@ -5011,7 +5015,7 @@ static struct clk tegra_pll_c2 = {
 		.input_max = 48000000,
 		.cf_min    = 12000000,
 		.cf_max    = 19200000,
-		.vco_min   = 739000000,
+		.vco_min   = 600000000,
 		.vco_max   = 1200000000,
 		.freq_table = tegra_pll_cx_freq_table,
 		.lock_delay = 300,
@@ -5032,7 +5036,7 @@ static struct clk tegra_pll_c3 = {
 		.input_max = 48000000,
 		.cf_min    = 12000000,
 		.cf_max    = 19200000,
-		.vco_min   = 739000000,
+		.vco_min   = 600000000,
 		.vco_max   = 1200000000,
 		.freq_table = tegra_pll_cx_freq_table,
 		.lock_delay = 300,
