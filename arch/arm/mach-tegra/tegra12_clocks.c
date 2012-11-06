5ed844c9515be33fb2a3c5358c97c8a1ee5f3942
Bhanu Chetlapalli <bchetlapalli@nvidia.com>
2012-11-05 23:21:34 -0800

    ARM: tegra12: clock: Add shared bus override users
    Picked from tegra11 & applied to tegra12
    Original Commit: f079f62e33c16f703afed0c71f17e6977600a980
    
    Added shared bus override users to allow testing of shared bus
    mechanisms.
    
    Signed-off-by: Bhanu Chetlapalli <bchetlapalli@nvidia.com>
    Change-Id: Ib630d3d61a6aa7ee88b9b1231cfd2d6dad033c2a
    Reviewed-on: http://git-master/r/194788
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Chao Xu <cxu@nvidia.com>
    Reviewed-on: http://git-master/r/200191
    Tested-by: Chao Xu <cxu@nvidia.com>

--- arch/arm/mach-tegra/tegra12_clocks.c
+++ arch/arm/mach-tegra/tegra12_clocks.c
@@ -4751,6 +4751,7 @@ static unsigned long tegra12_clk_shared_bus_update(
 	struct clk *slow = NULL;
 	struct clk *top = NULL;
 
+	unsigned long override_rate = 0;
 	unsigned long top_rate = 0;
 	unsigned long rate = bus->min_rate;
 	unsigned long bw = 0;
@@ -4780,6 +4781,10 @@ static unsigned long tegra12_clk_shared_bus_update(
 			case SHARED_CEILING:
 				ceiling = min(request_rate, ceiling);
 				break;
+			case SHARED_OVERRIDE:
+				if (override_rate == 0)
+					override_rate = request_rate;
+				break;
 			case SHARED_AUTO:
 				break;
 			case SHARED_FLOOR:
@@ -4807,7 +4812,7 @@ static unsigned long tegra12_clk_shared_bus_update(
 		bw = (bw < bus->max_rate / 100) ? (bw * 100) : bus->max_rate;
 	}
 
-	rate = min(max(rate, bw), ceiling);
+	rate = override_rate ? : min(max(rate, bw), ceiling);
 
 	if (bus_top)
 		*bus_top = top;
@@ -6489,6 +6494,7 @@ struct clk tegra_list_clks[] = {
 	SHARED_CLK("mon.avp",	"tegra_actmon",		"avp",	&tegra_clk_sbus_cmplx, NULL, 0, 0),
 	SHARED_CLK("cap.sclk",	"cap_sclk",		NULL,	&tegra_clk_sbus_cmplx, NULL, 0, SHARED_CEILING),
 	SHARED_CLK("floor.sclk", "floor_sclk",		NULL,	&tegra_clk_sbus_cmplx, NULL, 0, 0),
+	SHARED_CLK("override.sclk", "override_sclk",    NULL,   &tegra_clk_sbus_cmplx, NULL, 0, SHARED_OVERRIDE),
 	SHARED_CLK("sbc1.sclk", "tegra11-spi.0",	"sclk", &tegra_clk_sbus_cmplx, NULL, 0, 0),
 	SHARED_CLK("sbc2.sclk", "tegra11-spi.1",	"sclk", &tegra_clk_sbus_cmplx, NULL, 0, 0),
 	SHARED_CLK("sbc3.sclk", "tegra11-spi.2",	"sclk", &tegra_clk_sbus_cmplx, NULL, 0, 0),
@@ -6514,6 +6520,7 @@ struct clk tegra_list_clks[] = {
 	SHARED_CLK("camera.emc", "tegra_camera",	"emc",	&tegra_clk_emc, NULL, 0, SHARED_BW),
 	SHARED_CLK("iso.emc",	"iso",			"emc",	&tegra_clk_emc, NULL, 0, SHARED_BW),
 	SHARED_CLK("floor.emc",	"floor.emc",		NULL,	&tegra_clk_emc, NULL, 0, 0),
+	SHARED_CLK("override.emc", "override.emc",	NULL,	&tegra_clk_emc, NULL, 0, SHARED_OVERRIDE),
 #ifdef CONFIG_ARCH_TEGRA_VIC
 	SHARED_CLK("vic.emc",	"tegra_vic03",		"emc",  &tegra_clk_emc, NULL, 0, 0),
 #endif
@@ -6523,6 +6530,7 @@ struct clk tegra_list_clks[] = {
 	DUAL_CBUS_CLK("se.cbus",	"tegra12-se",		NULL,	 &tegra_clk_c2bus, "se",    0, 0),
 	SHARED_CLK("cap.c2bus",		"cap.c2bus",		NULL,	 &tegra_clk_c2bus, NULL,    0, SHARED_CEILING),
 	SHARED_CLK("floor.c2bus",	"floor.c2bus",		NULL,	 &tegra_clk_c2bus, NULL,    0, 0),
+	SHARED_CLK("override.c2bus",	"override.c2bus",	NULL,	 &tegra_clk_c2bus, NULL,  0, SHARED_OVERRIDE),
 
 #ifdef CONFIG_ARCH_TEGRA_VIC
 	DUAL_CBUS_CLK("vic03.cbus",	"tegra_vic03",		"vic03", &tegra_clk_c3bus, "vic03", 0, 0),
@@ -6530,6 +6538,7 @@ struct clk tegra_list_clks[] = {
 	DUAL_CBUS_CLK("tsec.cbus",	"tegra_tsec",		"tsec",  &tegra_clk_c3bus,  "tsec", 0, 0),
 	SHARED_CLK("cap.c3bus",		"cap.c3bus",		NULL,	 &tegra_clk_c3bus, NULL,    0, SHARED_CEILING),
 	SHARED_CLK("floor.c3bus",	"floor.c3bus",		NULL,	 &tegra_clk_c3bus, NULL,    0, 0),
+	SHARED_CLK("override.c3bus",	"override.c3bus",	NULL,	 &tegra_clk_c3bus, NULL,  0, SHARED_OVERRIDE),
 #else
 #ifdef CONFIG_ARCH_TEGRA_VIC
 	SHARED_CLK("vic03.cbus",  "tegra_vic03",	"vic03", &tegra_clk_cbus, "vic03", 0, 0),
@@ -6540,6 +6549,7 @@ struct clk tegra_list_clks[] = {
 	SHARED_CLK("se.cbus",	"tegra11-se",		NULL,	&tegra_clk_cbus, "se",  0, 0),
 	SHARED_CLK("cap.cbus",	"cap.cbus",		NULL,	&tegra_clk_cbus, NULL,  0, SHARED_CEILING),
 	SHARED_CLK("floor.cbus", "floor.cbus",		NULL,	&tegra_clk_cbus, NULL,  0, 0),
+	SHARED_CLK("override.cbus", "override.cbus",	NULL,	&tegra_clk_cbus, NULL,  0, SHARED_OVERRIDE),
 #endif
 };
 
