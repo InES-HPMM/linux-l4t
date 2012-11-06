e5a24961802cd41e8e0b332fae0ab52a1dd2227d
Sachin Nikam <snikam@nvidia.com>
2012-11-06 18:41:34 +0530

    arm: tegra: t148: add first DT support
    Add first Device Tree file for T148.
    
    Bug 1165178
    
    Change-Id: Ib1424d0b530eed6163c3d0d8919425345bcf0f57
    Signed-off-by: Sachin Nikam <snikam@nvidia.com>
    Reviewed-on: http://git-master/r/161635
    Reviewed-by: Varun Colbert <vcolbert@nvidia.com>
    Tested-by: Varun Colbert <vcolbert@nvidia.com>

---
+++ arch/arm/mach-tegra/board-dt-tegra148.c
@@ -0,0 +1,48 @@
+/*
+ * arch/arm/mach-tegra/board-dt-tegra148.c
+ *
+ * NVIDIA Tegra148 device tree board support
+ *
+ * Copyright (C) 2012 NVIDIA Corporation. All rights reserved.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#include <linux/kernel.h>
+#include <linux/of.h>
+
+#include <asm/mach/arch.h>
+#include <asm/hardware/gic.h>
+
+#include "board.h"
+#include "clock.h"
+#include "common.h"
+
+static void __init tegra148_dt_init(void)
+{
+}
+
+static const char * const tegra148_dt_board_compat[] = {
+	"nvidia,tegra148",
+	NULL
+};
+
+DT_MACHINE_START(TEGRA148_DT, "NVIDIA Tegra148 (Flattened Device Tree)")
+	.soc		= &tegra_soc_desc,
+	.map_io		= tegra_map_common_io,
+	.init_early	= tegra14x_init_early,
+	.init_irq	= tegra_dt_init_irq,
+	.handle_irq	= gic_handle_irq,
+	.timer		= &tegra_timer,
+	.init_machine	= tegra148_dt_init,
+	.restart	= tegra_assert_system_reset,
+	.dt_compat	= tegra148_dt_board_compat,
+MACHINE_END
