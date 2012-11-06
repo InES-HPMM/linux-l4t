335b33d34c01a0fe1810daa257be2c8d49167cce
Xiaohui Tao <xtao@nvidia.com>
2012-11-06 10:46:01 -0800

    ARM: tegra: dalmore: fix orientation vector
    Change of gyro orientation vector to fix the orientation issue
    with dalmore.
    
    Bug 1169860
    
    Change-Id: I652518273d6bf04bc44e992a1c71783c6b0e4f00
    Signed-off-by: Xiaohui Tao <xtao@nvidia.com>
    Reviewed-on: http://git-master/r/161737
    Reviewed-by: Simone Willett <swillett@nvidia.com>
    Tested-by: Simone Willett <swillett@nvidia.com>

--- arch/arm/mach-tegra/board-dalmore.h
+++ arch/arm/mach-tegra/board-dalmore.h
@@ -87,7 +87,7 @@
 #define MPU_GYRO_IRQ_GPIO       TEGRA_GPIO_PR3
 #define MPU_GYRO_ADDR           0x69
 #define MPU_GYRO_BUS_NUM        0
-#define MPU_GYRO_ORIENTATION    { -1, 0, 0, 0, -1, 0, 0, 0, 1 }
+#define MPU_GYRO_ORIENTATION	{ -1, 0, 0, 0, 1, 0, 0, 0, -1 }
 #define MPU_ACCEL_NAME          "kxtf9"
 #define MPU_ACCEL_IRQ_GPIO      0 /* DISABLE ACCELIRQ:  TEGRA_GPIO_PJ2 */
 #define MPU_ACCEL_ADDR          0x0F
