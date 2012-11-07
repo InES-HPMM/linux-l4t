2adfeffea1988d68e7cd5bc97d6d7c7fd1f50d3e
Bhanu Chetlapalli <bchetlapalli@nvidia.com>
2012-11-06 16:15:09 -0800

    ARM: tegra12: clock: Update emc to cpu rate ratio limits
    Original commit: 4d0f63f0082d88a719dd66f3f795d1cf6857be71
    
    Signed-off-by: Bhanu Chetlapalli <bchetlapalli@nvidia.com>
    Change-Id: Ie16d81d34e2d8825942ce87b8a0a049ddcecbd8e
    Reviewed-on: http://git-master/r/194786
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Chao Xu <cxu@nvidia.com>
    Reviewed-on: http://git-master/r/200189
    Tested-by: Chao Xu <cxu@nvidia.com>

--- arch/arm/mach-tegra/tegra12_clocks.c
+++ arch/arm/mach-tegra/tegra12_clocks.c
@@ -7040,12 +7040,16 @@ unsigned long tegra_emc_to_cpu_ratio(unsigned long cpu_rate)
 
 	/* Vote on memory bus frequency based on cpu frequency;
 	   cpu rate is in kHz, emc rate is in Hz */
-	if (cpu_rate >= 750000)
-		return emc_max_rate;	/* cpu >= 750 MHz, emc max */
-	else if (cpu_rate >= 450000)
-		return emc_max_rate/2;	/* cpu >= 500 MHz, emc max/2 */
-	else if (cpu_rate >= 250000)
-		return 100000000;	/* cpu >= 250 MHz, emc 100 MHz */
+	if (cpu_rate >= 1300000)
+		return emc_max_rate;	/* cpu >= 1.3GHz, emc max */
+	else if (cpu_rate >= 975000)
+		return 400000000;	/* cpu >= 975 MHz, emc 400 MHz */
+	else if (cpu_rate >= 725000)
+		return  200000000;	/* cpu >= 725 MHz, emc 200 MHz */
+	else if (cpu_rate >= 500000)
+		return  100000000;	/* cpu >= 500 MHz, emc 100 MHz */
+	else if (cpu_rate >= 275000)
+		return  50000000;	/* cpu >= 275 MHz, emc 50 MHz */
 	else
 		return 0;		/* emc min */
 }
