94396a3c49a381acffa026d50fed5d6f0275b0e7
Bhanu Chetlapalli <bchetlapalli@nvidia.com>
2012-11-13 21:51:47 -0800

    ARM: tegra12: dvfs: Update system bus dvfs table
    Picked from tegra11 & applied to tegra12
    Original Commit: 78e55efce27c4d1c3d81a94ff902b2f879e19174
    
    Bug 1161126
    
    Signed-off-by: Bhanu Chetlapalli <bchetlapalli@nvidia.com>
    Change-Id: I73a101b0c41edd00681110f88ca44fe054cec4c0
    Reviewed-on: http://git-master/r/194792
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Chao Xu <cxu@nvidia.com>
    Reviewed-on: http://git-master/r/200195
    Tested-by: Chao Xu <cxu@nvidia.com>

--- arch/arm/mach-tegra/tegra12_clocks.c
+++ arch/arm/mach-tegra/tegra12_clocks.c
@@ -5862,7 +5862,7 @@ static struct clk tegra_clk_sclk = {
 	.inputs	= mux_sclk,
 	.reg	= 0x28,
 	.ops	= &tegra_super_ops,
-	.max_rate = 334000000,
+	.max_rate = 336000000,
 	.min_rate = 12000000,
 };
 
@@ -5911,7 +5911,7 @@ static struct clk tegra_clk_cop = {
 	.name      = "cop",
 	.parent    = &tegra_clk_sclk,
 	.ops       = &tegra_cop_ops,
-	.max_rate  = 334000000,
+	.max_rate  = 336000000,
 };
 
 static struct clk tegra_clk_hclk = {
@@ -5921,7 +5921,7 @@ static struct clk tegra_clk_hclk = {
 	.reg		= 0x30,
 	.reg_shift	= 4,
 	.ops		= &tegra_bus_ops,
-	.max_rate       = 334000000,
+	.max_rate       = 336000000,
 	.min_rate       = 12000000,
 };
 
@@ -5932,7 +5932,7 @@ static struct clk tegra_clk_pclk = {
 	.reg		= 0x30,
 	.reg_shift	= 0,
 	.ops		= &tegra_bus_ops,
-	.max_rate       = 167000000,
+	.max_rate       = 168000000,
 	.min_rate       = 12000000,
 };
 
