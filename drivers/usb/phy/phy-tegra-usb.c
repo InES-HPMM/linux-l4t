272d57a91a939b2781ef4bb0a9a4bc6e66d87739
Petlozu Pravareshwar <petlozup@nvidia.com>
2012-11-16 20:14:14 +0530

    arm: tegra: usb:  make ehci-hcd as loadable module
    exporting the symbol for the proper behaviour
    while inserting the ehci-hcd module.
    
    Bug 1022439
    
    Change-Id: Ie3b49e59b46581882475fa5304fb3b8800b0f78a
    Signed-off-by: Petlozu Pravareshwar <petlozup@nvidia.com>
    Reviewed-on: http://git-master/r/164342
    Reviewed-by: Simone Willett <swillett@nvidia.com>
    Tested-by: Simone Willett <swillett@nvidia.com>

--- drivers/usb/phy/phy-tegra-usb.c
+++ drivers/usb/phy/phy-tegra-usb.c
@@ -448,6 +448,7 @@ int tegra_usb_phy_pre_suspend(struct tegra_usb_phy *phy)
 
 	return status;
 }
+EXPORT_SYMBOL_GPL(tegra_usb_phy_pre_suspend);
 
 int tegra_usb_phy_suspend(struct tegra_usb_phy *phy)
 {
