be0e060801598d30208415e5b1c53182d1fb4208
Alex Frid <afrid@nvidia.com>
2012-11-19 22:09:22 -0800

    ARM: tegra11: fuse: Set speedo/process IDs based on fuses
    Bug 1170986
    
    Change-Id: Id95f7eb8465204ba444c41b44d9736169c2ffd7b
    Signed-off-by: Alex Frid <afrid@nvidia.com>
    Reviewed-on: http://git-master/r/165539
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Diwakar Tundlam <dtundlam@nvidia.com>
    Tested-by: Diwakar Tundlam <dtundlam@nvidia.com>

--- arch/arm/mach-tegra/tegra11_speedo.c
+++ arch/arm/mach-tegra/tegra11_speedo.c
@@ -31,6 +31,9 @@
 
 #include "fuse.h"
 
+#define CORE_PROCESS_CORNERS_NUM	2
+#define CPU_PROCESS_CORNERS_NUM		2
+
 #define FUSE_CPU_SPEEDO_0 0x114
 #define FUSE_CPU_SPEEDO_1 0x12c
 #define FUSE_CPU_IDDQ 0x118
@@ -38,6 +41,10 @@
 #define FUSE_CORE_SPEEDO_1 0x138
 #define FUSE_CORE_IDDQ 0x140
 #define FUSE_FT_REV 0x128
+#define FUSE_OPT_CPU23_DISABLE 0x26c
+#define FUSE_OPT_CPU23_REENABLE 0x270
+
+static int threshold_index;
 
 static int cpu_process_id;
 static int core_process_id;
@@ -50,22 +57,68 @@ static int cpu_iddq_value;
 
 static int enable_app_profiles;
 
+static const u32 core_process_speedos[][CORE_PROCESS_CORNERS_NUM] = {
+/* proc_id  0,     1 */
+	{1123,     UINT_MAX}, /* [0]: threshold_index 0 */
+	{0,        UINT_MAX}, /* [1]: threshold_index 1 */
+};
+
+static const u32 cpu_process_speedos[][CPU_PROCESS_CORNERS_NUM] = {
+/* proc_id  0,     1 */
+	{1695,     UINT_MAX}, /* [0]: threshold_index 0 */
+	{0,        UINT_MAX}, /* [1]: threshold_index 1 */
+};
+
+static void rev_sku_to_speedo_ids(int rev, int sku)
+{
+	bool a01 = false;
+	cpu_speedo_id = 0;	/* For A01 rev, regardless of SKU */
+
+	if (rev == TEGRA_REVISION_A01) {
+		u32 a01p = tegra_fuse_readl(FUSE_OPT_CPU23_REENABLE) << 1;
+		a01p |= tegra_fuse_readl(FUSE_OPT_CPU23_DISABLE);
+		if (a01p == 0)
+			a01 = true;
+	}
+
+	switch (sku) {
+	case 0x00: /* Eng */
+	case 0x10: /* Eng */
+	case 0x05: /* T40S */
+	case 0x06: /* AP40 */
+		if (!a01)
+			cpu_speedo_id = 1;
+		soc_speedo_id = 0;
+		threshold_index = 0;
+		break;
+
+	case 0x03: /* T40X */
+	case 0x04: /* T40T */
+		if (!a01)
+			cpu_speedo_id = 1;
+		soc_speedo_id = 1;
+		threshold_index = 1;
+		break;
+
+	default:
+		/* FIXME: replace with BUG() when all SKU's valid */
+		pr_err("Tegra11 Unknown SKU %d\n", sku);
+		cpu_speedo_id = 0;
+		soc_speedo_id = 0;
+		threshold_index = 0;
+		break;
+	}
+}
 
 void tegra_init_speedo_data(void)
 {
+	int i;
 	u32 ft_rev, ft_rev_major, ft_rev_minor;
 
 	cpu_speedo_value = 1024 + tegra_fuse_readl(FUSE_CPU_SPEEDO_1);
 	core_speedo_value = tegra_fuse_readl(FUSE_CORE_SPEEDO_0);
 
-	cpu_speedo_id = 0; /* start with one SKU */
-
 	cpu_iddq_value = tegra_fuse_readl(FUSE_CPU_IDDQ);
-
-	pr_info("Tegra11: CPU Speedo ID %d, Soc Speedo ID %d",
-		cpu_speedo_id, soc_speedo_id);
-	pr_info("Tegra11: CPU Speedo Value %d, Soc Speedo Value %d",
-		cpu_speedo_value, core_speedo_value);
 
 	ft_rev = tegra_fuse_readl(FUSE_FT_REV);
 	ft_rev_minor = ft_rev & 0x1f;
@@ -80,6 +133,29 @@ void tegra_init_speedo_data(void)
 
 		pr_warn("Tegra11: CPU IDDQ and speedo may be bogus");
 	}
+
+	rev_sku_to_speedo_ids(tegra_revision, tegra_sku_id);
+
+	pr_info("Tegra11: CPU Speedo ID %d, Soc Speedo ID %d",
+		cpu_speedo_id, soc_speedo_id);
+	pr_info("Tegra11: CPU Speedo Value %d, Soc Speedo Value %d",
+		cpu_speedo_value, core_speedo_value);
+
+	for (i = 0; i < CPU_PROCESS_CORNERS_NUM; i++) {
+		if (cpu_speedo_value <
+		    cpu_process_speedos[threshold_index][i]) {
+			break;
+		}
+	}
+	cpu_process_id = i;
+
+	for (i = 0; i < CORE_PROCESS_CORNERS_NUM; i++) {
+		if (core_speedo_value <
+		    core_process_speedos[threshold_index][i]) {
+			break;
+		}
+	}
+	core_process_id = i;
 }
 
 int tegra_cpu_process_id(void)
