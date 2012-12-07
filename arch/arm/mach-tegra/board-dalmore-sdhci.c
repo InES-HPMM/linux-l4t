68a8c1324d0bfc4cf041c6445dd411389e134fb8
Pavan Kunapuli <pkunapuli@nvidia.com>
2012-12-07 19:11:30 +0530

    ARM: tegra: dalmore: Mask SDR104,DDR50 for SDIO
    Mask SDR104 and DDR50 UHS mode support for SDIO
    devices as CRC errors are observed in these modes.
    
    Bug 1181574
    
    Change-Id: I93fb9fecc5eadcccc4c1c7180100d723719bc74b
    Signed-off-by: Pavan Kunapuli <pkunapuli@nvidia.com>
    Reviewed-on: http://git-master/r/169397
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Venu Byravarasu <vbyravarasu@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- arch/arm/mach-tegra/board-dalmore-sdhci.c
+++ arch/arm/mach-tegra/board-dalmore-sdhci.c
@@ -148,6 +148,8 @@ static struct tegra_sdhci_platform_data tegra_sdhci_platform_data0 = {
 	.tap_delay = 0x2,
 	.trim_delay = 0x2,
 	.ddr_clk_limit = 41000000,
+	.uhs_mask = MMC_UHS_MASK_SDR104 |
+		MMC_UHS_MASK_DDR50,
 };
 
 static struct tegra_sdhci_platform_data tegra_sdhci_platform_data2 = {
