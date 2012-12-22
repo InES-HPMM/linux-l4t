690660cd92f0f3fec68752b5cc69a7f944ea7cec
Matt Longnecker <mlongnecker@nvidia.com>
2012-12-21 17:29:54 -0800

    arm: tegra: remove dead code in mach/thermal.h
    Change-Id: I1e12461d454801f3062c83121db49af53a770851
    Signed-off-by: Matt Longnecker <mlongnecker@nvidia.com>
    Reviewed-on: http://git-master/r/173817
    (cherry picked from commit 5df3de710d1f74254af1b4b30b851c0fc40e74cc)
    Reviewed-on: http://git-master/r/191738
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Diwakar Tundlam <dtundlam@nvidia.com>

--- arch/arm/mach-tegra/include/mach/thermal.h
+++ arch/arm/mach-tegra/include/mach/thermal.h
@@ -20,53 +20,7 @@
 #include <linux/therm_est.h>
 #include <linux/thermal.h>
 
-enum thermal_device_id {
-	THERMAL_DEVICE_ID_NULL = 0,
-	THERMAL_DEVICE_ID_NCT_EXT = 1,
-	THERMAL_DEVICE_ID_NCT_INT = 2,
-	THERMAL_DEVICE_ID_TSENSOR = 3,
-	THERMAL_DEVICE_ID_THERM_EST_SKIN = 4,
-};
-
-#define THERMAL_DEVICE_MAX	(5)
-
-enum cooling_device_id {
-	CDEV_BTHROT_ID_TJ      = 0x00010000,
-	CDEV_EDPTABLE_ID_EDP   = 0x00030000,
-	CDEV_EDPTABLE_ID_EDP_0 = 0x00030000,
-	CDEV_EDPTABLE_ID_EDP_1 = 0x00030001,
-	CDEV_EDPTABLE_ID_EDP_2 = 0x00030002,
-	CDEV_EDPTABLE_ID_EDP_3 = 0x00030003,
-	CDEV_EDPTABLE_ID_EDP_4 = 0x00030004,
-};
-
-struct tegra_thermal_bind {
-	enum thermal_device_id tdev_id;
-	enum cooling_device_id cdev_id;
-	int type;
-	int (*get_trip_temp) (void *, long);
-	int (*get_trip_size) (void);
-	struct passive_params {
-		long trip_temp;
-		int tc1;
-		int tc2;
-		long passive_delay;
-	} passive;
-};
-
-struct tegra_thermal_device {
-	char *name;
-	enum thermal_device_id id;
-	void *data;
-	int (*get_temp) (void *, long *);
-	int (*set_limits) (void *, long, long);
-	int (*set_alert)(void *, void (*)(void *), void *);
-	struct thermal_zone_device *thz;
-	struct list_head node;
-};
-
 struct tegra_cooling_device {
-	enum cooling_device_id id;
 	char *cdev_type;
 	int *trip_temperatures;
 	int trip_temperatures_num;
@@ -112,18 +66,5 @@ static inline unsigned int tegra_throttle_governor_speed(
 	unsigned int requested_speed)
 { return requested_speed; }
 #endif /* CONFIG_TEGRA_THERMAL_THROTTLE */
-
-#ifdef CONFIG_TEGRA_THERMAL
-int tegra_thermal_init(struct tegra_thermal_bind *thermal_binds);
-int tegra_thermal_device_register(struct tegra_thermal_device *device);
-int tegra_thermal_exit(void);
-#else
-static inline int tegra_thermal_init(struct tegra_thermal_bind *thermal_binds)
-{ return 0; }
-static inline int tegra_thermal_device_register(struct tegra_thermal_device *device)
-{ return 0; }
-static inline int tegra_thermal_exit(void)
-{ return 0; }
-#endif
 
 #endif	/* __MACH_THERMAL_H */
