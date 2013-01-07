850c6d08c8230f89f8062e4877818d69bb65340f
Pavan Kunapuli <pkunapuli@nvidia.com>
2013-01-07 15:42:52 +0530

    mmc: tegra: Add post resume support
    Adding post resume support to take care
    of any platform specific requirements like
    turning OFF clocks when device not present etc.
    
    Reading card detect gpio value in resume to
    get the proper card present status.
    
    Bug 1188788
    Bug 1161783
    
    Reviewed-on: http://git-master/r/171959
    (cherry picked from commit  e488835bbd5ff57ddd297489b7ad99d6f5cffd70)
    
    Change-Id: Ida5a7af031aafd3956198cede9ce12da03fa137b
    Signed-off-by: Pavan Kunapuli <pkunapuli@nvidia.com>
    Reviewed-on: http://git-master/r/172775
    Reviewed-by: Mrutyunjay Sawant <msawant@nvidia.com>
    Tested-by: Mrutyunjay Sawant <msawant@nvidia.com>

--- drivers/mmc/host/sdhci-tegra.c
+++ drivers/mmc/host/sdhci-tegra.c
@@ -1193,6 +1193,10 @@ static int tegra_sdhci_resume(struct sdhci_host *sdhci)
 
 	pdev = to_platform_device(mmc_dev(sdhci->mmc));
 	plat = pdev->dev.platform_data;
+
+	if (gpio_is_valid(plat->cd_gpio))
+		tegra_host->card_present = (gpio_get_value(plat->cd_gpio) == 0);
+
 	/* Enable the power rails if any */
 	if (tegra_host->card_present) {
 		if (!tegra_host->is_rail_enabled) {
@@ -1211,6 +1215,7 @@ static int tegra_sdhci_resume(struct sdhci_host *sdhci)
 			tegra_host->is_rail_enabled = 1;
 		}
 	}
+
 	/* Setting the min identification clock of freq 400KHz */
 	tegra_sdhci_set_clock(sdhci, 400000);
 
@@ -1252,6 +1257,16 @@ err_root:
 	return;
 }
 
+static void tegra_sdhci_post_resume(struct sdhci_host *sdhci)
+{
+	struct sdhci_pltfm_host *pltfm_host = sdhci_priv(sdhci);
+	struct sdhci_tegra *tegra_host = pltfm_host->priv;
+
+	/* Turn OFF the clocks if the card is not present */
+	if (!(tegra_host->card_present) && tegra_host->clk_enabled)
+		tegra_sdhci_set_clock(sdhci, 0);
+}
+
 static const struct sdhci_ops tegra_sdhci_ops = {
 #ifndef CONFIG_ARCH_TEGRA_11x_SOC
 	.get_ro     = tegra_sdhci_get_ro,
@@ -1264,6 +1279,7 @@ static const struct sdhci_ops tegra_sdhci_ops = {
 	.set_clock		= tegra_sdhci_set_clock,
 	.suspend		= tegra_sdhci_suspend,
 	.resume			= tegra_sdhci_resume,
+	.platform_resume	= tegra_sdhci_post_resume,
 	.platform_reset_exit	= tegra_sdhci_reset_exit,
 	.set_uhs_signaling	= tegra_sdhci_set_uhs_signaling,
 	.switch_signal_voltage	= tegra_sdhci_signal_voltage_switch,
