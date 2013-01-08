5c03b2f4e8d2a7319162aa4507be18963bae694d
Nitin Nagaraja <nitinn@nvidia.com>
2013-01-08 11:35:29 +0530

    sound: soc: codecs: Add ad1937 driver for E1860
    Changed number of channels supported for playback and record to 16.
    
    Bug 992424
    
    Change-Id: I43f75b0b943413bb01788c857dac1577f86ac0bd
    Reviewed-on: http://git-master/r/189435
    (cherry picked from commit ebf9427afd35373b1e19c643a934876600eb98df)
    Signed-off-by: Nitin Nagaraja <nitinn@nvidia.com>
    Reviewed-on: http://git-master/r/209839
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Bob Johnston <bjohnston@nvidia.com>
    Reviewed-by: Scott Peterson <speterson@nvidia.com>

--- sound/soc/codecs/ad193x.c
+++ sound/soc/codecs/ad193x.c
@@ -304,7 +304,7 @@ static struct snd_soc_dai_driver ad193x_dai = {
 	.playback = {
 		.stream_name = "Playback",
 		.channels_min = 2,
-		.channels_max = 8,
+		.channels_max = 16,
 		.rates = SNDRV_PCM_RATE_48000,
 		.formats = SNDRV_PCM_FMTBIT_S32_LE | SNDRV_PCM_FMTBIT_S16_LE |
 			SNDRV_PCM_FMTBIT_S20_3LE | SNDRV_PCM_FMTBIT_S24_LE,
@@ -312,7 +312,7 @@ static struct snd_soc_dai_driver ad193x_dai = {
 	.capture = {
 		.stream_name = "Capture",
 		.channels_min = 2,
-		.channels_max = 4,
+		.channels_max = 16,
 		.rates = SNDRV_PCM_RATE_48000,
 		.formats = SNDRV_PCM_FMTBIT_S32_LE | SNDRV_PCM_FMTBIT_S16_LE |
 			SNDRV_PCM_FMTBIT_S20_3LE | SNDRV_PCM_FMTBIT_S24_LE,
