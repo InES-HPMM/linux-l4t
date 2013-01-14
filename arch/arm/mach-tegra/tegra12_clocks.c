cd223e445008b638dc6a4cb351237a1749b7f1de
Xue Dong <xdong@nvidia.com>
2013-01-14 10:59:42 -0800

    tegra: remove netlist revision checking for QT
    Change-Id: Ica5b5dd680a2511856ddef1a410cd5d245785259
    Signed-off-by: Xue Dong <xdong@nvidia.com>
    Reviewed-on: http://git-master/r/190985
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Chao Xu <cxu@nvidia.com>

--- arch/arm/mach-tegra/tegra12_clocks.c
+++ arch/arm/mach-tegra/tegra12_clocks.c
@@ -3270,15 +3270,6 @@ static void __init tegra12_dfll_cpu_late_init(struct clk *c)
 {
 	int ret;
 
-#ifndef CONFIG_TEGRA_SILICON_PLATFORM
-	u32 netlist, patchid;
-	tegra_get_netlist_revision(&netlist, &patchid);
-	if (netlist < 12) {
-		pr_err("%s: CL-DVFS is not available on net %d\n",
-		       __func__, netlist);
-		return;
-	}
-#endif
 	/* release dfll clock source reset, init cl_dvfs control logic, and
 	   move dfll to initialized state, so it can be used as CPU source */
 	tegra_periph_reset_deassert(c);
