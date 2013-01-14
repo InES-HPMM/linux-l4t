50134d40749227079aef122662d7f67f214ece6c
Xue Dong <xdong@nvidia.com>
2013-01-13 16:50:57 -0800

    ARM: tegra: remove gpio enable/disable for bonaire
    Change-Id: I828adbb7a64eb384f9445e351dea765e90778f51
    Signed-off-by: Xue Dong <xdong@nvidia.com>
    Reviewed-on: http://git-master/r/190916
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Chao Xu <cxu@nvidia.com>

--- arch/arm/mach-tegra/board-bonaire-panel.c
+++ arch/arm/mach-tegra/board-bonaire-panel.c
@@ -1,7 +1,7 @@
 /*
  * arch/arm/mach-tegra/board-bonaire-panel.c
  *
- * Copyright (c) 2011, NVIDIA Corporation.
+ * Copyright (c) 2013, NVIDIA Corporation.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
