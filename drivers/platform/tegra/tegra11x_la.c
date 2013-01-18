fc5ac166c8cb65f6ffea2a5cce61338e21ff8288
Peng Du <pdu@nvidia.com>
2013-01-18 11:06:43 -0800

    drivers: #include sharable mach-tegra (TEMP)
    To share code between arm and arm64, we have to relocate
    sources from arch/arm/mach-tegra into drivers. As this is
    not done in the main branch but our local branch, we employ
    this "symlink" method to avoid merge nightmare.
    
    The generation of these files are all scripted. Note that
    in this first round we added only the minimum set of files
    which are built w.r.t the tegra_bonaire_android_defconfig.
    
    Bug 1237330
    
    Change-Id: I636013a8a0542697f6d28097da6ba5c9a549d639
    Signed-off-by: Peng Du <pdu@nvidia.com>
    Reviewed-on: http://git-master/r/196602
    Reviewed-on: http://git-master/r/211298
    Reviewed-on: http://git-master/r/219556
    Reviewed-on: http://git-master/r/224272
    Reviewed-on: http://git-master/r/226411
    Reviewed-on: http://git-master/r/250135
    Reviewed-on: http://git-master/r/257174
    Reviewed-on: http://git-master/r/249921
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Alexander Van Brunt <avanbrunt@nvidia.com>

---
+++ drivers/platform/tegra/tegra11x_la.c
@@ -0,0 +1,2 @@
+/* Automatically generated file; DO NOT EDIT. */
+#include "../../../arch/arm/mach-tegra/tegra11x_la.c"
---
+++ drivers/platform/tegra/timer.c
@@ -0,0 +1,10 @@
+/* Automatically generated file; DO NOT EDIT. */
+#include "../../../arch/arm/mach-tegra/timer.c"
+
+#ifdef CONFIG_ARM64
+/* Arch timer is always initialized by core ARM64. */
+bool arch_timer_initialized = true;
+
+void __init tegra_cpu_timer_init(void) { do {} while (0); }
+int __init tegra_init_arch_timer(void) { return 0; }
+#endif
