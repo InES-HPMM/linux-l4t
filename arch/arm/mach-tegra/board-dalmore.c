0f6b70e5993fe4ed9b8f91415b81947db0805a57
Mursalin Akon <makon@nvidia.com>
2013-01-24 14:19:22 -0800

    mach-tegra: tegra[3,11]: bluesleep as built-in or module
    allow bluesleep as built-in or module
    
    Bug 1219372
    
    Same CL (minus rebase) was
    reviewed-on: http://git-master/r/193946
    
    Change-Id: I2815783c38291c5493e0448cd1a0f9e3bc0daf2d
    Signed-off-by: Mursalin Akon <makon@nvidia.com>
    Reviewed-on: http://git-master/r/195161
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Allen Martin <amartin@nvidia.com>

--- arch/arm/mach-tegra/board-dalmore.c
+++ arch/arm/mach-tegra/board-dalmore.c
@@ -84,7 +84,7 @@
 
 static struct board_info board_info, display_board_info;
 
-#ifdef CONFIG_BT_BLUESLEEP
+#if defined(CONFIG_BT_BLUESLEEP) || defined(CONFIG_BT_BLUESLEEP_MODULE)
 static struct rfkill_gpio_platform_data dalmore_bt_rfkill_pdata = {
 		.name           = "bt_rfkill",
 		.shutdown_gpio  = TEGRA_GPIO_PQ7,
@@ -873,7 +873,7 @@ static void __init tegra_dalmore_late_init(void)
 		dalmore_panel_init();
 	dalmore_kbc_init();
 	dalmore_pmon_init();
-#ifdef CONFIG_BT_BLUESLEEP
+#if defined(CONFIG_BT_BLUESLEEP) || defined(CONFIG_BT_BLUESLEEP_MODULE)
 	dalmore_setup_bluesleep();
 	dalmore_setup_bt_rfkill();
 #elif defined CONFIG_BLUEDROID_PM
