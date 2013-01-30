68bfc1090c4f33e3d403f955301f2f2e224a952b
Bo Yan <byan@nvidia.com>
2013-01-30 09:37:06 -0800

    ARM: tegra11x: Decrease min_residency_noncpu
    2ms seems a reasonable value for entring non-CPU power gating based
    on available data.
    
    bug 1177454
    
    Change-Id: I4a28118cadc7804fec7a2832bc7584a3380505be
    Signed-off-by: Bo Yan <byan@nvidia.com>
    Reviewed-on: http://git-master/r/194854
    (cherry picked from commit 4db919895e01f976fa444fa88e6464c8b4cbcae0)
    Reviewed-on: http://git-master/r/196113
    Reviewed-by: Mrutyunjay Sawant <msawant@nvidia.com>
    Tested-by: Mrutyunjay Sawant <msawant@nvidia.com>

--- arch/arm/mach-tegra/board-dalmore-power.c
+++ arch/arm/mach-tegra/board-dalmore-power.c
@@ -991,7 +991,7 @@ static struct tegra_suspend_platform_data dalmore_suspend_data = {
 	.corereq_high	= true,
 	.sysclkreq_high	= true,
 	.cpu_lp2_min_residency = 1000,
-	.min_residency_noncpu = 4000,
+	.min_residency_noncpu = 2000,
 	.min_residency_crail = 8000,
 };
 #ifdef CONFIG_ARCH_TEGRA_HAS_CL_DVFS
--- arch/arm/mach-tegra/board-pluto-power.c
+++ arch/arm/mach-tegra/board-pluto-power.c
@@ -685,7 +685,7 @@ static struct tegra_suspend_platform_data pluto_suspend_data = {
 	.corereq_high	= true,
 	.sysclkreq_high	= true,
 	.cpu_lp2_min_residency = 1000,
-	.min_residency_noncpu = 4000,
+	.min_residency_noncpu = 2000,
 	.min_residency_crail = 8000,
 };
 
