8dc380d47613991477c0f3515cbb68835c7e3be5
Chaitanya Bandi <bandik@nvidia.com>
2013-02-12 14:29:29 +0530

    backlight: lm3528: Add backlight driver for LM3528
    Bug 1216857
    
    Change-Id: I2ec1a393769804d73191d690d8340c30885e16ae
    Signed-off-by: Chaitanya Bandi <bandik@nvidia.com>
    Reviewed-on: http://git-master/r/199925
    (cherry picked from commit 82de53cdad7de223cfefc9b5e33b370bd5c18957)
    Reviewed-on: http://git-master/r/204954
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Thomas Cherry <tcherry@nvidia.com>

---
+++ include/linux/lm3528.h
@@ -0,0 +1,12 @@
+#ifndef __LINUX_LM3528_BACKLIGHT_H
+#define __LINUX_LM3528_BACKLIGHT_H
+
+#include <linux/backlight.h>
+
+struct lm3528_platform_data {
+	unsigned int dft_brightness;
+	int (*notify)(struct device *dev, int brightness);
+};
+
+#endif
+
