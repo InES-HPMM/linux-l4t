ca635880b37b5628c8abce5bdb96933957d1e89b
Seshendra Gadagottu <sgadagottu@nvidia.com>
2013-02-15 16:43:05 -0800

    ARM: tegra14x: Do not put any pads into DPD
    Change-Id: Ia472efe1915c0ab6d378fef83f390d40c8eaae34
    Signed-off-by: Seshendra Gadagottu <sgadagottu@nvidia.com>
    Reviewed-on: http://git-master/r/201369
    Reviewed-by: Automatic_Commit_Validation_User

--- arch/arm/mach-tegra/pm.c
+++ arch/arm/mach-tegra/pm.c
@@ -829,7 +829,8 @@ static void tegra_pm_set(enum tegra_suspend_mode mode)
 		/* Enable DPD sample to trigger sampling pads data and direction
 		 * in which pad will be driven during lp0 mode*/
 		writel(0x1, pmc + PMC_DPD_SAMPLE);
-#if !defined(CONFIG_ARCH_TEGRA_3x_SOC) && !defined(CONFIG_ARCH_TEGRA_2x_SOC)
+#if !defined(CONFIG_ARCH_TEGRA_3x_SOC) && !defined(CONFIG_ARCH_TEGRA_2x_SOC) &&\
+					 !defined(CONFIG_ARCH_TEGRA_14x_SOC)
 		writel(0x800fdfff, pmc + PMC_IO_DPD_REQ);
 		writel(0x80001fff, pmc + PMC_IO_DPD2_REQ);
 #endif
