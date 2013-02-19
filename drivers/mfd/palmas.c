2ecf754ef6266f8500272b145d8c7954727382dd
Laxman Dewangan <ldewangan@nvidia.com>
2013-02-19 13:06:46 +0530

    mfd: palma: enable cache of SMPS regulator register.
    Enable caching of SMPS regulator registers. This reduces
    the number of i2c transaction from 7 to 1 when changing
    voltage of rail.
    
    bug 1238267
    
    (Cherrypicked commit d6330823db19136d97fcb92fc8707fc5de7b78f5)
    
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Reviewed-on: http://git-master/r/201949
    
    Change-Id: Ida1d1c3a634c1e3c1bf8a8b008aea17aeaa5ced3
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Reviewed-on: http://git-master/r/216436
    Reviewed-by: Simone Willett <swillett@nvidia.com>
    Tested-by: Simone Willett <swillett@nvidia.com>

--- drivers/mfd/palmas.c
+++ drivers/mfd/palmas.c
@@ -111,12 +111,22 @@ static const struct mfd_cell palmas_children[] = {
 	}
 };
 
+static bool is_volatile_palma_func_reg(struct device *dev, unsigned int reg)
+{
+	if ((reg >= (PALMAS_SMPS12_CTRL + 0x20)) &&
+			(reg <= (PALMAS_SMPS9_VOLTAGE + 0x20)))
+		return false;
+	return true;
+}
+
 static const struct regmap_config palmas_regmap_config[PALMAS_NUM_CLIENTS] = {
 	{
 		.reg_bits = 8,
 		.val_bits = 8,
 		.max_register = PALMAS_BASE_TO_REG(PALMAS_PU_PD_OD_BASE,
 					PALMAS_PRIMARY_SECONDARY_PAD3),
+		.volatile_reg = is_volatile_palma_func_reg,
+		.cache_type  = REGCACHE_RBTREE,
 	},
 	{
 		.reg_bits = 8,
