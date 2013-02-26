276eaf50e5f3a768e14ad05ffab9ad7d9604b8eb
Dan Willemsen <dwillemsen@nvidia.com>
2013-02-25 17:22:23 -0800

    fixup board-curacao.h gpio.h removal
    Signed-off-by: Dan Willemsen <dwillemsen@nvidia.com>

--- arch/arm/mach-tegra/board-curacao.h
+++ arch/arm/mach-tegra/board-curacao.h
@@ -20,7 +20,6 @@
 #ifndef _MACH_TEGRA_BOARD_CURACAO_H
 #define _MACH_TEGRA_BOARD_CURACAO_H
 
-#include <mach/gpio.h>
 #include <mach/irqs.h>
 #include <linux/mfd/max77663-core.h>
 #include "gpio-names.h"
