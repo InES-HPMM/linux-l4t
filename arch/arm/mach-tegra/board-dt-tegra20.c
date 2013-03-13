f3d7dbabd9373c7991373e72901b8ce5c44e6be8
Dan Willemsen <dwillemsen@nvidia.com>
2013-03-13 15:07:06 -0700

    Revert "ARM: tegra: remove dead code"
    This reverts commit bab53ce38e8735519aa3cfdcc5bfa438c0891086.
    
    Signed-off-by: Dan Willemsen <dwillemsen@nvidia.com>

--- arch/arm/mach-tegra/board-dt-tegra20.c
+++ arch/arm/mach-tegra/board-dt-tegra20.c
@@ -27,11 +27,9 @@
 #include <linux/of_fdt.h>
 #include <linux/of_platform.h>
 #include <linux/pda_power.h>
-#include <linux/platform_data/tegra_usb.h>
 #include <linux/io.h>
 #include <linux/i2c.h>
 #include <linux/i2c-tegra.h>
-#include <linux/usb/tegra_usb_phy.h>
 #include <linux/clk/tegra.h>
 
 #include <asm/mach-types.h>
@@ -40,32 +38,9 @@
 #include <asm/setup.h>
 
 #include "board.h"
+#include "devices.h"
 #include "common.h"
 #include "iomap.h"
-
-struct tegra_ehci_platform_data tegra_ehci1_pdata = {
-	.operating_mode = TEGRA_USB_OTG,
-	.power_down_on_bus_suspend = 1,
-	.vbus_gpio = -1,
-};
-
-struct tegra_ulpi_config tegra_ehci2_ulpi_phy_config = {
-	.reset_gpio = -1,
-	.clk = "cdev2",
-};
-
-struct tegra_ehci_platform_data tegra_ehci2_pdata = {
-	.phy_config = &tegra_ehci2_ulpi_phy_config,
-	.operating_mode = TEGRA_USB_HOST,
-	.power_down_on_bus_suspend = 1,
-	.vbus_gpio = -1,
-};
-
-struct tegra_ehci_platform_data tegra_ehci3_pdata = {
-	.operating_mode = TEGRA_USB_HOST,
-	.power_down_on_bus_suspend = 1,
-	.vbus_gpio = -1,
-};
 
 struct of_dev_auxdata tegra20_auxdata_lookup[] __initdata = {
 	OF_DEV_AUXDATA("nvidia,tegra20-sdhci", TEGRA_SDMMC1_BASE, "sdhci-tegra.0", NULL),
