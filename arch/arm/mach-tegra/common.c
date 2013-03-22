505f00198503b5f2e467d298ef30a1e1c7fcb255
Seshendra Gadagottu <sgadagottu@nvidia.com>
2013-03-22 11:41:08 -0700

    ARM: tegra: Correct fuse bits check for RAM repair
    If any one of spare_10 or spare_11 bits are set then
    trigger RAM repair
    
    Bug 1211371
    
    Change-Id: I55f7b2a5d03f2d61cd7c840392241b1b92dafb37
    Signed-off-by: Seshendra Gadagottu <sgadagottu@nvidia.com>
    Reviewed-on: http://git-master/r/212183
    (cherry picked from commit 239de7427989eb987e77bd25a4303fd2c0a66914)
    Reviewed-on: http://git-master/r/214619
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Bo Yan <byan@nvidia.com>

--- arch/arm/mach-tegra/common.c
+++ arch/arm/mach-tegra/common.c
@@ -530,7 +530,7 @@ static void __init tegra_perf_init(void)
 #if defined(CONFIG_ARCH_TEGRA_11x_SOC) || defined(CONFIG_ARCH_TEGRA_14x_SOC)
 static void __init tegra_ramrepair_init(void)
 {
-	if (tegra_spare_fuse(10) & tegra_spare_fuse(11) & 1) {
+	if (tegra_spare_fuse(10)  | tegra_spare_fuse(11)) {
 		u32 reg;
 		reg = readl(FLOW_CTRL_RAM_REPAIR);
 		reg &= ~FLOW_CTRL_RAM_REPAIR_BYPASS_EN;
