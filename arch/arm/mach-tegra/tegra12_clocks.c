9ade9c947ca322069760d3580be1b97a3d5aff01
Kaz Fukuoka <kfukuoka@nvidia.com>
2013-03-26 18:43:22 -0700

    ARM: tegra12: Add dpaux
    bug 1160031
    
    Change-Id: I618fe8f76bd6849064c34579514d2a47787d21e2
    Signed-off-by: Kaz Fukuoka <kfukuoka@nvidia.com>
    Reviewed-on: http://git-master/r/216266
    Reviewed-by: Chao Xu <cxu@nvidia.com>

--- arch/arm/mach-tegra/tegra12_clocks.c
+++ arch/arm/mach-tegra/tegra12_clocks.c
@@ -6487,7 +6487,7 @@ struct clk tegra_list_clks[] = {
 	PERIPH_CLK("disp1",	"tegradc.0",		NULL,	27,	0x138,	600000000, mux_pllp_pllm_plld_plla_pllc_plld2_clkm,	MUX | MUX8),
 	PERIPH_CLK("disp2",	"tegradc.1",		NULL,	26,	0x13c,	600000000, mux_pllp_pllm_plld_plla_pllc_plld2_clkm,	MUX | MUX8),
 	PERIPH_CLK_EX("sor0",	"sor0",			NULL,	182,	0x414,	198000000, mux_pllp_pllm_plld_plla_pllc_plld2_clkm,	MUX | MUX8,	&tegra_sor_clk_ops),
-	/* PERIPH_CLK("dpaux",	"dpaux",		NULL,	181,	0x414,	198000000, mux_pllp_pllm_plld_plla_pllc_plld2_clkm,	MUX | MUX8), */
+	PERIPH_CLK("dpaux",	"dpaux",		NULL,	181,	0,	24000000, mux_clk_m,			0),
 	/* PERIPH_CLK("hdmi-audio",	"hdmi-audio",		NULL,	176,	0x414,	198000000, mux_pllp_pllm_plld_plla_pllc_plld2_clkm,	MUX | MUX8), */
 	PERIPH_CLK("usbd",	"tegra-udc.0",		NULL,	22,	0,	480000000, mux_clk_m,			0),
 	PERIPH_CLK("usb2",	"tegra-ehci.1",		NULL,	58,	0,	480000000, mux_clk_m,			0),
