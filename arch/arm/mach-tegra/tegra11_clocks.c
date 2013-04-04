66f81f6c6181bbddf4dc021112acb9b56fe87dde
Alex Frid <afrid@nvidia.com>
2013-04-03 19:48:21 -0700

    ARM: tegra11: dvfs: Update XUSB SS and HS dvfs table
    Update XUSB SS and HS clocks dvfs table and maximum rate limits.
    
    Bug 1250832
    
    Change-Id: Ie18fae844c87a529be7cd2dbd956ce30aa9ee07d
    Signed-off-by: Alex Frid <afrid@nvidia.com>
    Reviewed-on: http://git-master/r/216361
    (cherry picked from commit b8d7cee548e19c538429f4766c301a7d9dc1d1c9)
    Reviewed-on: http://git-master/r/221375
    Reviewed-by: Simone Willett <swillett@nvidia.com>
    Tested-by: Simone Willett <swillett@nvidia.com>

--- arch/arm/mach-tegra/tegra11_clocks.c
+++ arch/arm/mach-tegra/tegra11_clocks.c
@@ -6859,7 +6859,7 @@ static struct clk tegra_xusb_source_clks[] = {
 	PERIPH_CLK("xusb_host_src",	XUSB_ID, "host_src",	143,	0x600,	120000000, mux_clkm_pllp_pllc_pllre,	MUX | MUX8 | DIV_U71 | DIV_U71_INT | PERIPH_NO_RESET | PERIPH_ON_APB),
 	PERIPH_CLK("xusb_falcon_src",	XUSB_ID, "falcon_src",	143,	0x604,	350000000, mux_clkm_pllp_pllc_pllre,	MUX | MUX8 | DIV_U71 | DIV_U71_INT | PERIPH_NO_RESET),
 	PERIPH_CLK("xusb_fs_src",	XUSB_ID, "fs_src",	143,	0x608,	 48000000, mux_clkm_48M_pllp_480M,	MUX | DIV_U71 | DIV_U71_INT | PERIPH_NO_RESET),
-	PERIPH_CLK("xusb_ss_src",	XUSB_ID, "ss_src",	143,	0x610,	120000000, mux_clkm_pllre_clk32_480M_pllc_ref,	MUX | MUX8 | DIV_U71 | DIV_U71_INT | PERIPH_NO_RESET),
+	PERIPH_CLK("xusb_ss_src",	XUSB_ID, "ss_src",	143,	0x610,	122400000, mux_clkm_pllre_clk32_480M_pllc_ref,	MUX | MUX8 | DIV_U71 | DIV_U71_INT | PERIPH_NO_RESET),
 	PERIPH_CLK("xusb_dev_src",	XUSB_ID, "dev_src",	95,	0x60c,	120000000, mux_clkm_pllp_pllc_pllre,	MUX | MUX8 | DIV_U71 |  DIV_U71_INT | PERIPH_NO_RESET | PERIPH_ON_APB),
 	SHARED_EMC_CLK("xusb.emc",	XUSB_ID, "emc",	&tegra_clk_emc,	NULL,	0,	SHARED_BW, 0),
 };
@@ -6871,7 +6871,7 @@ static struct clk tegra_xusb_ss_div2 = {
 	.mul       = 1,
 	.div       = 2,
 	.state     = OFF,
-	.max_rate  = 60000000,
+	.max_rate  = 61200000,
 };
 
 static struct clk_mux_sel mux_ss_div2_pllu_60M[] = {
@@ -6890,7 +6890,7 @@ static struct clk tegra_xusb_hs_src = {
 	.reg       = 0x610,
 	.inputs    = mux_ss_div2_pllu_60M,
 	.flags     = PLLU | PERIPH_NO_ENB,
-	.max_rate  = 60000000,
+	.max_rate  = 61200000,
 	.u.periph = {
 		.src_mask  = 0x1 << 25,
 		.src_shift = 25,
