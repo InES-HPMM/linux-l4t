c468794fe0d3a48a37353bc7b84638a440726c51
Alex Frid <afrid@nvidia.com>
2013-04-06 21:03:58 -0700

    ARM: tegra14: clock: Add emc use case table
    Added initial table to specify iso bandwidth share dependency on emc
    use cases. Just two use cases: display only and display + camera are
    identified for now.
    
    Change-Id: Ie2ab75f9f773cc2229e12f25b77b5dbd3aba3690
    Signed-off-by: Alex Frid <afrid@nvidia.com>
    Reviewed-on: http://git-master/r/217122
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Krishna Reddy <vdumpa@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Jon Mayo <jmayo@nvidia.com>

--- arch/arm/mach-tegra/tegra14_emc.c
+++ arch/arm/mach-tegra/tegra14_emc.c
@@ -46,6 +46,10 @@ static bool emc_enable;
 module_param(emc_enable, bool, 0644);
 
 u8 tegra_emc_bw_efficiency = 100;
+static struct emc_iso_usage tegra14_emc_iso_usage[] = {
+	{ BIT(EMC_USER_DC),			80 },
+	{ BIT(EMC_USER_DC) | BIT(EMC_USER_VI),	45 },
+};
 
 #define PLL_C_DIRECT_FLOOR		333500000
 #define EMC_STATUS_UPDATE_TIMEOUT	100
@@ -1081,7 +1085,13 @@ static struct platform_driver tegra14_emc_driver = {
 
 int __init tegra14_emc_init(void)
 {
-	return platform_driver_register(&tegra14_emc_driver);
+	int ret = platform_driver_register(&tegra14_emc_driver);
+	if (!ret) {
+		/* FIXME: set monitor boot rate */
+		tegra_emc_iso_usage_table_init(tegra14_emc_iso_usage,
+			ARRAY_SIZE(tegra14_emc_iso_usage));
+	}
+	return ret;
 }
 
 void tegra_emc_timing_invalidate(void)
@@ -1366,6 +1376,9 @@ static int __init tegra_emc_debug_init(void)
 
 	if (!debugfs_create_file("dsr_override", S_IRUGO | S_IWUSR,
 				 emc_debugfs_root, NULL, &dsr_override_fops))
+		goto err_out;
+
+	if (tegra_emc_iso_usage_debugfs_init(emc_debugfs_root))
 		goto err_out;
 
 	return 0;
