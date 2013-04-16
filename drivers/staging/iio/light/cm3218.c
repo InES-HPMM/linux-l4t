17c33af76538f9e1e2bec359d04e94fb06eac2ba
Sri Krishna chowdary <schowdary@nvidia.com>
2013-04-16 12:31:52 +0530

    staging: iio: light: cm3218: support CM32181
    CM32181 is used on Ardbeg.
    Modify the cm3218 driver to be compatible with k3.8.
    Also, change default value for Command register on cm32181.
    
    Bug 1256462
    Bug 1327616
    
    Change-Id: I6a3d8875befbe0f18d79ed478fb7f96a20300c0b
    Signed-off-by: Sri Krishna chowdary <schowdary@nvidia.com>
    Reviewed-on: http://git-master/r/219714
    (cherry picked from commit 6fabee72fa38a5eb99ca90095fb4cdba7700ee1d)
    Signed-off-by: Ajay Nandakumar <anandakumarm@nvidia.com>
    Reviewed-on: http://git-master/r/264210
    Reviewed-by: Automatic_Commit_Validation_User

--- drivers/staging/iio/light/cm3218.c
+++ drivers/staging/iio/light/cm3218.c
@@ -1,16 +1,22 @@
-/* drivers/input/misc/cm3218.c - cm3218 Ambient Light Sensor driver
+/*
+ * A iio driver for the light sensors Cm3218 and Cm32181.
  *
- * Copyright (c) 2012-2013, NVIDIA CORPORATION. All Rights Reserved.
+ * IIO Light driver for monitoring ambient light intensity in lux and proximity
+ * ir.
  *
- * This software is licensed under the terms of the GNU General Public
- * License version 2, as published by the Free Software Foundation, and
- * may be copied, distributed, and modified under those terms.
+ * Copyright (c) 2012-2013, NVIDIA CORPORATION. All rights reserved.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
  *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <linux/module.h>
@@ -31,15 +37,26 @@
 #define CM3218_REG_ALS_DATA		0x04
 #define CM3218_REG_MAX			0x04
 
-
-#define CONFIGURE_ALS_MASK		0x01
+#define CONFIGURE_ALS_MASK		BIT(0)
 #define CONFIGURE_ALS_EN		0x00
 
 #define CONFIGURE_SHDN_MASK		CONFIGURE_ALS_MASK
 #define CONFIGURE_SHDN_EN		0x01
 
-#define LS_POLLING_DELAY	1000 /* mSec */
-#define I2C_MAX_TIMEOUT		msecs_to_jiffies(20) /* 20 mSec */
+#define I2C_MAX_TIMEOUT			msecs_to_jiffies(20) /* 20 mSec */
+
+#ifdef CONFIG_SENSORS_CM3218
+#define CONFIGURE_DEFAULT_VAL		0x0400
+#else
+#define CONFIGURE_DEFAULT_VAL		0x0000
+#endif
+
+#define CM3218_ALS_PEAK_VAL		0xFFFF
+#define CM3218_ALS_RESOLUTION		0
+#define CM3218_ALS_RESOLUTION_MICRO	10000 /* 10 mLux/lsb */
+#define CM3218_ALS_INTEGRATION_TIME	700 /* mSec */
+#define CM3218_VENDOR			"Capella"
+#define CM3218_NAME			"cm3218"
 
 enum {
 	CHIP_POWER_OFF,
@@ -50,7 +67,6 @@ enum {
 struct cm3218_chip {
 	struct i2c_client		*client;
 	struct i2c_device_id		*id;
-	struct mutex			lock;
 	struct regulator_bulk_data	*consumers;
 	struct notifier_block		regulator_nb;
 	wait_queue_head_t		i2c_wait_queue;
@@ -77,8 +93,8 @@ bool cm3218_volatile_reg(struct device *dev, unsigned int reg)
 }
 
 static const struct reg_default cm3218_reg_defaults = {
-	.reg = 0x00,
-	.def = 0x0400,
+	.reg = CM3218_REG_CONFIGURE,
+	.def = CONFIGURE_DEFAULT_VAL,
 };
 
 /* TODO * in linux-next we have to add
@@ -107,6 +123,7 @@ static void change_endianness_16(int *val)
 static int _cm3218_register_read(struct cm3218_chip *chip, int reg, int *val)
 {
 	int ret;
+	struct iio_dev *indio_dev = iio_priv_to_dev(chip);
 
 	if (!chip->regmap)
 		return -ENODEV;
@@ -114,26 +131,27 @@ static int _cm3218_register_read(struct cm3218_chip *chip, int reg, int *val)
 	ret = wait_event_timeout(chip->i2c_wait_queue,
 					chip->i2c_xfer_ready, I2C_MAX_TIMEOUT);
 	if (!ret) {
-		dev_err(&chip->client->dev, "idname:%s func:%s line:%d " \
-				"error_msg: device not ready for i2c xfer\n",
-				chip->id->name, __func__, __LINE__);
+		dev_err(&chip->client->dev,
+			"idname:%s func:%s line:%d device not ready for i2c xfer\n",
+			chip->id->name, __func__, __LINE__);
 		return -ETIMEDOUT;
 	}
 
-	mutex_lock(&chip->lock);
+	mutex_lock(&indio_dev->mlock);
 	ret = regmap_read(chip->regmap, reg, val);
 	if (ret)
-		dev_err(&chip->client->dev, "idname:%s func:%s line:%d " \
-				"error_msg:regmap_read fails\n",
-				chip->id->name, __func__, __LINE__);
+		dev_err(&chip->client->dev,
+			"idname:%s func:%s line:%d regmap_read fails\n",
+			chip->id->name, __func__, __LINE__);
 
 	change_endianness_16(val);
 /*
 	temp = i2c_smbus_read_word_data(chip->client, CM3218_REG_ALS_DATA);
-	dev_err(&chip->client->dev, "idname:%s func:%s line:%d " \
+	dev_err(&chip->client->dev, "idname:%s func:%s line:%d ""
 				"als_i2c_data = %d\n",
 				chip->id->name, __func__, __LINE__, temp);
-*/	mutex_unlock(&chip->lock);
+*/
+	mutex_unlock(&indio_dev->mlock);
 	return ret;
 }
 
@@ -141,6 +159,7 @@ static int _cm3218_register_write(struct cm3218_chip *chip, int reg, int mask,
 				int val)
 {
 	int ret;
+	struct iio_dev *indio_dev = iio_priv_to_dev(chip);
 
 	if (!chip->regmap)
 		return -ENODEV;
@@ -148,27 +167,57 @@ static int _cm3218_register_write(struct cm3218_chip *chip, int reg, int mask,
 	ret = wait_event_timeout(chip->i2c_wait_queue,
 					chip->i2c_xfer_ready, I2C_MAX_TIMEOUT);
 	if (!ret) {
-		dev_err(&chip->client->dev, "idname:%s func:%s line:%d " \
-				"error_msg: device not ready for i2c xfer\n",
-				chip->id->name, __func__, __LINE__);
+		dev_err(&chip->client->dev,
+			"idname:%s func:%s line:%d device not ready for i2c xfer\n",
+			chip->id->name, __func__, __LINE__);
 		return -ETIMEDOUT;
 	}
 
-	mutex_lock(&chip->lock);
+	mutex_lock(&indio_dev->mlock);
 	change_endianness_16(&val);
 	ret = regmap_update_bits(chip->regmap, reg, mask, val);
 	if (ret)
-		dev_err(&chip->client->dev, "idname:%s func:%s line:%d " \
-				"error_msg:regmap_write fails\n",
-				chip->id->name, __func__, __LINE__);
-	mutex_unlock(&chip->lock);
+		dev_err(&chip->client->dev,
+			"idname:%s func:%s line:%d regmap_write fails\n",
+			chip->id->name, __func__, __LINE__);
+	mutex_unlock(&indio_dev->mlock);
+
+	return ret;
+}
+
+/* sync the device's registers with cache after power up during resume */
+static int _cm3218_register_sync(struct cm3218_chip *chip)
+{
+	int ret;
+	struct iio_dev *indio_dev = iio_priv_to_dev(chip);
+
+	if (!chip->regmap)
+		return -ENODEV;
+
+	ret = wait_event_timeout(chip->i2c_wait_queue,
+					chip->i2c_xfer_ready, I2C_MAX_TIMEOUT);
+	if (!ret) {
+		dev_err(&chip->client->dev,
+			"idname:%s func:%s line:%d device not ready for i2c xfer\n",
+			chip->id->name, __func__, __LINE__);
+		return -ETIMEDOUT;
+	}
+
+	mutex_lock(&indio_dev->mlock);
+	regcache_mark_dirty(chip->regmap);
+	ret = regcache_sync(chip->regmap);
+	if (ret)
+		dev_err(&chip->client->dev,
+			"idname:%s func:%s line:%d regmap_write fails\n",
+			chip->id->name, __func__, __LINE__);
+	mutex_unlock(&indio_dev->mlock);
 
 	return ret;
 }
 
 /* device's registration with iio to facilitate user operations */
 static ssize_t cm3218_chan_regulator_enable(struct iio_dev *indio_dev,
-		uintptr_t private, struct iio_chan_spec const *chan,
+		uintptr_t priv, struct iio_chan_spec const *chan,
 		const char *buf, size_t len)
 {
 	u8 enable;
@@ -185,7 +234,7 @@ static ssize_t cm3218_chan_regulator_enable(struct iio_dev *indio_dev,
 		return -EINVAL;
 
 	if (enable == (chip->als_state != CHIP_POWER_OFF))
-		return 1;
+		goto success;
 
 	if (!chip->consumers)
 		goto success;
@@ -196,9 +245,9 @@ static ssize_t cm3218_chan_regulator_enable(struct iio_dev *indio_dev,
 		ret = regulator_bulk_disable(1, chip->consumers);
 
 	if (ret) {
-		dev_err(&chip->client->dev, "idname:%s func:%s line:%d " \
-				"error_msg:_cm3218_register_read fails\n",
-				chip->id->name, __func__, __LINE__);
+		dev_err(&chip->client->dev,
+		"idname:%s func:%s line:%d _cm3218_register_read fails\n",
+		chip->id->name, __func__, __LINE__);
 		goto fail;
 	}
 
@@ -208,8 +257,9 @@ fail:
 	return ret ? ret : 1;
 }
 
-static ssize_t cm3218_chan_enable(struct iio_dev *indio_dev, uintptr_t private,
-		struct iio_chan_spec const *chan, const char *buf, size_t len)
+static ssize_t cm3218_chan_enable(struct iio_dev *indio_dev,
+		uintptr_t priv, struct iio_chan_spec const *chan,
+		const char *buf, size_t len)
 {
 	u8 enable;
 	int ret = 0;
@@ -222,20 +272,24 @@ static ssize_t cm3218_chan_enable(struct iio_dev *indio_dev, uintptr_t private,
 		return -EINVAL;
 
 	if (chip->als_state == CHIP_POWER_OFF) {
-		dev_err(&chip->client->dev, "idname:%s func:%s line:%d " \
-				"error_msg:please enable regulator first\n",
-				chip->id->name, __func__, __LINE__);
+		dev_err(&chip->client->dev,
+			"idname:%s func:%s line:%d please enable regulator first\n",
+			chip->id->name, __func__, __LINE__);
 		return -EINVAL;
 	}
-
-	if (!((enable && (chip->als_state == CHIP_POWER_ON_ALS_OFF)) ||
-		(!enable && (chip->als_state == CHIP_POWER_ON_ALS_ON))))
-		return -EINVAL;
 
 	if (chan->type != IIO_LIGHT)
 		return -EINVAL;
 
-	/* a small optimization*/
+	/* a small optimization
+	 * chip->als_state = either CHIP_POWER_ON_ALS_OFF (val = 1)
+	 * or CHIP_POWER_ON_ALS_ON (val = 2)
+	 * if enable = 0 => request for
+	 * CHIP_POWER_ON_ALS_ON -> CHIP_POWER_ON_ALS_OFF has come
+	 * if enable = 1 => request for
+	 * CHIP_POWER_ON_ALS_OFF -> CHIP_POWER_ON_ALS_ON has come
+	 * if there is no state change, goto success
+	 */
 	if (enable == (chip->als_state - 1))
 		goto success;
 
@@ -259,7 +313,6 @@ success:
 	return ret ? ret : 1;
 }
 
-
 /* chan_regulator_enable is used to enable regulators used by
  * particular channel.
  * chan_enable actually configures various registers to activate
@@ -281,6 +334,10 @@ static const struct iio_chan_spec_ext_info cm3218_ext_info[] = {
 static const struct iio_chan_spec cm3218_channels[] = {
 	{
 		.type = IIO_LIGHT,
+		.info_mask = IIO_CHAN_INFO_RAW_SEPARATE_BIT |
+				IIO_CHAN_INFO_SCALE_SEPARATE_BIT |
+				IIO_CHAN_INFO_PEAK_SEPARATE_BIT |
+				IIO_CHAN_INFO_SAMP_FREQ_SEPARATE_BIT,
 		.ext_info = cm3218_ext_info,
 	},
 };
@@ -289,44 +346,65 @@ static int cm3218_read_raw(struct iio_dev *indio_dev,
 	struct iio_chan_spec const *chan, int *val, int *val2, long mask)
 {
 	struct cm3218_chip *chip = iio_priv(indio_dev);
-	int ret;
+	int ret = -EINVAL;
 	int value;
-
-	if (chip->als_state != CHIP_POWER_ON_ALS_ON)
-		return -EINVAL;
 
 	if (chan->type != IIO_LIGHT)
 		return -EINVAL;
 
-	ret = _cm3218_register_read(chip, CM3218_REG_ALS_DATA, &value);
-	if (ret)
-		dev_err(&chip->client->dev, "idname:%s func:%s line:%d " \
-			"error_msg:_cm3218_register_read fails\n",
-			chip->id->name, __func__, __LINE__);
-
-	if (!ret) {
-		*val = value;
-		ret = IIO_VAL_INT;
+	switch (mask) {
+	case IIO_CHAN_INFO_SCALE:
+		*val = CM3218_ALS_RESOLUTION;
+		*val2 = CM3218_ALS_RESOLUTION_MICRO; /* 10 mlux/lsb */
+		return IIO_VAL_INT_PLUS_MICRO;
+	case IIO_CHAN_INFO_PEAK:
+		*val = CM3218_ALS_PEAK_VAL;
+		return IIO_VAL_INT;
+	case IIO_CHAN_INFO_SAMP_FREQ:
+		*val = CM3218_ALS_INTEGRATION_TIME;
+		return IIO_VAL_INT;
+	case IIO_CHAN_INFO_RAW:
+		if (chip->als_state != CHIP_POWER_ON_ALS_ON)
+			return -EINVAL;
+		ret = _cm3218_register_read(chip, CM3218_REG_ALS_DATA, &value);
+		if (ret)
+			dev_err(&chip->client->dev,
+				"idname:%s func:%s line:%d _cm3218_register_read fails\n",
+				chip->id->name, __func__, __LINE__);
+		if (!ret) {
+			*val = value;
+			ret = IIO_VAL_INT;
+		}
 	}
 	return ret;
 }
+
+IIO_CONST_ATTR(vendor, CM3218_VENDOR);
+
+static struct attribute *cm3218_attributes[] = {
+	&iio_const_attr_vendor.dev_attr.attr,
+	NULL,
+};
+
+static const struct attribute_group cm3218_attr_group = {
+	.attrs = cm3218_attributes,
+};
 
 /* read_raw is used to report a channel's data to user
  * in non SI units
  */
 static const struct iio_info cm3218_iio_info = {
+	.attrs = &cm3218_attr_group,
 	.driver_module = THIS_MODULE,
-	.read_raw = &cm3218_read_raw,
+	.read_raw = cm3218_read_raw,
 };
 
 /* chip's power management helpers */
 static int cm3218_activate_standby_mode(struct cm3218_chip *chip)
 {
-	int ret;
-	ret = _cm3218_register_write(chip, CM3218_REG_CONFIGURE,
+	return _cm3218_register_write(chip, CM3218_REG_CONFIGURE,
 					CONFIGURE_SHDN_MASK,
 					CONFIGURE_SHDN_EN);
-	return 0;
 }
 
 /* this detects the regulator enable/disable event and puts
@@ -360,25 +438,22 @@ static int cm3218_suspend(struct device *dev)
 	if (!chip->consumers)
 		return 0;
 
-
 	/* assumes all other devices stop using this regulator */
 	if (chip->als_state != CHIP_POWER_OFF)
 		ret = regulator_bulk_disable(1, chip->consumers);
 
 	if (ret) {
-		dev_err(&chip->client->dev, "idname:%s func:%s line:%d " \
-				"error_msg:regulator_bulk_disable fails" \
-				"\ncm3218_suspend fails\n",
-				chip->id->name, __func__, __LINE__);
+		dev_err(&chip->client->dev,
+			"idname:%s func:%s line:%d regulator_bulk_disable fails",
+			chip->id->name, __func__, __LINE__);
 		return ret;
 	}
 
-	if (!ret && regulator_is_enabled(chip->consumers[0].consumer))
+	if (regulator_is_enabled(chip->consumers[0].consumer))
 		ret = cm3218_activate_standby_mode(chip);
 	if (ret)
-		dev_err(&chip->client->dev, "idname:%s func:%s line:%d " \
-				"error_msg:cm3218_activate_standby_mode fails" \
-				"\ncm3218_suspend fails\n",
+		dev_err(&chip->client->dev,
+			"idname: %s func:%s line:%d activate_standby fails\n",
 				chip->id->name, __func__, __LINE__);
 	return ret;
 }
@@ -389,36 +464,26 @@ static int cm3218_resume(struct device *dev)
 	struct iio_dev *indio_dev = i2c_get_clientdata(client);
 	struct cm3218_chip *chip = iio_priv(indio_dev);
 	int ret = 0;
-	int temp;
 
 	if (chip->als_state == CHIP_POWER_OFF)
 		return 0;
 
 	ret = regulator_bulk_enable(1, cm3218_consumers);
 	if (ret) {
-		dev_err(&chip->client->dev, "idname:%s func:%s line:%d " \
-				"error_msg:regulator_bulk_enable fails" \
-				"\ncm3218_suspend fails\n",
-				chip->id->name, __func__, __LINE__);
+		dev_err(&chip->client->dev,
+			"idname:%s func:%s line:%d regulator_bulk_enable fails\n",
+			chip->id->name, __func__, __LINE__);
 		return ret;
 	}
 
-	mutex_lock(&chip->lock);
+	mutex_lock(&indio_dev->mlock);
 	if (chip->als_state == CHIP_POWER_ON_ALS_ON)
-		ret = _cm3218_register_write(chip, CM3218_REG_CONFIGURE,
-						CONFIGURE_ALS_MASK,
-						CONFIGURE_ALS_EN);
-	if (ret) {
-		dev_err(&chip->client->dev, "idname:%s func:%s line:%d " \
-				"error_msg:_cm3218_register_write fails" \
-				"\ncm3218_suspend fails\n",
-				chip->id->name, __func__, __LINE__);
-		/* fall back to CHIP_POWER_OFF, we don't need partial resume */
-		do {
-			temp = regulator_bulk_disable(1, cm3218_consumers);
-		} while (temp);
-	}
-	mutex_unlock(&chip->lock);
+		ret = _cm3218_register_sync(chip);
+	if (ret)
+		dev_err(&chip->client->dev,
+			"idname:%s func:%s line:%d _cm3218_register_sync fails\n",
+			chip->id->name, __func__, __LINE__);
+	mutex_unlock(&indio_dev->mlock);
 	return ret;
 }
 
@@ -427,7 +492,6 @@ static SIMPLE_DEV_PM_OPS(cm3218_pm_ops, cm3218_suspend, cm3218_resume);
 #else
 #define CM3218_PM_OPS NULL
 #endif
-
 
 /* device's i2c registration */
 static int cm3218_probe(struct i2c_client *client,
@@ -440,8 +504,8 @@ static int cm3218_probe(struct i2c_client *client,
 
 	indio_dev = iio_device_alloc(sizeof(*chip));
 	if (indio_dev == NULL) {
-		dev_err(&client->dev, "idname:%s func:%s line:%d " \
-			"error_msg:iio_device_alloc fails\n",
+		dev_err(&client->dev,
+			"idname:%s func:%s line:%d iio_allocate_device fails\n",
 			id->name, __func__, __LINE__);
 		return -ENOMEM;
 	}
@@ -449,36 +513,6 @@ static int cm3218_probe(struct i2c_client *client,
 
 	i2c_set_clientdata(client, indio_dev);
 	chip->client = client;
-	mutex_init(&chip->lock);
-
-	regmap = devm_regmap_init_i2c(client, &cm3218_regmap_config);
-	if (IS_ERR_OR_NULL(regmap)) {
-		dev_err(&client->dev, "idname:%s func:%s line:%d " \
-		"error_msg:devm_regmap_init_i2c fails\n",
-		id->name, __func__, __LINE__);
-		return -ENOMEM;
-	}
-	chip->regmap = regmap;
-
-	ret = devm_regulator_bulk_get(&client->dev,
-					ARRAY_SIZE(cm3218_consumers),
-					cm3218_consumers);
-	if (ret)
-		dev_err(&client->dev, "idname:%s func:%s line:%d " \
-			"error_msg:regulator_get fails\n",
-			id->name, __func__, __LINE__);
-	else
-		chip->consumers = cm3218_consumers;
-
-	if (chip->consumers) {
-		chip->regulator_nb.notifier_call = cm3218_power_manager;
-		ret = regulator_register_notifier(chip->consumers[0].consumer,
-						&chip->regulator_nb);
-		if (ret)
-			dev_err(&client->dev, "idname:%s func:%s line:%d " \
-				"error_msg:regulator_register_notifier fails\n",
-				id->name, __func__, __LINE__);
-	}
 
 	indio_dev->info = &cm3218_iio_info;
 	indio_dev->channels = cm3218_channels;
@@ -488,27 +522,62 @@ static int cm3218_probe(struct i2c_client *client,
 	indio_dev->modes = INDIO_DIRECT_MODE;
 	ret = iio_device_register(indio_dev);
 	if (ret) {
-		dev_err(&client->dev, "idname:%s func:%s line:%d " \
-			"error_msg:iio_device_register fails\n",
+		dev_err(&client->dev,
+			"idname:%s func:%s line:%d iio_device_register fails\n",
 			id->name, __func__, __LINE__);
-		goto err_iio_free;
+		goto fail;
+	}
+
+	regmap = devm_regmap_init_i2c(client, &cm3218_regmap_config);
+	if (IS_ERR_OR_NULL(regmap)) {
+		dev_err(&client->dev,
+			"idname:%s func:%s line:%d devm_regmap_init_i2c fails\n",
+			id->name, __func__, __LINE__);
+		ret = -ENOMEM;
+		goto fail;
+	}
+	chip->regmap = regmap;
+
+	ret = devm_regulator_bulk_get(&client->dev,
+					ARRAY_SIZE(cm3218_consumers),
+					cm3218_consumers);
+	if (ret)
+		dev_err(&client->dev,
+			"idname:%s func:%s line:%d regulator_get fails\n",
+			id->name, __func__, __LINE__);
+	else
+		chip->consumers = cm3218_consumers;
+
+	if (chip->consumers) {
+		chip->regulator_nb.notifier_call = cm3218_power_manager;
+		ret = regulator_register_notifier(chip->consumers[0].consumer,
+						&chip->regulator_nb);
+		if (ret) {
+			dev_err(&client->dev,
+				"idname:%s func:%s line:%d regulator_register_notifier fails\n",
+				id->name, __func__, __LINE__);
+			goto fail;
+		}
 	}
 
 	init_waitqueue_head(&chip->i2c_wait_queue);
 	chip->als_state = 0;
 	if (regulator_is_enabled(chip->consumers[0].consumer)) {
 		chip->i2c_xfer_ready = 1;
-		cm3218_activate_standby_mode(chip);
+		ret = cm3218_activate_standby_mode(chip);
+		if (ret) {
+			dev_err(&client->dev,
+				"idname:%s func:%s line:%d regulator_register_notifier fails\n",
+				id->name, __func__, __LINE__);
+			goto fail;
+		}
 	}
 
-	dev_info(&client->dev, "idname:%s func:%s line:%d " \
-			"probe success\n",
+	dev_info(&client->dev, "idname:%s func:%s line:%d probe success\n",
 			id->name, __func__, __LINE__);
-
 	return 0;
 
-err_iio_free:
-	mutex_destroy(&chip->lock);
+fail:
 	iio_device_free(indio_dev);
 	return ret;
 }
@@ -517,17 +586,15 @@ static void cm3218_shutdown(struct i2c_client *client)
 {
 	struct iio_dev *indio_dev = i2c_get_clientdata(client);
 	struct cm3218_chip *chip = iio_priv(indio_dev);
-	mutex_lock(&chip->lock);
+	mutex_lock(&indio_dev->mlock);
 	chip->shutdown_complete = 1;
-	mutex_unlock(&chip->lock);
+	mutex_unlock(&indio_dev->mlock);
 }
 
 static int cm3218_remove(struct i2c_client *client)
 {
 	struct iio_dev *indio_dev = i2c_get_clientdata(client);
-	struct cm3218_chip *chip = iio_priv(indio_dev);
 
-	mutex_destroy(&chip->lock);
 	iio_device_unregister(indio_dev);
 	iio_device_free(indio_dev);
 	return 0;
@@ -535,12 +602,14 @@ static int cm3218_remove(struct i2c_client *client)
 
 static const struct i2c_device_id cm3218_id[] = {
 	{"cm3218", 0},
-	{}
+	{"cm32181", 1},
+	{ }
 };
 MODULE_DEVICE_TABLE(i2c, cm3218_id);
 
 static const struct of_device_id cm3218_of_match[] = {
 	{ .compatible = "capella,cm3218", },
+	{ .compatible = "capella,cm32181", },
 	{ },
 };
 MODULE_DEVICE_TABLE(of, cm3218_of_match);
@@ -548,7 +617,7 @@ MODULE_DEVICE_TABLE(of, cm3218_of_match);
 static struct i2c_driver cm3218_driver = {
 	.class = I2C_CLASS_HWMON,
 	.driver = {
-		.name = "cm3218",
+		.name = CM3218_NAME,
 		.owner = THIS_MODULE,
 		.of_match_table = cm3218_of_match,
 		.pm = CM3218_PM_OPS,
