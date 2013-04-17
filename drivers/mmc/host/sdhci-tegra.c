a041f8fbd9c22be37460f83113f75e535116fb68
rrajk <rrajk@nvidia.com>
2013-04-17 16:01:30 +0530

    mmc: tegra: no need of choosing either pll_c/pll_p
    It is not required to choose either pll_c or pll_p as clk
    source on FPGA platform.
    
    Change-Id: I2ee159c1f113266788e6d2330b868faa20cab341
    Signed-off-by: rrajk <rrajk@nvidia.com>
    Reviewed-on: http://git-master/r/220145
    Reviewed-by: Pavan Kunapuli <pkunapuli@nvidia.com>
    Reviewed-by: Venu Byravarasu <vbyravarasu@nvidia.com>

--- drivers/mmc/host/sdhci-tegra.c
+++ drivers/mmc/host/sdhci-tegra.c
@@ -970,6 +970,9 @@ static void tegra_sdhci_clock_set_parent(struct sdhci_host *host,
 	unsigned long pll_p_freq;
 	int rc;
 
+#ifdef CONFIG_TEGRA_FPGA_PLATFORM
+	return;
+#endif
 	pll_c_freq = (pll_c_rate >= desired_rate) ?
 		get_nearest_clock_freq(pll_c_rate, desired_rate) : pll_c_rate;
 	pll_p_freq = (pll_p_rate >= desired_rate) ?
