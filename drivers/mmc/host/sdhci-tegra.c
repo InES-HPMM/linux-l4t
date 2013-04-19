c9ebcd12fd55cc996f5e23a35e8484bc631b05a0
Naveen Kumar Arepalli <naveenk@nvidia.com>
2013-04-19 14:14:25 +0530

    mmc: host: Enable packed command for eMMC4.5
    Enable  packed command support for eMMC4.5 devices
    
    Bug 1242730
    
    Reviewed-on: http://git-master/r/203327
    (cherry picked from commit 054cea30d66b938258d3778cb9cf18026c77d714)
    Change-Id: I6c68c5477cd20b49230ac04f17351de8ff4a3b1d
    Signed-off-by: Naveen Kumar Arepalli <naveenk@nvidia.com>
    Reviewed-on: http://git-master/r/220963
    Reviewed-by: Riham Haidar <rhaidar@nvidia.com>
    Tested-by: Riham Haidar <rhaidar@nvidia.com>

--- drivers/mmc/host/sdhci-tegra.c
+++ drivers/mmc/host/sdhci-tegra.c
@@ -2032,6 +2032,7 @@ static int sdhci_tegra_probe(struct platform_device *pdev)
 	tegra_sdhost_std_freq = TEGRA3_SDHOST_STD_FREQ;
 	host->mmc->caps2 |= MMC_CAP2_CACHE_CTRL;
 	host->mmc->caps |= MMC_CAP_CMD23;
+	host->mmc->caps2 |= MMC_CAP2_PACKED_CMD;
 #endif
 
 	if (plat->nominal_vcore_uV)
