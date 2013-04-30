12327a62449326bfc46729f223ea6cb9c6724200
Krishna Reddy <vdumpa@nvidia.com>
2013-04-29 17:57:42 -0700

    arm: tegra: t14x: update iso efficiency for display
    update iso efficiency for display when display alone
    is enabled.
    Bug 1275589
    
    Change-Id: Ieecea6e8291a17c8b1536a46d05b8e2a96f2090b
    Signed-off-by: Krishna Reddy <vdumpa@nvidia.com>
    Reviewed-on: http://git-master/r/224065
    Reviewed-by: Riham Haidar <rhaidar@nvidia.com>
    Tested-by: Riham Haidar <rhaidar@nvidia.com>

--- arch/arm/mach-tegra/tegra14_emc.c
+++ arch/arm/mach-tegra/tegra14_emc.c
@@ -48,7 +48,7 @@ module_param(emc_enable, bool, 0644);
 
 u8 tegra_emc_bw_efficiency = 100;
 static struct emc_iso_usage tegra14_emc_iso_usage[] = {
-	{ BIT(EMC_USER_DC),			50 },
+	{ BIT(EMC_USER_DC),			80 },
 	{ BIT(EMC_USER_DC) | BIT(EMC_USER_VI),	50 },
 	{ BIT(EMC_USER_DC) | BIT(EMC_USER_BB),	50 },
 };
