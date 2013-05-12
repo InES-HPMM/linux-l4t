d439f0415444df2f262155f06a288cfaf3860fc4
Alex Frid <afrid@nvidia.com>
2013-05-12 00:06:36 -0700

    ARM: tegra11: power: Specify PMU undershoot gaurd-band
    Specified PMU undershoot gaurd-band 100mV for tegratab, macallan, and
    pluto boards.
    
    Bug 1285525
    
    Change-Id: I950630c1d3208657fde0a8dda7a538c60cfc706d
    Signed-off-by: Alex Frid <afrid@nvidia.com>
    Reviewed-on: http://git-master/r/227756
    (cherry picked from commit 31a740326f55b922edc0b3c89839dc2332b4ed35)
    Reviewed-on: http://git-master/r/275555
    (cherry picked from commit 5b96474ba092c4e32ddece3574d4c63b0ae5400a)
    Reviewed-on: http://git-master/r/277520
    Reviewed-by: Yu-Huan Hsu <yhsu@nvidia.com>

--- arch/arm/mach-tegra/board-pluto-power.c
+++ arch/arm/mach-tegra/board-pluto-power.c
@@ -591,6 +591,7 @@ static struct tegra_cl_dvfs_platform_data pluto_cl_dvfs_data = {
 	},
 	.vdd_map = pmu_cpu_vdd_map,
 	.vdd_map_size = PMU_CPU_VDD_MAP_SIZE,
+	.pmu_undershoot_gb = 100,
 
 	.cfg_param = &pluto_cl_dvfs_param,
 };
