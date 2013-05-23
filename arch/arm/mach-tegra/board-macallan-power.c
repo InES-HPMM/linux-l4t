864825fe32896cac7bf28be6d0a9440f2cad5b2a
Diwakar Tundlam <dtundlam@nvidia.com>
2013-05-23 14:55:09 -0700

    arm: tegra: macallan: update board file
    Update initial soctherm config for Macallan.
    
    Bug 1216782
    
    Change-Id: Id53d353ae0913ee6c361c538db6362f442ef9099
    Signed-off-by: Diwakar Tundlam <dtundlam@nvidia.com>
    Reviewed-on: http://git-master/r/232317
    Reviewed-by: Mrutyunjay Sawant <msawant@nvidia.com>
    Tested-by: Mrutyunjay Sawant <msawant@nvidia.com>

--- arch/arm/mach-tegra/board-macallan-power.c
+++ arch/arm/mach-tegra/board-macallan-power.c
@@ -809,7 +809,33 @@ static struct soctherm_platform_data macallan_soctherm_data = {
 		},
 		[THERM_GPU] = {
 			.zone_enable = true,
+			.passive_delay = 1000,
 			.hotspot_offset = 6000,
+			.num_trips = 3,
+			.trips = {
+				{
+					.cdev_type = "tegra-balanced",
+					.trip_temp = 90000,
+					.trip_type = THERMAL_TRIP_PASSIVE,
+					.upper = THERMAL_NO_LIMIT,
+					.lower = THERMAL_NO_LIMIT,
+				},
+				{
+					.cdev_type = "tegra-heavy",
+					.trip_temp = 100000,
+					.trip_type = THERMAL_TRIP_HOT,
+					.upper = THERMAL_NO_LIMIT,
+					.lower = THERMAL_NO_LIMIT,
+				},
+				{
+					.cdev_type = "tegra-shutdown",
+					.trip_temp = 102000,
+					.trip_type = THERMAL_TRIP_CRITICAL,
+					.upper = THERMAL_NO_LIMIT,
+					.lower = THERMAL_NO_LIMIT,
+				},
+			},
+			.tzp = &macallan_soctherm_therm_cpu_tzp,
 		},
 		[THERM_PLL] = {
 			.zone_enable = true,
