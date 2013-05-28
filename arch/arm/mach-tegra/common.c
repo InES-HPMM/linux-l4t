3391b851d67890972ffda2dfec79d15406111f88
Ashwini Ghuge <aghuge@nvidia.com>
2013-05-28 15:18:00 +0530

    ARM: tegra: ahb: enable WR_WAIT_COMMIT_ON_1K bit
    Enable WR_WAIT_COMMIT_ON_1K bit in AHB_GIZMO_AHB_MEM
    for T114 A02 chip on Dalmore
    
    Bug 1286714
    
    Change-Id: I264818f7183e5dce7d6d2c3e6a4ccdc31281582b
    Signed-off-by: Ashwini Ghuge <aghuge@nvidia.com>
    Reviewed-on: http://git-master/r/233258
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Venkat Moganty <vmoganty@nvidia.com>

--- arch/arm/mach-tegra/common.c
+++ arch/arm/mach-tegra/common.c
@@ -598,6 +598,10 @@ static void __init tegra_init_ahb_gizmo_settings(void)
 
 	val = gizmo_readl(AHB_GIZMO_AHB_MEM);
 	val |= ENB_FAST_REARBITRATE | IMMEDIATE | DONT_SPLIT_AHB_WR;
+
+	if (tegra_get_chipid() == TEGRA_CHIPID_TEGRA11 &&
+		tegra_revision == TEGRA_REVISION_A02)
+		val |= WR_WAIT_COMMIT_ON_1K;
 #ifdef CONFIG_ARCH_TEGRA_14x_SOC
 	val |= WR_WAIT_COMMIT_ON_1K | EN_USB_WAIT_COMMIT_ON_1K_STALL;
 #endif
