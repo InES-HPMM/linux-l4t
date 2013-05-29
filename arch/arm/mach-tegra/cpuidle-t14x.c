ad0a00a6f356b53273ccb5e51aeb1bd01409d65e
Scott Long <scottl@nvidia.com>
2013-05-29 16:32:11 -0700

    ARM: tegra14x: add TLK LP2 SMC enter call to tegra_cpu_core_power_down
    We were missing a TLK SMC hook in the LP2 power-down-in-idle path on
    T148 that could lead to random data aborts (w/o this call we don't get
    a chance to flush caches before the reset occurs).
    
    Note this same hook already exists for T114.
    
    Tested with tot TLK by running testapp in a while loop for 1+ hours.
    
    Bug 1222572
    
    Change-Id: Ifc8a34ad0468c594f22983dac4014a05cac0ac26
    Signed-off-by: Scott Long <scottl@nvidia.com>
    Reviewed-on: http://git-master/r/233914
    Reviewed-by: Mrutyunjay Sawant <msawant@nvidia.com>
    Tested-by: Mrutyunjay Sawant <msawant@nvidia.com>

--- arch/arm/mach-tegra/cpuidle-t14x.c
+++ arch/arm/mach-tegra/cpuidle-t14x.c
@@ -457,6 +457,13 @@ static bool tegra_cpu_core_power_down(struct cpuidle_device *dev,
 	tegra_cpu_wake_by_time[dev->cpu] = ktime_to_us(entry_time) + request;
 	smp_wmb();
 
+#ifdef CONFIG_TRUSTED_LITTLE_KERNEL
+	if (dev->cpu == 0) {
+		tegra_generic_smc(0xFFFFFFFC, 0xFFFFFFE4,
+				(TEGRA_RESET_HANDLER_BASE +
+				tegra_cpu_reset_handler_offset));
+	}
+#endif
 	cpu_suspend(0, tegra3_sleep_cpu_secondary_finish);
 
 	tegra_cpu_wake_by_time[dev->cpu] = LLONG_MAX;
