702feba9c22ecb6aa72b25388bd207084073adc2
Dan Willemsen <dwillemsen@nvidia.com>
2013-06-09 22:48:16 -0700

    Revert "ARM: tegra30: cpuidle: add powered-down state for CPU0"
    This reverts commit d552920a02759cdc45d8507868de10ac2f5b9a18.
    
    Signed-off-by: Dan Willemsen <dwillemsen@nvidia.com>

--- arch/arm/mach-tegra/pm.c
+++ arch/arm/mach-tegra/pm.c
@@ -20,33 +20,14 @@
 #include <linux/spinlock.h>
 #include <linux/io.h>
 #include <linux/cpumask.h>
-#include <linux/delay.h>
-#include <linux/cpu_pm.h>
-#include <linux/err.h>
-#include <linux/clk/tegra.h>
-
-#include <asm/smp_plat.h>
-#include <asm/cacheflush.h>
-#include <asm/suspend.h>
-#include <asm/idmap.h>
-#include <asm/proc-fns.h>
-#include <asm/tlbflush.h>
 
 #include "iomap.h"
-#include "reset.h"
-#include "flowctrl.h"
 #include "pmc.h"
-#include "sleep.h"
-
-#define TEGRA_POWER_CPU_PWRREQ_OE	(1 << 16)  /* CPU pwr req enable */
-
-#define PMC_CTRL		0x0
+#include "reset.h"
 
 #ifdef CONFIG_PM_SLEEP
 static unsigned int g_diag_reg;
 static DEFINE_SPINLOCK(tegra_lp2_lock);
-static void __iomem *pmc = IO_ADDRESS(TEGRA_PMC_BASE);
-void (*tegra_tear_down_cpu)(void);
 
 void save_cpu_arch_register(void)
 {
@@ -60,57 +41,6 @@ void restore_cpu_arch_register(void)
 	/* write diagnostic register */
 	asm("mcr p15, 0, %0, c15, c0, 1" : : "r"(g_diag_reg) : "cc");
 	return;
-}
-
-/*
- * restore_cpu_complex
- *
- * restores cpu clock setting, clears flow controller
- *
- * Always called on CPU 0.
- */
-static void restore_cpu_complex(void)
-{
-	int cpu = smp_processor_id();
-
-	BUG_ON(cpu != 0);
-
-#ifdef CONFIG_SMP
-	cpu = cpu_logical_map(cpu);
-#endif
-
-	/* Restore the CPU clock settings */
-	tegra_cpu_clock_resume();
-
-	flowctrl_cpu_suspend_exit(cpu);
-
-	restore_cpu_arch_register();
-}
-
-/*
- * suspend_cpu_complex
- *
- * saves pll state for use by restart_plls, prepares flow controller for
- * transition to suspend state
- *
- * Must always be called on cpu 0.
- */
-static void suspend_cpu_complex(void)
-{
-	int cpu = smp_processor_id();
-
-	BUG_ON(cpu != 0);
-
-#ifdef CONFIG_SMP
-	cpu = cpu_logical_map(cpu);
-#endif
-
-	/* Save the CPU clock settings */
-	tegra_cpu_clock_suspend();
-
-	flowctrl_cpu_suspend_enter(cpu);
-
-	save_cpu_arch_register();
 }
 
 void __cpuinit tegra_clear_cpu_in_lp2(int phy_cpu_id)
@@ -141,43 +71,5 @@ bool __cpuinit tegra_set_cpu_in_lp2(int phy_cpu_id)
 
 	spin_unlock(&tegra_lp2_lock);
 	return last_cpu;
-}
-
-static int tegra_sleep_cpu(unsigned long v2p)
-{
-	/* Switch to the identity mapping. */
-	cpu_switch_mm(idmap_pgd, &init_mm);
-
-	/* Flush the TLB. */
-	local_flush_tlb_all();
-
-	tegra_sleep_cpu_finish(v2p);
-
-	/* should never here */
-	BUG();
-
-	return 0;
-}
-
-void tegra_idle_lp2_last(u32 cpu_on_time, u32 cpu_off_time)
-{
-	u32 mode;
-
-	/* Only the last cpu down does the final suspend steps */
-	mode = readl(pmc + PMC_CTRL);
-	mode |= TEGRA_POWER_CPU_PWRREQ_OE;
-	writel(mode, pmc + PMC_CTRL);
-
-	set_power_timers(cpu_on_time, cpu_off_time);
-
-	cpu_cluster_pm_enter();
-	suspend_cpu_complex();
-	outer_disable();
-
-	cpu_suspend(PHYS_OFFSET - PAGE_OFFSET, &tegra_sleep_cpu);
-
-	outer_resume();
-	restore_cpu_complex();
-	cpu_cluster_pm_exit();
 }
 #endif
--- arch/arm/mach-tegra/sleep.h
+++ arch/arm/mach-tegra/sleep.h
@@ -73,7 +73,6 @@
 .endm
 #else
 void tegra_resume(void);
-int tegra_sleep_cpu_finish(unsigned long);
 
 #ifdef CONFIG_HOTPLUG_CPU
 void tegra20_hotplug_init(void);
@@ -84,7 +83,6 @@ static inline void tegra30_hotplug_init(void) {}
 #endif
 
 int tegra30_sleep_cpu_secondary_finish(unsigned long);
-void tegra30_tear_down_cpu(void);
 
 #endif
 #endif
