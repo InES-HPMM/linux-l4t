2a062f6d3f3bd9acc64cababfc75738bc0cf5fa9
Bo Yan <byan@nvidia.com>
2013-06-29 11:57:56 -0700

    thermal: Fix merge error in Kconfig
    Remove redundant entries
    Add name for PID thermal governor
    
    Change-Id: I90d4e9f4aa8a5690250dc5231ad1e98355f9bb82
    Signed-off-by: Bo Yan <byan@nvidia.com>
    Reviewed-on: http://git-master/r/243691
    Reviewed-by: Seshendra Gadagottu <sgadagottu@nvidia.com>

--- include/linux/thermal.h
+++ include/linux/thermal.h
@@ -56,6 +56,8 @@
 #define DEFAULT_THERMAL_GOVERNOR       "fair_share"
 #elif defined(CONFIG_THERMAL_DEFAULT_GOV_USER_SPACE)
 #define DEFAULT_THERMAL_GOVERNOR       "user_space"
+#elif defined(CONFIG_THERMAL_DEFAULT_GOV_PID)
+#define DEFAULT_THERMAL_GOVERNOR       "pid_thermal_gov"
 #endif
 
 struct thermal_zone_device;
