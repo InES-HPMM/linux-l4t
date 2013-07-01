ebd3305faa609018c5161a958d81d73cae6dddf9
Jong Kim <jongk@nvidia.com>
2013-07-01 13:40:32 -0700

    ARM: tegra: dalmore: clear fb if bl fb not defined.
    Clear primary framebuffer if bootloader framebuffer is not defined.
    (Some bootloader such as u-boot may not support LCD display and not
    supporting LCD in bootloader is purely customer's choice and the
    kernel display driver should survive such configuration).
    
    bug 1301464
    bug 1264520
    
    Change-Id: Ia37f1f9db08166509bf02673268180f277286dc2
    Signed-off-by: Jong Kim <jongk@nvidia.com>
    Reviewed-on: http://git-master/r/243977
    (cherry picked from commit 6e713daa1b729e341f682568a12bd0ef0249be72)
    Reviewed-on: http://git-master/r/259254
    Reviewed-by: Jon Mayo <jmayo@nvidia.com>

--- arch/arm/mach-tegra/board-dalmore-panel.c
+++ arch/arm/mach-tegra/board-dalmore-panel.c
@@ -537,9 +537,13 @@ int __init dalmore_panel_init(void)
 	res->end = tegra_fb_start + tegra_fb_size - 1;
 
 	/* Copy the bootloader fb to the fb. */
-	__tegra_move_framebuffer(&dalmore_nvmap_device,
-		tegra_fb_start, tegra_bootloader_fb_start,
-			min(tegra_fb_size, tegra_bootloader_fb_size));
+	if (tegra_bootloader_fb_size)
+		__tegra_move_framebuffer(&dalmore_nvmap_device,
+				tegra_fb_start, tegra_bootloader_fb_start,
+				min(tegra_fb_size, tegra_bootloader_fb_size));
+	else
+		__tegra_clear_framebuffer(&dalmore_nvmap_device,
+					  tegra_fb_start, tegra_fb_size);
 
 	res = platform_get_resource_byname(&dalmore_disp2_device,
 					 IORESOURCE_MEM, "fbmem");
