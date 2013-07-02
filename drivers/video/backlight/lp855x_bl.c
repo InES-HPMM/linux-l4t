2f085fc6b740c8d3cd060b18c03884898956c486
Shingo Nakao <shingo.x.nakao@sonymobile.com>
2013-07-02 14:15:54 +0200

    backlight: lp855x: set zero brightness at FBBLANK
    When backlight turns on early from display, a white line can be
    seen on the screen. Therefore make sure backlight is off when we
    are under an fb blank event.
    
    Bug 200089489
    
    Change-Id: I5cd70bfda9902a982d168f2c5b4388424e7d800c
    Signed-off-by: Shingo Nakao <shingo.x.nakao@sonymobile.com>
    Cc: Milo Kim <milo.kim@ti.com>
    Cc: Richard Purdie <rpurdie@rpsys.net>
    Signed-off-by: Oskar Andero <oskar.andero@sonymobile.com>
    Acked-by: Milo Kim <milo.kim@ti.com>
    Acked-by: Jingoo Han <jg1.han@samsung.com>
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>
    (cherry picked from commit 9f0a511f4a227c58c45b65c10fd2b85586360f67)
    Signed-off-by: Shu Zhong <shuz@nvidia.com>
    
    backlight: lp855x: Don't clear level on suspend/blank
    
    Don't clear the backlight level when we're going into suspend or
    blanking. Instead, just temporarily set the level to 0 so we retain
    the value when we resume.
    
    Bug 200089489
    
    Change-Id: If86de80c15400db7ed28832f369328b5031d6d02
    Reported-by: Benson Leung <bleung@chromium.org>
    Signed-off-by: Sean Paul <seanpaul@chromium.org>
    Tested-by: Stephen Barber <smbarber@chromium.org>
    Reviewed-by: Benson Leung <bleung@chromium.org>
    Acked-by: Milo Kim <milo.kim@ti.com>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    (cherry picked from commit 61c1c6147f69d8dea31fd133d2ec0b1594c9a3eb)
    Signed-off-by: Shu Zhong <shuz@nvidia.com>
    Reviewed-on: http://git-master/r/789222
    (cherry picked from commit a9c396f9f0e3d4075be7d04a3d8b349c5000b24a)
    Reviewed-on: http://git-master/r/797976
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/video/backlight/lp855x_bl.c
+++ drivers/video/backlight/lp855x_bl.c
@@ -214,29 +214,21 @@ static void lp855x_pwm_ctrl(struct lp855x *lp, int br, int max_br)
 static int lp855x_bl_update_status(struct backlight_device *bl)
 {
 	struct lp855x *lp = bl_get_data(bl);
+	int brightness = bl->props.brightness;
 
-	if (bl->props.state & BL_CORE_SUSPENDED)
-		bl->props.brightness = 0;
+	if (bl->props.state & (BL_CORE_SUSPENDED | BL_CORE_FBBLANK))
+		brightness = 0;
 
 	if (lp->mode == PWM_BASED) {
-		int max_br, br;
-
-		br = bl->props.brightness;
-
 		if (lp->notify)
-			br = lp->notify(lp->dev, br);
+			brightness = lp->notify(lp->dev, brightness);
 
-		max_br = bl->props.max_brightness;
-
-		lp855x_pwm_ctrl(lp, br, max_br);
-
+		lp855x_pwm_ctrl(lp, brightness, bl->props.max_brightness);
 	} else if (lp->mode == REGISTER_BASED) {
-		u8 val = bl->props.brightness;
-
 		if (lp->notify)
-			val = lp->notify(lp->dev, val);
+			brightness = lp->notify(lp->dev, (u8)brightness);
 
-		lp855x_write_byte(lp, lp->cfg->reg_brightness, val);
+		lp855x_write_byte(lp, lp->cfg->reg_brightness, (u8)brightness);
 	}
 
 	return 0;
