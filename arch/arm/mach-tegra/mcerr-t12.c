9c0f9ab8deba730966c856403c624616cdd82a19
Alex Waterman <alexw@nvidia.com>
2013-07-23 15:12:21 -0700

    ARM: tegra: mc: Add updated T124 support for mcerr
    The default info print function now supports T124 physical addresses.
    
    Change-Id: I5b8b91bed508008221bff67a48462f01dd935287
    Signed-off-by: Alex Waterman <alexw@nvidia.com>
    Reviewed-on: http://git-master/r/252618
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Krishna Reddy <vdumpa@nvidia.com>

--- arch/arm/mach-tegra/mcerr-t12.c
+++ arch/arm/mach-tegra/mcerr-t12.c
@@ -24,8 +24,6 @@
 
 #define dummy_client	client("dummy")
 
-static char unknown_buf[30];
-
 struct mc_client mc_clients[] = {
 	client("ptc"),
 	client("display0_wina"),	client("display1_wina"),
@@ -100,63 +98,25 @@ struct mc_client mc_clients[] = {
 
 int mc_client_last = ARRAY_SIZE(mc_clients) - 1;
 
-static const char *mcerr_t12x_info(u32 stat)
-{
-	if (stat & MC_INT_DECERR_EMEM)
-		return "MC_DECERR";
-	else if (stat & MC_INT_SECURITY_VIOLATION)
-		return "MC_SECURITY_ERR";
-	else if (stat & MC_INT_INVALID_SMMU_PAGE)
-		return "MC_SMMU_ERR";
-	else if (stat & MC_INT_INVALID_APB_ASID_UPDATE)
-		return "MC_APB_ERR";
-	else if (stat & MC_INT_DECERR_VPR)
-		return "MC_DECERR_VPR";
-	else if (stat & MC_INT_SECERR_SEC)
-		return "MC_DECERR_SEC";
-	else if (stat & MC_INT_DECERR_MTS)
-		return "MC_DECERR_MST";
-
-	/* Otherwise we have an unknown type... */
-	snprintf(unknown_buf, 30, "unknown - 0x%04x", stat);
-	return unknown_buf;
-}
-
 static void mcerr_t12x_info_update(struct mc_client *c, u32 stat)
 {
 	if (stat & MC_INT_DECERR_EMEM)
 		c->intr_counts[0]++;
-	else if (stat & MC_INT_SECURITY_VIOLATION)
+	if (stat & MC_INT_SECURITY_VIOLATION)
 		c->intr_counts[1]++;
-	else if (stat & MC_INT_INVALID_SMMU_PAGE)
+	if (stat & MC_INT_INVALID_SMMU_PAGE)
 		c->intr_counts[2]++;
-	else if (stat & MC_INT_INVALID_APB_ASID_UPDATE)
+	if (stat & MC_INT_INVALID_APB_ASID_UPDATE)
 		c->intr_counts[3]++;
-	else if (stat & MC_INT_DECERR_VPR)
+	if (stat & MC_INT_DECERR_VPR)
 		c->intr_counts[4]++;
-	else if (stat & MC_INT_SECERR_SEC)
+	if (stat & MC_INT_SECERR_SEC)
 		c->intr_counts[5]++;
-	else if (stat & MC_INT_DECERR_MTS)
+	if (stat & MC_INT_DECERR_MTS)
 		c->intr_counts[6]++;
-	else
-		c->intr_counts[7]++;
-}
 
-/*
- * T12x supports 34 bit physical address and 40bits virtual address.
- */
-static void mcerr_t12x_print(const char *mc_type, u32 err, u32 addr,
-			     const struct mc_client *client, int is_secure,
-			     int is_write, const char *mc_err_info)
-{
-	u64 hi_addr = err & 0x300000;
-	u64 fin_addr = (addr | ((hi_addr >> 20) << 32));
-	pr_err("%s [0x%llx] %s (%s %s %s), %lx\n", mc_type,
-	       fin_addr,
-	       (client) ? client->name : "unknown",
-	       (is_secure) ? "secure" : "non-secure",
-	       (is_write) ? "write" : "read",
-	       mc_err_info, err);
+	if (stat & ~MC_INT_EN_MASK)
+		c->intr_counts[7]++;
 }
 
 static int mcerr_t12x_debugfs_show(struct seq_file *s, void *v)
@@ -201,8 +161,6 @@ static int mcerr_t12x_debugfs_show(struct seq_file *s, void *v)
  */
 void mcerr_chip_specific_setup(struct mcerr_chip_specific *spec)
 {
-	spec->mcerr_info = mcerr_t12x_info;
-	spec->mcerr_print = mcerr_t12x_print;
 	spec->mcerr_info_update = mcerr_t12x_info_update;
 	spec->mcerr_debugfs_show = mcerr_t12x_debugfs_show;
 	spec->nr_clients = ARRAY_SIZE(mc_clients);
