59327d14193e43612305bbb959e01be12194a65b
Dan Willemsen <dwillemsen@nvidia.com>
2013-07-29 13:38:14 -0700

    Revert "ARM: tegra: moving the clock gating procedure to tegra_cpu_kill"
    This reverts commit b811943160cf3b040341c50d23440cf6d68ae079.
    
    Signed-off-by: Dan Willemsen <dwillemsen@nvidia.com>

--- arch/arm/mach-tegra/hotplug.c
+++ arch/arm/mach-tegra/hotplug.c
@@ -19,17 +19,6 @@
 
 static void (*tegra_hotplug_shutdown)(void);
 
-int tegra_cpu_kill(unsigned cpu)
-{
-	cpu = cpu_logical_map(cpu);
-
-	/* Clock gate the CPU */
-	tegra_wait_cpu_in_reset(cpu);
-	tegra_disable_cpu_clock(cpu);
-
-	return 1;
-}
-
 /*
  * platform-specific code to shutdown a CPU
  *
@@ -37,11 +26,17 @@ int tegra_cpu_kill(unsigned cpu)
  */
 void __ref tegra_cpu_die(unsigned int cpu)
 {
+	cpu = cpu_logical_map(cpu);
+
 	/* Clean L1 data cache */
 	tegra_disable_clean_inv_dcache();
 
 	/* Shut down the current CPU. */
 	tegra_hotplug_shutdown();
+
+	/* Clock gate the CPU */
+	tegra_wait_cpu_in_reset(cpu);
+	tegra_disable_cpu_clock(cpu);
 
 	/* Should never return here. */
 	BUG();
