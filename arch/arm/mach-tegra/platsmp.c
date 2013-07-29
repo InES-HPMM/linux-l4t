6782871660b4d5238ca9b54ba6bbb18b4fa041cc
Dan Willemsen <dwillemsen@nvidia.com>
2013-07-29 13:36:11 -0700

    Revert "ARM: tegra: Skip scu_enable(scu_base) if not Cortex A9"
    This reverts commit 909444ab20629ebc4478642b660a391700aa7e33.
    
    Signed-off-by: Dan Willemsen <dwillemsen@nvidia.com>

--- arch/arm/mach-tegra/platsmp.c
+++ arch/arm/mach-tegra/platsmp.c
@@ -37,6 +37,7 @@
 extern void tegra_secondary_startup(void);
 
 static cpumask_t tegra_cpu_init_mask;
+static void __iomem *scu_base = IO_ADDRESS(TEGRA_ARM_PERIF_BASE);
 
 #define EVP_CPU_RESET_VECTOR \
 	(IO_ADDRESS(TEGRA_EXCEPTION_VECTORS_BASE) + 0x100)
@@ -181,8 +182,7 @@ static void __init tegra_smp_prepare_cpus(unsigned int max_cpus)
 	cpumask_set_cpu(0, &tegra_cpu_init_mask);
 
 	tegra_cpu_reset_handler_init();
-	if (scu_a9_has_base())
-		scu_enable(IO_ADDRESS(scu_a9_get_base()));
+	scu_enable(scu_base);
 }
 
 struct smp_operations tegra_smp_ops __initdata = {
