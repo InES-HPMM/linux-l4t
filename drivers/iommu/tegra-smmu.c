9b55bf4eaeb9906c207f5b823c8abe77772135ce
Dan Willemsen <dwillemsen@nvidia.com>
2013-07-30 20:32:23 -0700

    UPSTREAM Revert 3.9 "iommu: Convert to devm_ioremap_resource()"
    This reverts commit bc5e6dea81b025aba8fecec5c18f6faf81db9562.
    
    Signed-off-by: Dan Willemsen <dwillemsen@nvidia.com>

--- drivers/iommu/tegra-smmu.c
+++ drivers/iommu/tegra-smmu.c
@@ -19,7 +19,6 @@
 
 #define pr_fmt(fmt)	"%s(): " fmt, __func__
 
-#include <linux/err.h>
 #include <linux/module.h>
 #include <linux/platform_device.h>
 #include <linux/spinlock.h>
@@ -1233,9 +1232,9 @@ static int tegra_smmu_probe(struct platform_device *pdev)
 		res = platform_get_resource(pdev, IORESOURCE_MEM, i);
 		if (!res)
 			return -ENODEV;
-		smmu->regs[i] = devm_ioremap_resource(&pdev->dev, res);
-		if (IS_ERR(smmu->regs[i]))
-			return PTR_ERR(smmu->regs[i]);
+		smmu->regs[i] = devm_request_and_ioremap(&pdev->dev, res);
+		if (!smmu->regs[i])
+			return -EBUSY;
 	}
 
 	err = of_get_dma_window(dev->of_node, NULL, 0, NULL, &base, &size);
