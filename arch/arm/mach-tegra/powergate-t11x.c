192c460aa3e60de5dd9775a5b9437f419e4cd4d5
Terje Bergstrom <tbergstrom@nvidia.com>
2013-07-31 08:34:10 +0300

    ARM: tegra: Assert reset when power ungating
    Ensure unit is in reset when powering it back on.
    
    Bug 1329416
    Bug 1331777
    Bug 1322046
    
    Change-Id: I8ef6646a8fe2ae85fdb836f3222678e5f77a784c
    Signed-off-by: Terje Bergstrom <tbergstrom@nvidia.com>
    Reviewed-on: http://git-master/r/256139
    (cherry picked from commit 814f276c512cd49abbf15c64e14f8a9bbeb22d39)
    Reviewed-on: http://git-master/r/264235
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Juha Tukkinen <jtukkinen@nvidia.com>

--- arch/arm/mach-tegra/powergate-t11x.c
+++ arch/arm/mach-tegra/powergate-t11x.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2012-2013, NVIDIA CORPORATION.  All rights reserved.
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -530,6 +530,10 @@ static int tegra11x_unpowergate(int id,
 	ret = tegra_powergate_set(id, true);
 	if (ret)
 		goto err_power;
+
+	udelay(10);
+
+	powergate_partition_assert_reset(pg_info);
 
 	udelay(10);
 
