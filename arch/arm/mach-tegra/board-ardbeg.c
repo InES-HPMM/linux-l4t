6758881cd1bb428be481bc626938bb5f74c75576
Xiaohui Tao <xtao@nvidia.com>
2013-08-01 10:03:39 -0700

    ARM: tegra: ardbeg: Change to use the right platform ID
    Bug 1330952
    
    Change-Id: I5c177ab7b6e41923a793e95150c35675c0deb89e
    Signed-off-by: Xiaohui Tao <xtao@nvidia.com>
    Reviewed-on: http://git-master/r/256923
    Reviewed-by: Riham Haidar <rhaidar@nvidia.com>
    Tested-by: Riham Haidar <rhaidar@nvidia.com>

--- arch/arm/mach-tegra/board-ardbeg.c
+++ arch/arm/mach-tegra/board-ardbeg.c
@@ -994,7 +994,7 @@ static __initdata struct tegra_clk_init_table touch_clk_init_table[] = {
 struct rm_spi_ts_platform_data rm31080ts_ardbeg_data = {
 	.gpio_reset = TOUCH_GPIO_RST_RAYDIUM_SPI,
 	.config = 0,
-	.platform_id = RM_PLATFORM_D010,
+	.platform_id = RM_PLATFORM_A010,
 	.name_of_clock = "clk_out_2",
 	.name_of_clock_con = "extern2",
 };
