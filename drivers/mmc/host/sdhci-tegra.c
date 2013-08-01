8bc756646814c6a09ec3ccecbd18b05d1a5154f6
Dan Willemsen <dwillemsen@nvidia.com>
2013-07-31 22:21:55 -0700

    fixup! mmc: tegra: Set broken preset values quirk
    Signed-off-by: Dan Willemsen <dwillemsen@nvidia.com>

--- drivers/mmc/host/sdhci-tegra.c
+++ drivers/mmc/host/sdhci-tegra.c
@@ -2048,7 +2048,7 @@ static struct sdhci_pltfm_data sdhci_tegra20_pdata = {
 		  SDHCI_QUIRK_BROKEN_ADMA_ZEROLEN_DESC |
 		  SDHCI_QUIRK_BROKEN_CARD_DETECTION |
 		  SDHCI_QUIRK_NO_CALC_MAX_DISCARD_TO,
-	.quirks2 = SDHCI_QUIRK2_BROKEN_PRESET_VALUES,
+	.quirks2 = SDHCI_QUIRK2_PRESET_VALUE_BROKEN,
 	.ops  = &tegra_sdhci_ops,
 };
 
