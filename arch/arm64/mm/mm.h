444f2d0a3780fcb5575082289d3974abd0c8e610
Rich Wiley <rwiley@nvidia.com>
2013-08-01 09:14:32 -0700

    arm64: dma-mapping: arm64 mm.h tweaks for DMA
    Change-Id: I8540e2cdaa0957e9e894dbcf2ab98d0b7c9d4270
    Signed-off-by: Rich Wiley <rwiley@nvidia.com>
    Reviewed-on: http://git-master/r/256905
    Reviewed-by: Hiroshi Doyu <hdoyu@nvidia.com>
    Tested-by: Hiroshi Doyu <hdoyu@nvidia.com>

--- arch/arm64/mm/mm.h
+++ arch/arm64/mm/mm.h
@@ -1,3 +1,18 @@
 extern void __flush_dcache_page(struct page *page);
 extern void __init bootmem_init(void);
 extern void __init arm64_swiotlb_init(void);
+
+static inline pmd_t *pmd_off_k(unsigned long virt)
+{
+	return pmd_offset(pud_offset(pgd_offset_k(virt), virt), virt);
+}
+
+/* consistent regions used by dma_alloc_attrs() */
+#define VM_ARM_DMA_CONSISTENT  0x20000000
+
+
+#ifdef CONFIG_ZONE_DMA
+extern phys_addr_t arm_dma_limit;
+#else
+#define arm_dma_limit ((phys_addr_t)~0)
+#endif
