4a8be3dbb688f754ec2474540041f7061e25f5c6
Chun Xu <chunx@nvidia.com>
2013-08-05 16:55:17 +0800

    net: fix preempt_count non-zero schedule
    Resolve "BUG: scheduling while atomic" issue when
    sk_get_waiting_task is being called.
    
    Bug 1342554
    
    Change-Id: I1673d56751a8a95b988b325b3857c8a5fe4c78ce
    Signed-off-by: Chun Xu <chunx@nvidia.com>
    Reviewed-on: http://git-master/r/258545
    (cherry picked from commit 4516e7c330bb4c5da5020df0d2cc1cb5e9274d9f)
    Reviewed-on: http://git-master/r/260033
    Reviewed-by: Simone Willett <swillett@nvidia.com>
    Tested-by: Simone Willett <swillett@nvidia.com>

--- net/core/sock.c
+++ net/core/sock.c
@@ -2533,6 +2533,7 @@ char *sk_get_waiting_task_cmdline(struct sock *sk, char *cmdline)
 	struct pid *pid = NULL;
 	struct fown_struct *fown = NULL;
 	struct file *file;
+	int preempt_count;
 
 	*program_name = '\0';
 
@@ -2598,6 +2599,11 @@ char *sk_get_waiting_task_cmdline(struct sock *sk, char *cmdline)
 			softirq_enabled = true;
 			local_bh_enable();
 		}
+		if (preempt_count()) {
+			preempt_count = preempt_count();
+			preempt_count() = 0;
+		}
+
 		res = access_process_vm(task, mm->arg_start, cmdline, len, 0);
 
 		if (res > 0 && cmdline[res-1] != '\0' && len < PAGE_SIZE) {
@@ -2613,6 +2619,9 @@ char *sk_get_waiting_task_cmdline(struct sock *sk, char *cmdline)
 				res = strnlen(cmdline, res);
 			}
 		}
+
+		if (preempt_count)
+			preempt_count() = preempt_count;
 		if (softirq_enabled)
 			local_bh_disable();
 
