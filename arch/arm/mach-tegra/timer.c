4b33a7c98e25871355a0453eef15e495d378a788
Dan Willemsen <dwillemsen@nvidia.com>
2013-08-08 00:47:17 -0700

    ARM: tegra: Move arch_timer init later
    If we initialize the architecutral timers before we register our
    clockevents device, we run into a kernel panic where the event handler
    in tegra_timer_interrupt is null. Instead of putting a null-check there,
    re-order initialization to fix this.
    
    Signed-off-by: Dan Willemsen <dwillemsen@nvidia.com>

--- arch/arm/mach-tegra/timer.c
+++ arch/arm/mach-tegra/timer.c
@@ -778,14 +778,6 @@ void __init tegra_init_timer(void)
 	tegra30_init_timer();
 #endif
 
-	/* Architectural timers take precedence over broadcast timers.
-	   Only register a broadcast clockevent device if architectural
-	   timers do not exist or cannot be initialized. */
-	if (tegra_init_arch_timer())
-		/* Architectural timers do not exist or cannot be initialzied.
-		   Fall back to using the broadcast timer as the sched clock. */
-		setup_sched_clock(tegra_read_sched_clock, 32, 1000000);
-
 	ret = clocksource_mmio_init(timer_reg_base + TIMERUS_CNTR_1US,
 		"timer_us", 1000000, 300, 32,
 		clocksource_mmio_readl_up);
@@ -810,6 +802,14 @@ void __init tegra_init_timer(void)
 	tegra_clockevent.cpumask = cpu_all_mask;
 	tegra_clockevent.irq = tegra_timer_irq.irq;
 	clockevents_register_device(&tegra_clockevent);
+
+	/* Architectural timers take precedence over broadcast timers.
+	   Only register a broadcast clockevent device if architectural
+	   timers do not exist or cannot be initialized. */
+	if (tegra_init_arch_timer())
+		/* Architectural timers do not exist or cannot be initialzied.
+		   Fall back to using the broadcast timer as the sched clock. */
+		setup_sched_clock(tegra_read_sched_clock, 32, 1000000);
 
 	register_syscore_ops(&tegra_timer_syscore_ops);
 
