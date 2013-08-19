89981a1b9f07213db06fe642c920a0e412d79447
Ajay Gupta <ajayg@nvidia.com>
2013-08-19 14:32:41 -0700

    usb: gadget: fmtp: add super speed support
    Change-Id: Ia8c4438d4522a5e4304d3689e17a1c1a124fc919
    Signed-off-by: Hui Fu <hfu@nvidia.com>
    Reviewed-on: http://git-master/r/439408
    Reviewed-by: Ashutosh Jha <ajha@nvidia.com>

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -1510,8 +1510,6 @@ android_setup(struct usb_gadget *gadget, const struct usb_ctrlrequest *c)
 	/* Special case the accessory function.
 	 * It needs to handle control requests before it is enabled.
 	 */
-	if (value < 0)
-		value = acc_ctrlrequest(cdev, c);
 
 	if (value < 0)
 		value = composite_setup_func(gadget, c);
