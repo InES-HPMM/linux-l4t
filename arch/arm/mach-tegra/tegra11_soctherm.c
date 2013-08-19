7fa6683e9c4b7108d8303832feb1032568baf6e6
Diwakar Tundlam <dtundlam@nvidia.com>
2013-08-19 15:27:24 -0700

    arm: tegra: soctherm: return early if therm-trip info is null
    Fixes hang-on-boot on ERS-S boards due to soc_them init
    
    bug 1309557
    
    Change-Id: Ie2c5ec1eb95d3b4f0f1065ca4075ab8e7ef588ce
    Signed-off-by: Diwakar Tundlam <dtundlam@nvidia.com>
    Reviewed-on: http://git-master/r/263532
    Reviewed-by: Xue Dong <xdong@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- arch/arm/mach-tegra/tegra11_soctherm.c
+++ arch/arm/mach-tegra/tegra11_soctherm.c
@@ -1847,14 +1847,14 @@ static void soctherm_therm_trip_init(struct tegra_tsensor_pmu_data *data)
 {
 	u32 val, checksum;
 
+	if (!data)
+		return;
+
 	/* enable therm trip at PMC */
 	val = pmc_readl(PMC_SENSOR_CTRL);
 	val = REG_SET(val, PMC_SCRATCH_WRITE, 1);
 	val = REG_SET(val, PMC_ENABLE_RST, 1);
 	pmc_writel(val, PMC_SENSOR_CTRL);
-
-	if (!data)
-		return;
 
 	/* Fill scratch registers to shutdown device on therm TRIP */
 	val = REG_SET(0, PMU_OFF_DATA, data->poweroff_reg_data);
