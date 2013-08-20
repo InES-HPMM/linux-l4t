8025640f674c3a783a48f99b32483f7c292d63ce
Dan Willemsen <dwillemsen@nvidia.com>
2013-08-19 20:49:28 -0700

    Revert "ARM: tegra: add speedo-based process id for Tegra114"
    This reverts commit 7495b2eb0770b85e58af98b99faaf853e9563784.
    
    Signed-off-by: Dan Willemsen <dwillemsen@nvidia.com>

--- arch/arm/mach-tegra/fuse.c
+++ arch/arm/mach-tegra/fuse.c
@@ -2,7 +2,6 @@
  * arch/arm/mach-tegra/fuse.c
  *
  * Copyright (C) 2010 Google, Inc.
- * Copyright (c) 2013, NVIDIA CORPORATION.  All rights reserved.
  *
  * Author:
  *	Colin Cross <ccross@android.com>
@@ -137,9 +136,6 @@ void tegra_init_fuse(void)
 	case TEGRA30:
 		tegra_fuse_spare_bit = TEGRA30_FUSE_SPARE_BIT;
 		tegra_init_speedo_data = &tegra30_init_speedo_data;
-		break;
-	case TEGRA114:
-		tegra_init_speedo_data = &tegra114_init_speedo_data;
 		break;
 	default:
 		pr_warn("Tegra: unknown chip id %d\n", tegra_chip_id);
