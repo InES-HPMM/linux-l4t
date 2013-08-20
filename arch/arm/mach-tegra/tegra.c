44f1847cbd39d756e68c85746a183c7de76b88e0
Dan Willemsen <dwillemsen@nvidia.com>
2013-08-19 20:49:22 -0700

    Revert "i2c: tegra: assume CONFIG_OF, remove platform data"
    This reverts commit 49a64ac555f1dabd2b94325553187d0db6ecac16.
    
    Signed-off-by: Dan Willemsen <dwillemsen@nvidia.com>

--- arch/arm/mach-tegra/tegra.c
+++ arch/arm/mach-tegra/tegra.c
@@ -31,6 +31,8 @@
 #include <linux/pda_power.h>
 #include <linux/platform_data/tegra_usb.h>
 #include <linux/io.h>
+#include <linux/i2c.h>
+#include <linux/i2c-tegra.h>
 #include <linux/slab.h>
 #include <linux/sys_soc.h>
 #include <linux/usb/tegra_usb_phy.h>
