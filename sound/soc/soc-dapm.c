e9a15ee0273c9364dbf5057f409c0db18fcf0aec
Songhee Baek <sbaek@nvidia.com>
2013-08-23 10:59:30 -0700

    ASoC: dapm: Workaround for double addition prefix
    When we use identical codec for dual codec use case,
    we need to use prefix for codec driver, but dapm making double
    addition prefix for codec DAI widget in snd_soc_dapm_add_route.
    So, this change is for avoiding double addition prefix for codec
    DAI widget.
    
    This change is needed by dual codec use case.
    
    Bug 1354235
    Bug 1373091
    
    Change-Id: I3e9214ca87ccd14c9729b4accb40ec3cf4f65ed0
    Signed-off-by: Songhee Baek <sbaek@nvidia.com>
    Reviewed-on: http://git-master/r/289442
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Bob Johnston <bjohnston@nvidia.com>
    Reviewed-by: Scott Peterson <speterson@nvidia.com>

--- sound/soc/soc-dapm.c
+++ sound/soc/soc-dapm.c
@@ -2200,7 +2200,8 @@ int snd_soc_dapm_sync(struct snd_soc_dapm_context *dapm)
 EXPORT_SYMBOL_GPL(snd_soc_dapm_sync);
 
 static int snd_soc_dapm_add_route(struct snd_soc_dapm_context *dapm,
-				  const struct snd_soc_dapm_route *route)
+				  const struct snd_soc_dapm_route *route,
+				  unsigned int prefixed)
 {
 	struct snd_soc_dapm_path *path;
 	struct snd_soc_dapm_widget *wsource = NULL, *wsink = NULL, *w;
@@ -2212,7 +2213,7 @@ static int snd_soc_dapm_add_route(struct snd_soc_dapm_context *dapm,
 	char prefixed_source[80];
 	int ret = 0;
 
-	if (dapm->codec && dapm->codec->name_prefix) {
+	if (dapm->codec && dapm->codec->name_prefix && !prefixed) {
 		snprintf(prefixed_sink, sizeof(prefixed_sink), "%s %s",
 			 dapm->codec->name_prefix, route->sink);
 		sink = prefixed_sink;
@@ -2431,7 +2432,7 @@ int snd_soc_dapm_add_routes(struct snd_soc_dapm_context *dapm,
 
 	mutex_lock_nested(&dapm->card->dapm_mutex, SND_SOC_DAPM_CLASS_INIT);
 	for (i = 0; i < num; i++) {
-		r = snd_soc_dapm_add_route(dapm, route);
+		r = snd_soc_dapm_add_route(dapm, route, false);
 		if (r < 0) {
 			dev_err(dapm->dev, "ASoC: Failed to add route %s -> %s -> %s\n",
 				route->source,
@@ -3451,7 +3452,7 @@ int snd_soc_dapm_link_dai_widgets(struct snd_soc_card *card)
 				dev_dbg(dai->dev, "%s -> %s\n",
 					 r.source, r.sink);
 
-				snd_soc_dapm_add_route(w->dapm, &r);
+				snd_soc_dapm_add_route(w->dapm, &r, true);
 			}
 
 			if (dai->driver->capture.stream_name &&
@@ -3462,7 +3463,7 @@ int snd_soc_dapm_link_dai_widgets(struct snd_soc_card *card)
 				dev_dbg(dai->dev, "%s -> %s\n",
 					r.source, r.sink);
 
-				snd_soc_dapm_add_route(w->dapm, &r);
+				snd_soc_dapm_add_route(w->dapm, &r, true);
 			}
 		}
 	}
