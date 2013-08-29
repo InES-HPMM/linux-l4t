23ad615ddbb035b811c60f4590966d48c96ffb00
Greg Hackmann <ghackmann@google.com>
2013-08-29 12:41:05 -0700

    video: tegra: move nvdps sysfs to fb.c
    nvdps as implemented is inherently fbdev-specific
    
    Bug 1459374
    
    Change-Id: I163076a1c65dd86ef341d902004bec5a6ff8758e
    Signed-off-by: Greg Hackmann <ghackmann@google.com>
    Signed-off-by: Xia Yang <xiay@nvidia.com>
    Reviewed-on: http://git-master/r/366184
    Reviewed-by: Automatic_Commit_Validation_User
    Tested-by: Jon Mayo <jmayo@nvidia.com>
    Reviewed-by: Jon Mayo <jmayo@nvidia.com>

--- arch/arm/mach-tegra/include/mach/fb.h
+++ arch/arm/mach-tegra/include/mach/fb.h
@@ -4,7 +4,7 @@
  * Copyright (C) 2010 Google, Inc.
  * Author: Erik Gilling <konkers@google.com>
  *
- * Copyright (c) 2010-2013, NVIDIA CORPORATION, All rights reserved.
+ * Copyright (c) 2010-2014, NVIDIA CORPORATION, All rights reserved.
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -28,9 +28,6 @@ struct tegra_fb_data;
 struct tegra_fb_info;
 struct resource;
 
-int tegra_fb_get_mode(struct tegra_dc *dc);
-int tegra_fb_set_mode(struct tegra_dc *dc, int fps);
-
 #ifdef CONFIG_FB_TEGRA
 struct tegra_fb_info *tegra_fb_register(struct platform_device *ndev,
 					struct tegra_dc *dc,
@@ -42,6 +39,8 @@ void tegra_fb_update_monspecs(struct tegra_fb_info *fb_info,
 			      struct fb_monspecs *specs,
 			      bool (*mode_filter)(const struct tegra_dc *dc,
 						  struct fb_videomode *mode));
+int tegra_fb_create_sysfs(struct device *dev);
+void tegra_fb_remove_sysfs(struct device *dev);
 #else
 static inline struct tegra_fb_info *tegra_fb_register(
 	struct platform_device *ndev, struct tegra_dc *dc,
@@ -61,6 +60,15 @@ static inline void tegra_fb_pan_display_reset(struct tegra_fb_info *fb_info)
 static inline void tegra_fb_update_monspecs(struct tegra_fb_info *fb_info,
 					    struct fb_monspecs *specs,
 					    bool (*mode_filter)(struct fb_videomode *mode))
+{
+}
+
+static inline int tegra_fb_create_sysfs(struct device *dev)
+{
+	return -ENOENT;
+}
+
+static inline void tegra_fb_remove_sysfs(struct device *dev)
 {
 }
 #endif
