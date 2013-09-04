fa7c748e0cc3943e4660279f4d052bc0bccb7b60
Terry Wang <terwang@nvidia.com>
2013-09-04 19:53:42 +0800

    ARM: tegra: Ardbeg: Changing LDO8 LP0 voltage
    Changing ldo8 LP0 voltage from 1V to 0.9V for
    Ardbeg with TI PMIC module E1735.
    
    Bug 1317293
    
    Change-Id: I3766ee888156591bca5bb6816409a43f91b4ec52
    Signed-off-by: Terry Wang <terwang@nvidia.com>
    Reviewed-on: http://git-master/r/264216
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>
    Tested-by: Laxman Dewangan <ldewangan@nvidia.com>

--- arch/arm/mach-tegra/board-ardbeg-power.c
+++ arch/arm/mach-tegra/board-ardbeg-power.c
@@ -463,7 +463,7 @@ PALMAS_REGS_PDATA(ti913_ldo4, 1200, 1200, palmas_rails(ti913_smps6),
 PALMAS_REGS_PDATA(ti913_ldo5, 2700, 2700, NULL, 0, 0, 1, 0, 0, 0, 0, 0, 0);
 PALMAS_REGS_PDATA(ti913_ldo6, 1800, 1800, NULL, 0, 0, 1, 0, 0, 0, 0, 0, 0);
 PALMAS_REGS_PDATA(ti913_ldo7, 2700, 2700, NULL, 0, 0, 1, 0, 0, 0, 0, 0, 0);
-PALMAS_REGS_PDATA(ti913_ldo8, 1000, 1000, NULL, 1, 1, 1, 0, 0, 0, 0, 0, 0);
+PALMAS_REGS_PDATA(ti913_ldo8, 900, 900, NULL, 1, 1, 1, 0, 0, 0, 0, 0, 0);
 PALMAS_REGS_PDATA(ti913_ldo9, 1800, 3300, NULL, 0, 0, 1, 0, 0, 0, 0, 0, 0);
 PALMAS_REGS_PDATA(ti913_ldoln, 1050, 1050, palmas_rails(ti913_smps6),
 		0, 0, 1, 0, 0, 0, 0, 0, 0);
