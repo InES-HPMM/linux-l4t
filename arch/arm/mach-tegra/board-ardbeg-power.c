ce8de3a9bd0ed8e037c80b29ea6a2af19033a76f
Pradeep Goudagunta <pgoudagunta@nvidia.com>
2013-09-06 12:04:53 +0530

    ARM: tegra: palmas: Correct vbus detect pin name
    Correct palmas vbus detect pin name in ardbeg and macallan.
    
    Bug 1362846
    
    Change-Id: Idedc599cd877ef657e6750317642ca948543c014
    Signed-off-by: Pradeep Goudagunta <pgoudagunta@nvidia.com>
    Reviewed-on: http://git-master/r/271349
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>
    Tested-by: Laxman Dewangan <ldewangan@nvidia.com>

--- arch/arm/mach-tegra/board-ardbeg-power.c
+++ arch/arm/mach-tegra/board-ardbeg-power.c
@@ -562,7 +562,7 @@ static struct palmas_pinctrl_config palmas_ti913_pincfg[] = {
 	PALMAS_PINMUX("powergood", "powergood", NULL, NULL),
 	PALMAS_PINMUX("vac", "vac", NULL, NULL),
 	PALMAS_PINMUX("gpio0", "id", "pull-up", NULL),
-	PALMAS_PINMUX("gpio1", "vbus-det", NULL, NULL),
+	PALMAS_PINMUX("gpio1", "vbus_det", NULL, NULL),
 	PALMAS_PINMUX("gpio2", "gpio", NULL, NULL),
 	PALMAS_PINMUX("gpio3", "gpio", NULL, NULL),
 	PALMAS_PINMUX("gpio4", "gpio", NULL, NULL),
