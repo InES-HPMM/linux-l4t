a1729405c072be3c2ea8a0249fa781ffa954fe08
Jinyoung Park <jinyoungp@nvidia.com>
2013-09-09 18:13:22 +0900

    ARM: tegra: dt: Correct wrong pointer passing
    Corrected a wrong device node pointer passing.
    
    Bug 1352073
    
    Signed-off-by: Jinyoung Park <jinyoungp@nvidia.com>
    Reviewed-on: http://git-master/r/298885
    (cherry picked from commit b1ea386359b2331c4ac7ee496fcba2aeac64227a)
    
    Change-Id: Ib4354e0b74d1babf671d141cea238e9a55299343
    Signed-off-by: Jinyoung Park <jinyoungp@nvidia.com>
    Reviewed-on: http://git-master/r/384849
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>

--- arch/arm/mach-tegra/tegra_emc_dt_parse.c
+++ arch/arm/mach-tegra/tegra_emc_dt_parse.c
@@ -35,7 +35,7 @@ static struct device_node *tegra_emc_ramcode_devnode(
 	u32 reg;
 
 	for_each_child_of_node(np, iter) {
-		if (of_property_read_u32(np, "nvidia,ram-code", &reg))
+		if (of_property_read_u32(iter, "nvidia,ram-code", &reg))
 			continue;
 		if (reg == tegra_get_bct_strapping())
 			return of_node_get(iter);
