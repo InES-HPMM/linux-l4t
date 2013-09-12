3d5125389b7764d310b0c40682ef15f22e33945b
Peng Du <pdu@nvidia.com>
2013-09-11 17:34:50 -0700

    ARM: tegra: adjust IO_LAST_ADDR for ARM64
    Change-Id: Iaf388b56a1f0ad80eef766ca0f91e98ac7eca5e6
    Signed-off-by: Peng Du <pdu@nvidia.com>

--- arch/arm/mach-tegra/iomap.h
+++ arch/arm/mach-tegra/iomap.h
@@ -967,8 +967,11 @@ defined(CONFIG_ARCH_TEGRA_12x_SOC))
 /* Virtual aperture limits are packed into the I/O space from the higest
    address to lowest with each aperture base address adjusted as necessary
    for proper section mapping boundary (2 MB) rounding. */
-
+#ifdef CONFIG_ARM64
+#define IO_LAST_ADDR		VMALLOC_END
+#else
 #define IO_LAST_ADDR		IOMEM(0xFEC00000)
+#endif
 #define IO_HOST1X_VIRT		IOMEM((IO_LAST_ADDR - IO_VIRT_ROUND_UP(IO_HOST1X_SIZE)))
 #define IO_SDMMC_VIRT		IOMEM((IO_HOST1X_VIRT - IO_VIRT_ROUND_UP(IO_SDMMC_SIZE)))
 #define IO_USB_VIRT		IOMEM((IO_SDMMC_VIRT - IO_VIRT_ROUND_UP(IO_USB_SIZE)))
