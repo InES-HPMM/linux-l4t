Unlegacy-Android/android_kernel_nvidia_tegra3

cdc_ncm: Adding rmnet interface for Icera Nemo modem
Bug 1176649

Change-Id: I9c89b907649ac66d9e85c66e964574a2fe9d368f
Signed-off-by: Steve Lin <stlin@nvidia.com>
Reviewed-on: http://git-master/r/167784
Reviewed-by: Automatic_Commit_Validation_User
Reviewed-by: Frederic Bossy <fbossy@nvidia.com>

drivers/net/usb/cdc_ncm.c
@@ -166,6 +166,13 @@ static const struct usb_device_id cdc_devs[] = {
 		.driver_info = (unsigned long)&cdc_mbm_info,
 	},
 	{
+		/* Icera Nemo */
+		.match_flags = USB_DEVICE_ID_MATCH_INT_INFO
+			| USB_DEVICE_ID_MATCH_DEVICE,
+		USB_DEVICE(0x1983, 0x1006),
+		.driver_info = (unsigned long)&cdc_mbm_info,
+	},
+	{
 		/* Standard NCM class device */
 		USB_INTERFACE_INFO(USB_CLASS_COMM,
 		USB_CDC_SUBCLASS_NCM, USB_CDC_PROTO_NONE),

InES-HPMM/linux-l4t

drivers/net/usb/cdc_ncm.c
@@ -1268,6 +1268,14 @@ static const struct usb_device_id cdc_devs[] = {
 		.driver_info = (unsigned long)&wwan_info,
 	},
 
+	/* Icera Nemo */
+	{
+		.match_flags = USB_DEVICE_ID_MATCH_INT_INFO
+			| USB_DEVICE_ID_MATCH_DEVICE,
+		USB_DEVICE(0x1983, 0x1006),
+		.driver_info = (unsigned long)&wwan_info,
+	},
+
 	/* Generic CDC-NCM devices */
 	{ USB_INTERFACE_INFO(USB_CLASS_COMM,
 		USB_CDC_SUBCLASS_NCM, USB_CDC_PROTO_NONE),