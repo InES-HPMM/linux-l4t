fosser2/android-tegra-nv-3.1.10-rel-15r7

arm: tegra: usb: Exclude incompatible device from CDC-ACM driver.
A certain flashless modem has a boot ROM which incorrectly reports
itself as being USB CDC-ACM class compliant. Prevent the CDC-ACM
driver from printing error message about "This device cannot do
calls on its own.  It is not a modem." when this device is present
on usb bus.

Change-Id: If1ef27980b3cc454233309a5066070e6250dd24e
Reviewed-on: http://git-master/r/41105
Reviewed-by: Michael Hsu <mhsu@nvidia.com>
Tested-by: Michael Hsu <mhsu@nvidia.com>
Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>

drivers/usb/class/cdc-acm.c
@@ -1619,6 +1619,11 @@ static const struct usb_device_id acm_ids[] = {
 	.driver_info = NOT_A_MODEM,
        	},
 
+	/* Exclude XMM6260 boot rom (not running modem software yet) */
+	{ USB_DEVICE(0x058b, 0x0041),
+	.driver_info = NOT_A_MODEM,
+	},
+
 	/* control interfaces without any protocol set */
 	{ USB_INTERFACE_INFO(USB_CLASS_COMM, USB_CDC_SUBCLASS_ACM,
 		USB_CDC_PROTO_NONE) },

InES-HPMM/linux-l4t

Original-Change-Id: If1ef27980b3cc454233309a5066070e6250dd24e
Rebase-Id: Ra7b68fb265acffe22a2de5d31bad82363607b87e

drivers/usb/class/cdc-acm.c
@@ -1691,6 +1691,11 @@ static const struct usb_device_id acm_ids[] = {
 	},
 #endif
 
+	/* Exclude XMM6260 boot rom (not running modem software yet) */
+	{ USB_DEVICE(0x058b, 0x0041),
+	.driver_info = IGNORE_DEVICE,
+	},
+
 	/* control interfaces without any protocol set */
 	{ USB_INTERFACE_INFO(USB_CLASS_COMM, USB_CDC_SUBCLASS_ACM,
 		USB_CDC_PROTO_NONE) },