GalaxyTab101/samsung-kernel-galaxytab101

ARM: usb: Add PH450 modem to the cdc ether device list
Adding PH450 modem to the cdc ether device list. The modem network interface
is assigned as wwan0, which is fully supported by the GB telephony framework.
Bug 800301

Original-Change-Id: I616aa5e39a336a47c5f72baec02be6ff8d958859
Reviewed-on: http://git-master/r/28389
Reviewed-by: Varun Colbert <vcolbert@nvidia.com>
Tested-by: Varun Colbert <vcolbert@nvidia.com>
Change-Id: Ice65d33a5d31b0abea4e0b8c73a02a6cca7f662c

drivers/net/usb/cdc_ether.c
@@ -594,6 +594,14 @@ static const struct usb_device_id	products [] = {
 	.driver_info		= 0,
 },
 
+/* PH450 */
+{
+         .match_flags = USB_DEVICE_ID_MATCH_INT_INFO
+                | USB_DEVICE_ID_MATCH_DEVICE,
+         USB_DEVICE(0x1983,0x0310),
+         .driver_info = (unsigned long)&mbm_info,
+},
+
 /*
  * WHITELIST!!!
  *

InES-HPMM/linux-l4t

Rebase-Id: Ra2842afd9c2a854a0b74034e6e10cccc1230d851

drivers/net/usb/cdc_ether.c
@@ -642,6 +642,14 @@ static const struct usb_device_id	products [] = {
 },
 #endif
 
+/* PH450 */
+{
+         .match_flags = USB_DEVICE_ID_MATCH_INT_INFO
+                | USB_DEVICE_ID_MATCH_DEVICE,
+         USB_DEVICE(0x1983,0x0310),
+         .driver_info = (unsigned long)&wwan_info,
+},
+
 /*
  * WHITELIST!!!
  *