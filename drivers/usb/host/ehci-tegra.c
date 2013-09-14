fosser2/android-tegra-nv-3.1.10-rel-15r7

tegra: ehci: clock disable
Disable the clock only when the device clocks are enabled.

Bug 858490

Change-Id: I2f22890b11ec98b88457ffe6c90411f9927d8390
Reviewed-on: http://git-master/r/45007
Reviewed-by: Suresh Mangipudi <smangipudi@nvidia.com>
Tested-by: Suresh Mangipudi <smangipudi@nvidia.com>
Tested-by: Bala Murali Krishna <balam@nvidia.com>
Reviewed-by: Hanumanth Venkateswa Moganty <vmoganty@nvidia.com>

drivers/usb/host/ehci-tegra.c
@@ -1088,10 +1088,11 @@ static int tegra_ehci_remove(struct platform_device *pdev)
 	clk_disable(tegra->clk);
 	clk_put(tegra->clk);
 
-	clk_disable(tegra->sclk_clk);
+	if (tegra->clock_enabled){
+		clk_disable(tegra->sclk_clk);
+		clk_disable(tegra->emc_clk);
+	}
 	clk_put(tegra->sclk_clk);
-
-	clk_disable(tegra->emc_clk);
 	clk_put(tegra->emc_clk);
 
 	kfree(tegra);

InES-HPMM/linux-l4t

Original-Change-Id: I2f22890b11ec98b88457ffe6c90411f9927d8390
Rebase-Id: R7a42a8bb412327a2c1d84877d84b722e33092ce9

drivers/usb/host/ehci-tegra.c
@@ -1193,9 +1193,10 @@ static int tegra_ehci_remove(struct platform_device *pdev)
 
 	clk_disable_unprepare(tegra->clk);
 
-	clk_disable_unprepare(tegra->sclk_clk);
-
-	clk_disable_unprepare(tegra->emc_clk);
+	if (tegra->clock_enabled) {
+		clk_disable_unprepare(tegra->sclk_clk);
+		clk_disable_unprepare(tegra->emc_clk);
+	}
 
 	return 0;
 }