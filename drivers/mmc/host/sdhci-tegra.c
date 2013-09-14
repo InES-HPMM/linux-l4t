venkatarajasekhar/Wiko-Highway-4G-Kernel

mmc: tegra: Restore tuned tap value in resume
For devices that are powered on across suspend, restore the tuned
tap value during resume. In other cases, set the default tap value.

Bug 1237695

Signed-off-by: Pavan Kunapuli <pkunapuli@nvidia.com>
Reviewed-on: http://git-master/r/210752
(cherry picked from commit 186ebd8e3570c611a328e71bb7a2853b610efcfd)

Change-Id: I38ba01931267d7e6c79b32a4855b4bb60a1ce003
Signed-off-by: Pavan Kunapuli <pkunapuli@nvidia.com>
Reviewed-on: http://git-master/r/212423
Reviewed-by: Riham Haidar <rhaidar@nvidia.com>
Tested-by: Riham Haidar <rhaidar@nvidia.com>

drivers/mmc/host/sdhci-tegra.c
@@ -451,11 +451,20 @@ static void tegra_sdhci_reset_exit(struct sdhci_host *sdhci, u8 mask)
 				~SDHCI_VNDR_CLK_CTRL_INPUT_IO_CLK;
 		}
 		if (soc_data->nvquirks & NVQUIRK_SET_TAP_DELAY) {
-			if (plat->tap_delay) {
+			if ((tegra_host->tuning_status == TUNING_STATUS_DONE) &&
+				(sdhci->mmc->pm_flags & MMC_PM_KEEP_POWER)) {
 				vendor_ctrl &= ~(0xFF <<
-				SDHCI_VNDR_CLK_CTRL_TAP_VALUE_SHIFT);
-				vendor_ctrl |= (plat->tap_delay <<
-				SDHCI_VNDR_CLK_CTRL_TAP_VALUE_SHIFT);
+					SDHCI_VNDR_CLK_CTRL_TAP_VALUE_SHIFT);
+				vendor_ctrl |=
+					(tegra_host->tuning_data.best_tap_value
+					<< SDHCI_VNDR_CLK_CTRL_TAP_VALUE_SHIFT);
+			} else {
+				if (plat->tap_delay) {
+					vendor_ctrl &= ~(0xFF <<
+					SDHCI_VNDR_CLK_CTRL_TAP_VALUE_SHIFT);
+					vendor_ctrl |= (plat->tap_delay <<
+					SDHCI_VNDR_CLK_CTRL_TAP_VALUE_SHIFT);
+				}
 			}
 		}
 		if (soc_data->nvquirks & NVQUIRK_SET_TRIM_DELAY) {

InES-HPMM/linux-l4t

drivers/mmc/host/sdhci-tegra.c
@@ -522,11 +522,20 @@ static void tegra_sdhci_reset_exit(struct sdhci_host *host, u8 mask)
 			~SDHCI_VNDR_CLK_CTRL_INPUT_IO_CLK;
 	}
 	if (soc_data->nvquirks & NVQUIRK_SET_TAP_DELAY) {
-		if (plat->tap_delay) {
+		if ((tegra_host->tuning_status == TUNING_STATUS_DONE) &&
+			(host->mmc->pm_flags & MMC_PM_KEEP_POWER)) {
 			vendor_ctrl &= ~(0xFF <<
-			SDHCI_VNDR_CLK_CTRL_TAP_VALUE_SHIFT);
-			vendor_ctrl |= (plat->tap_delay <<
-			SDHCI_VNDR_CLK_CTRL_TAP_VALUE_SHIFT);
+				SDHCI_VNDR_CLK_CTRL_TAP_VALUE_SHIFT);
+			vendor_ctrl |=
+				(tegra_host->tuning_data.best_tap_value
+				<< SDHCI_VNDR_CLK_CTRL_TAP_VALUE_SHIFT);
+		} else {
+			if (plat->tap_delay) {
+				vendor_ctrl &= ~(0xFF <<
+				SDHCI_VNDR_CLK_CTRL_TAP_VALUE_SHIFT);
+				vendor_ctrl |= (plat->tap_delay <<
+				SDHCI_VNDR_CLK_CTRL_TAP_VALUE_SHIFT);
+			}
 		}
 	}
 	if (soc_data->nvquirks & NVQUIRK_SET_TRIM_DELAY) {