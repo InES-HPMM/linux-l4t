venkatarajasekhar/Wiko-Highway-4G-Kernel

ARM: tegra: init irq from device-tree
- enable the intc node
- hook irq init via device-tree for boards that support DT
- for non-DT boards, fallback to non-dt method
- deprecate tegra_init_irq and change tegra_dt_init_irq
to check if dtb is passed. If passed, use it to init irq
otherwise fall-back to non-dt path

bug 1164943

Change-Id: Idd87945df250c3cdef38226a9dbf2d6ffd34ce48
Signed-off-by: Mayuresh Kulkarni <mkulkarni@nvidia.com>
Reviewed-on: http://git-master/r/147496
Reviewed-by: Simone Willett <swillett@nvidia.com>
Tested-by: Simone Willett <swillett@nvidia.com>

arch/arm/mach-tegra/irq.c
@@ -22,6 +22,7 @@
 #include <linux/irq.h>
 #include <linux/io.h>
 #include <linux/of.h>
+#include <linux/of_irq.h>
 #include <linux/syscore_ops.h>
 
 #include <asm/hardware/gic.h>
@@ -249,10 +250,19 @@ subsys_initcall(tegra_legacy_irq_syscore_init);
 #define tegra_set_wake NULL
 #endif
 
-void __init tegra_init_irq(void)
+#ifdef CONFIG_OF
+static const struct of_device_id tegra_dt_irq_match[] __initconst = {
+	{ .compatible = "arm,cortex-a9-gic", .data = gic_of_init },
+	{ .compatible = "arm,cortex-a15-gic", .data = gic_of_init },
+	{ }
+};
+#endif
+
+void __init tegra_dt_init_irq(void)
 {
 	int i;
 	void __iomem *distbase;
+	bool is_dt = false;
 
 	distbase = IO_ADDRESS(TEGRA_ARM_INT_DIST_BASE);
 	num_ictlrs = readl_relaxed(distbase + GIC_DIST_CTR) & 0x1f;
@@ -279,7 +289,16 @@ void __init tegra_init_irq(void)
 	gic_arch_extn.irq_set_wake = tegra_set_wake;
 	gic_arch_extn.flags = IRQCHIP_MASK_ON_SUSPEND;
 
-	tegra_gic_init();
+	/* check if DT is passed */
+	is_dt = of_have_populated_dt();
+
+	tegra_gic_init(is_dt);
+
+#ifdef CONFIG_OF
+	/* If DT is passed, init the irq via DT */
+	if (is_dt)
+		of_irq_init(tegra_dt_irq_match);
+#endif
 }
 
 void tegra_init_legacy_irq_cop(void)

InES-HPMM/linux-l4t

arch/arm/mach-tegra/irq.c
@@ -23,7 +23,9 @@
 #include <linux/io.h>
 #include <linux/of.h>
 #include <linux/irqchip/arm-gic.h>
+#include <linux/irqchip.h>
 #include <linux/syscore_ops.h>
+#include <linux/clk/tegra.h>
 
 #include <mach/legacy_irq.h>
 
@@ -247,10 +249,13 @@ subsys_initcall(tegra_legacy_irq_syscore_init);
 #define tegra_set_wake NULL
 #endif
 
-void __init tegra_init_irq(void)
+void __init tegra_dt_init_irq(void)
 {
 	int i;
 	void __iomem *distbase;
+	bool is_dt = false;
+
+	tegra_clocks_init();
 
 	distbase = IO_ADDRESS(TEGRA_ARM_INT_DIST_BASE);
 	num_ictlrs = readl_relaxed(distbase + GIC_DIST_CTR) & 0x1f;
@@ -277,7 +282,16 @@ void __init tegra_init_irq(void)
 	gic_arch_extn.irq_set_wake = tegra_set_wake;
 	gic_arch_extn.flags = IRQCHIP_MASK_ON_SUSPEND;
 
-	tegra_gic_init();
+	/* check if DT is passed */
+	is_dt = of_have_populated_dt();
+
+	tegra_gic_init(is_dt);
+
+#ifdef CONFIG_OF
+	/* If DT is passed, init the irq via DT */
+	if (is_dt)
+		irqchip_init();
+#endif
 }
 
 void tegra_init_legacy_irq_cop(void)