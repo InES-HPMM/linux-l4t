danielhk/android_kernel_asus_tegra

ARM: tegra: pm: turn off VDEC partition in LP1
- turn off VDEC partition while going into LP1, if it is
still powered; and turn it on while coming back from LP1,
if it was powergated during entry
- this ensures that VDEC parition is available to any device
driver that needs it after LP1 resume
- the LP0 path is un-touched

bug 1248779

Change-Id: If6fd97ef21bbc8acb2c6f26ac7723638403d0623
Signed-off-by: Mayuresh Kulkarni <mkulkarni@nvidia.com>
Reviewed-on: http://git-master/r/216727
Reviewed-by: Daniel Solomon <daniels@nvidia.com>
Tested-by: Daniel Solomon <daniels@nvidia.com>
Reviewed-by: Automatic_Commit_Validation_User
GVS: Gerrit_Virtual_Submit
Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>

arch/arm/mach-tegra/pm.c
@@ -918,11 +918,20 @@ static void tegra_suspend_check_pwr_stats(void)
 	return;
 }
 
+static void tegra_suspend_powergate_control(int partid, bool turn_off)
+{
+	if (turn_off)
+		tegra_powergate_partition(partid);
+	else
+		tegra_unpowergate_partition(partid);
+}
+
 int tegra_suspend_dram(enum tegra_suspend_mode mode, unsigned int flags)
 {
 	int err = 0;
 	u32 scratch37 = 0xDEADBEEF;
 	u32 reg;
+	bool tegra_suspend_vde_powergated = false;
 
 	if (WARN_ON(mode <= TEGRA_SUSPEND_NONE ||
 		mode >= TEGRA_MAX_SUSPEND_MODE)) {
@@ -946,6 +955,15 @@ int tegra_suspend_dram(enum tegra_suspend_mode mode, unsigned int flags)
 
 	if ((mode == TEGRA_SUSPEND_LP0) || (mode == TEGRA_SUSPEND_LP1))
 		tegra_suspend_check_pwr_stats();
+
+	/* turn off VDE partition in LP1 */
+	if (mode == TEGRA_SUSPEND_LP1 &&
+		tegra_powergate_is_powered(TEGRA_POWERGATE_VDEC)) {
+		pr_warning("turning off partition %s in LP1\n",
+			tegra_powergate_get_name(TEGRA_POWERGATE_VDEC));
+		tegra_suspend_powergate_control(TEGRA_POWERGATE_VDEC, true);
+		tegra_suspend_vde_powergated = true;
+	}
 
 	tegra_common_suspend();
 
@@ -1057,6 +1075,13 @@ int tegra_suspend_dram(enum tegra_suspend_mode mode, unsigned int flags)
 	local_fiq_enable();
 
 	tegra_common_resume();
+
+	/* turn on VDE partition in LP1 */
+	if (mode == TEGRA_SUSPEND_LP1 && tegra_suspend_vde_powergated) {
+		pr_warning("turning on partition %s in LP1\n",
+			tegra_powergate_get_name(TEGRA_POWERGATE_VDEC));
+		tegra_suspend_powergate_control(TEGRA_POWERGATE_VDEC, false);
+	}
 
 fail:
 	return err;

InES-HPMM/linux-l4t

Reviewed-on: http://git-master/r/216099
(cherry picked from commit 8825199f91361961eadf95a977294bf6611310cc)
Reviewed-on: http://git-master/r/226242
Tested-by: Bharat Nihalani <bnihalani@nvidia.com>

arch/arm/mach-tegra/pm.c
@@ -1024,7 +1024,7 @@ static void tegra_suspend_check_pwr_stats(void)
 	for (partid = 0; partid < TEGRA_NUM_POWERGATE; partid++)
 		if ((1 << partid) & pwrgate_partid_mask)
 			if (tegra_powergate_is_powered(partid))
-				pr_warning("partition %s is left on before suspend\n",
+				pr_info("partition %s is left on before suspend\n",
 					tegra_powergate_get_name(partid));
 
 	return;
@@ -1058,14 +1058,24 @@ static void tegra_disable_lp1bb_interrupt(void)
 }
 #endif
 
+static void tegra_suspend_powergate_control(int partid, bool turn_off)
+{
+	if (turn_off)
+		tegra_powergate_partition(partid);
+	else
+		tegra_unpowergate_partition(partid);
+}
+
 int tegra_suspend_dram(enum tegra_suspend_mode mode, unsigned int flags)
 {
 	int err = 0;
 	u32 scratch37 = 0xDEADBEEF;
 	u32 reg;
+
 #if defined(CONFIG_ARCH_TEGRA_14x_SOC)
 	u32 enter_state = 0;
 #endif
+	bool tegra_suspend_vde_powergated = false;
 
 	if (WARN_ON(mode <= TEGRA_SUSPEND_NONE ||
 		mode >= TEGRA_MAX_SUSPEND_MODE)) {
@@ -1089,6 +1099,15 @@ int tegra_suspend_dram(enum tegra_suspend_mode mode, unsigned int flags)
 
 	if ((mode == TEGRA_SUSPEND_LP0) || (mode == TEGRA_SUSPEND_LP1))
 		tegra_suspend_check_pwr_stats();
+
+	/* turn off VDE partition in LP1 */
+	if (mode == TEGRA_SUSPEND_LP1 &&
+		tegra_powergate_is_powered(TEGRA_POWERGATE_VDEC)) {
+		pr_info("turning off partition %s in LP1\n",
+			tegra_powergate_get_name(TEGRA_POWERGATE_VDEC));
+		tegra_suspend_powergate_control(TEGRA_POWERGATE_VDEC, true);
+		tegra_suspend_vde_powergated = true;
+	}
 
 	tegra_common_suspend();
 
@@ -1229,6 +1248,13 @@ int tegra_suspend_dram(enum tegra_suspend_mode mode, unsigned int flags)
 	local_fiq_enable();
 
 	tegra_common_resume();
+
+	/* turn on VDE partition in LP1 */
+	if (mode == TEGRA_SUSPEND_LP1 && tegra_suspend_vde_powergated) {
+		pr_info("turning on partition %s in LP1\n",
+			tegra_powergate_get_name(TEGRA_POWERGATE_VDEC));
+		tegra_suspend_powergate_control(TEGRA_POWERGATE_VDEC, false);
+	}
 
 fail:
 	return err;