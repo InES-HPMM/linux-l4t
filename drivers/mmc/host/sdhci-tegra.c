Unlegacy-Android/android_kernel_nvidia_tegra3

mmc: tegra: Option to disable all UHS modes
Adding option to disable all the UHS modes - SDR12, SDR25, SDR50,
DDR50, SDR104, HS200.

Bug 1189241

Change-Id: I673cf5c819cb4c2ec0525f6e47b493ad1a4b7112
Signed-off-by: Pavan Kunapuli <pkunapuli@nvidia.com>
Reviewed-on: http://git-master/r/199931
Reviewed-by: Rama Kandhala <rkandhala@nvidia.com>

drivers/mmc/host/sdhci-tegra.c
@@ -505,6 +505,12 @@ static void tegra_sdhci_reset_exit(struct sdhci_host *sdhci, u8 mask)
 
 		if (plat->uhs_mask & MMC_UHS_MASK_SDR25)
 			sdhci->mmc->caps &= ~MMC_CAP_UHS_SDR25;
+
+		if (plat->uhs_mask & MMC_UHS_MASK_SDR12)
+			sdhci->mmc->caps &= ~MMC_CAP_UHS_SDR12;
+
+		if (plat->uhs_mask & MMC_MASK_HS200)
+			sdhci->mmc->caps2 &= ~MMC_CAP2_HS200;
 	}
 }
 

InES-HPMM/linux-l4t

(cherry picked from commit 26d32b09e4bca90b8b10da9b88f370fe451bab52)
Reviewed-on: http://git-master/r/212401
Reviewed-by: Riham Haidar <rhaidar@nvidia.com>
Tested-by: Riham Haidar <rhaidar@nvidia.com>

drivers/mmc/host/sdhci-tegra.c
@@ -554,6 +554,12 @@ static void tegra_sdhci_reset_exit(struct sdhci_host *host, u8 mask)
 
 	if (plat->uhs_mask & MMC_UHS_MASK_SDR25)
 		host->mmc->caps &= ~MMC_CAP_UHS_SDR25;
+
+	if (plat->uhs_mask & MMC_UHS_MASK_SDR12)
+		host->mmc->caps &= ~MMC_CAP_UHS_SDR12;
+
+	if (plat->uhs_mask & MMC_MASK_HS200)
+		host->mmc->caps2 &= ~MMC_CAP2_HS200;
 }
 
 static int tegra_sdhci_buswidth(struct sdhci_host *sdhci, int bus_width)