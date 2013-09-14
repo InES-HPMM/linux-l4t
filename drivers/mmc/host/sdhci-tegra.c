venkatarajasekhar/Wiko-Highway-4G-Kernel

mmc: tegra: HS200 mode disable
Kernel config MMC_SDHCI_TEGRA_HS200_DISABLE introduced
to disable default HS200 mode on EMMC devices.

bug 1330807

Change-Id: I4237c159c642a81b859c6559f9fbdac7c3593ed1
Signed-off-by: Bitan Biswas <bbiswas@nvidia.com>
Reviewed-on: http://git-master/r/254480
Reviewed-by: Riham Haidar <rhaidar@nvidia.com>
Tested-by: Riham Haidar <rhaidar@nvidia.com>

drivers/mmc/host/sdhci-tegra.c
@@ -1014,8 +1014,12 @@ static void tegra_sdhci_reset_exit(struct sdhci_host *sdhci, u8 mask)
 		if (plat->uhs_mask & MMC_UHS_MASK_SDR12)
 			sdhci->mmc->caps &= ~MMC_CAP_UHS_SDR12;
 
+#ifdef CONFIG_MMC_SDHCI_TEGRA_HS200_DISABLE
+		sdhci->mmc->caps2 &= ~MMC_CAP2_HS200;
+#else
 		if (plat->uhs_mask & MMC_MASK_HS200)
 			sdhci->mmc->caps2 &= ~MMC_CAP2_HS200;
+#endif
 	}
 }
 

InES-HPMM/linux-l4t

(cherry picked from commit 82a7c2b88e765857a65840050f5ae0d4327f4055)
Reviewed-on: http://git-master/r/256256

drivers/mmc/host/sdhci-tegra.c
@@ -952,8 +952,12 @@ static void tegra_sdhci_reset_exit(struct sdhci_host *host, u8 mask)
 	if (plat->uhs_mask & MMC_UHS_MASK_SDR12)
 		host->mmc->caps &= ~MMC_CAP_UHS_SDR12;
 
+#ifdef CONFIG_MMC_SDHCI_TEGRA_HS200_DISABLE
+	host->mmc->caps2 &= ~MMC_CAP2_HS200;
+#else
 	if (plat->uhs_mask & MMC_MASK_HS200)
 		host->mmc->caps2 &= ~MMC_CAP2_HS200;
+#endif
 }
 
 static int tegra_sdhci_buswidth(struct sdhci_host *sdhci, int bus_width)