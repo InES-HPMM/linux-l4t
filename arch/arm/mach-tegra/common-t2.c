CyanogenMod/lge-kernel-star

ARM: tegra: Move tegra_mc_init to arch_initcall
It's current spot, in tegra_early_init, was too early. It was being
called before we could allocate memory.

arch/arm/mach-tegra/common-t2.c
@@ -189,3 +189,4 @@ void __init tegra_mc_init(void)
 		writel(reg, mc + MC_INT_MASK);
 	}
 }
+arch_initcall(tegra_mc_init);

arch/arm/mach-tegra/common-t3.c
@@ -206,3 +206,4 @@ void __init tegra_mc_init(void)
 		writel(reg, mc + MC_INT_MASK);
 	}
 }
+arch_initcall(tegra_mc_init);

InES-HPMM/linux-l4t

Rebase-Id: Rff026504107e75b33dccf714c5219d78c0d1dac9

arch/arm/mach-tegra/common-t2.c
@@ -177,7 +177,7 @@ out:
 	return IRQ_HANDLED;
 }
 
-void __init tegra20_mc_init(void)
+static void __init tegra20_mc_init(void)
 {
 	if (request_irq(INT_MC_GENERAL, tegra_mc_error_isr, 0,
 			"mc_status", NULL)) {
@@ -189,3 +189,4 @@ void __init tegra20_mc_init(void)
 		writel(reg, mc + MC_INT_MASK);
 	}
 }
+arch_initcall(tegra20_mc_init);

arch/arm/mach-tegra/common-t3.c
@@ -185,7 +185,7 @@ out:
 	return IRQ_HANDLED;
 }
 
-void __init tegra30_mc_init(void)
+static void __init tegra30_mc_init(void)
 {
 	void __iomem *mc = IO_ADDRESS(TEGRA_MC_BASE);
 	u32 reg;
@@ -206,3 +206,4 @@ void __init tegra30_mc_init(void)
 		writel(reg, mc + MC_INT_MASK);
 	}
 }
+arch_initcall(tegra30_mc_init);