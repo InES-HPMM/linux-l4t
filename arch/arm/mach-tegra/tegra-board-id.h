cebcf760855899de563d3b495089ab0b149ceab6
Mitch Luban <mluban@nvidia.com>
2013-09-19 12:02:41 -0700

    arm: tegra: T124 support for LGD 7" panel
    Bug 1364398
    
    Change-Id: Ic78dc2be089675a642cb129a0dccb2b7e5b517c2
    Signed-off-by: Mitch Luban <mluban@nvidia.com>
    Reviewed-on: http://git-master/r/276791
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Seema Khowala <seemaj@nvidia.com>

--- arch/arm/mach-tegra/tegra-board-id.h
+++ arch/arm/mach-tegra/tegra-board-id.h
@@ -60,6 +60,7 @@
 #define    BOARD_E1813    0x0715
 #define    BOARD_E1824    0x0720
 #define    BOARD_PM366    0x016e
+#define    BOARD_E1549    0x060D
 
 #define    BOARD_E1563    0x061b
 #define BOARD_P2560   0x0A00
