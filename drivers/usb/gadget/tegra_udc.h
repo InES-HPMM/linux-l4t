b0be3cdc61bc17398d2363d484366053ea097bcb
Rakesh Bodla <rbodla@nvidia.com>
2013-09-24 11:42:44 +0530

    usb: gadget: tegra: Reduce CPU boost trigger size
    Decrease the CPU frequency boosting trigger size.
    
    Bug 1343869
    
    Change-Id: Ice4584196c053f06f58ca662dd7a4f67a7c57ab3
    Signed-off-by: Rakesh Bodla <rbodla@nvidia.com>
    Reviewed-on: http://git-master/r/278169
    (cherry picked from commit cdd87642793e0eb42a4119ec21bfe78a8d75af99)
    Reviewed-on: http://git-master/r/279763
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Venkat Moganty <vmoganty@nvidia.com>
    (cherry picked from commit 7ce46eb9866a3a12a6244244a08d4e7706faf86e)
    Signed-off-by: Ajay Nandakumar <anandakumarm@nvidia.com>

--- drivers/usb/gadget/tegra_udc.h
+++ drivers/usb/gadget/tegra_udc.h
@@ -46,7 +46,7 @@
 
  /* 1 sec wait time for non-std charger detection after vbus is detected */
 #define NON_STD_CHARGER_DET_TIME_MS 1000
-#define BOOST_TRIGGER_SIZE 4096
+#define BOOST_TRIGGER_SIZE 20
 
 #define UDC_RESET_TIMEOUT_MS 1000
 #define UDC_RUN_TIMEOUT_MS 1000
