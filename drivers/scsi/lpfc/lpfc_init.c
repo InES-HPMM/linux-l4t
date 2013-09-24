965a60606bdeb7512f64737b5c89e4e3722578b8
Jingoo Han <jg1.han@samsung.com>
2013-09-24 10:16:20 +0900

    SCSI: remove unnecessary pci_set_drvdata()
    Since commit 0998d0631001288a5974afc0b2a5f568bcdecb4d
    (device-core: Ensure drvdata = NULL when no driver is bound),
    the driver core clears the driver data to NULL after device_release
    or on probe failure. Thus, it is not needed to manually clear the
    device driver data to NULL.
    
    Signed-off-by: Jingoo Han <jg1.han@samsung.com>
    Cc: James Bottomley <JBottomley@parallels.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>
    (cherry picked from commit 08b7e10716a518af01b07915dbb1938868bbf878)
    
    Bug 200071965
    
    Change-Id: I8fe8423f083eba124f8be0b9945a6fe02ef9db77
    Signed-off-by: Naveen Kumar Arepalli <naveenk@nvidia.com>
    Reviewed-on: http://git-master/r/678343
    Reviewed-by: Bitan Biswas <bbiswas@nvidia.com>

--- drivers/scsi/lpfc/lpfc_init.c
+++ drivers/scsi/lpfc/lpfc_init.c
@@ -4595,8 +4595,6 @@ lpfc_disable_pci_dev(struct lpfc_hba *phba)
 	/* Release PCI resource and disable PCI device */
 	pci_release_selected_regions(pdev, bars);
 	pci_disable_device(pdev);
-	/* Null out PCI private reference to driver */
-	pci_set_drvdata(pdev, NULL);
 
 	return;
 }
@@ -9427,7 +9425,6 @@ lpfc_pci_remove_one_s3(struct pci_dev *pdev)
 	/* Disable interrupt */
 	lpfc_sli_disable_intr(phba);
 
-	pci_set_drvdata(pdev, NULL);
 	scsi_host_put(shost);
 
 	/*
--- drivers/scsi/pmcraid.c
+++ drivers/scsi/pmcraid.c
@@ -6050,7 +6050,6 @@ out_release_regions:
 
 out_disable_device:
 	atomic_dec(&pmcraid_adapter_count);
-	pci_set_drvdata(pdev, NULL);
 	pci_disable_device(pdev);
 	return -ENODEV;
 }
