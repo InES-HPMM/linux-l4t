a1198f6aab0fe59b76ff2828e0015e38f554e485
Adeel Raza <araza@nvidia.com>
2013-09-24 12:57:25 -0700

    ARM: tegra: always enable RAM repair
    Always enable RAM repair (irrespective of fuse bits) for non-Tegra11
    platforms.
    
    Bug 1366617
    
    Change-Id: I548934ca6fbe9e89b27cf99f8929b261fdbddb89
    Signed-off-by: Adeel Raza <araza@nvidia.com>
    Reviewed-on: http://git-master/r/335231
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Seshendra Gadagottu <sgadagottu@nvidia.com>
    Reviewed-by: Bo Yan <byan@nvidia.com>

--- arch/arm/mach-tegra/common.c
+++ arch/arm/mach-tegra/common.c
@@ -733,12 +733,16 @@ static void __init tegra_perf_init(void)
 #if !defined(CONFIG_ARCH_TEGRA_2x_SOC) && !defined(CONFIG_ARCH_TEGRA_3x_SOC)
 static void __init tegra_ramrepair_init(void)
 {
+#if defined(CONFIG_ARCH_TEGRA_11x_SOC)
 	if (tegra_spare_fuse(10)  | tegra_spare_fuse(11)) {
+#endif
 		u32 reg;
 		reg = readl(FLOW_CTRL_RAM_REPAIR);
 		reg &= ~FLOW_CTRL_RAM_REPAIR_BYPASS_EN;
 		writel(reg, FLOW_CTRL_RAM_REPAIR);
+#if defined(CONFIG_ARCH_TEGRA_11x_SOC)
 	}
+#endif
 }
 #endif
 
