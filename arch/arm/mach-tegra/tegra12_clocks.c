41fdd1057123f7bd3ba0867f558c7996eb4fb7cd
Kaz Fukuoka <kfukuoka@nvidia.com>
2013-09-26 16:49:49 -0700

    ARM: tegra12: clock: Update PLLD/U cpcon settings
    Using old values until PROD values are ready for Tegra12.
    
    Ported from Tegra11 Change-Id: I5b24b0375f8e756d44cd64220ac4655bc0624d2d
    
    Bug 1339555
    
    Change-Id: I104929d06b12833b8bb4378aba3c94661f91ed08
    Signed-off-by: Kaz Fukuoka <kfukuoka@nvidia.com>
    Reviewed-on: http://git-master/r/289947
    Reviewed-by: Krishna Sitaraman <ksitaraman@nvidia.com>
    Tested-by: Krishna Sitaraman <ksitaraman@nvidia.com>
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Chao Xu <cxu@nvidia.com>

--- arch/arm/mach-tegra/tegra12_clocks.c
+++ arch/arm/mach-tegra/tegra12_clocks.c
@@ -2300,6 +2300,21 @@ unsigned long get_pll_cfreq_common(struct clk *c, unsigned long input_rate,
 	return cfreq;
 }
 
+static u8 get_pll_cpcon(struct clk *c, u16 n)
+{
+	if (c->flags & PLLD) {
+		if (n >= 600)
+			return 12;
+		else if (n >= 300)
+			return 8;
+		else if (n >= 50)
+			return 3;
+		else
+			return 2;
+	}
+	return c->u.pll.cpcon_default ? : OUT_OF_TABLE_CPCON;
+}
+
 static int tegra12_pll_clk_set_rate(struct clk *c, unsigned long rate)
 {
 	u32 val, p_div, old_base;
@@ -2358,7 +2373,7 @@ static int tegra12_pll_clk_set_rate(struct clk *c, unsigned long rate)
 		cfg.p = 0x1 << p_div;
 		cfg.m = input_rate / cfreq;
 		cfg.n = cfg.output_rate / cfreq;
-		cfg.cpcon = c->u.pll.cpcon_default ? : OUT_OF_TABLE_CPCON;
+		cfg.cpcon = get_pll_cpcon(c, cfg.n);
 
 		if ((cfg.m > (PLL_BASE_DIVM_MASK >> PLL_BASE_DIVM_SHIFT)) ||
 		    (cfg.n > (PLL_BASE_DIVN_MASK >> PLL_BASE_DIVN_SHIFT)) ||
@@ -6427,7 +6442,6 @@ static struct clk tegra_pll_d = {
 		.vco_max   = 1500000000,
 		.freq_table = tegra_pll_d_freq_table,
 		.lock_delay = 1000,
-		.cpcon_default = 8,
 	},
 };
 
@@ -6464,7 +6478,6 @@ static struct clk tegra_pll_u = {
 		.vco_max   = 960000000,
 		.freq_table = tegra_pll_u_freq_table,
 		.lock_delay = 1000,
-		.cpcon_default = 12,
 	},
 };
 
