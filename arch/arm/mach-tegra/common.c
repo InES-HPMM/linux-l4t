f5531e05f88f4db09bf7d89d27a8cd43218628d4
Xue Dong <xdong@nvidia.com>
2013-09-26 18:05:52 -0700

    arm: tegra: hard code pllc to 600Mhz
    Change-Id: If15da4065e9851e9489c87fad50d82f13f7c0215
    Signed-off-by: Xue Dong <xdong@nvidia.com>
    Reviewed-on: http://git-master/r/299044
    Reviewed-by: Mitch Luban <mluban@nvidia.com>
    Tested-by: Mitch Luban <mluban@nvidia.com>

--- arch/arm/mach-tegra/common.c
+++ arch/arm/mach-tegra/common.c
@@ -458,7 +458,7 @@ static __initdata struct tegra_clk_init_table tegra12x_cbus_init_table[] = {
 #ifdef CONFIG_TEGRA_DUAL_CBUS
 	{ "c2bus",	"pll_c2",	250000000,	false },
 	{ "c3bus",	"pll_c3",	250000000,	false },
-	{ "pll_c",	NULL,		792000000,	false },
+	{ "pll_c",	NULL,		600000000,	false },
 #else
 	{ "cbus",	"pll_c",	200000000,	false },
 #endif
