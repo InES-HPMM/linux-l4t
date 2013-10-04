62321b9b219745f4054652e0673e80a5e041fba5
Bharat Nihalani <bnihalani@nvidia.com>
2013-10-04 17:59:23 +0530

    Revert "arm: tegra: poll CLAMP_STATUS"
    This reverts commit 8c73a49068b7cde77d9e466f00a30fa1280c9c26 since
    it causes issues with power-gating GPU
    
    Bug 1382374
    
    Change-Id: Ic8fd1483f88d66b22548d559b86043807bc6315b
    Signed-off-by: Bharat Nihalani <bnihalani@nvidia.com>
    Reviewed-on: http://git-master/r/281769
    Reviewed-by: Automatic_Commit_Validation_User

--- arch/arm/mach-tegra/powergate.c
+++ arch/arm/mach-tegra/powergate.c
@@ -362,7 +362,7 @@ int tegra_powergate_remove_clamping(int id)
 		udelay(1);
 		contention_timeout--;
 	} while ((contention_timeout > 0)
-			&& (pmc_read(PWRGATE_CLAMP_STATUS) & mask));
+			&& (pmc_read(REMOVE_CLAMPING) & mask));
 
 	WARN(contention_timeout <= 0, "Couldn't remove clamping");
 
