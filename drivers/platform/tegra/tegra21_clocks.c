3e8cd6c9a17c034de6bdcbeaa80ebdf0552c7597
Bo Yan <byan@nvidia.com>
2013-10-11 13:02:19 -0700

    platform: tegra: Round t210 emc rate up/down
    This follows the footsteps of other chips to support
    bidirectional up/down rounding of emc rate
    
    Change-Id: I97a54dce7c83926fd0a1f88abf34641345e3952a
    Signed-off-by: Bo Yan <byan@nvidia.com>

--- drivers/platform/tegra/tegra21_clocks.c
+++ drivers/platform/tegra/tegra21_clocks.c
@@ -4653,7 +4653,7 @@ static long tegra21_emc_clk_round_updown(struct clk *c, unsigned long rate,
 	long new_rate = max(rate, c->min_rate);
 
 	/* FIXME: emc updown rounding */
-	new_rate = tegra_emc_round_rate(new_rate);
+	new_rate = tegra_emc_round_rate_updown(new_rate, up);
 	if (new_rate < 0)
 		new_rate = c->max_rate;
 
