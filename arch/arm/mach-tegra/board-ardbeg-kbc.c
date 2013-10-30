13fa7268df21d651c19f01552afc7ab05b7a10f0
Laxman Dewangan <ldewangan@nvidia.com>
2013-10-30 17:25:31 +0530

    ARM: tegra: ardbeg: cleanup on kbc initialisation
    Remove unused code and rearrange function as part of
    cleaning up ardbeg key initialisation code.
    
    Change-Id: Id2055da1f013eb1e13666890244d1ee75f8a654d
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Reviewed-on: http://git-master/r/309603
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit

--- arch/arm/mach-tegra/board-ardbeg-kbc.c
+++ arch/arm/mach-tegra/board-ardbeg-kbc.c
@@ -23,18 +23,12 @@
 #include <linux/platform_device.h>
 #include <linux/input.h>
 #include <linux/io.h>
-#include <linux/input/tegra_kbc.h>
 #include <linux/gpio.h>
 #include <linux/gpio_keys.h>
-#include <linux/mfd/palmas.h>
 
-#include "tegra-board-id.h"
-#include "board.h"
-#include "board-ardbeg.h"
-#include "devices.h"
 #include "iomap.h"
 #include "wakeups-t12x.h"
-
+#include "gpio-names.h"
 
 #define GPIO_KEY(_id, _gpio, _iswake)           \
 	{                                       \
@@ -47,46 +41,8 @@
 		.debounce_interval = 10,        \
 	}
 
-#define GPIO_IKEY(_id, _irq, _iswake, _deb)	\
-	{					\
-		.code = _id,			\
-		.gpio = -1,			\
-		.irq = _irq,			\
-		.desc = #_id,			\
-		.type = EV_KEY,			\
-		.wakeup = _iswake,		\
-		.debounce_interval = _deb,	\
-	}
-
 #define PMC_WAKE2_STATUS         0x168
 #define TEGRA_WAKE_PWR_INT      (1UL << 19)
-
-static int ardbeg_wakeup_key(void);
-
-static struct gpio_keys_button ardbeg_int_keys[] = {
-	[0] = GPIO_IKEY(KEY_POWER, 0, 1, 10),
-	[1] = GPIO_KEY(KEY_VOLUMEUP, PQ6, 0),
-	[2] = GPIO_KEY(KEY_VOLUMEDOWN, PQ7, 0),
-	[3] = GPIO_KEY(KEY_HOME, PI5, 0),
-	[4] = GPIO_KEY(KEY_CAMERA_FOCUS, PQ2, 0),
-#if 0
-	[5] = GPIO_KEY(KEY_CAMERA, PV3, 0),
-#endif
-};
-
-static struct gpio_keys_platform_data ardbeg_int_keys_pdata = {
-	.buttons	= ardbeg_int_keys,
-	.nbuttons	= ARRAY_SIZE(ardbeg_int_keys),
-	.wakeup_key	= ardbeg_wakeup_key,
-};
-
-static struct platform_device ardbeg_int_keys_device = {
-	.name	= "gpio-keys",
-	.id	= 0,
-	.dev	= {
-		.platform_data  = &ardbeg_int_keys_pdata,
-	},
-};
 
 static int ardbeg_wakeup_key(void)
 {
@@ -103,22 +59,33 @@ static int ardbeg_wakeup_key(void)
 	return KEY_RESERVED;
 }
 
+static struct gpio_keys_button ardbeg_int_keys[] = {
+	[0] = GPIO_KEY(KEY_POWER, PQ0, 1),
+	[1] = GPIO_KEY(KEY_VOLUMEUP, PQ6, 0),
+	[2] = GPIO_KEY(KEY_VOLUMEDOWN, PQ7, 0),
+	[3] = GPIO_KEY(KEY_HOME, PI5, 0),
+	[4] = GPIO_KEY(KEY_CAMERA_FOCUS, PQ2, 0),
+};
+
+static struct gpio_keys_platform_data ardbeg_int_keys_pdata = {
+	.buttons	= ardbeg_int_keys,
+	.nbuttons	= ARRAY_SIZE(ardbeg_int_keys),
+	.wakeup_key	= ardbeg_wakeup_key,
+};
+
+static struct platform_device ardbeg_int_keys_device = {
+	.name	= "gpio-keys",
+	.id	= 0,
+	.dev	= {
+		.platform_data  = &ardbeg_int_keys_pdata,
+	},
+};
+
 int __init ardbeg_kbc_init(void)
 {
-	struct board_info board_info;
-	int ret;
-
-	tegra_get_board_info(&board_info);
-	pr_info("Boardid:SKU = 0x%04x:0x%04x\n",
-			board_info.board_id, board_info.sku);
-
 	/* GPIO-Keys from DT for TN8 */
 	if (of_machine_is_compatible("nvidia,tn8"))
 		return 0;
 
-	ardbeg_int_keys[0].gpio = TEGRA_GPIO_PQ0;
-	ardbeg_int_keys[0].active_low = 1;
-
-	ret = platform_device_register(&ardbeg_int_keys_device);
-	return ret;
+	return platform_device_register(&ardbeg_int_keys_device);
 }
