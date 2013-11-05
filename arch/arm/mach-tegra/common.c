01f5fb8bbeaa13e95d5709305f0454b1bbdf25ec
Alex Frid <afrid@nvidia.com>
2013-11-04 21:06:30 -0800

    video: tegra: host: Lower initial GPU rate
    Set initial GPCPLL rate to 504MHz - half of minimum VCO rate.
    Respectively initialize GPU bus rate to half of GPCPLL rate - 252MHz
    (was - maximum GPU rate supported).
    
    These changes made sure that initial GPU voltage is low enough for
    tegra SiMon to evaluate GPU state on boot even on the slowest part.
    
    Bug 1343366
    
    Change-Id: I972ea504e36550a304c4c70d9c5d56f3c00286cb
    Signed-off-by: Alex Frid <afrid@nvidia.com>
    Reviewed-on: http://git-master/r/326389
    Reviewed-by: Yu-Huan Hsu <yhsu@nvidia.com>

--- arch/arm/mach-tegra/common.c
+++ arch/arm/mach-tegra/common.c
@@ -458,6 +458,7 @@ static __initdata struct tegra_clk_init_table tegra12x_clk_init_table[] = {
 	{ "sbc6.sclk",	NULL,		40000000,	false},
 	{ "cpu.mselect", NULL,		102000000,	true},
 	{ "gpu_ref",	NULL,		0,		true},
+	{ "gk20a.gbus",	NULL,		252000000,	false},
 #ifdef CONFIG_TEGRA_PLLM_SCALED
 	{ "vi",		"pll_p",	0,		false},
 	{ "isp",	"pll_p",	0,		false},
