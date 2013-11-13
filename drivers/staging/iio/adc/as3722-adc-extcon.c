ce66a06161255a71dcd34f2d91902ed859e147b3
Laxman Dewangan <ldewangan@nvidia.com>
2013-11-13 18:34:39 +0530

    staging: iio: adc: as3722: use new API for register access
    use as3722_read(), as3722_write() and as3722_update_bits() for
    register access in place of legacy API.
    
    Also get rid of inclusion of as3722-reg.h and as3722-plat.h header and
    include only as3772.h as this header contain all information which is
    required by driver.
    
    Change-Id: If733c17e3b5569568549151a2229fafb3c4bed1d
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Reviewed-on: http://git-master/r/330342

--- drivers/staging/iio/adc/as3722-adc-extcon.c
+++ drivers/staging/iio/adc/as3722-adc-extcon.c
@@ -28,8 +28,7 @@
 #include <linux/delay.h>
 #include <linux/pm.h>
 #include <linux/extcon.h>
-#include <linux/mfd/as3722-reg.h>
-#include <linux/mfd/as3722-plat.h>
+#include <linux/mfd/as3722.h>
 
 struct as3722_adc {
 	struct device		*dev;
@@ -52,14 +51,14 @@ static int as3722_read_adc1_cable_update(struct as3722_adc *adc)
 	int ret;
 	u32 val;
 
-	ret = as3722_reg_read(as3722, AS3722_ADC1_MSB_RESULT_REG , &val);
+	ret = as3722_read(as3722, AS3722_ADC1_MSB_RESULT_REG , &val);
 	if (ret < 0) {
 		dev_err(adc->dev, "ADC1_MSB_RESULT read failed %d\n", ret);
 		return ret;
 	}
 	result = ((val & AS3722_ADC_MASK_MSB_VAL) << 3);
 
-	ret = as3722_reg_read(as3722, AS3722_ADC1_LSB_RESULT_REG, &val);
+	ret = as3722_read(as3722, AS3722_ADC1_LSB_RESULT_REG, &val);
 	if (ret < 0) {
 		dev_err(adc->dev, "ADC1_LSB_RESULT read failed %d\n", ret);
 		return ret;
@@ -117,7 +116,7 @@ static int as3722_adc_extcon_probe(struct platform_device *pdev)
 		goto skip_adc_config;
 
 	/* Set ADC threshold values */
-	ret = as3722_reg_write(as3722, AS3722_ADC1_THRESHOLD_HI_MSB_REG,
+	ret = as3722_write(as3722, AS3722_ADC1_THRESHOLD_HI_MSB_REG,
 				(extcon_pdata->hi_threshold >> 3) & 0x7F);
 	if (ret < 0) {
 		dev_err(adc->dev, "ADC1_THRESHOLD_HI_MSB write failed %d\n",
@@ -125,7 +124,7 @@ static int as3722_adc_extcon_probe(struct platform_device *pdev)
 		return ret;
 	}
 
-	ret = as3722_reg_write(as3722, AS3722_ADC1_THRESHOLD_HI_LSB_REG,
+	ret = as3722_write(as3722, AS3722_ADC1_THRESHOLD_HI_LSB_REG,
 				(extcon_pdata->hi_threshold) & 0x7);
 	if (ret < 0) {
 		dev_err(adc->dev, "ADC1_THRESHOLD_HI_LSB write failed %d\n",
@@ -133,7 +132,7 @@ static int as3722_adc_extcon_probe(struct platform_device *pdev)
 		return ret;
 	}
 
-	ret = as3722_reg_write(as3722, AS3722_ADC1_THRESHOLD_LO_MSB_REG,
+	ret = as3722_write(as3722, AS3722_ADC1_THRESHOLD_LO_MSB_REG,
 				(extcon_pdata->low_threshold >> 3) & 0x7F);
 	if (ret < 0) {
 		dev_err(adc->dev, "ADC1_THRESHOLD_LO_MSB write failed %d\n",
@@ -141,7 +140,7 @@ static int as3722_adc_extcon_probe(struct platform_device *pdev)
 		return ret;
 	}
 
-	ret = as3722_reg_write(as3722, AS3722_ADC1_THRESHOLD_LO_LSB_REG,
+	ret = as3722_write(as3722, AS3722_ADC1_THRESHOLD_LO_LSB_REG,
 				(extcon_pdata->low_threshold) & 0x7);
 	if (ret < 0) {
 		dev_err(adc->dev, "ADC1_THRESHOLD_LO_LSB write failed %d\n",
@@ -154,14 +153,14 @@ static int as3722_adc_extcon_probe(struct platform_device *pdev)
 			AS3722_ADC1_INTEVAL_SCAN_MASK;
 	if (extcon_pdata->enable_low_voltage_range)
 		val |= AS3722_ADC1_LOW_VOLTAGE_RANGE_MASK;
-	ret = as3722_reg_write(as3722, AS3722_ADC1_CONTROL_REG, val);
+	ret = as3722_write(as3722, AS3722_ADC1_CONTROL_REG, val);
 	if (ret < 0) {
 		dev_err(adc->dev, "ADC1_CONTROL write failed %d\n", ret);
 		return ret;
 	}
 
 	/* Start ADC */
-	ret = as3722_set_bits(as3722, AS3722_ADC1_CONTROL_REG,
+	ret = as3722_update_bits(as3722, AS3722_ADC1_CONTROL_REG,
 				AS3722_ADC1_CONVERSION_START_MASK,
 				AS3722_ADC1_CONVERSION_START_MASK);
 	if (ret < 0) {
@@ -171,7 +170,7 @@ static int as3722_adc_extcon_probe(struct platform_device *pdev)
 
 	/* Wait for 1 conversion */
 	do {
-		ret = as3722_reg_read(as3722, AS3722_ADC1_MSB_RESULT_REG ,
+		ret = as3722_read(as3722, AS3722_ADC1_MSB_RESULT_REG ,
 				&val);
 		if (ret < 0) {
 			dev_err(adc->dev, "ADC1_MSB_RESULT read failed %d\n",
@@ -199,7 +198,7 @@ static int as3722_adc_extcon_probe(struct platform_device *pdev)
 		goto scrub_edev;
 	}
 
-	ret = as3722_set_bits(as3722, AS3722_ADC1_CONTROL_REG,
+	ret = as3722_update_bits(as3722, AS3722_ADC1_CONTROL_REG,
 		AS3722_ADC1_INTEVAL_SCAN_MASK, AS3722_ADC1_INTEVAL_SCAN_MASK);
 	if (ret < 0) {
 		dev_err(adc->dev, "ADC1 INTEVAL_SCAN set failed: %d\n", ret);
@@ -219,7 +218,7 @@ skip_adc_config:
 	return 0;
 
 stop_adc1:
-	as3722_set_bits(as3722, AS3722_ADC1_CONTROL_REG,
+	as3722_update_bits(as3722, AS3722_ADC1_CONTROL_REG,
 			AS3722_ADC1_CONVERSION_START_MASK, 0);
 scrub_edev:
 	extcon_dev_unregister(&adc->edev);
@@ -232,7 +231,7 @@ static int as3722_adc_extcon_remove(struct platform_device *pdev)
 	struct as3722_adc *adc = dev_get_drvdata(&pdev->dev);
 	struct as3722 *as3722 = adc->as3722;
 
-	as3722_set_bits(as3722, AS3722_ADC1_CONTROL_REG,
+	as3722_update_bits(as3722, AS3722_ADC1_CONTROL_REG,
 			AS3722_ADC1_CONVERSION_START_MASK, 0);
 	extcon_dev_unregister(&adc->edev);
 	free_irq(adc->irq, adc);
