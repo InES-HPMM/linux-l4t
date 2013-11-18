f24d81b3752118a9f17396b09b1f4e9495d21a76
Laxman Dewangan <ldewangan@nvidia.com>
2013-11-18 17:15:11 +0530

    regulator: core: set regualtor min/max if constraints have min=max voltage
    If regualtor device has the constraints for min and max voltage equal then
    set the regulator client's min/max to these value.
    
    bug 1399395
    
    Change-Id: Ie3cf60edced431111c30fe59f7657f42bff6db56
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Reviewed-on: http://git-master/r/332395

--- drivers/regulator/core.c
+++ drivers/regulator/core.c
@@ -1383,6 +1383,12 @@ static struct regulator *create_regulator(struct regulator_dev *rdev,
 				   &regulator->max_uV);
 	}
 
+	if (rdev->constraints->max_uV &&
+		(rdev->constraints->max_uV == rdev->constraints->min_uV)) {
+		regulator->min_uV = rdev->constraints->min_uV;
+		regulator->max_uV = rdev->constraints->max_uV;
+	}
+
 	/*
 	 * Check now if the regulator is an always on regulator - if
 	 * it is then we don't need to do nearly so much work for
