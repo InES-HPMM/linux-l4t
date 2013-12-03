631a3621935c1f212654700e147660486daa8b69
Antti P Miettinen <amiettinen@nvidia.com>
2013-12-03 11:57:42 +0200

    arm64: add flushing to LoUIS
    For power gating a CPU, the cache in the partition to be
    power gated needs to be cleaned. This level most probably
    is the same as level of unification inner shareable.
    
    Change-Id: I9798df5ad17121f8cf4141ff18884e98ed6bd744
    Reviewed-on: http://git-master/r/337851
    Reviewed-by: Juha Tukkinen <jtukkinen@nvidia.com>
    Tested-by: Juha Tukkinen <jtukkinen@nvidia.com>
    Signed-off-by: Antti P Miettinen <amiettinen@nvidia.com>

--- arch/arm64/include/asm/cacheflush.h
+++ arch/arm64/include/asm/cacheflush.h
@@ -70,6 +70,7 @@
  *		- size   - region size
  */
 extern void flush_cache_all(void);
+extern void flush_dcache_louis(void);
 extern void flush_cache_range(struct vm_area_struct *vma, unsigned long start, unsigned long end);
 extern void flush_icache_range(unsigned long start, unsigned long end);
 extern void __flush_dcache_area(void *addr, size_t len);
