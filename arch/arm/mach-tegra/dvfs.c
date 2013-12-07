15747edd53c8637838d77e4ed1b3f055178a564a
Alex Frid <afrid@nvidia.com>
2013-12-06 16:21:42 -0800

    ARM: tegra: dvfs: Export core rail voltage accessors
    Bug 1236550
    Bug 1462358
    
    Change-Id: Icf5fae188f54bb632e05e12702f399e212d6028a
    Signed-off-by: Alex Frid <afrid@nvidia.com>
    Signed-off-by: R Raj Kumar <rrajk@nvidia.com>
    Reviewed-on: http://git-master/r/367150
    Reviewed-by: Naveen Kumar Arepalli <naveenk@nvidia.com>
    Reviewed-by: Pavan Kunapuli <pkunapuli@nvidia.com>
    Reviewed-by: Venu Byravarasu <vbyravarasu@nvidia.com>

--- arch/arm/mach-tegra/dvfs.c
+++ arch/arm/mach-tegra/dvfs.c
@@ -63,6 +63,24 @@ static inline int tegra_dvfs_rail_get_suspend_level(struct dvfs_rail *rail)
 	return rail->suspend_millivolts ? : rail->nominal_millivolts;
 }
 
+int tegra_dvfs_get_core_override_floor(void)
+{
+	return tegra_dvfs_rail_get_override_floor(tegra_core_rail);
+}
+EXPORT_SYMBOL(tegra_dvfs_get_core_override_floor);
+
+int tegra_dvfs_get_core_nominal_millivolts(void)
+{
+	return tegra_dvfs_rail_get_nominal_millivolts(tegra_core_rail);
+}
+EXPORT_SYMBOL(tegra_dvfs_get_core_nominal_millivolts);
+
+int tegra_dvfs_get_core_boot_level(void)
+{
+	return tegra_dvfs_rail_get_boot_level(tegra_core_rail);
+}
+EXPORT_SYMBOL(tegra_dvfs_get_core_boot_level);
+
 void tegra_dvfs_add_relationships(struct dvfs_relationship *rels, int n)
 {
 	int i;
