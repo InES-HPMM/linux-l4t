837d128ac0e3f410010995fd5ca0aa2d22b4a1e7
Kyeong Kim <kyeongk@nvidia.com>
2013-12-11 11:03:52 -0800

    bcmdhd: key installation timing fix
    In case 43241 is a GO and it RECEIVES 4/4
    key msg, so dhd_wait_pend8021x() is not
    required for GO mode, hence we have skipped
    this for AP/GO mode.
    
    Bug 1392645
    
    Change-Id: Icb1f86c5bfcb2afbd7e26fca9f75d73598d4ca07
    Signed-off-by: Kyeong Kim <kyeongk@nvidia.com>
    Reviewed-on: http://git-master/r/344360
    Reviewed-by: Steve Lin <stlin@nvidia.com>

--- drivers/net/wireless/bcmdhd/wl_cfg80211.c
+++ drivers/net/wireless/bcmdhd/wl_cfg80211.c
@@ -3713,7 +3713,8 @@ wl_add_keyext(struct wiphy *wiphy, struct net_device *dev,
 		}
 		swap_key_from_BE(&key);
 		/* need to guarantee EAPOL 4/4 send out before set key */
-		dhd_wait_pend8021x(dev);
+		if (mode != WL_MODE_AP)
+			dhd_wait_pend8021x(dev);
 		err = wldev_iovar_setbuf_bsscfg(dev, "wsec_key", &key, sizeof(key),
 			wl->ioctl_buf, WLC_IOCTL_MAXLEN, bssidx, &wl->ioctl_buf_sync);
 		if (unlikely(err)) {
