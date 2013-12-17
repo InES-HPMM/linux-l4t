2a139ef105b3507ed034e467aadb3ee732b61b2f
David Wang <davidw@nvidia.com>
2013-12-16 18:11:31 -0800

    driver: media: tegra: ov5693 fuse ID support
    Add fuse ID read support for the ov5693 from OTP Bank 0
    
    Bug 1399569
    
    Change-Id: I340a355b091081bdd87cb65a9dfd63d2e35b760b
    Reviewed-on: http://git-master/r/346150
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Tested-by: David Wang (SW-TEGRA) <davidw@nvidia.com>
    Reviewed-by: Frank Chen <frankc@nvidia.com>
    Reviewed-by: Philip Breczinski <pbreczinski@nvidia.com>
    Reviewed-by: Thomas Cherry <tcherry@nvidia.com>

--- drivers/media/platform/tegra/ov5693.c
+++ drivers/media/platform/tegra/ov5693.c
@@ -47,6 +47,7 @@
 #define OV5693_LENS_VIEW_ANGLE_H	60000	/* _INT2FLOAT_DIVISOR */
 #define OV5693_LENS_VIEW_ANGLE_V	60000	/* _INT2FLOAT_DIVISOR */
 #define OV5693_OTP_BUF_SIZE		16
+#define OV5693_FUSE_ID_SIZE		8
 
 static struct nvc_gpio_init ov5693_gpio[] = {
 	{ OV5693_GPIO_TYPE_PWRDN, GPIOF_OUT_INIT_LOW, "pwrdn", false, true, },
@@ -3007,11 +3008,16 @@ ov5693_mode_wr_err:
 
 static int ov5693_get_fuse_id(struct ov5693_info *info)
 {
-	ov5693_i2c_rd8(info, 0x300A, &info->fuseid.data[0]);
-	ov5693_i2c_rd8(info, 0x300B, &info->fuseid.data[1]);
-	info->fuseid.size = 2;
-	dev_dbg(&info->i2c_client->dev, "ov5693 fuse_id: %x,%x\n",
-		info->fuseid.data[0], info->fuseid.data[1]);
+	int i;
+	regmap_write(info->regmap, 0x3D84, 0xC0);
+	regmap_write(info->regmap, 0x3D81, 0x40);
+	for (i = 0; i < OV5693_FUSE_ID_SIZE; i++) {
+		ov5693_i2c_rd8(info, 0x3D00 + i, &info->fuseid.data[i]);
+		dev_dbg(&info->i2c_client->dev, "ov5693 fuse_id byte %d:\t0x%0x\n",
+				i, info->fuseid.data[i]);
+	}
+	info->fuseid.size = OV5693_FUSE_ID_SIZE;
+
 	return 0;
 }
 
