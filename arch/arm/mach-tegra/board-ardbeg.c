4dcd4a79406216301bea0494e5e3c678c9a48444
Adeel Raza <araza@nvidia.com>
2014-01-05 13:37:14 -0800

    arm64: tegra: add bowmore ers-s support
    Change-Id: Idf26b0a265e4d7bf1924304c1d38955ef049537d
    Reviewed-on: http://git-master/r/355862
    Signed-off-by: Adeel Raza <araza@nvidia.com>
    Signed-off-by: Kerwin Wan <kerwinw@nvidia.com>
    Reviewed-on: http://git-master/r/361688
    Reviewed-by: Seema Khowala <seemaj@nvidia.com>
    Tested-by: Seema Khowala <seemaj@nvidia.com>
    Reviewed-by: Thomas Cherry <tcherry@nvidia.com>

--- arch/arm/mach-tegra/board-ardbeg.c
+++ arch/arm/mach-tegra/board-ardbeg.c
@@ -1158,6 +1158,8 @@ static void __init tegra_ardbeg_early_init(void)
 		tegra_soc_device_init("ardbeg_sata");
 	else if (of_machine_is_compatible("nvidia,norrin"))
 		tegra_soc_device_init("norrin");
+	else if (of_machine_is_compatible("nvidia,bowmore"))
+		tegra_soc_device_init("bowmore");
 	else
 		tegra_soc_device_init("ardbeg");
 }
@@ -1352,6 +1354,11 @@ static const char * const norrin_dt_board_compat[] = {
 	NULL
 };
 
+static const char * const bowmore_dt_board_compat[] = {
+	"nvidia,bowmore",
+	NULL
+};
+
 DT_MACHINE_START(LAGUNA, "laguna")
 	.atag_offset	= 0x100,
 	.smp		= smp_ops(tegra_smp_ops),
@@ -1391,6 +1398,20 @@ DT_MACHINE_START(NORRIN, "norrin")
 	.init_machine	= tegra_ardbeg_dt_init,
 	.restart	= tegra_assert_system_reset,
 	.dt_compat	= norrin_dt_board_compat,
+	.init_late      = tegra_init_late
+MACHINE_END
+
+DT_MACHINE_START(BOWMORE, "bowmore")
+	.atag_offset	= 0x100,
+	.smp		= smp_ops(tegra_smp_ops),
+	.map_io		= tegra_map_common_io,
+	.reserve	= tegra_ardbeg_reserve,
+	.init_early	= tegra_ardbeg_init_early,
+	.init_irq	= irqchip_init,
+	.init_time	= clocksource_of_init,
+	.init_machine	= tegra_ardbeg_dt_init,
+	.restart	= tegra_assert_system_reset,
+	.dt_compat	= bowmore_dt_board_compat,
 	.init_late      = tegra_init_late
 MACHINE_END
 
