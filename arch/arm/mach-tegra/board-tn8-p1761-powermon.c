72c5c4c3d8783fe6a9735cfeeaa995ff972049b4
Timo Alho <talho@nvidia.com>
2014-01-16 01:35:37 +0200

    ARM: tegra: tn8: update p1761 INA power monitor configuration
    This patch updates p1761 power monitors configuration as follows:
     * Update rail names and resistor values for boards from A02 revision
       onwards.
     * Set critical and warning alarm limit values for battery over current
       protection (9A and 8A, respectively)
     * Configure power monitor for 140us conversion time with 512 sample
       averages
    
    Bug 1435950
    Bug 1429694
    
    Change-Id: I5e33c3db1e0dd7d477b5ce7c0c05a44186186012
    Signed-off-by: Timo Alho <talho@nvidia.com>
    Reviewed-on: http://git-master/r/356372
    Reviewed-by: Steve Rogers <srogers@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Robert Shih <rshih@nvidia.com>
    Reviewed-by: Juha Tukkinen <jtukkinen@nvidia.com>

--- arch/arm/mach-tegra/board-tn8-p1761-powermon.c
+++ arch/arm/mach-tegra/board-tn8-p1761-powermon.c
@@ -1,7 +1,7 @@
 /*
  * arch/arm/mach-tegra/board-tn8-p1761-powermon.c
  *
- * Copyright (c) 2013, NVIDIA CORPORATION. All rights reserved.
+ * Copyright (c) 2013-2014, NVIDIA CORPORATION. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
@@ -20,22 +20,33 @@
 #include <linux/i2c.h>
 #include <linux/ina3221.h>
 
+#include "board.h"
+#include "board-ardbeg.h"
+#include "tegra-board-id.h"
+
 /* rails on TN8_P1761 i2c */
 enum {
 	VDD_BAT_CPU_GPU,
+};
+/* rails on TN8_P1761-A02 i2c */
+enum {
+	VDD_BAT_USB_MDM,
 };
 enum {
 	INA_I2C_ADDR_40,
 };
 
+/* Configure 140us conversion time, 512 averages */
+#define P1761_CONT_CONFIG_DATA 0x7c07
+
 static struct ina3221_platform_data tn8_p1761_power_mon_info[] = {
 	[VDD_BAT_CPU_GPU] = {
 		.rail_name = {"VDD_BAT", "VDD_CPU", "VDD_GPU"},
 		.shunt_resistor = {1, 1, 1},
-		.cont_conf_data = INA3221_CONT_CONFIG_DATA,
+		.cont_conf_data = P1761_CONT_CONFIG_DATA,
 		.trig_conf_data = INA3221_TRIG_CONFIG_DATA,
-		.warn_conf_limits = {-1, -1, -1},
-		.crit_conf_limits = {6450, -1, -1},
+		.warn_conf_limits = {8000, -1, -1},
+		.crit_conf_limits = {9000, -1, -1},
 	},
 };
 
@@ -47,10 +58,37 @@ static struct i2c_board_info tn8_p1761_i2c_ina3221_info[] = {
 	},
 };
 
+static struct ina3221_platform_data tn8_p1761_a02_power_mon_info[] = {
+	[VDD_BAT_USB_MDM] = {
+		.rail_name = {"VDD_BAT", "VDD_USB_5V0", "VDD_SYS_MDM"},
+		.shunt_resistor = {1, 10, 1},
+		.cont_conf_data = P1761_CONT_CONFIG_DATA,
+		.trig_conf_data = INA3221_TRIG_CONFIG_DATA,
+		.warn_conf_limits = {8000, -1, -1},
+		.crit_conf_limits = {9000, -1, -1},
+	},
+};
+
+static struct i2c_board_info tn8_p1761_a02_i2c_ina3221_info[] = {
+	[INA_I2C_ADDR_40] = {
+		I2C_BOARD_INFO("ina3221", 0x40),
+		.platform_data = &tn8_p1761_a02_power_mon_info[VDD_BAT_USB_MDM],
+		.irq = -1,
+	},
+};
+
 int __init tn8_p1761_pmon_init(void)
 {
+	struct board_info bi;
 	int ret;
-	ret = i2c_register_board_info(1, tn8_p1761_i2c_ina3221_info,
-				      ARRAY_SIZE(tn8_p1761_i2c_ina3221_info));
+	tegra_get_board_info(&bi);
+
+	if (bi.fab >= BOARD_FAB_A02)
+		ret = i2c_register_board_info(1, tn8_p1761_a02_i2c_ina3221_info,
+					      ARRAY_SIZE(tn8_p1761_a02_i2c_ina3221_info));
+	else
+		ret = i2c_register_board_info(1, tn8_p1761_i2c_ina3221_info,
+					      ARRAY_SIZE(tn8_p1761_i2c_ina3221_info));
+
 	return ret;
 }
