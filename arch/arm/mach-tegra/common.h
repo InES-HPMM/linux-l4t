d6b4261b54127fc655954befcc9e09c30e88c62d
Shravani Dingari <shravanid@nvidia.com>
2014-01-21 12:23:34 +0530

    ARM: mach-tegra: Modify copyright statement
    Remove NVIDIA copyright in common.h retaining
    the GPL v2 license header
    
    Bug 1415426
    
    Change-Id: Id9f00aba373b8810df4e8d1726bb44992e55aeae
    Signed-off-by: Shravani Dingari <shravanid@nvidia.com>
    Reviewed-on: http://git-master/r/356869
    (cherry picked from commit 9afec337b1cb9c09db78a0dca70207d46b4c6a2f)
    Reviewed-on: http://git-master/r/360120
    Reviewed-by: Venkat Moganty <vmoganty@nvidia.com>

--- arch/arm/mach-tegra/common.h
+++ arch/arm/mach-tegra/common.h
@@ -1,6 +1,4 @@
 /*
- * Copyright (C) 2013, NVIDIA CORPORATION. All rights reserved.
- *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
  * version 2, as published by the Free Software Foundation.
@@ -12,7 +10,6 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
  */
 
 #ifndef __MACH_TEGRA_COMMON_H
