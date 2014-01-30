c681eb8cf0c3cc7a040eccc3f0f1d797af18b495
Naveen Kumar Arepalli <naveenk@nvidia.com>
2014-01-30 16:12:01 +0530

    ARM: tegra: sdhci: Set SDIO max clock to 204 MHz.
    -Set SDIO max clock limit to 204MHz on P1761.
    And limit SDIO max clock to 136MHz for platforms other than
    P1761.
    
    Bug 1423429
    
    Reviewed-on: http://git-master/r/361957
    (cherry picked from commit 3f9f2b37ede319ecfecf8d070f1f2d996bc32d2f)
    
    Change-Id: Iae2ef43440d3bc473ae13fa8fa39eec8936203ff
    Signed-off-by: Naveen Kumar Arepalli <naveenk@nvidia.com>
    Signed-off-by: R Raj Kumar <rrajk@nvidia.com>
    Reviewed-on: http://git-master/r/363814
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Bitan Biswas <bbiswas@nvidia.com>

--- arch/arm/mach-tegra/board-ardbeg-sdhci.c
+++ arch/arm/mach-tegra/board-ardbeg-sdhci.c
@@ -189,6 +189,7 @@ static struct tegra_sdhci_platform_data tegra_sdhci_platform_data0 = {
 		MMC_UHS_MASK_SDR50,
 	.calib_3v3_offsets = 0x7676,
 	.calib_1v8_offsets = 0x7676,
+	.max_clk_limit = 136000000,
 };
 
 static struct tegra_sdhci_platform_data tegra_sdhci_platform_data2 = {
@@ -434,7 +435,10 @@ int __init ardbeg_sdhci_init(void)
 	tegra_get_board_info(&board_info);
 	if (board_info.board_id == BOARD_E1780)
 		tegra_sdhci_platform_data2.max_clk_limit = 204000000;
-	tegra_sdhci_platform_data0.max_clk_limit = 136000000;
+
+	if (board_info.board_id == BOARD_P1761)
+		tegra_sdhci_platform_data0.max_clk_limit = 204000000;
+
 	if ((board_info.board_id == BOARD_E1781) ||
 		(board_info.board_id == BOARD_PM374) ||
 		(board_info.board_id == BOARD_PM359))
