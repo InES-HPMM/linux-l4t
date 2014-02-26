bee06d7ab5ae05c023401f7c591fa1ae42b65539
Arto Merilainen <amerilainen@nvidia.com>
2014-02-26 11:48:20 +0200

    video: tegra: host: gk20a: Implement debug iface
    This patch adds debug interface implementation inside gk20a. As
    part of the process, this patch separates debug dump facilities
    from the nvhost driver.
    
    Bug 1468086
    
    Change-Id: I8bb6eb90117e8f2fcda86d3936aafce9bc86b271
    Signed-off-by: Arto Merilainen <amerilainen@nvidia.com>
    Reviewed-on: http://git-master/r/376650
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Terje Bergstrom <tbergstrom@nvidia.com>

--- include/linux/gk20a.h
+++ include/linux/gk20a.h
@@ -20,13 +20,14 @@
 #define __GK20A_H
 
 struct channel_gk20a;
+struct platform_device;
 
 #ifdef CONFIG_GK20A
 void gk20a_channel_update(struct channel_gk20a *c, int nr_completed);
+void gk20a_debug_dump_device(struct platform_device *pdev);
 #else
 static inline void gk20a_channel_update(struct channel_gk20a *c,
-					int nr_completed)
-{
-}
+					int nr_completed) {}
+static inline void gk20a_debug_dump_device(struct platform_device *pdev) {}
 #endif
 #endif
