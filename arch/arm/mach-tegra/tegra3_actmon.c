ea6fde84113493f627772f052fbf27d67a4a8c18
Aaron Huang <aaronh@nvidia.com>
2014-02-26 15:56:46 +0800

    ARM: tegra: modify actmon emc boost threshold
    Tune and modify actmon emc boost up and down threshold
    for T124 platforms to increate the target utilizaiton
    and reduce frequency overhead, and hence power.
    
    Bug 1396075
    
    Change-Id: Ie0bf96077e8aae3398b89b22007929c0604367a3
    Signed-off-by: Aaron Huang <aaronh@nvidia.com>
    Reviewed-on: http://git-master/r/374716
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Peter Boonstoppel <pboonstoppel@nvidia.com>
    Reviewed-by: Aleksandr Frid <afrid@nvidia.com>
    Reviewed-by: Wen Yi <wyi@nvidia.com>
    Tested-by: Wen Yi <wyi@nvidia.com>
    Reviewed-by: Peter Zu <pzu@nvidia.com>
    Reviewed-by: Mitch Luban <mluban@nvidia.com>

--- arch/arm/mach-tegra/tegra3_actmon.c
+++ arch/arm/mach-tegra/tegra3_actmon.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2013, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2012-2014, NVIDIA CORPORATION.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -541,7 +541,7 @@ static struct actmon_dev actmon_dev_emc = {
 	.boost_freq_step	= 16000,
 	.boost_up_coef		= 200,
 	.boost_down_coef	= 50,
-#if defined(CONFIG_ARCH_TEGRA_14x_SOC)
+#if defined(CONFIG_ARCH_TEGRA_14x_SOC) || defined(CONFIG_ARCH_TEGRA_12x_SOC)
 	.boost_up_threshold	= 70,
 	.boost_down_threshold	= 50,
 #else
