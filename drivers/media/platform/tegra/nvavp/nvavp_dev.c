88171883aca5a5a74e94be74aeaecb80fa9a8a83
Shridhar Rasal <srasal@nvidia.com>
2014-02-27 15:23:31 +0530

    media: tegra: avp: update syncpt on success.
    Only on success, update avp syncpoint value given by nvhost.
    
    Bug 1469913
    
    Change-Id: I064f2e630e022f275af9f55e705446dce541a148
    Signed-off-by: Shridhar Rasal <srasal@nvidia.com>
    Reviewed-on: http://git-master/r/375432
    Reviewed-by: Terje Bergstrom <tbergstrom@nvidia.com>
    Tested-by: Terje Bergstrom <tbergstrom@nvidia.com>

--- drivers/media/platform/tegra/nvavp/nvavp_dev.c
+++ drivers/media/platform/tegra/nvavp/nvavp_dev.c
@@ -829,6 +829,7 @@ static void nvavp_pushbuffer_free(struct nvavp_info *nvavp)
 static int nvavp_pushbuffer_init(struct nvavp_info *nvavp)
 {
 	int ret, channel_id;
+	u32 val;
 
 	for (channel_id = 0; channel_id < NVAVP_NUM_CHANNELS; channel_id++) {
 		ret = nvavp_pushbuffer_alloc(nvavp, channel_id);
@@ -840,8 +841,9 @@ static int nvavp_pushbuffer_init(struct nvavp_info *nvavp)
 		nvavp_set_channel_control_area(nvavp, channel_id);
 		if (IS_VIDEO_CHANNEL_ID(channel_id)) {
 			nvavp->syncpt_id = NVSYNCPT_AVP_0;
-			nvavp->syncpt_value = nvhost_syncpt_read_ext(
-				nvavp->nvhost_dev, nvavp->syncpt_id);
+			if (!nvhost_syncpt_read_ext_check(nvavp->nvhost_dev,
+					nvavp->syncpt_id, &val))
+				nvavp->syncpt_value = val;
 		}
 
 	}
