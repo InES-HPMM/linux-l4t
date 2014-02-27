35d9b4a3521a259b14b669bfb82c96eb896819ca
Prashant Gaikwad <pgaikwad@nvidia.com>
2014-02-26 22:59:18 -0800

    arm64: t210: remove PMC init call
    PMC initialization is done using post core init call.
    
    Change-Id: I383a9d97eabb7385081264ed7c1b72eeb0cec57a
    Signed-off-by: Prashant Gaikwad <pgaikwad@nvidia.com>
    Reviewed-on: http://git-master/r/375305
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Bo Yan <byan@nvidia.com>

--- arch/arm64/mach-tegra/tegra21_init.c
+++ arch/arm64/mach-tegra/tegra21_init.c
@@ -180,7 +180,6 @@ void __init tegra21x_init_early(void)
 	tegra_common_init_clock();
 	tegra_clk_init_from_table(tegra21x_clk_init_table);
 	tegra_clk_init_cbus_plls_from_table(tegra21x_cbus_init_table);
-	tegra_pmc_init();
 	tegra_powergate_init();
 	tegra_init_power();
 	tegra_init_ahb_gizmo_settings();
