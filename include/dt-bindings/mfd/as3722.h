ad82ee1ceb9585c8a780cd5ce765bc56eec38ca7
Laxman Dewangan <ldewangan@nvidia.com>
2014-03-12 15:46:11 +0530

    ARM: dt-binding: add dt binding header for the AS3722
    Define some of the macro to make DTS file entry easy for
    AS3722 device.
    
    Change-Id: I449e78687676b3b531f3647b5d0acb1f9af7b991
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Reviewed-on: http://git-master/r/380888

---
+++ include/dt-bindings/mfd/as3722.h
@@ -0,0 +1,11 @@
+/*
+ * This header provides constants for binding ams,as3722.
+ */
+
+#ifndef _DT_BINDINGS_MFD_AS3722_H
+#define _DT_BINDINGS_MFD_AS3722_H
+
+#define AS3722_EXT_CONTROL_ENABLE1 1
+#define AS3722_EXT_CONTROL_ENABLE2 2
+
+#endif
