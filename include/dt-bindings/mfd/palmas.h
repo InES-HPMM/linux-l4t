4c872f7c79e8e8c21ade281e6cbb35e26cfaa1a1
Laxman Dewangan <ldewangan@nvidia.com>
2014-03-14 20:10:45 +0530

    ARM: dt-binding: add dt binding header for the palmas
    Define some of the macro to make DTS file entry easy for
    Palmas device.
    
    Change-Id: I679908b807df6add0f24417af9fb8512816d53c6
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Reviewed-on: http://git-master/r/382068
    GVS: Gerrit_Virtual_Submit

---
+++ include/dt-bindings/mfd/palmas.h
@@ -0,0 +1,18 @@
+/*
+ * This header provides macros for TI,palams device bindings.
+ *
+ * Copyright (c) 2013, NVIDIA Corporation.
+ *
+ * Author: Laxman Dewangan <ldewangan@nvidia.com>
+ *
+ */
+
+#ifndef __DT_BINDINGS_PALMAS_H__
+#define __DT_BINDINGS_PALMAS_H__
+
+/* External control pins */
+#define PALMAS_EXT_CONTROL_ENABLE1	1
+#define PALMAS_EXT_CONTROL_ENABLE2	2
+#define PALMAS_EXT_CONTROL_NSLEEP	3
+
+#endif /* __DT_BINDINGS_PALMAS_H__ */
