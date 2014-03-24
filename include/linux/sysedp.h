8eda89462e2e691731f654285dab44540c2c8c45
Steve Rogers <srogers@nvidia.com>
2014-03-24 09:18:48 -0500

    EDP: sysedp: Add sysedp_set_state_by_name()
    Bug 1486148
    
    Change-Id: I8bf6af83530273ad5b04f1903f2d76048ff2b514
    Signed-off-by: Steve Rogers <srogers@nvidia.com>
    Reviewed-on: http://git-master/r/386462
    (cherry-picked from commmit 2583d5bbd2910bf4420329786bb3d7560bab1615)
    Reviewed-on: http://git-master/r/387831
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Timo Alho <talho@nvidia.com>

--- include/linux/sysedp.h
+++ include/linux/sysedp.h
@@ -145,6 +145,7 @@ struct sysedp_reactive_capping_platform_data {
 extern struct dentry *edp_debugfs_dir;
 
 void sysedp_set_state(struct sysedp_consumer *, unsigned int);
+void sysedp_set_state_by_name(const char *, unsigned int);
 unsigned int sysedp_get_state(struct sysedp_consumer *);
 struct sysedp_consumer *sysedp_create_consumer(const char *, const char *);
 int sysedp_register_consumer(struct sysedp_consumer *);
@@ -153,6 +154,8 @@ void sysedp_free_consumer(struct sysedp_consumer *);
 
 #else
 static inline void sysedp_set_state(struct sysedp_consumer *c, unsigned int i)
+{ return; }
+static inline void sysedp_set_state_by_name(const char *c, unsigned int i)
 { return; }
 static inline unsigned int sysedp_get_state(struct sysedp_consumer *c)
 { return 0; }
