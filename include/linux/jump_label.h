c035fbe9185530ead171c747b1b27f5b1dbcc9e5
Peng Du <pdu@nvidia.com>
2014-04-01 15:05:30 -0700

    arm64: Optimize jump label implementation for ARM64
    This patchset tries to optimize arch specfic jump label implementation
    for ARM64 by dynamic kernel text patching.
    
    To enable this feature, your toolchain must support "asm goto" extension
    and "%c" constraint extesion.
    
    It has been tested on ARM Fast mode and a real hardware platform.
    
    Jiang Liu (7):
      arm64: introduce basic aarch64 instruction decoding helpers
      arm64: introduce interfaces to hotpatch kernel and module code
      arm64: move encode_insn_immediate() from module.c to insn.c
      arm64: introduce aarch64_insn_gen_{nop|branch_imm}() helper functions
      arm64, jump label: detect %c support for ARM64
      arm64, jump label: optimize jump label implementation
      jump_label: use defined macros instead of hard-coding for better
        readability
    
    Reviewed-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Jiang Liu <liuj97@gmail.com>
    
    Change-Id: I2ccd72995852193d491b342787c8e5c1e7b4c30d
    Signed-off-by: Peng Du <pdu@nvidia.com>
    
    Conflicts:
    	arch/arm64/kernel/Makefile
    
    Signed-off-by: Noah Imam <nimam@nvidia.com>
    Change-Id: Ieeca17c1326fc71918d66fe56046eebd903f9bd2
    Reviewed-on: http://git-master/r/451085
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Mark Peters <mpeters@nvidia.com>
    Reviewed-by: Mitch Luban <mluban@nvidia.com>

--- include/linux/jump_label.h
+++ include/linux/jump_label.h
@@ -80,18 +80,21 @@ struct module;
 
 #ifdef HAVE_JUMP_LABEL
 
-#define JUMP_LABEL_TRUE_BRANCH 1UL
+#define JUMP_LABEL_TYPE_FALSE_BRANCH	0UL
+#define JUMP_LABEL_TYPE_TRUE_BRANCH	1UL
+#define JUMP_LABEL_TYPE_MASK		1UL
 
 static
 inline struct jump_entry *jump_label_get_entries(struct static_key *key)
 {
 	return (struct jump_entry *)((unsigned long)key->entries
-						& ~JUMP_LABEL_TRUE_BRANCH);
+						& ~JUMP_LABEL_TYPE_MASK);
 }
 
 static inline bool jump_label_get_branch_default(struct static_key *key)
 {
-	if ((unsigned long)key->entries & JUMP_LABEL_TRUE_BRANCH)
+	if (((unsigned long)key->entries & JUMP_LABEL_TYPE_MASK) ==
+	    JUMP_LABEL_TYPE_TRUE_BRANCH)
 		return true;
 	return false;
 }
@@ -124,10 +127,12 @@ extern void jump_label_apply_nops(struct module *mod);
 extern void
 jump_label_rate_limit(struct static_key_deferred *key, unsigned long rl);
 
-#define STATIC_KEY_INIT_TRUE ((struct static_key) \
-	{ .enabled = ATOMIC_INIT(1), .entries = (void *)1 })
-#define STATIC_KEY_INIT_FALSE ((struct static_key) \
-	{ .enabled = ATOMIC_INIT(0), .entries = (void *)0 })
+#define STATIC_KEY_INIT_TRUE ((struct static_key)		\
+	{ .enabled = ATOMIC_INIT(1),				\
+	  .entries = (void *)JUMP_LABEL_TYPE_TRUE_BRANCH })
+#define STATIC_KEY_INIT_FALSE ((struct static_key)		\
+	{ .enabled = ATOMIC_INIT(0),				\
+	  .entries = (void *)JUMP_LABEL_TYPE_FALSE_BRANCH })
 
 #else  /* !HAVE_JUMP_LABEL */
 
