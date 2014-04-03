e3db7e91c2bdf31c3c2840b034383a43364ee1ca
Sumit Singh <sumsingh@nvidia.com>
2014-04-03 19:12:00 +0530

    irq_work: enhance power efficiency
    Trying to improve power efficiency through the use of
    macros cpu_relaxed_read_long and cpu_read_relax.
    
    Bug 1440421
    
    Change-Id: Ib0825bdbc6d96def858edbf12d9214ed1724975a
    Signed-off-by: Sumit Singh <sumsingh@nvidia.com>
    Reviewed-on: http://git-master/r/391906
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>

--- kernel/irq_work.c
+++ kernel/irq_work.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2010 Red Hat, Inc., Peter Zijlstra <pzijlstr@redhat.com>
+ * Copyright (C) 2014, NVIDIA CORPORATION.  All rights reserved.
  *
  * Provides a framework for enqueueing and running callbacks from hardirq
  * context. The enqueueing is NMI-safe.
@@ -17,7 +18,7 @@
 #include <linux/cpu.h>
 #include <linux/notifier.h>
 #include <asm/processor.h>
-
+#include <asm/relaxed.h>
 
 static DEFINE_PER_CPU(struct llist_head, irq_work_list);
 static DEFINE_PER_CPU(int, irq_work_raised);
@@ -37,12 +38,13 @@ static bool irq_work_claim(struct irq_work *work)
 	for (;;) {
 		nflags = flags | IRQ_WORK_FLAGS;
 		oflags = cmpxchg(&work->flags, flags, nflags);
+		cpu_relaxed_read_long(&work->flags);
 		if (oflags == flags)
 			break;
 		if (oflags & IRQ_WORK_PENDING)
 			return false;
 		flags = oflags;
-		cpu_relax();
+		cpu_read_relax();
 	}
 
 	return true;
@@ -165,8 +167,8 @@ void irq_work_sync(struct irq_work *work)
 {
 	WARN_ON_ONCE(irqs_disabled());
 
-	while (work->flags & IRQ_WORK_BUSY)
-		cpu_relax();
+	while (cpu_relaxed_read_long(&work->flags) & IRQ_WORK_BUSY)
+		cpu_read_relax();
 }
 EXPORT_SYMBOL_GPL(irq_work_sync);
 
