0c0a4be09e8af3be5f7e85a86e58fd0f438b9da8
Hiroshi Doyu <hdoyu@nvidia.com>
2014-04-10 12:56:40 +0300

    arm64: dma-mapping: fix build warnings
    Also fix the build warnings for incorrect print size.
    Please refer to "Documentation/printk-formats.txt".
    
    Change-Id: Ic699b1bb8e4ee5c0e2f7f68ae311022cd49405af
    Signed-off-by: Hiroshi Doyu <hdoyu@nvidia.com>
    Reviewed-on: http://git-master/r/394700
    Reviewed-by: Krishna Reddy <vdumpa@nvidia.com>
    Tested-by: Krishna Reddy <vdumpa@nvidia.com>

--- arch/arm64/mm/dma-mapping.c
+++ arch/arm64/mm/dma-mapping.c
@@ -1050,11 +1050,13 @@ static void seq_print_dma_areas(struct seq_file *s, void *bitmap,
 	size_t pos = find_first_bit(bitmap, bits), end;
 
 	for (; pos < bits; pos = find_next_bit(bitmap, bits, end + 1)) {
+		dma_addr_t start_addr, end_addr;
+
 		end = find_next_zero_bit(bitmap, bits, pos);
-		seq_printf(s, "    0x%lx-0x%lx pages=%zu\n",
-			   bit_to_addr(pos, base, order),
-			   bit_to_addr(end, base, order) - 1,
-			   (end - pos) << order);
+		start_addr = bit_to_addr(pos, base, order);
+		end_addr = bit_to_addr(end, base, order) - 1;
+		seq_printf(s, "    %pa-%pa pages=%zu\n",
+			   &start_addr, &end_addr, (end - pos) << order);
 	}
 }
 
