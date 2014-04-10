7547c1bb58dbd2376707bcb1235ad8ebdce2f813
Hoang Pham <hopham@nvidia.com>
2014-04-10 16:59:15 -0700

    clock: tegra21: set/clear XUSB pll pad iddq
    Set XUSB pll pad override and iddq when disable pll clock.
    Clear XUSB pll pad override and iddq when enable pll clock.
    
    Ported from tegra12 Change-Id: Iefe1dba52d861ad1d2892e6cdfa89b5b53825cb8
    
    Change-Id: I0baaa2a08f785ba4a7c825c0bca0e4f9b89d17be
    Signed-off-by: Hoang Pham <hopham@nvidia.com>
    Reviewed-on: http://git-master/r/394923
    Reviewed-by: Bo Yan <byan@nvidia.com>
    Tested-by: Bo Yan <byan@nvidia.com>

--- drivers/platform/tegra/tegra21_clocks.c
+++ drivers/platform/tegra/tegra21_clocks.c
@@ -575,6 +575,11 @@
 #define USB_PLLS_USE_LOCKDET			(1<<6)
 #define USB_PLLS_ENABLE_SWCTL			((1<<2) | (1<<0))
 
+/* XUSB PLL PAD controls */
+#define XUSB_PADCTL_IOPHY_PLL_P0_CTL1_0         0x40
+#define XUSB_PADCTL_IOPHY_PLL_P0_CTL1_0_PLL_PWR_OVRD    (1<<3)
+#define XUSB_PADCTL_IOPHY_PLL_P0_CTL1_0_PLL_IDDQ        (1<<0)
+
 /* DFLL */
 #define DFLL_BASE				0x2f4
 #define DFLL_BASE_RESET				(1<<0)
@@ -638,6 +643,7 @@ static const struct utmi_clk_param utmi_parameters[] =
 static void __iomem *reg_clk_base = IO_ADDRESS(TEGRA_CLK_RESET_BASE);
 static void __iomem *reg_pmc_base = IO_ADDRESS(TEGRA_PMC_BASE);
 static void __iomem *misc_gp_base = IO_ADDRESS(TEGRA_APB_MISC_BASE);
+static void __iomem *reg_xusb_padctl_base = IO_ADDRESS(TEGRA_XUSB_PADCTL_BASE);
 
 #define MISC_GP_HIDREV				0x804
 #define MISC_GP_TRANSACTOR_SCRATCH_0		0x864
@@ -663,6 +669,10 @@ static int tegra_periph_clk_enable_refcount[CLK_OUT_ENB_NUM * 32];
 	__raw_readl((void *)((uintptr_t)reg_pmc_base + (reg)))
 #define chipid_readl() \
 	__raw_readl((void *)((uintptr_t)misc_gp_base + MISC_GP_HIDREV))
+#define xusb_padctl_writel(value, reg) \
+	 __raw_writel(value, reg_xusb_padctl_base + (reg))
+#define xusb_padctl_readl(reg) \
+	__raw_readl(reg_xusb_padctl_base + (reg))
 
 #define clk_writel_delay(value, reg) 					\
 	do {								\
@@ -2141,6 +2151,15 @@ static void tegra21_pll_clk_init(struct clk *c)
 		val = clk_readl(c->reg + PLL_BASE);
 		val &= ~PLLU_BASE_OVERRIDE;
 		clk_writel(val, c->reg + PLL_BASE);
+
+/* FIXME: Disable for initial Si bringup */
+#if 0
+		/* Set XUSB PLL pad pwr override and iddq */
+		val = xusb_padctl_readl(XUSB_PADCTL_IOPHY_PLL_P0_CTL1_0);
+		val |= XUSB_PADCTL_IOPHY_PLL_P0_CTL1_0_PLL_PWR_OVRD;
+		val |= XUSB_PADCTL_IOPHY_PLL_P0_CTL1_0_PLL_IDDQ;
+		xusb_padctl_writel(val, XUSB_PADCTL_IOPHY_PLL_P0_CTL1_0);
+#endif
 	}
 }
 
@@ -3729,6 +3748,16 @@ static void tegra21_plle_clk_disable(struct clk *c)
 	val = clk_readl(c->reg + PLL_MISC(c));
 	val |= PLLE_MISC_IDDQ_SW_CTRL | PLLE_MISC_IDDQ_SW_VALUE;
 	pll_writel_delay(val, c->reg + PLL_MISC(c));
+
+/* FIXME: Disable for initial Si bringup */
+#if 0
+	/* Set XUSB PLL pad pwr override and iddq */
+	val = xusb_padctl_readl(XUSB_PADCTL_IOPHY_PLL_P0_CTL1_0);
+	val |= XUSB_PADCTL_IOPHY_PLL_P0_CTL1_0_PLL_PWR_OVRD;
+	val |= XUSB_PADCTL_IOPHY_PLL_P0_CTL1_0_PLL_IDDQ;
+	xusb_padctl_writel(val, XUSB_PADCTL_IOPHY_PLL_P0_CTL1_0);
+#endif
+
 }
 
 static int tegra21_plle_clk_enable(struct clk *c)
@@ -3819,6 +3848,15 @@ static int tegra21_plle_clk_enable(struct clk *c)
 	val |= PLLE_AUX_SEQ_ENABLE;
 	pll_writel_delay(val, PLLE_AUX);
 #endif
+/* FIXME: Disable for initial Si bringup */
+#if 0
+	/* clear XUSB PLL pad pwr override and iddq */
+	val = xusb_padctl_readl(XUSB_PADCTL_IOPHY_PLL_P0_CTL1_0);
+	val &= ~XUSB_PADCTL_IOPHY_PLL_P0_CTL1_0_PLL_PWR_OVRD;
+	val &= ~XUSB_PADCTL_IOPHY_PLL_P0_CTL1_0_PLL_IDDQ;
+	xusb_padctl_writel(val, XUSB_PADCTL_IOPHY_PLL_P0_CTL1_0);
+#endif
+
 	/* enable hw control of xusb brick pll */
 	usb_plls_hw_control_enable(XUSBIO_PLL_CFG0);
 
