54f24cebaaf86dfafdc204a7a9588d01ddaea9b4
Hoang Pham <hopham@nvidia.com>
2014-04-16 15:04:14 -0700

    clock: tegra21: Fix round rate error checking
    Some callers of clk_round_rate() API assume that return signed value
    is negative in case of error, and positive if API succeeded. The latter
    is not true when the clock rate crosses ~2.1 GHz (2,147,483,647 Hz)
    boundary on 32-bit machine. To prevent false bailing out on 2.1GHz and
    above rates callers of round rate API should cast the rounded rate as
    unsigned, and use IS_ERR_VALUE() macro to detect errors instead of
    checking sign of the returned value.
    
    Ported from tegra12: Change-Id: I675f18473a0fe03b3a0b26978784e25dfd7c0280
    
    Change-Id: I6c1e10c9ee654f27092cfb474714ba6d30ec2c2c
    Signed-off-by: Hoang Pham <hopham@nvidia.com>
    Reviewed-on: http://git-master/r/398139
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Bo Yan <byan@nvidia.com>

--- drivers/platform/tegra/tegra21_clocks.c
+++ drivers/platform/tegra/tegra21_clocks.c
@@ -4860,11 +4860,10 @@ static void tegra21_emc_clk_init(struct clk *c)
 static long tegra21_emc_clk_round_updown(struct clk *c, unsigned long rate,
 					 bool up)
 {
-	long new_rate = max(rate, c->min_rate);
+	unsigned long new_rate = max(rate, c->min_rate);
 
-	/* FIXME: emc updown rounding */
 	new_rate = tegra_emc_round_rate_updown(new_rate, up);
-	if (new_rate < 0)
+	if (IS_ERR_VALUE(new_rate))
 		new_rate = c->max_rate;
 
 	return new_rate;
