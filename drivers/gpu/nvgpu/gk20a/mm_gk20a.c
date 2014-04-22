b5c0ad4e00dfc86b65e8efe3d8691b5cfaafbe4c
Shridhar Rasal <srasal@nvidia.com>
2014-04-22 21:15:25 +0530

    gpu: nvgpu: mm: free allocations on validate error
    Free allocated virtual address when marking PTE for validation
    or update fails.
    
    Bug 1479803
    
    Change-Id: I9a8bd7c245b478f4252a261f246002fcc65d750d
    Signed-off-by: Shridhar Rasal <srasal@nvidia.com>

--- drivers/gpu/nvgpu/gk20a/mm_gk20a.c
+++ drivers/gpu/nvgpu/gk20a/mm_gk20a.c
@@ -1104,6 +1104,7 @@ static u64 __locked_gmmu_map(struct vm_gk20a *vm,
 				int rw_flag)
 {
 	int err = 0, i = 0;
+	bool allocated = false;
 	u32 pde_lo, pde_hi;
 	struct device *d = dev_from_vm(vm);
 
@@ -1114,8 +1115,9 @@ static u64 __locked_gmmu_map(struct vm_gk20a *vm,
 		if (!map_offset) {
 			gk20a_err(d, "failed to allocate va space");
 			err = -ENOMEM;
-			goto fail;
+			goto fail_alloc;
 		}
+		allocated = true;
 	}
 
 	pde_range_from_vaddr_range(vm,
@@ -1130,7 +1132,7 @@ static u64 __locked_gmmu_map(struct vm_gk20a *vm,
 		if (err) {
 			gk20a_err(d, "failed to validate page table %d: %d",
 							   i, err);
-			goto fail;
+			goto fail_validate;
 		}
 	}
 
@@ -1144,11 +1146,14 @@ static u64 __locked_gmmu_map(struct vm_gk20a *vm,
 				      rw_flag);
 	if (err) {
 		gk20a_err(d, "failed to update ptes on map");
-		goto fail;
+		goto fail_validate;
 	}
 
 	return map_offset;
- fail:
+fail_validate:
+	if (allocated)
+		gk20a_vm_free_va(vm, map_offset, size, pgsz_idx);
+fail_alloc:
 	gk20a_err(d, "%s: failed with err=%d\n", __func__, err);
 	return 0;
 }
