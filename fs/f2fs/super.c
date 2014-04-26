a97c5010b36d88e9b73114a771e56696fcdb43f5
Russ W. Knize <Russ.Knize@motorola.com>
2014-04-25 17:27:32 -0700

    IKJBXLINE-5233: f2fs: remount read-only support
    If the file system is remounted read-only, shutdown the garbage
    collector and leave a complete checkpoint.
    
    Change-Id: Ie7614a930113179fcabb4bd34c84e5e74bfbe72b
    Signed-off-by: Russ Knize <Russ.Knize@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/536696
    SLT-Approved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Christopher Fries <qcf001@motorola.com>
    Reviewed-by: Klocwork kwcheck <klocwork-kwcheck@sourceforge.mot.com>
    Reviewed-by: Check Patch <CHEKPACH@motorola.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Reviewed-by: John Zafiris <qemx13@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    
    Since 8e22cc88d68ca1a46d7d582938f979eb640ed30f no lock_super().
    So removed.
    
    Signed-off-by: JP Abgrall <jpa@google.com>

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -231,6 +231,30 @@ static int f2fs_statfs(struct dentry *dentry, struct kstatfs *buf)
 	return 0;
 }
 
+static int f2fs_remount(struct super_block *sb, int *flags, char *data)
+{
+	struct f2fs_sb_info *sbi = F2FS_SB(sb);
+	int err = 0;
+
+	if ((*flags & MS_RDONLY) != (sb->s_flags & MS_RDONLY)) {
+		if (*flags & MS_RDONLY) {
+			sb->s_flags |= MS_RDONLY;
+
+			mutex_lock(&sbi->gc_mutex);
+			stop_gc_thread(sbi);
+			write_checkpoint(sbi, true);
+			mutex_unlock(&sbi->gc_mutex);
+
+			f2fs_msg(sb, KERN_INFO, "re-mounted read-only.");
+		} else {
+			err = -EINVAL;
+			f2fs_msg(sb, KERN_ERR, "re-mount not supported.");
+		}
+	}
+
+	return err;
+}
+
 static int f2fs_show_options(struct seq_file *seq, struct dentry *root)
 {
 	struct f2fs_sb_info *sbi = F2FS_SB(root->d_sb);
@@ -291,6 +315,7 @@ static struct super_operations f2fs_sops = {
 	.freeze_fs	= f2fs_freeze,
 	.unfreeze_fs	= f2fs_unfreeze,
 	.statfs		= f2fs_statfs,
+	.remount_fs	= f2fs_remount,
 };
 
 static struct inode *f2fs_nfs_get_inode(struct super_block *sb,
