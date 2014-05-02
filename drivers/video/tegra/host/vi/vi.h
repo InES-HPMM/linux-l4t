e8aaa368d67d1434bc74a38cc04ca256aafa2080
Shridhar Rasal <srasal@nvidia.com>
2014-05-02 15:25:42 +0530

    video: tegra: host: reset correct sensor on powergate
    - add reset callback for vi single device separately
    - don't set device id explicitly for vi single device
    
    Bug 1436477
    
    Change-Id: I2b5956a49da46b7a0a681a6ad84c116016cab05e
    Signed-off-by: Shridhar Rasal <srasal@nvidia.com>
    Reviewed-on: http://git-master/r/404329
    Reviewed-by: Sachin Nikam <snikam@nvidia.com>
    Tested-by: Sachin Nikam <snikam@nvidia.com>

--- drivers/video/tegra/host/vi/vi.h
+++ drivers/video/tegra/host/vi/vi.h
@@ -56,6 +56,8 @@ int nvhost_vi_prepare_poweroff(struct platform_device *);
 int nvhost_vi_finalize_poweron(struct platform_device *);
 int nvhost_vi_init(struct platform_device *);
 void nvhost_vi_deinit(struct platform_device *);
+
+void nvhost_vi_reset_all(struct platform_device *);
 void nvhost_vi_reset(struct platform_device *);
 
 #endif
