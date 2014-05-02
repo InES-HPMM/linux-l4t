79e3ee96bd8da31f7882f9eabfdc122b47c801a1
Vinayak Pane <vpane@nvidia.com>
2014-05-02 16:21:10 -0700

    HID: usbhid: add safety check at _report
    Add device disconnect check in usbhid_get_raw_report()
    
    Bug 200000762
    
    Change-Id: Ia56a5ba746ee197a0be967f4cb09e38e44be8a8f
    Reviewed-on: http://git-master/r/404650
    Signed-off-by: Vinayak Pane <vpane@nvidia.com>
    Signed-off-by: martin gao <marting@nvidia.com>
    (cherry picked from commit 929d583167244e67558c5d0019050d27562d3def)
    (cherry picked from commit e8ba988bd654317fdedc3df1e3aae955bca48fee)
    (cherry picked from commit b3328b0c2ae1b4e0f825376bc328f41c11e7a66d)
    (cherry picked from commit 476349792790c4e33bba1a81954fc85753618160)
    Reviewed-on: http://git-master/r/428513
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Raymond Poudrier <rapoudrier@nvidia.com>

--- drivers/hid/usbhid/hid-core.c
+++ drivers/hid/usbhid/hid-core.c
@@ -908,11 +908,20 @@ static int usbhid_get_raw_report(struct hid_device *hid,
 		unsigned char report_type)
 {
 	struct usbhid_device *usbhid = hid->driver_data;
-	struct usb_device *dev = hid_to_usb_dev(hid);
-	struct usb_interface *intf = usbhid->intf;
-	struct usb_host_interface *interface = intf->cur_altsetting;
+	struct usb_device *dev;
+	struct usb_interface *intf;
+	struct usb_host_interface *interface;
 	int skipped_report_id = 0;
 	int ret;
+
+	if (test_bit(HID_DISCONNECTED, &usbhid->iofl)) {
+		pr_err("hid device disconnected\n");
+		return -ESHUTDOWN;
+	}
+
+	dev = hid_to_usb_dev(hid);
+	intf = usbhid->intf;
+	interface = intf->cur_altsetting;
 
 	/* Byte 0 is the report number. Report data starts at byte 1.*/
 	buf[0] = report_number;
