f1876ca15a337510e5d850acd31820208a540539
Hoang Pham <hopham@nvidia.com>
2014-05-09 03:36:02 -0700

    clock: tegra21: Add CEC support
    Bug 1413190
    
    Change-Id: I1666aa56122c7d04e8f9f9ef7f8f45bafcd1a299
    Signed-off-by: Hoang Pham <hopham@nvidia.com>

--- drivers/platform/tegra/tegra21_clocks.c
+++ drivers/platform/tegra/tegra21_clocks.c
@@ -7923,6 +7923,7 @@ struct clk tegra_list_clks[] = {
 	PERIPH_CLK("vcp",	"nvavp",		"vcp",	29,	0,	250000000, mux_clk_m, 			0),
 	PERIPH_CLK("bsea",	"nvavp",		"bsea",	62,	0,	250000000, mux_clk_m, 			0),
 	PERIPH_CLK("bsev",	"tegra-aes",		"bsev",	63,	0,	250000000, mux_clk_m, 			0),
+	PERIPH_CLK("cec",	"tegra_cec",		NULL,	136,	0,	250000000, mux_clk_m,			PERIPH_ON_APB),
 	PERIPH_CLK("csite",	"csite",		NULL,	73,	0x1d4,	144000000, mux_pllp_pllc_clkm,	MUX | DIV_U71 | PERIPH_ON_APB),
 	PERIPH_CLK("la",	"la",			NULL,	76,	0x1f8,	26000000,  mux_pllp_pllc_clkm,	MUX | DIV_U71 | PERIPH_ON_APB),
 	PERIPH_CLK("owr",	"tegra_w1",		NULL,	71,	0x1cc,	26000000,  mux_pllp_pllc_clkm,	MUX | DIV_U71 | PERIPH_ON_APB),
