66c1598635f1eac907704909113bd20552ed81a1
Alex Waterman <alexw@nvidia.com>
2014-05-08 18:49:37 -0700

    video: tegra: nvmap: increase set/ways threshold for denver
    Doing full invalidates (and similarly flushes) causes everything
    to be invalidated including the MTS translations. Thus all
    translation must be redone/revalidated which is a very expensive
    operation.
    
    However, it still makes sense to do full invalidates and flushes
    is the buffers are big enough. Thus we just increase the threshold
    to 16 MB instead of the previous 2 MB.
    
    Change-Id: I3648dd820051535cbc5e9390a0194b4e2041cff0
    Signed-off-by: Alex Waterman <alexw@nvidia.com>
    Signed-off-by: Rich Wiley <rwiley@nvidia.com>
    Reviewed-on: http://git-master/r/407216
    Reviewed-by: Krishna Reddy <vdumpa@nvidia.com>

--- drivers/video/tegra/nvmap/nvmap_dev.c
+++ drivers/video/tegra/nvmap/nvmap_dev.c
@@ -52,7 +52,11 @@
 #define NVMAP_CARVEOUT_KILLER_RETRY_TIME 100 /* msecs */
 
 /* this is basically the L2 cache size */
+#ifdef CONFIG_DENVER_CPU
+size_t cache_maint_inner_threshold = SZ_2M * 8;
+#else
 size_t cache_maint_inner_threshold = SZ_2M;
+#endif
 
 #ifdef CONFIG_NVMAP_OUTER_CACHE_MAINT_BY_SET_WAYS
 size_t cache_maint_outer_threshold = SZ_1M;
