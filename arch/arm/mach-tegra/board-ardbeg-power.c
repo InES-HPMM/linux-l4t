7ac1eabaa63dc4d219751aef92916c119e89bafb
Hridya <hvalsaraju@nvidia.com>
2014-05-14 11:54:16 -0700

    arm: tegra: Enable Power Features For E1765
    Bug 1513802
    
    Change-Id: I9db782c3f014f7fdef0ae3bc68fd0bca8bd46acf
    Signed-off-by: Hridya <hvalsaraju@nvidia.com>
    Reviewed-on: http://git-master/r/409709
    (cherry picked from commit 3e53778a572f06ce86d23bbd0fb338162d3b51d9)
    Reviewed-on: http://git-master/r/414767
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Diwakar Tundlam <dtundlam@nvidia.com>
    Tested-by: Diwakar Tundlam <dtundlam@nvidia.com>

--- arch/arm/mach-tegra/board-ardbeg-power.c
+++ arch/arm/mach-tegra/board-ardbeg-power.c
@@ -354,6 +354,7 @@ int __init ardbeg_regulator_init(void)
 	case BOARD_E1936:
 	case BOARD_E1769:
 	case BOARD_P1761:
+	case BOARD_P1765:
 		tn8_regulator_init();
 		return 0;
 	default:
@@ -829,6 +830,7 @@ int __init ardbeg_soctherm_init(void)
 	/* Bowmore and P1761 are T132 platforms */
 	if (board_info.board_id == BOARD_E1971 ||
 			board_info.board_id == BOARD_P1761 ||
+			board_info.board_id == BOARD_P1765 ||
 			board_info.board_id == BOARD_E1991) {
 		cpu_edp_temp_margin = t13x_cpu_edp_temp_margin;
 		gpu_edp_temp_margin = t13x_gpu_edp_temp_margin;
@@ -876,6 +878,7 @@ int __init ardbeg_soctherm_init(void)
 	}
 
 	if (board_info.board_id == BOARD_P1761 ||
+		board_info.board_id == BOARD_P1765 ||
 		board_info.board_id == BOARD_E1784 ||
 		board_info.board_id == BOARD_E1971 ||
 		board_info.board_id == BOARD_E1991 ||
@@ -900,6 +903,7 @@ int __init ardbeg_soctherm_init(void)
 		 pmu_board_info.board_id == BOARD_E1736 ||
 		 pmu_board_info.board_id == BOARD_E1769 ||
 		 pmu_board_info.board_id == BOARD_P1761 ||
+		 pmu_board_info.board_id == BOARD_P1765 ||
 		 pmu_board_info.board_id == BOARD_E1936)
 		ardbeg_soctherm_data.tshut_pmu_trip_data = &tpdata_palmas;
 	else
@@ -915,6 +919,7 @@ int __init ardbeg_soctherm_init(void)
 		break;
 	case BOARD_P1761:
 	case BOARD_E1936:
+	case BOARD_P1765:
 		memcpy(&ardbeg_soctherm_data.throttle[THROTTLE_OC4],
 		       &battery_oc_throttle,
 		       sizeof(battery_oc_throttle));
--- arch/arm/mach-tegra/tegra-board-id.h
+++ arch/arm/mach-tegra/tegra-board-id.h
@@ -39,6 +39,7 @@
 #define BOARD_E1782   0x06F6
 #define BOARD_P2530   0x09E2
 #define BOARD_P1761   0x06E1
+#define BOARD_P1765   0x06E5
 #define BOARD_E1762   0x06E2
 #define BOARD_E1791   0x06ff
 #define BOARD_E1784   0x06F8
