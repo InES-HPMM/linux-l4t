fcadb2203523168912113acf8d0529bd9632de39
Amlan Kundu <akundu@nvidia.com>
2014-05-16 16:24:02 +0530

    drivers: spi: Add qspi controller driver
    This driver is derived from tegra spi master driver.
    The changes include new controller driver for quad spi.
    The driver supports programming X1/X2/X4, SDR/DDR, Dummy cycle,
    DMA and PIO mode. Any request to the controller driver should combine
    multiple transactions (command/address/data) into single message.
    
    Bug 1375793
    
    Change-Id: If6b1689ed6b6b93c07ea54d1cfee773f1ce34be1
    Signed-off-by: Amlan Kundu <akundu@nvidia.com>
    Reviewed-on: http://git-master/r/405745
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>
    Reviewed-by: Ashutosh Patel <ashutoshp@nvidia.com>

---
+++ include/linux/spi/qspi-tegra.h
@@ -0,0 +1,84 @@
+/*
+ * qspi-tegra.h: SPI interface for Nvidia Tegra210 QSPI controller.
+ *
+ * Copyright (C) 2011-2014 NVIDIA Corporation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef _LINUX_QSPI_TEGRA_H
+#define _LINUX_QSPI_TEGRA_H
+
+struct tegra_qspi_platform_data {
+	int dma_req_sel;
+	u32 qspi_max_frequency;
+	bool is_clkon_always;
+};
+
+/*
+ * Controller data from device to pass some info like
+ * bus width, mode, tap delay etc.
+ */
+struct tegra_qspi_device_controller_data {
+	bool is_hw_based_cs;
+	int cs_setup_clk_count;
+	int cs_hold_clk_count;
+	int rx_clk_tap_delay;
+	int tx_clk_tap_delay;
+	u32 x1_len_limit;
+	u32 x1_bus_speed;
+	u32 x1_dymmy_cycle;
+	u32 x4_bus_speed;
+	u32 x4_dymmy_cycle;
+	bool x4_is_ddr;
+};
+
+enum qspi_operation_mode {
+	NORMAL_READ,
+	FAST_READ,
+	DUAL_OUT_READ,
+	QUAD_OUT_READ,
+	DUAL_IO_READ,
+	QUAD_IO_READ,
+	DDR_FAST_READ,
+	DDR_DUAL_IO_READ,
+	DDR_QUAD_IO_READ,
+	PAGE_PROGRAM,
+	QUAD_PAGE_PROGRAM,
+	OPERATION_MAX_LIMIT,
+};
+
+enum qspi_bus_width {
+	X1,
+	X2,
+	X4,
+};
+
+/* Bits 11,10,9,8 for op mode */
+#define get_op_mode(x)	(((x) >> 8) & 0xF)
+#define set_op_mode(x)	(((x) & 0xF) << 8)
+
+/* Use bit 13,12 for x1/x2/x4 */
+#define get_bus_width(x)  (((x) >> 12) & 0x3)
+#define set_bus_width(x)  (((x) & 0x3) << 12)
+
+/* Set bit 14 for ddr */
+#define get_sdr_ddr(x)  (((x) >> 14) & 0x1)
+#define set_sdr_ddr	(1 << 14)
+
+#define get_dummy_cyl(x) ((x) & 0xff)
+#define set_dummy_cyl(x) ((x) & 0xff)
+
+#endif /* _LINUX_QSPI_TEGRA_H */
