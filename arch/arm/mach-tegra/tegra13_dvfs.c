536a37d7c99c9401f875248dcd86c0f2f0ba97fe
Alex Frid <afrid@nvidia.com>
2014-06-03 23:54:39 -0700

    ARM: tegra: dvfs: Update DFLL tune parameters
    - Changed Tune1 parameter according to recent characterization results
    - Made Tune0 selection forward looking: applied current settings to
    possible future speedo ids (if any is introduced).
    
    Bug 1442659
    
    Change-Id: I3430fc43a3ff045fd6fbcbfddbe5feda4671a727
    Signed-off-by: Alex Frid <afrid@nvidia.com>
    Reviewed-on: http://git-master/r/418775
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Seema Khowala <seemaj@nvidia.com>
    Tested-by: Seema Khowala <seemaj@nvidia.com>
    Reviewed-by: Yu-Huan Hsu <yhsu@nvidia.com>

--- arch/arm/mach-tegra/tegra13_dvfs.c
+++ arch/arm/mach-tegra/tegra13_dvfs.c
@@ -226,7 +226,7 @@ static struct cpu_cvb_dvfs cpu_cvb_dvfs_table[] = {
 		.speedo_id = 1,
 		.process_id = -1,
 		.dfll_tune_data  = {
-			.tune1		= 0x000000FF,
+			.tune1		= 0x00000099,
 			.droop_rate_min = 1000000,
 			.tune_high_min_millivolts = 900,
 			.min_millivolts = 700,
@@ -734,7 +734,7 @@ static int round_voltage(int mv, struct rail_alignment *align, bool up)
 
 static void __init set_cpu_dfll_tuning_data(struct cpu_cvb_dvfs *d, int speedo)
 {
-	if (d->speedo_id == 1) {
+	if (d->speedo_id >= 1) {
 		if (speedo <= 2336) {
 			d->dfll_tune_data.tune0 = 0x9315FF;
 			d->dfll_tune_data.tune0_high_mv = 0x9340FF;
