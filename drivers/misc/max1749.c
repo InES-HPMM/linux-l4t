d42e5af670780c7ba970e99da295a2b132cd4496
Laxman Dewangan <ldewangan@nvidia.com>
2014-06-17 20:31:20 +0530

    misc: max1749: cleanups and DT porting on driver
    Done multiple cleanups on driver as:
    - Remove global driver data.
    - Port for DT.
    - Remove boilerplate module initialisation.
    - Use devm_* for simplify error return path.
    
    Change-Id: I317c7057f59103a6cd9abe86448b500c55f0774d
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Reviewed-on: http://git-master/r/424265
    GVS: Gerrit_Virtual_Submit

--- drivers/misc/max1749.c
+++ drivers/misc/max1749.c
@@ -24,6 +24,8 @@
 #include <linux/regulator/consumer.h>
 #include <linux/kernel.h>
 #include <linux/platform_device.h>
+#include <linux/of.h>
+#include <linux/of_platform.h>
 #include <linux/err.h>
 #include <linux/hrtimer.h>
 #include <linux/delay.h>
@@ -33,44 +35,52 @@
 #include "../staging/android/timed_output.h"
 
 struct vibrator_data {
-	struct timed_output_dev dev;
+	struct device *dev;
+	struct timed_output_dev to_dev;
 	struct hrtimer timer;
 	struct regulator *regulator;
 	struct work_struct work;
 	bool vibrator_on;
 };
 
-static struct vibrator_data *data;
-
-
-static void vibrator_start(void)
+static void vibrator_start(struct vibrator_data *vdata)
 {
-	if (!data->vibrator_on) {
-		regulator_enable(data->regulator);
-		data->vibrator_on = true;
+	if (!vdata->vibrator_on) {
+		int ret;
+
+		ret = regulator_enable(vdata->regulator);
+		if (ret < 0)
+			dev_err(vdata->dev, "Regulator can not enabled: %d\n",
+				ret);
+		vdata->vibrator_on = true;
 	}
 }
 
-static void vibrator_stop(void)
+static void vibrator_stop(struct vibrator_data *vdata)
 {
 	int ret;
-	if (data->vibrator_on) {
-		ret = regulator_is_enabled(data->regulator);
+
+	if (vdata->vibrator_on) {
+		ret = regulator_is_enabled(vdata->regulator);
 		if (ret > 0) {
-			regulator_disable(data->regulator);
-			data->vibrator_on = false;
+			regulator_disable(vdata->regulator);
+			vdata->vibrator_on = false;
 		}
 	}
 }
 
 static void vibrator_work_func(struct work_struct *work)
 {
-	vibrator_stop();
+	struct vibrator_data *vdata = container_of(work, struct vibrator_data,
+					work);
+	vibrator_stop(vdata);
 }
 
 static enum hrtimer_restart vibrator_timer_func(struct hrtimer *timer)
 {
-	schedule_work(&data->work);
+	struct vibrator_data *vdata = container_of(timer, struct vibrator_data,
+					timer);
+	schedule_work(&vdata->work);
 	return HRTIMER_NORESTART;
 }
 
@@ -79,27 +89,35 @@ static enum hrtimer_restart vibrator_timer_func(struct hrtimer *timer)
  * created by timed_output_dev.
  * echo 100 > /sys/class/timed_output/vibrator/enable
  */
-static void vibrator_enable(struct timed_output_dev *dev, int value)
+static void vibrator_enable(struct timed_output_dev *to_dev, int value)
 {
-	hrtimer_cancel(&data->timer);
+	struct vibrator_data *vdata = container_of(to_dev, struct vibrator_data,
+						to_dev);
+
+	hrtimer_cancel(&vdata->timer);
 	if (value > 0) {
-		vibrator_start();
-		hrtimer_start(&data->timer,
+		vibrator_start(vdata);
+		hrtimer_start(&vdata->timer,
 			  ktime_set(value / 1000, (value % 1000) * 1000000),
 			  HRTIMER_MODE_REL);
-	} else
-		vibrator_stop();
+	} else {
+		vibrator_stop(vdata);
+	}
 	return;
 }
 
-static int vibrator_get_time(struct timed_output_dev *dev)
+static int vibrator_get_time(struct timed_output_dev *to_dev)
 {
-	if (hrtimer_active(&data->timer)) {
-		ktime_t r = hrtimer_get_remaining(&data->timer);
+	struct vibrator_data *vdata = container_of(to_dev, struct vibrator_data,
+						to_dev);
+
+	if (hrtimer_active(&vdata->timer)) {
+		ktime_t r = hrtimer_get_remaining(&vdata->timer);
 		struct timeval t = ktime_to_timeval(r);
 		return t.tv_sec * 1000 + t.tv_usec / 1000;
-	} else
-		return 0;
+	}
+
+	return 0;
 }
 
 static struct timed_output_dev vibrator_dev = {
@@ -110,46 +128,53 @@ static struct timed_output_dev vibrator_dev = {
 
 static int vibrator_probe(struct platform_device *pdev)
 {
+	struct vibrator_data *vdata;
+	struct device *dev = &pdev->dev;
 	int ret;
-	data = kzalloc(sizeof(struct vibrator_data), GFP_KERNEL);
-	if (!data)
+
+	vdata = devm_kzalloc(&pdev->dev, sizeof(*vdata), GFP_KERNEL);
+	if (!vdata)
 		return -ENOMEM;
 
-	data->regulator = regulator_get(NULL, "vdd_vbrtr");
-	if (IS_ERR_OR_NULL(data->regulator)) {
-		pr_err("vibrator_init:Couldn't get regulator vdd_vbrtr\n");
-		data->regulator = NULL;
-		ret = PTR_ERR(data->regulator);
-		goto err;
+	vdata->regulator = devm_regulator_get(&pdev->dev, "vdd-vbrtr");
+	if (IS_ERR(vdata->regulator)) {
+		ret = PTR_ERR(vdata->regulator);
+		dev_err(dev, "Couldn't get regulator vdd-vbrtr: %d\n", ret);
+		return ret;
 	}
-	hrtimer_init(&data->timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
-	/* Intialize the work queue */
-	INIT_WORK(&data->work, vibrator_work_func);
-	data->timer.function = vibrator_timer_func;
-	data->dev = vibrator_dev;
-	data->vibrator_on = false;
 
-	ret = timed_output_dev_register(&data->dev);
-	if (ret)
-		goto err2;
+	vdata->dev = &pdev->dev;
+	platform_set_drvdata(pdev, vdata);
+
+	hrtimer_init(&vdata->timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
+
+	/* Intialize the work queue */
+	INIT_WORK(&vdata->work, vibrator_work_func);
+	vdata->timer.function = vibrator_timer_func;
+	vdata->to_dev = vibrator_dev;
+	vdata->vibrator_on = false;
+	ret = timed_output_dev_register(&vdata->to_dev);
+	if (ret < 0) {
+		dev_err(dev, "Timeout device registration failed: %d\n", ret);
+		return ret;
+	}
 
 	return 0;
-
-err2:
-	regulator_put(data->regulator);
-err:
-	kfree(data);
-	return ret;
 }
 
 static int vibrator_remove(struct platform_device *pdev)
 {
-	timed_output_dev_unregister(&data->dev);
-	regulator_put(data->regulator);
-	kfree(data);
+	struct vibrator_data *vdata = platform_get_drvdata(pdev);
 
+	timed_output_dev_unregister(&vdata->to_dev);
 	return 0;
 }
+
+static struct of_device_id of_vibrator_match_tbl[] = {
+	{ .compatible = "nvidia,vibrator", },
+	{ /* end */ }
+};
+MODULE_DEVICE_TABLE(of, of_vibrator_match_tbl);
 
 static struct platform_driver vibrator_driver = {
 	.probe = vibrator_probe,
@@ -157,21 +182,11 @@ static struct platform_driver vibrator_driver = {
 	.driver = {
 		.name = "tegra-vibrator",
 		.owner = THIS_MODULE,
+		.of_match_table =  of_vibrator_match_tbl,
 	},
 };
 
-static int __init vibrator_init(void)
-{
-	return platform_driver_register(&vibrator_driver);
-}
-
-static void __exit vibrator_exit(void)
-{
-	platform_driver_unregister(&vibrator_driver);
-}
+module_platform_driver(vibrator_driver);
 
 MODULE_DESCRIPTION("timed output vibrator device");
 MODULE_AUTHOR("GPL");
-
-module_init(vibrator_init);
-module_exit(vibrator_exit);
