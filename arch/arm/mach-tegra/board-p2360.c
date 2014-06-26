bb2d7ba7135be437b8198d23136273b8295866b7
Krishna Reddy <vdumpa@nvidia.com>
2014-06-26 14:11:05 -0700

    Revert "arm: tegra: Remove redundant initialization"
    This reverts commit ff0de49b0dc9a365b23d0706a5384d186da94a8e.
    
    Bug 1528528
    
    Change-Id: I50bc347dbcbbac00f385a4092dfacc51b2cacb5a
    Signed-off-by: Krishna Reddy <vdumpa@nvidia.com>
    Reviewed-on: http://git-master/r/429083
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>
    Tested-by: Bharat Nihalani <bnihalani@nvidia.com>

--- arch/arm/mach-tegra/board-p2360.c
+++ arch/arm/mach-tegra/board-p2360.c
@@ -504,6 +504,10 @@ static void __init tegra_p2360_dt_init(void)
 	tegra_get_display_board_info(&display_board_info);
 
 	tegra_p2360_early_init();
+#ifdef CONFIG_NVMAP_USE_CMA_FOR_CARVEOUT
+	carveout_linear_set(&tegra_generic_cma_dev);
+	carveout_linear_set(&tegra_vpr_cma_dev);
+#endif
 #ifdef CONFIG_USE_OF
 	of_platform_populate(NULL,
 		of_default_bus_match_table, p2360_auxdata_lookup,
