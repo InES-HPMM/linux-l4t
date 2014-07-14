661fa17f929bd7950b63b0fe6bb77783584cb694
Mallikarjun Kasoju <mkasoju@nvidia.com>
2014-07-14 15:54:03 +0530

    thermal: Add Maxim 77620 thermal support
    Maxim 77620 pmic chip supports hot die interrupt configurable for
    two different critical temperature thresholds.
    
    Bug 1477154
    
    Change-Id: Idd8cf74608208d3a0be1628fcef563ef94a62e8f
    Signed-off-by: Mallikarjun Kasoju <mkasoju@nvidia.com>
    Reviewed-on: http://git-master/r/437317
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>

---
+++ drivers/thermal/max77620-thermal.c
@@ -0,0 +1,164 @@
+/*
+ * Driver for Maxim 77620 thermal
+ *
+ * Copyright (c) 2014, NVIDIA Corporation. All rights reserved.
+ *
+ * Author: Mallikarjun Kasoju <mkasoju@nvidia.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation version 2.
+ *
+ * This program is distributed "as is" WITHOUT ANY WARRANTY of any kind,
+ * whether express or implied; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307, USA
+ */
+
+#include <linux/module.h>
+#include <linux/err.h>
+#include <linux/irq.h>
+#include <linux/interrupt.h>
+#include <linux/platform_device.h>
+#include <linux/pm.h>
+#include <linux/thermal.h>
+#include <linux/of.h>
+#include <linux/mfd/max77620.h>
+
+#define MAX77620_NORMAL_OPERATING_TEMP 100000
+
+struct max77620_therm_zone {
+	struct device			*dev;
+	struct max77620_chip			*max77620;
+	struct thermal_zone_device	*tz_device;
+	int				irq;
+	long				hd_threshold_temp;
+};
+
+static int max77620_thermal_read_temp(void *data, long *temp)
+{
+	struct max77620_therm_zone *ptherm_zone = data;
+	u8 val;
+	int ret;
+
+	ret = max77620_reg_read(ptherm_zone->max77620->dev,
+				MAX77620_PWR_SLAVE,
+				MAX77620_REG_STATLBT, &val);
+	if (ret < 0) {
+		dev_err(ptherm_zone->dev,
+			"%s: Failed to read STATLBT, %d\n",
+			__func__, ret);
+		return -EINVAL;
+	}
+
+	if ((val & MAX77620_IRQ_TJALRM1_MASK) ||
+		(val & MAX77620_IRQ_TJALRM2_MASK))
+		*temp = ptherm_zone->hd_threshold_temp + 1;
+	else
+		*temp = MAX77620_NORMAL_OPERATING_TEMP;
+
+	return 0;
+}
+
+static irqreturn_t max77620_thermal_irq(int irq, void *data)
+{
+	struct max77620_therm_zone *ptherm_zone = data;
+
+	thermal_zone_device_update(ptherm_zone->tz_device);
+	return IRQ_HANDLED;
+}
+
+static int max77620_thermal_probe(struct platform_device *pdev)
+{
+	struct max77620_chip *max77620 = dev_get_drvdata(pdev->dev.parent);
+	struct device_node *np = pdev->dev.parent->of_node;
+	struct max77620_therm_zone *ptherm_zone;
+	u32 hd_threshold_temp = 0;
+	u32 pval;
+	int ret;
+
+	if (np) {
+		ret = of_property_read_u32(np,
+			"maxim,hot-die-threshold-temp", &pval);
+		if (!ret)
+			hd_threshold_temp = pval;
+	}
+
+	if (!hd_threshold_temp) {
+		dev_err(&pdev->dev, "Hot die temp is not provided\n");
+		return -EINVAL;
+	}
+
+	ptherm_zone = devm_kzalloc(&pdev->dev, sizeof(*ptherm_zone),
+			GFP_KERNEL);
+	if (!ptherm_zone) {
+		dev_err(&pdev->dev, "No available free memory\n");
+		return -ENOMEM;
+	}
+
+	platform_set_drvdata(pdev, ptherm_zone);
+	ptherm_zone->dev = &pdev->dev;
+	ptherm_zone->max77620 = max77620;
+	ptherm_zone->hd_threshold_temp = hd_threshold_temp;
+
+	ptherm_zone->tz_device = thermal_zone_of_sensor_register(&pdev->dev, 0,
+					ptherm_zone, max77620_thermal_read_temp,
+					NULL);
+	if (IS_ERR(ptherm_zone->tz_device)) {
+		ret = PTR_ERR(ptherm_zone->tz_device);
+		dev_err(ptherm_zone->dev,
+			"Device can not register as thermal sensor: %d\n", ret);
+		return ret;
+	}
+
+	if (hd_threshold_temp <= 120000UL)
+		ptherm_zone->irq = platform_get_irq(pdev, 0);
+	else
+		ptherm_zone->irq = platform_get_irq(pdev, 1);
+
+	ret = request_threaded_irq(ptherm_zone->irq, NULL,
+			max77620_thermal_irq,
+			IRQF_ONESHOT, dev_name(&pdev->dev),
+			ptherm_zone);
+	if (ret < 0) {
+		dev_err(&pdev->dev,
+			"request irq %d failed: %dn", ptherm_zone->irq, ret);
+		goto int_req_failed;
+	}
+
+	return 0;
+
+int_req_failed:
+	thermal_zone_of_sensor_unregister(&pdev->dev, ptherm_zone->tz_device);
+	return ret;
+}
+
+static int max77620_thermal_remove(struct platform_device *pdev)
+{
+	struct max77620_therm_zone *ptherm_zone = platform_get_drvdata(pdev);
+
+	free_irq(ptherm_zone->irq, ptherm_zone);
+	thermal_zone_of_sensor_unregister(&pdev->dev, ptherm_zone->tz_device);
+	return 0;
+}
+
+static struct platform_driver max77620_thermal_driver = {
+	.probe = max77620_thermal_probe,
+	.remove = max77620_thermal_remove,
+	.driver = {
+		.name = "max77620-thermal",
+		.owner = THIS_MODULE,
+	},
+};
+
+module_platform_driver(max77620_thermal_driver);
+
+MODULE_DESCRIPTION("Max77620 Thermal driver");
+MODULE_AUTHOR("Mallikarjun Kasoju<mkasoju@nvidia.com>");
+MODULE_ALIAS("platform:max77620-thermal");
+MODULE_LICENSE("GPL v2");
