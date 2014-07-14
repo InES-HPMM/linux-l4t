3d4bef5a9522c96163b5ef6b89ba855342908a46
Will Deacon <will.deacon@arm.com>
2014-07-14 19:47:39 +0100

    iommu/arm-smmu: allow translation stage to be forced on the cmdline
    When debugging and testing code on an SMMU that supports nested
    translation, it can be useful to restrict the driver to a particular
    stage of translation.
    
    This patch adds a module parameter to the ARM SMMU driver to allow this
    by restricting the ability of the probe() code to detect support for
    only the specified stage.
    
    Change-Id: Ib361a8932bc811c7050787a7a1ce9d0929f58e57
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    (cherry-picked from next-20141106)
    Signed-off-by: Hiroshi Doyu <hdoyu@nvidia.com>
    Reviewed-by: Sri Krishna Chowdary <schowdary@nvidia.com>
    Reviewed-on: http://git-master/r/594882

--- drivers/iommu/arm-smmu.c
+++ drivers/iommu/arm-smmu.c
@@ -338,6 +338,11 @@
 
 #define FSYNR0_WNR			(1 << 4)
 
+static int force_stage;
+module_param_named(force_stage, force_stage, int, S_IRUGO | S_IWUSR);
+MODULE_PARM_DESC(force_stage,
+	"Force SMMU mappings to be installed at a particular stage of translation. A value of '1' or '2' forces the corresponding stage. All other values are ignored (i.e. no stage is forced). Note that selecting a specific stage will disable support for nested translation.");
+
 struct arm_smmu_smr {
 	u8				idx;
 	u16				mask;
@@ -2039,6 +2044,13 @@ static int arm_smmu_device_cfg_probe(struct arm_smmu_device *smmu)
 		return -ENODEV;
 	}
 #endif
+
+	/* Restrict available stages based on module parameter */
+	if (force_stage == 1)
+		id &= ~(ID0_S2TS | ID0_NTS);
+	else if (force_stage == 2)
+		id &= ~(ID0_S1TS | ID0_NTS);
+
 	if (id & ID0_S1TS) {
 		smmu->features |= ARM_SMMU_FEAT_TRANS_S1;
 		dev_notice(smmu->dev, "\tstage 1 translation\n");
@@ -2055,8 +2067,7 @@ static int arm_smmu_device_cfg_probe(struct arm_smmu_device *smmu)
 	}
 
 	if (!(smmu->features &
-		(ARM_SMMU_FEAT_TRANS_S1 | ARM_SMMU_FEAT_TRANS_S2 |
-		 ARM_SMMU_FEAT_TRANS_NESTED))) {
+		(ARM_SMMU_FEAT_TRANS_S1 | ARM_SMMU_FEAT_TRANS_S2))) {
 		dev_err(smmu->dev, "\tno translation support!\n");
 		return -ENODEV;
 	}
