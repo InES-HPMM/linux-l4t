a1c21cda1f46f5bc03c8ef69534af349a7aa751c
Sang-Hun Lee <sanlee@nvidia.com>
2014-07-17 23:39:57 -0400

    mmc: tegra: unlock mutex before returning
    To avoid hanging the entire system, unlock mutex lock before
    returning
    
    Bug 1526837
    
    Change-Id: I523b5ad2ea7787ca780163e1e1db91c475c81ce1
    Signed-off-by: Sang-Hun Lee <sanlee@nvidia.com>
    Reviewed-on: http://git-master/r/439707
    Reviewed-by: Mitch Luban <mluban@nvidia.com>
    Tested-by: Mitch Luban <mluban@nvidia.com>

--- drivers/mmc/host/sdhci-tegra.c
+++ drivers/mmc/host/sdhci-tegra.c
@@ -1554,6 +1554,7 @@ static void tegra_sdhci_set_clock(struct sdhci_host *sdhci, unsigned int clock)
 			if (ret) {
 				dev_err(mmc_dev(sdhci->mmc),
 				"clock enable is failed, ret: %d\n", ret);
+				mutex_unlock(&tegra_host->set_clock_mutex);
 				return;
 			}
 			tegra_host->clk_enabled = true;
@@ -1569,6 +1570,7 @@ static void tegra_sdhci_set_clock(struct sdhci_host *sdhci, unsigned int clock)
 			if (ret) {
 				dev_err(mmc_dev(sdhci->mmc),
 				"clock enable is failed, ret: %d\n", ret);
+				mutex_unlock(&tegra_host->set_clock_mutex);
 				return;
 			}
 			tegra_host->is_sdmmc_emc_clk_on = true;
@@ -1578,6 +1580,7 @@ static void tegra_sdhci_set_clock(struct sdhci_host *sdhci, unsigned int clock)
 			if (ret) {
 				dev_err(mmc_dev(sdhci->mmc),
 				"clock enable is failed, ret: %d\n", ret);
+				mutex_unlock(&tegra_host->set_clock_mutex);
 				return;
 			}
 			tegra_host->is_sdmmc_sclk_on = true;
