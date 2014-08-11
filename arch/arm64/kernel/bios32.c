de30e7490fcdf1fae40dfb1dc4c4c2521a70f39e
Vidya Sagar <vidyas@nvidia.com>
2014-08-11 11:42:59 +0530

    ARM: pci: create pci_common_init_dev()
    When working with device tree support for PCI on ARM you run
    into a problem when mapping IRQs from the device tree irqmaps:
    doing this the code in drivers/of/of_pci_irq.c will try to
    find the OF node on the root bridge and this fails, because
    bus->dev.of_node is NULL, and that in turn boils down to
    the fact that pci_set_bus_of_node() has called
    pcibios_get_phb_of_node() from drivers/pci/of.c to obtain
    the OF node of the bridge or its parent and none is set
    and thus NULL is returned.
    Fix this by adding an additional parent argument API for
    registering PCI bridges on the ARM architecture called
    pci_common_init_dev(), and pass along this parent to
    pci_scan_root_bus() called from pcibios_init_hw() in
    bios32.c and voila: the IRQ mappings start working:
    the OF node can be retrieved from the parent.
    Create the old pci_common_init() as a wrapper around
    the new call.
    
    vidyas: make similar change for ARM64
    based on upstream commit 14d86e725ed034917bc721cf5deea019857b6cf0
    
    Bug 1496843
    
    Change-Id: Ic3bb98f059e9d42396956cf166ccc06b84b70c92
    Signed-off-by: Vidya Sagar <vidyas@nvidia.com>
    Reviewed-on: http://git-master/r/454896
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>

--- arch/arm64/kernel/bios32.c
+++ arch/arm64/kernel/bios32.c
@@ -466,7 +466,8 @@ static int __init pcibios_init_resources(int busnr, struct pci_sys_data *sys)
 	return 0;
 }
 
-static void __init pcibios_init_hw(struct hw_pci *hw, struct list_head *head)
+static void pcibios_init_hw(struct device *parent, struct hw_pci *hw,
+			    struct list_head *head)
 {
 	struct pci_sys_data *sys = NULL;
 	int ret;
@@ -503,7 +504,7 @@ static void __init pcibios_init_hw(struct hw_pci *hw, struct list_head *head)
 			if (hw->scan)
 				sys->bus = hw->scan(nr, sys);
 			else
-				sys->bus = pci_scan_root_bus(NULL, sys->busnr,
+				sys->bus = pci_scan_root_bus(parent, sys->busnr,
 						hw->ops, sys, &sys->resources);
 
 			if (!sys->bus)
@@ -520,7 +521,7 @@ static void __init pcibios_init_hw(struct hw_pci *hw, struct list_head *head)
 	}
 }
 
-void __init pci_common_init(struct hw_pci *hw)
+void pci_common_init_dev(struct device *parent, struct hw_pci *hw)
 {
 	struct pci_sys_data *sys;
 	LIST_HEAD(head);
@@ -528,7 +529,7 @@ void __init pci_common_init(struct hw_pci *hw)
 	pci_add_flags(PCI_REASSIGN_ALL_RSRC);
 	if (hw->preinit)
 		hw->preinit();
-	pcibios_init_hw(hw, &head);
+	pcibios_init_hw(parent, hw, &head);
 	if (hw->postinit)
 		hw->postinit();
 
