d60d75efd249c60d61ce75db71fb0789cbce3c8a
Sai Gurrappadi <sgurrappadi@nvidia.com>
2014-08-12 13:32:12 -0700

    drivers: cpuidle: Added release for file ops
    Added release functions for the debugfs file operators so that memory is
    properly freed.
    
    Change-Id: Iac0684189095b8ad4cf9107945a4b3c1156673a3
    Signed-off-by: Sai Gurrappadi <sgurrappadi@nvidia.com>
    (cherry picked from commit ade5f1f71bd2854e33425b801836eab3cb3a6e0b)
    Reviewed-on: http://git-master/r/455761
    Reviewed-by: Sivaram Nair <sivaramn@nvidia.com>
    Tested-by: Sivaram Nair <sivaramn@nvidia.com>

--- drivers/cpuidle/cpuidle-tegra210.c
+++ drivers/cpuidle/cpuidle-tegra210.c
@@ -684,6 +684,7 @@ static const struct file_operations fast_cluster_enable_fops = {
 	.read	=	seq_read,
 	.llseek	=	seq_lseek,
 	.write	=	fast_enable_write,
+	.release =	single_release,
 };
 
 static int slow_enable_show(struct seq_file *s, void *data)
@@ -714,6 +715,7 @@ static const struct file_operations slow_cluster_enable_fops = {
 	.read	=	seq_read,
 	.llseek	=	seq_lseek,
 	.write	=	slow_enable_write,
+	.release =	single_release,
 };
 
 static void suspend_all_device_irqs(void)
