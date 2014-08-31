06fb9da2ae62efbf313541c98c614afe2400fd7f
Alex Frid <afrid@nvidia.com>
2014-08-30 20:28:00 -0700

    clock: tegra: Add default PLL MDIV setting
    Added default settings for PLL input divider MDIV. If specified this
    setting is used in fixed input divider configuration (otherwise, keep
    maximum possible MDIV value in fixed configuration).
    
    Change-Id: I98968c2b5fa00696a0d3344aece1800c8a512554
    Signed-off-by: Alex Frid <afrid@nvidia.com>
    Reviewed-on: http://git-master/r/494289
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Bo Yan <byan@nvidia.com>

--- arch/arm/mach-tegra/clock.h
+++ arch/arm/mach-tegra/clock.h
@@ -271,6 +271,7 @@ struct clk {
 			unsigned long			cf_max;
 			unsigned long			vco_min;
 			unsigned long			vco_max;
+			u16				mdiv_default;
 			u8				cpcon_default;
 			const struct clk_pll_freq_table	*freq_table;
 			int				lock_delay;
