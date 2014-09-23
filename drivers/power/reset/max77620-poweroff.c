5d9a4663209cc0cb182fd8901ab89ca68998b622
Mallikarjun Kasoju <mkasoju@nvidia.com>
2014-09-23 06:53:16 +0530

    power: reset: max77620: Add power on through RTC expire
    Add support to power on system through PMIC RTC event
    
    Bug 200033812
    
    Change-Id: I86876f162e440ad35bd15751195be3d38b1cec7d
    Signed-off-by: Mallikarjun Kasoju <mkasoju@nvidia.com>
    Reviewed-on: http://git-master/r/501428
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>

--- drivers/power/reset/max77620-poweroff.c
+++ drivers/power/reset/max77620-poweroff.c
@@ -22,7 +22,13 @@
 #include <linux/platform_device.h>
 #include <linux/power/reset/system-pmic.h>
 #include <linux/slab.h>
+#include <linux/delay.h>
 #include <linux/mfd/max77620.h>
+
+#define MAX77620_PM_RTC_AUTO_CLEAR_MASK	(1 << 1)
+#define MAX77620_PM_RTC_WAKE_MASK			(1 << 3)
+#define MAX77620_PM_RTC_RB_UPDATE_MASK		(1 << 4)
+#define MAX77620_PM_RTC_INT1_MASK			(1 << 1)
 
 struct max77620_poweroff {
 	struct device *dev;
@@ -30,7 +36,96 @@ struct max77620_poweroff {
 	struct system_pmic_dev *system_pmic_dev;
 	bool use_power_off;
 	bool use_power_reset;
+	bool need_rtc_power_on;
 };
+
+static void max77620_auto_power_on(struct max77620_poweroff *max77620_poweroff)
+{
+	int ret;
+	u8 reg_val;
+	u8 val =  MAX77620_PM_RTC_AUTO_CLEAR_MASK | MAX77620_PM_RTC_WAKE_MASK |
+			MAX77620_PM_RTC_RB_UPDATE_MASK;
+
+	dev_info(max77620_poweroff->dev, "Resetting through Max77620 RTC\n");
+
+	ret = max77620_reg_write(max77620_poweroff->max77620->dev,
+						MAX77620_RTC_SLAVE,
+						MAX77620_REG_RTCUPDATE0, val);
+	if (ret < 0) {
+		dev_err(max77620_poweroff->dev,
+			"MAX77620_REG_RTCUPDATE0 write failed\n");
+		goto scrub;
+	}
+
+	/* Must wait 16ms for buffer update */
+	usleep_range(16000, 16000);
+
+	ret = max77620_reg_read(max77620_poweroff->max77620->dev,
+			MAX77620_RTC_SLAVE,
+			MAX77620_REG_RTCINTM, &reg_val);
+	if (ret < 0) {
+		dev_err(max77620_poweroff->dev,
+			"Failed to read MAX77620_REG_RTCINTM: %d\n", ret);
+		goto scrub;
+	}
+
+	reg_val = (reg_val & ~MAX77620_PM_RTC_INT1_MASK);
+	ret = max77620_reg_writes(max77620_poweroff->max77620->dev,
+			MAX77620_RTC_SLAVE, MAX77620_REG_RTCINTM, 1, &reg_val);
+	if (ret < 0) {
+		dev_err(max77620_poweroff->dev,
+			"Failed to write MAX77620_REG_RTCINTM: %d\n", ret);
+		goto scrub;
+	}
+
+	val =  MAX77620_PM_RTC_AUTO_CLEAR_MASK | MAX77620_PM_RTC_WAKE_MASK |
+			MAX77620_PM_RTC_RB_UPDATE_MASK;
+	ret = max77620_reg_write(max77620_poweroff->max77620->dev,
+						MAX77620_RTC_SLAVE,
+						MAX77620_REG_RTCUPDATE0, val);
+	if (ret < 0) {
+		dev_err(max77620_poweroff->dev,
+			"MAX77620_REG_RTCUPDATE0 write failed\n");
+		goto scrub;
+	}
+
+	/* Must wait 16ms for buffer update */
+	usleep_range(16000, 16000);
+	max77620_allow_atomic_xfer(max77620_poweroff->max77620);
+
+	ret = max77620_reg_update(max77620_poweroff->max77620->dev,
+		MAX77620_PWR_SLAVE, MAX77620_REG_ONOFFCNFG2,
+		MAX77620_ONOFFCNFG2_SFT_RST_WK, 0);
+
+	ret = max77620_reg_update(max77620_poweroff->max77620->dev,
+		MAX77620_PWR_SLAVE, MAX77620_REG_ONOFFCNFG1,
+		MAX77620_ONOFFCNFG1_SFT_RST, MAX77620_ONOFFCNFG1_SFT_RST);
+	if (ret < 0)
+		dev_err(max77620_poweroff->dev,
+			"REG_ONOFFCNFG1 update failed, %d\n", ret);
+
+	 while (1)
+		dev_err(max77620_poweroff->dev, "Device should not be here\n");
+scrub:
+	return;
+}
+
+static int max77620_configure_power_on(void *drv_data,
+	enum system_pmic_power_on_event event, void *event_data)
+{
+	struct max77620_poweroff *max77620_poweroff = drv_data;
+
+	switch (event) {
+	case SYSTEM_PMIC_RTC_ALARM:
+		max77620_poweroff->need_rtc_power_on = true;
+		break;
+
+	default:
+		dev_err(max77620_poweroff->dev, "power on event does not support\n");
+		break;
+	}
+	return 0;
+}
 
 static void max77620_pm_power_off(void *drv_data)
 {
@@ -40,6 +135,9 @@ static void max77620_pm_power_off(void *drv_data)
 	dev_info(max77620_poweroff->dev, "Powering off system\n");
 
 	max77620_allow_atomic_xfer(max77620_poweroff->max77620);
+
+	if (max77620_poweroff->need_rtc_power_on)
+		max77620_auto_power_on(max77620_poweroff);
 
 	ret = max77620_reg_update(max77620_poweroff->max77620->dev,
 		MAX77620_PWR_SLAVE, MAX77620_REG_ONOFFCNFG1,
@@ -73,6 +171,7 @@ static void max77620_pm_power_reset(void *drv_data)
 static struct system_pmic_ops max77620_pm_ops = {
 	.power_off = max77620_pm_power_off,
 	.power_reset = max77620_pm_power_reset,
+	.configure_power_on = max77620_configure_power_on,
 };
 
 static int max77620_poweroff_probe(struct platform_device *pdev)
