1b11bb250e7453a9222b8b8ae0845916b5f12b1a
Krishna Reddy <vdumpa@nvidia.com>
2014-10-01 10:46:15 -0700

    Revert "mm: cma: force cma page migration during __get_user_pages"
    This reverts commit 15cb9035ebab5f18b48bfd28e3ca6de70cdd83a3.
    
    Change-Id: I3053bb5dd7389f32c041ff674ccd278aace72316
    Signed-off-by: Krishna Reddy <vdumpa@nvidia.com>
    Reviewed-on: http://git-master/r/552639

--- mm/memory.c
+++ mm/memory.c
@@ -1856,7 +1856,8 @@ long __get_user_pages(struct task_struct *tsk, struct mm_struct *mm,
 				int ret;
 				unsigned int fault_flags = 0;
 
-				fault_flags = FAULT_FLAG_NO_CMA;
+				if (gup_flags & FOLL_DURABLE)
+					fault_flags = FAULT_FLAG_NO_CMA;
 
 				/* For mlock, just skip the stack guard page. */
 				if (foll_flags & FOLL_MLOCK) {
@@ -1924,18 +1925,8 @@ long __get_user_pages(struct task_struct *tsk, struct mm_struct *mm,
 			if (IS_ERR(page))
 				return i ? i : PTR_ERR(page);
 
-			if (is_cma_page(page)) {
-				struct page *old_page = page;
-				unsigned long timeout = jiffies + msecs_to_jiffies(8000);
-retry:
+			if ((gup_flags & FOLL_DURABLE) && is_cma_page(page))
 				page = migrate_replace_cma_page(page);
-				if (old_page == page && time_before(jiffies, timeout)) {
-					cond_resched();
-					goto retry;
-				}
-			}
-
-			BUG_ON(is_cma_page(page));
 
 			if (pages) {
 				pages[i] = page;
