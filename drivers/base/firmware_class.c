a52fd317ea988b2b2e92c837588e104fa7748e99
Dan Willemsen <dwillemsen@nvidia.com>
2014-10-16 13:19:48 -0700

    firmware: Fix android vendor path
    The proper android firmware path is /vendor/firmware, not
    /vendor/etc/firmware. That's where the userspace loader loads from.
    
    This adds both /vendor/... and /system/vendor/... Right now one is a
    symlink to the other, but they'll be swapping on some devices, and
    eventually /system/vendor/... will likely disappear.
    
    Change-Id: I63b568f5081de3734d20f2d8fa652ebbc68c6ad0
    Signed-off-by: Dan Willemsen <dwillemsen@nvidia.com>

--- drivers/base/firmware_class.c
+++ drivers/base/firmware_class.c
@@ -267,7 +267,8 @@ static char fw_path_para[256];
 static const char * const fw_path[] = {
 	fw_path_para,
 	"/system/etc/firmware",
-	"/system/vendor/etc/firmware",
+	"/vendor/firmware",
+	"/system/vendor/firmware",
 	"/lib/firmware/updates/" UTS_RELEASE,
 	"/lib/firmware/updates",
 	"/lib/firmware/" UTS_RELEASE,
