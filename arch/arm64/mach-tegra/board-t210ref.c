1b0f4b2a2f6a3693718e85d8f505dd917e99af48
Shreshtha SAHU <ssahu@nvidia.com>
2014-10-21 12:37:28 +0530

    arm64: tegra: add jetson-e dt board compat
    Bug 200044360
    
    Change-Id: Ie8101e6e8cbb6f1ce583741308ecff627ff7e227
    Signed-off-by: Shreshtha SAHU <ssahu@nvidia.com>
    Reviewed-on: http://git-master/r/601599
    (cherry picked from commit 0a8a89698a88c8daf1057a9e6e466c2fbe24deef)
    Reviewed-on: http://git-master/r/656647
    Tested-by: Kerwin Wan <kerwinw@nvidia.com>
    Reviewed-by: Hayden Du <haydend@nvidia.com>
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>

--- arch/arm64/mach-tegra/board-t210ref.c
+++ arch/arm64/mach-tegra/board-t210ref.c
@@ -312,6 +312,8 @@ static void __init tegra_t210ref_early_init(void)
 		tegra_soc_device_init("loki_e");
 	else if (of_machine_is_compatible("nvidia,foster-e"))
 		tegra_soc_device_init("foster_e");
+	else if (of_machine_is_compatible("nvidia,jetson-e"))
+		tegra_soc_device_init("jetson_e");
 }
 
 static struct tegra_io_dpd pexbias_io = {
@@ -507,6 +509,7 @@ static const char * const t210ref_dt_board_compat[] = {
 	"nvidia,grenada",
 	"nvidia,loki-e",
 	"nvidia,foster-e",
+	"nvidia,jetson-e",
 	NULL
 };
 
