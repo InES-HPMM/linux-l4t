9f10e6f7afd5b546427565a0b38565e073d9040b
Jay Agarwal <jagarwal@nvidia.com>
2014-10-28 16:24:00 +0530

    pcie: host: tegra: Resolve build after Werror flag
    Resolve below build error for 32b platforms:
    "error: right shift count >= width of type"
    
    Bug 1566945
    
    Change-Id: I55d3e975d5f5bcba0d40b87e204e4af7ff1afdb2
    Signed-off-by: Jay Agarwal <jagarwal@nvidia.com>
    Reviewed-on: http://git-master/r/590743
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>
    Tested-by: Bharat Nihalani <bnihalani@nvidia.com>

--- drivers/pci/host/pci-tegra.c
+++ drivers/pci/host/pci-tegra.c
@@ -2704,7 +2704,11 @@ static int tegra_msi_setup_irq(struct msi_chip *chip, struct pci_dev *pdev,
 	irq_set_msi_desc(irq, desc);
 
 	msg.address_lo = virt_to_phys((void *)msi->pages) & 0xFFFFFFFF;
+#ifdef CONFIG_ARM64
 	msg.address_hi = virt_to_phys((void *)msi->pages) >> 32;
+#else
+	msg.address_hi = 0;
+#endif
 	msg.data = hwirq;
 
 	write_msi_msg(irq, &msg);
