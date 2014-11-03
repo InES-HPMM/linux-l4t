14e66de327f067f1e226c76d02351738fe990ebb
Alex Waterman <alexw@nvidia.com>
2014-11-03 10:00:53 -0800

    arm: tegra: emc: add emc training timer
    Add an EMC training timer by making the dram_therm code more generic.
    The old dram_therm code maintained a single timer. This patch reuses
    some of that old code and cleans up some other code as well. Many of
    the debugfs nodes were simplifed. Other functions and bits of data
    were renamed.
    
    Change-Id: I5cf7f11d2684c8d5d422fc64bf47ae38e194f63e
    Signed-off-by: Alex Waterman <alexw@nvidia.com>
    Reviewed-on: http://git-master/r/660450
    (cherry picked from commit dfa173e6ce60d4c2c54a57c26cb0553b0043325d)
    Reviewed-on: http://git-master/r/663394
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Mitch Luban <mluban@nvidia.com>

--- drivers/platform/tegra/mc/tegra_emc_timers.c
+++ drivers/platform/tegra/mc/tegra_emc_timers.c
@@ -13,6 +13,9 @@
  * You should have received a copy of the GNU General Public License along
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * This file has various timers that may or may not be running in the
+ * EMC.
  */
 
 #include <linux/kernel.h>
@@ -28,25 +31,37 @@
 
 #define TEGRA_DRAM_THERM_MAX_STATE     1
 
-/* In ms - time between taking MR4 samples. */
-static unsigned long emc_mr4_sample_interval = 1000;
-static atomic_t do_poll;
-static int prev_temp = -1;
+/* In ms - timer periods. */
+static u32 timer_period_mr4 = 1000;
 
-static struct timer_list emc_mr4_timer;
-
-/*
- * Set to 1 to allow debugfs to control the mr4 read value.
- */
-static int test_mode;
+/* MR4 specific. */
+static atomic_t mr4_do_poll;
+static u32 prev_temp = 0xffffffff; /* i.e -1. */
+static u32 test_mode;
 static int dram_temp_override;
+
+/* The timers themselves. */
+static struct timer_list emc_timer_mr4;
+
+#ifdef CONFIG_ARCH_TEGRA_21x_SOC
+static u32 timer_period_training = 100;
+static struct timer_list emc_timer_training;
+
+static void emc_train(unsigned long nothing)
+{
+	emc_do_periodic_compensation();
+
+	mod_timer(&emc_timer_training,
+		  jiffies + msecs_to_jiffies(timer_period_training));
+}
+#endif
 
 static void emc_mr4_poll(unsigned long nothing)
 {
-	int dram_temp;
+	u32 dram_temp;
 
 	if (!test_mode)
-		dram_temp = tegra_emc_get_dram_temperature();
+		dram_temp = (u32)tegra_emc_get_dram_temperature();
 	else
 		dram_temp = dram_temp_override;
 
@@ -84,11 +99,11 @@ static void emc_mr4_poll(unsigned long nothing)
 	prev_temp = dram_temp;
 
 reset:
-	if (atomic_read(&do_poll) == 0)
+	if (atomic_read(&mr4_do_poll) == 0)
 		return;
 
-	if (mod_timer(&emc_mr4_timer,
-		      jiffies + msecs_to_jiffies(emc_mr4_sample_interval)))
+	if (mod_timer(&emc_timer_mr4,
+		      jiffies + msecs_to_jiffies(timer_period_mr4)))
 		pr_err("[dram-therm] Failed to restart timer!!!\n");
 }
 
@@ -103,14 +118,14 @@ static int tegra_dram_temp_start(void)
 
 	pr_info("[dram-therm] Starting DRAM temperature polling.\n");
 
-	err = mod_timer(&emc_mr4_timer,
-			jiffies + msecs_to_jiffies(emc_mr4_sample_interval));
+	err = mod_timer(&emc_timer_mr4,
+			jiffies + msecs_to_jiffies(timer_period_mr4));
 	if (err)
 		return err;
 
-	atomic_set(&do_poll, 1);
-	return mod_timer(&emc_mr4_timer,
-			 jiffies + msecs_to_jiffies(emc_mr4_sample_interval));
+	atomic_set(&mr4_do_poll, 1);
+	return mod_timer(&emc_timer_mr4,
+			 jiffies + msecs_to_jiffies(timer_period_mr4));
 }
 
 /*
@@ -121,7 +136,7 @@ static int tegra_dram_temp_start(void)
 static void tegra_dram_temp_stop(void)
 {
 	pr_info("[dram-therm] Stopping DRAM temperature polling.\n");
-	atomic_set(&do_poll, 0);
+	atomic_set(&mr4_do_poll, 0);
 }
 
 static int tegra_dram_cd_max_state(struct thermal_cooling_device *tcd,
@@ -134,14 +149,14 @@ static int tegra_dram_cd_max_state(struct thermal_cooling_device *tcd,
 static int tegra_dram_cd_cur_state(struct thermal_cooling_device *tcd,
 				   unsigned long *state)
 {
-	*state = (unsigned long)atomic_read(&do_poll);
+	*state = (unsigned long)atomic_read(&mr4_do_poll);
 	return 0;
 }
 
 static int tegra_dram_cd_set_state(struct thermal_cooling_device *tcd,
 				   unsigned long state)
 {
-	if (state == (unsigned long)atomic_read(&do_poll))
+	if (state == (unsigned long)atomic_read(&mr4_do_poll))
 		return 0;
 
 	if (state)
@@ -161,67 +176,91 @@ static struct thermal_cooling_device_ops emc_dram_cd_ops = {
 };
 
 #ifdef CONFIG_DEBUG_FS
-static struct dentry *dram_therm_debugfs;
+static struct dentry *emc_timers_debugfs;
 
-static int __get_sample_interval(void *data, u64 *val)
+static int __get_mr4_do_poll(void *data, u64 *val)
 {
-	*val = emc_mr4_sample_interval;
-	return 0;
-}
-static int __set_sample_interval(void *data, u64 val)
-{
-	emc_mr4_sample_interval = (unsigned long) val;
-	return 0;
-}
-DEFINE_SIMPLE_ATTRIBUTE(sample_interval_fops, __get_sample_interval,
-			__set_sample_interval, "%llu\n");
-
-static int __get_test_mode(void *data, u64 *val)
-{
-	*val = test_mode;
-	return 0;
-}
-static int __set_test_mode(void *data, u64 val)
-{
-	test_mode = !!val;
-	return 0;
-}
-DEFINE_SIMPLE_ATTRIBUTE(test_mode_fops, __get_test_mode,
-			__set_test_mode, "%llu\n");
-
-static int __get_dram_temp_override(void *data, u64 *val)
-{
-	*val = dram_temp_override;
-	return 0;
-}
-static int __set_dram_temp_override(void *data, u64 val)
-{
-	dram_temp_override = (unsigned int) val;
-	return 0;
-}
-DEFINE_SIMPLE_ATTRIBUTE(dram_temp_override_fops, __get_dram_temp_override,
-			__set_dram_temp_override, "%llu\n");
-
-static int __get_do_poll(void *data, u64 *val)
-{
-	*val = atomic_read(&do_poll);
+	*val = atomic_read(&mr4_do_poll);
 
 	return 0;
 }
-static int __set_do_poll(void *data, u64 val)
+static int __set_mr4_do_poll(void *data, u64 val)
 {
-	atomic_set(&do_poll, (unsigned int)val);
+	atomic_set(&mr4_do_poll, (unsigned int)val);
 
 	/* Explicitly wake up the DRAM monitoring thread. */
-	if (atomic_read(&do_poll))
+	if (atomic_read(&mr4_do_poll))
 		tegra_dram_temp_start();
 
 	return 0;
 }
-DEFINE_SIMPLE_ATTRIBUTE(do_poll_fops, __get_do_poll, __set_do_poll, "%llu\n");
+DEFINE_SIMPLE_ATTRIBUTE(mr4_do_poll_fops, __get_mr4_do_poll, __set_mr4_do_poll,
+								     "%llu\n");
 #endif
 
-static int __init tegra_dram_therm_init(void)
+int tegra_emc_timers_init(struct dentry *parent)
+{
+	struct dentry *dram_therm_debugfs;
+#ifdef CONFIG_ARCH_TEGRA_21x_SOC
+	struct dentry *training_debugfs;
+
+	if (tegra_emc_get_dram_type() == DRAM_TYPE_LPDDR4) {
+		pr_info("tegra: Starting EMC periodic training timer\n");
+		setup_timer(&emc_timer_training, emc_train, 0);
+		mod_timer(&emc_timer_training, timer_period_training);
+	}
+#endif
+	setup_timer(&emc_timer_mr4, emc_mr4_poll, 0);
+
+#ifdef CONFIG_DEBUG_FS
+	/*
+	 * If caller doesn't want us to make a debugfs dir it can pass NULL.
+	 * This should never be a top level debugfs dir. For subsequent debugfs
+	 * errors return 0 - don't fail the entire EMC driver init if debugfs
+	 * has problems. We do a best effort here.
+	 */
+	if (!parent)
+		return 0;
+
+	emc_timers_debugfs = debugfs_create_dir("emc_timers", parent);
+	if (!emc_timers_debugfs)
+		return 0;
+
+	dram_therm_debugfs = debugfs_create_dir("dram_therm",
+						emc_timers_debugfs);
+	if (!dram_therm_debugfs)
+		return 0;
+
+	/* DRAM thermals. */
+	debugfs_create_u32("timer_period", S_IRUGO | S_IWUSR,
+			   dram_therm_debugfs, &timer_period_mr4);
+
+	debugfs_create_u32("test_mode", S_IRUGO | S_IWUSR,
+			   dram_therm_debugfs, &test_mode);
+	debugfs_create_u32("dram_temp_override", S_IRUGO | S_IWUSR,
+			   dram_therm_debugfs, &dram_temp_override);
+	debugfs_create_file("do_poll", S_IRUGO | S_IWUSR,
+			    dram_therm_debugfs, NULL, &mr4_do_poll_fops);
+
+#ifdef CONFIG_ARCH_TEGRA_21x_SOC
+	/* Training. */
+	training_debugfs = debugfs_create_dir("training", emc_timers_debugfs);
+	if (!training_debugfs)
+		return 0;
+
+	debugfs_create_u32("timer_period", S_IRUGO | S_IWUSR,
+			   training_debugfs, &timer_period_training);
+#endif
+#endif
+	return 0;
+}
+
+/*
+ * This is separated from the generic tegra_emc_timers_init() since this must
+ * run later than the regular EMC init runs. How annoying. Thankfully this has
+ * no dependency on the debugfs or timer portion of this code.
+ */
+static __init int tegra_emc_therm_init(void)
 {
 	void *ret;
 
@@ -232,29 +271,6 @@ static int __init tegra_dram_therm_init(void)
 	if (ret == NULL)
 		return -ENODEV;
 
-	setup_timer(&emc_mr4_timer, emc_mr4_poll, 0);
-
-#ifdef CONFIG_DEBUG_FS
-	dram_therm_debugfs = debugfs_create_dir("dram-therm", NULL);
-	if (!dram_therm_debugfs)
-		return -ENOMEM;
-
-	if (!debugfs_create_file("sample_interval", S_IRUGO | S_IWUSR,
-				 dram_therm_debugfs, NULL,
-				 &sample_interval_fops))
-		return -ENOMEM;
-	if (!debugfs_create_file("test_mode", S_IRUGO | S_IWUSR,
-				 dram_therm_debugfs, NULL, &test_mode_fops))
-		return -ENOMEM;
-	if (!debugfs_create_file("dram_temp_override", S_IRUGO | S_IWUSR,
-				 dram_therm_debugfs, NULL,
-				 &dram_temp_override_fops))
-		return -ENOMEM;
-	if (!debugfs_create_file("do_poll", S_IRUGO | S_IWUSR,
-				 dram_therm_debugfs, NULL, &do_poll_fops))
-		return -ENOMEM;
-#endif
-
 	return 0;
 }
-late_initcall(tegra_dram_therm_init);
+late_initcall(tegra_emc_therm_init);
