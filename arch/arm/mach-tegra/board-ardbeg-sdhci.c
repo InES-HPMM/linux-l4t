d196a72f997a841a7daf647720557df8e6baca4e
Anubhav Kumar Jain <anubhavj@nvidia.com>
2014-11-06 11:12:51 +0530

    ARM: tegra: DT: Enabled auto-calibration
      - Enabled auto calibration for eMMC device on
        T124-loki and T124-ardbeg platforms
        by removing sdhci property disable-auto-cal.
    
    Bug 200028271
    
    Change-Id: Id0a1105657482d06a2dfd5b6214f1f70eb70c8c1
    Signed-off-by: Anubhav Kumar Jain <anubhavj@nvidia.com>
    Reviewed-on: http://git-master/r/592045
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Naveen Kumar Arepalli <naveenk@nvidia.com>
    Reviewed-by: Venu Byravarasu <vbyravarasu@nvidia.com>

--- arch/arm/mach-tegra/board-ardbeg-sdhci.c
+++ arch/arm/mach-tegra/board-ardbeg-sdhci.c
@@ -223,7 +223,6 @@ static struct tegra_sdhci_platform_data tegra_sdhci_platform_data3 = {
 	.max_clk_limit = 200000000,
 	.calib_3v3_offsets = 0x0202,
 	.calib_1v8_offsets = 0x0202,
-	.disable_auto_cal = 1,
 };
 
 static struct platform_device tegra_sdhci_device0 = {
