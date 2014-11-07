89bca7fbb5891ef3f466c35b2901f15bdacbc28e
Manikanta <mmaddireddy@nvidia.com>
2014-11-07 10:41:55 +0530

    net: wireless: bcmdhd: add shutdown handler for bcmdhd driver
    If we do 'adb reboot' with wifi on, BOOT_SEL1 (UART4_TXD)
    and BOOT_SEL0 (UART4_RTS) stays at above 1.2V level.
    This is causing boot issues. Add shutdown handler to fix
    this issue.
    
    bug 200042650
    
    Change-Id: I4219c54c5c90a75f1c9586dd3102bfbe1310cfc7
    Signed-off-by: Manikanta <mmaddireddy@nvidia.com>
    Reviewed-on: http://git-master/r/595785
    Reviewed-by: Nagarjuna Kristam <nkristam@nvidia.com>
    Reviewed-by: Vinayak Pane <vpane@nvidia.com>
    Tested-by: Vinayak Pane <vpane@nvidia.com>
    Reviewed-by: Mitch Luban <mluban@nvidia.com>

--- drivers/net/wireless/bcmdhd/dhd_linux_platdev.c
+++ drivers/net/wireless/bcmdhd/dhd_linux_platdev.c
@@ -421,6 +421,17 @@ static int wifi_plat_dev_drv_resume(struct platform_device *pdev)
 	return 0;
 }
 
+static void wifi_plat_dev_drv_shutdown(struct platform_device *pdev)
+{
+	wifi_adapter_info_t *adapter;
+
+	ASSERT(dhd_wifi_platdata != NULL);
+	ASSERT(dhd_wifi_platdata->num_adapters == 1);
+	adapter = &dhd_wifi_platdata->adapters[0];
+
+	wifi_platform_set_power(adapter, FALSE, 0);
+}
+
 static const struct of_device_id wifi_device_dt_match[] = {
 	{ .compatible = "android,bcmdhd_wlan", },
 	{},
@@ -431,6 +442,7 @@ static struct platform_driver wifi_platform_dev_driver = {
 	.remove         = wifi_plat_dev_drv_remove,
 	.suspend        = wifi_plat_dev_drv_suspend,
 	.resume         = wifi_plat_dev_drv_resume,
+	.shutdown       = wifi_plat_dev_drv_shutdown,
 	.driver         = {
 	.name   = WIFI_PLAT_NAME,
 	.of_match_table = wifi_device_dt_match,
