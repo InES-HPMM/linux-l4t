986ddec7d9cc3355fa5a71a2b4d2808d21ae978b
Andre Przywara <andre.przywara@arm.com>
2014-11-14 15:54:09 +0000

    arm64: detect silicon revisions and set cap bits accordingly
    After each CPU has been started, we iterate through a list of
    CPU features or bugs to detect CPUs which need (or could benefit
    from) kernel code patches.
    For each feature/bug there is a function which checks if that
    particular CPU is affected. We will later provide some more generic
    functions for common things like testing for certain MIDR ranges.
    We do this for every CPU to cover big.LITTLE systems properly as
    well.
    If a certain feature/bug has been detected, the capability bit will
    be set, so that later the call to apply_alternatives() will trigger
    the actual code patching.
    
    Change-Id: I19501eaa5164fd9dd82b4e6911d84527f5080efa
    Signed-off-by: Andre Przywara <andre.przywara@arm.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Reviewed-on: http://git-master/r/721973
    (cherry picked from commit 18c8112d8a9e9582997e9881bcfe3e2826528b70)
    Reviewed-on: http://git-master/r/731448
    Reviewed-by: Bo Yan <byan@nvidia.com>
    Tested-by: Bo Yan <byan@nvidia.com>

--- arch/arm64/kernel/cpuinfo.c
+++ arch/arm64/kernel/cpuinfo.c
@@ -18,6 +18,7 @@
 #include <asm/cachetype.h>
 #include <asm/cpu.h>
 #include <asm/cputype.h>
+#include <asm/cpufeature.h>
 
 #include <linux/init.h>
 #include <linux/smp.h>
@@ -56,6 +57,9 @@ static void __cpuinfo_store_cpu(struct cpuinfo_arm64 *info)
 	info->reg_id_mmfr3 = read_cpuid(ID_MMFR3_EL1);
 	info->reg_id_pfr0 = read_cpuid(ID_PFR0_EL1);
 	info->reg_id_pfr1 = read_cpuid(ID_PFR1_EL1);
+
+
+	check_local_cpu_errata();
 }
 
 void cpuinfo_store_cpu(void)
