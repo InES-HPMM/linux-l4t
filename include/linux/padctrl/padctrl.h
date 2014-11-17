37d149b4df79dd07854347dae85ab525ff0c6a80
Laxman Dewangan <ldewangan@nvidia.com>
2014-11-17 21:19:12 +0530

    padctrl: add new driver susbsystem padcontrol
    Add new driver subsystem as padcontrol to control the pads of SoCs.
    
    bug 200038063
    
    Change-Id: I69b8e8f2449b68ec477f8ec1571e84cd0224652c
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Reviewed-on: http://git-master/r/604271
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit

---
+++ include/linux/padctrl/padctrl.h
@@ -0,0 +1,127 @@
+/*
+ * Copyright (c) 2014, NVIDIA CORPORATION.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ */
+
+#ifndef __LINUX_PADCTRL_H
+#define __LINUX_PADCTRL_H
+
+#include <linux/device.h>
+#include <linux/of.h>
+
+struct padctrl;
+struct padctrl_dev;
+
+/*
+ * padctrl_ops: Callback API to implement the padcontrol specific ops.
+ */
+struct padctrl_ops {
+	int (*set_voltage)(struct padctrl_dev *pad_dev,
+			int pad_id, u32 voltage);
+	int (*get_voltage)(struct padctrl_dev *pad_dev,
+			int pad_id, u32 *voltage);
+};
+
+/*
+ * padctrl_desc: Padcontrol description describe the HW behavior.
+ */
+struct padctrl_desc {
+	const char *name;
+	struct padctrl_ops *ops;
+};
+
+/*
+ * padctrl_confif: Padcontrol configuration.
+ */
+struct padctrl_config {
+	const char *name;
+	struct device_node *of_node;
+};
+
+#ifdef CONFIG_PADCTRL
+extern struct padctrl *padctrl_get(struct device *dev, const char *name);
+extern void padctrl_put(struct padctrl *pctrl);
+
+extern struct padctrl *devm_padctrl_get(struct device *dev, const char *name);
+
+extern int padctrl_set_voltage(struct padctrl *pctrl, u32 voltage);
+extern int padctrl_get_voltage(struct padctrl *pctrl, u32 *voltage);
+
+extern struct padctrl_dev *padctrl_register(struct device *dev,
+	struct padctrl_desc *desc, struct padctrl_config *config);
+extern void padctrl_unregister(struct padctrl_dev *pad_dev);
+
+extern struct padctrl_dev *devm_padctrl_register(struct device *dev,
+		struct padctrl_desc *desc, struct padctrl_config *config);
+extern void devm_padctrl_unregister(struct device *dev,
+		struct padctrl_dev *pad_dev);
+
+extern void padctrl_set_drvdata(struct padctrl_dev *pad_dev, void *drv_data);
+extern void *padctrl_get_drvdata(struct padctrl_dev *pad_dev);
+
+#else
+
+static inline struct padctrl *padctrl_get(struct device *dev,
+		const char *name)
+{
+	return ERR_PTR(-ENODEV);
+}
+
+static inline void *padctrl_put(struct padctrl *pctrl)
+{
+}
+
+static inline struct padctrl *devm_padctrl_get(struct device *dev,
+	const char *name)
+{
+	return ERR_PTR(-ENODEV);
+}
+
+static inline int padctrl_set_voltage(struct padctrl *pctrl, u32 voltage)
+{
+	return -EINVAL;
+}
+
+static int padctrl_get_voltage(struct padctrl *ctrl, u32 *voltage)
+{
+	return -EINVAL;
+}
+
+static inline struct padctrl_dev *padctrl_register(struct device *dev,
+	struct padctrl_desc *desc, struct padctrl_config *config)
+{
+	return ERR_PTR(-ENODEV);
+}
+static inline void padctrl_unregister(struct padctrl_dev *pad_dev)
+{
+}
+
+static inline struct padctrl_dev *devm_padctrl_register(struct device *dev,
+		struct padctrl_desc *desc, struct padctrl_config *config)
+{
+	return ERR_PTR(-ENODEV);
+}
+static inline void devm_padctrl_unregister(struct device *dev,
+		struct padctrl_dev *pad_dev)
+{
+}
+static inline void padctrl_set_drvdata(struct padctrl_dev *pad_dev,
+			void *drv_data)
+{
+}
+
+static inline void *padctrl_get_drvdata(struct padctrl_dev *pad_dev)
+{
+	return NULL;
+}
+#endif
+
+#endif
