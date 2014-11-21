6fdde938b3deb3f8f9197b14034364a70398f519
Shreshtha SAHU <ssahu@nvidia.com>
2014-11-21 14:05:49 -0800

    arm: tegra: add P2595 jetson-erista board ID
    Bug 200044360
    
    Change-Id: Icc580bcd5416802f298bc9149d35890b7f84cac4
    Signed-off-by: Shreshtha SAHU <ssahu@nvidia.com>
    Reviewed-on: http://git-master/r/654269
    (cherry picked from commit 0487d5583214dd6c1399fb1c2385345a92339cd1)
    Reviewed-on: http://git-master/r/657242
    Reviewed-by: Kerwin Wan <kerwinw@nvidia.com>
    Tested-by: Kerwin Wan <kerwinw@nvidia.com>
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Venkat Moganty <vmoganty@nvidia.com>
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>
    Tested-by: Mike Thompson <mikthompson@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- arch/arm/mach-tegra/tegra-board-id.h
+++ arch/arm/mach-tegra/tegra-board-id.h
@@ -38,6 +38,7 @@
 #define BOARD_E2549   0x09F5
 #define BOARD_E1782   0x06F6
 #define BOARD_P2530   0x09E2
+#define BOARD_P2595   0x0A23
 #define BOARD_P1761   0x06E1
 #define BOARD_P1765   0x06E5
 #define BOARD_E1762   0x06E2
