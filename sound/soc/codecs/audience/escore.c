2abc09253a67cddd445c98182d70d2143d7b5163
Dara Ramesh <dramesh@nvidia.com>
2014-11-27 14:06:01 +0530

    asoc: codec: audience: reduce resume time
    remove extra wait from escore wakeup
    
    bug 200056794
    bug 200056954
    
    Change-Id: Icebc163b87d89ea326b8e032ce8a019f9ed5537d
    Signed-off-by: Dara Ramesh <dramesh@nvidia.com>
    Reviewed-on: http://git-master/r/656626
    (cherry picked from commit 3871a1f2ee0bf81b579d130d30c5cf38707235a5)
    Reviewed-on: http://git-master/r/658883
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Chandrakanth Gorantla <cgorantla@nvidia.com>

--- sound/soc/codecs/audience/escore.c
+++ sound/soc/codecs/audience/escore.c
@@ -898,9 +898,6 @@ int escore_wakeup(struct escore_priv *escore)
 		msleep(ES_PM_CLOCK_STABILIZATION);
 	}
 
-	if (escore->pri_intf == ES_SPI_INTF)
-		msleep(ES_WAKEUP_TIME);
-
 	do {
 		/* Toggle the wakeup pin H->L the L->H */
 		if (escore->wakeup_intf == ES_UART_INTF &&
