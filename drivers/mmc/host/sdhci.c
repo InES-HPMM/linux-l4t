79e67137f6a426ab11a32bb5caf169b63f16f37f
R Raj Kumar <rrajk@nvidia.com>
2014-12-03 16:14:14 +0530

    mmc: sdhci: Issue DAT & CMD reset together
    Issue Data and command reset together when error
    is occurred during data transfers
    
    Bug 200061596
    
    Change-Id: I244f20217f1929b7f96c084d847123dc9097f83c
    Signed-off-by: R Raj Kumar <rrajk@nvidia.com>
    Reviewed-on: http://git-master/r/658932
    (cherry picked from commit c30f6f2ab1f60193ad1df583452e04b314527673)
    Reviewed-on: http://git-master/r/660684
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Venu Byravarasu <vbyravarasu@nvidia.com>

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -1322,10 +1322,8 @@ static void sdhci_finish_data(struct sdhci_host *host)
 		 * The controller needs a reset of internal state machines
 		 * upon error conditions.
 		 */
-		if (data->error) {
-			sdhci_reset(host, SDHCI_RESET_DATA);
-			sdhci_reset(host, SDHCI_RESET_CMD);
-		}
+		if (data->error)
+			sdhci_reset(host, SDHCI_RESET_CMD | SDHCI_RESET_DATA);
 
 		sdhci_send_command(host, data->stop);
 	} else
@@ -2588,8 +2586,7 @@ static void sdhci_card_event(struct mmc_host *mmc)
 		pr_err("%s: Resetting controller.\n",
 			mmc_hostname(host->mmc));
 
-		sdhci_reset(host, SDHCI_RESET_DATA);
-		sdhci_reset(host, SDHCI_RESET_CMD);
+		sdhci_reset(host, SDHCI_RESET_CMD | SDHCI_RESET_DATA);
 
 		host->mrq->cmd->error = -ENOMEDIUM;
 		tasklet_schedule(&host->finish_tasklet);
@@ -2821,10 +2818,8 @@ static void sdhci_tasklet_finish(unsigned long param)
 			/* This is to force an update */
 			sdhci_update_clock(host);
 
-		/* Spec says we should do both at the same time, but Ricoh
-		   controllers do not like that. */
-		sdhci_reset(host, SDHCI_RESET_DATA);
-		sdhci_reset(host, SDHCI_RESET_CMD);
+		/* Spec says we should do both at the same time */
+		sdhci_reset(host, SDHCI_RESET_CMD | SDHCI_RESET_DATA);
 	}
 
 	host->mrq = NULL;
