d362cfa118f4fb31a66e047666af25b42daca665
Mallikarjun Kasoju <mkasoju@nvidia.com>
2014-12-22 23:59:09 +0530

    extcon: adc-jack: Make device wakeup capable
    Make adc-jack as device wakeup capable
    
    Bug 200057344
    
    Change-Id: I9cd2bc4226a9ac195114c4a2766450bb6eb56fac
    Signed-off-by: Mallikarjun Kasoju <mkasoju@nvidia.com>
    Reviewed-on: http://git-master/r/666780
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>
    Reviewed-on: http://git-master/r/667886

--- drivers/extcon/extcon-adc-jack.c
+++ drivers/extcon/extcon-adc-jack.c
@@ -294,7 +294,7 @@ static int adc_jack_probe(struct platform_device *pdev)
 		goto err_irq;
 	}
 
-	device_set_wakeup_capable(data->dev, true);
+	device_init_wakeup(data->dev, true);
 	adc_jack_handler(&data->handler.work);
 
 	return 0;
