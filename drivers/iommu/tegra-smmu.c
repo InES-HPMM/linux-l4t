9efd7e7f90a2e24acb834e898a2e42510bd0666e
Hiroshi Doyu <hdoyu@nvidia.com>
2014-12-27 15:41:48 +0200

    iommu/tegra: smmu: LiveUpdate a 4MB PDE to 1024*4KB PTEs
    Optimize LiveUpdate a single 4MB PDE into 1024*4KB PTEs
    
    1, Prepare 1024*4KB PTE filled in advance, keeping obsolete PDE.
    2, Replace a obsolete PDE with a new one.
    3, Flush obsolete PTC/TLB
    
    This way client would see either old PDE or new PDE, which traslations
    are always same but their granularities are different.
    
    Bug 1583295
    
    Change-Id: I142276b120eac66381714d0f0f206b3531839d42
    Reported-by Krishna Reddy <vdumpa@nvidia.com>
    Signed-off-by: Hiroshi Doyu <hdoyu@nvidia.com>
    Reviewed-by: Krishna Reddy <vdumpa@nvidia.com>
    Reviewed-on: http://git-master/r/667961

--- drivers/iommu/tegra-smmu.c
+++ drivers/iommu/tegra-smmu.c
@@ -1100,54 +1100,66 @@ static int smmu_iommu_map_sg(struct iommu_domain *domain, unsigned long iova,
 }
 
 /* Remap a 4MB large page entry to 1024 * 4KB pages entries */
-static void __smmu_iommu_remap_largepage(struct smmu_as *as, dma_addr_t iova)
+static int __smmu_iommu_remap_largepage(struct smmu_as *as, dma_addr_t iova)
 {
 	int pdn = SMMU_ADDR_TO_PDN(iova);
 	u32 *pdir = page_address(as->pdir_page);
-	phys_addr_t pa = pdir[pdn] << SMMU_PDE_SHIFT;
-	size_t unmapped;
+	unsigned long pfn = __phys_to_pfn(pdir[pdn] << SMMU_PDE_SHIFT);
+	unsigned int *rest = &as->pte_count[pdn];
+	gfp_t gfp = GFP_ATOMIC;
+	u32 *pte;
+	struct page *page;
 	int i;
 
 	BUG_ON(!IS_ALIGNED(iova, SZ_4M));
 	BUG_ON(pdir[pdn] & _PDE_NEXT);
 
-	unmapped = __smmu_iommu_unmap_largepage(as, iova);
-	BUG_ON(unmapped != SZ_4M);
-	for (i = 0; i < unmapped / SZ_4K; i++) {
-		int err;
+	/* Prepare L2 in advance */
+	if (IS_ENABLED(CONFIG_PREEMPT) && !in_atomic())
+		gfp = GFP_KERNEL;
+	page = alloc_page(gfp);
+	if (!page)
+		return -ENOMEM;
+	pte = (u32 *)page_address(page);
+	*rest = SMMU_PTBL_COUNT;
+	for (i = 0; i < SMMU_PTBL_COUNT; i++)
+		*(pte + i) = SMMU_PFN_TO_PTE(pfn + i, as->pte_attr);
+	FLUSH_CPU_DCACHE(pte, page, SMMU_PTBL_SIZE);
 
-		err = __smmu_iommu_map_page(as, iova, pa, 0); /* FIXME: prot */
-		BUG_ON(err);
-		iova += SZ_4K;
-		pa += SZ_4K;
-	}
+	/* Update pde */
+	pdir[pdn] = SMMU_MK_PDE(page, as->pde_attr | _PDE_NEXT);
+	FLUSH_CPU_DCACHE(&pdir[pdn], as->pdir_page, sizeof(pdir[pdn]));
+	flush_ptc_and_tlb(as->smmu, as, iova, &pdir[pdn], as->pdir_page, 1);
+	return 0;
 }
 
 static size_t __smmu_iommu_unmap(struct smmu_as *as, dma_addr_t iova,
-	size_t bytes)
+				 size_t bytes)
 {
 	int pdn = SMMU_ADDR_TO_PDN(iova);
 	u32 *pdir = page_address(as->pdir_page);
-	size_t unmapped;
 
 	if (pdir[pdn] == _PDE_VACANT(pdn)) {
 		WARN(1, "No map: as=%d iova=%pa bytes=%zx\n",
 		     as->asid, &iova, bytes);
-		unmapped = 0;
+		return 0;
 	} else if (pdir[pdn] & _PDE_NEXT) {
-		unmapped = __smmu_iommu_unmap_pages(as, iova, bytes);
-	} else if (bytes >= SZ_4M) {
+		return __smmu_iommu_unmap_pages(as, iova, bytes);
+	} else { /* 4MB PDE */
 		BUG_ON(config_enabled(CONFIG_TEGRA_IOMMU_SMMU_NO4MB));
 		BUG_ON(!IS_ALIGNED(iova, SZ_4M));
 
-		unmapped = __smmu_iommu_unmap_largepage(as, iova);
-	} else {
-		pr_warn("Unmapping %zx@%pa 4MB entry as=%d\n", bytes, &iova, as->asid);
-		__smmu_iommu_remap_largepage(as, iova);
-		unmapped = __smmu_iommu_unmap_pages(as, iova, bytes);
-	}
+		if (bytes < SZ_4M) {
+			int err;
 
-	return unmapped;
+			err = __smmu_iommu_remap_largepage(as, iova);
+			if (err)
+				return 0;
+			return __smmu_iommu_unmap_pages(as, iova, bytes);
+		}
+
+		return __smmu_iommu_unmap_largepage(as, iova);
+	}
 }
 
 static size_t smmu_iommu_unmap(struct iommu_domain *domain, unsigned long iova,
