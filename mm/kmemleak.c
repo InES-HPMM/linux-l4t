8e02b65c036d2fd790e6c0cbe8a75f05e8c1f87b
Sri Krishna chowdary <schowdary@nvidia.com>
2015-01-07 12:25:16 +0530

    kmemleak: Add config to select auto scan
    Add new config DEBUG_KMEMLEAK_SCAN_ON.
    Initiate auto scan thread only if the config is enabled.
    
    Bug 200066404
    
    Change-Id: I357634d44a008487e20a62fae9992c94be5efa3f
    Signed-off-by: Sri Krishna chowdary <schowdary@nvidia.com>
    Reviewed-on: http://git-master/r/670251
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>
    Reviewed-by: Hiroshi Doyu <hdoyu@nvidia.com>
    Reviewed-by: Shridhar Rasal <srasal@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Sachin Nikam <snikam@nvidia.com>

--- mm/kmemleak.c
+++ mm/kmemleak.c
@@ -1397,6 +1397,7 @@ static int kmemleak_scan_thread(void *arg)
 	pr_info("Automatic memory scanning thread started\n");
 	set_user_nice(current, 10);
 
+#ifdef CONFIG_DEBUG_KMEMLEAK_SCAN_ON
 	/*
 	 * Wait before the first scan to allow the system to fully initialize.
 	 */
@@ -1404,6 +1405,7 @@ static int kmemleak_scan_thread(void *arg)
 		first_run = 0;
 		ssleep(SECS_FIRST_SCAN);
 	}
+#endif
 
 	while (!kthread_should_stop()) {
 		signed long timeout = jiffies_scan_wait;
@@ -1855,9 +1857,11 @@ static int __init kmemleak_late_init(void)
 				     &kmemleak_fops);
 	if (!dentry)
 		pr_warning("Failed to create the debugfs kmemleak file\n");
+#ifdef CONFIG_DEBUG_KMEMLEAK_SCAN_ON
 	mutex_lock(&scan_mutex);
 	start_scan_thread();
 	mutex_unlock(&scan_mutex);
+#endif
 
 	pr_info("Kernel memory leak detector initialized\n");
 
