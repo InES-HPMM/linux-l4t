2eac711a89d8eee9c71d1c5798ed78de76865ddb
Eric Miao <emiao@nvidia.com>
2015-01-06 17:48:34 -0800

    crypto: tegra-se: lower priority for AES
    Lowering the priority of AES crypto algorithms for TEGRA_SE
    from 300 to 100, which will favor the ARMv8 crypto algorithms
    with default priority of 300.
    
    TEGRA_SE is still needed for LP0.
    
    Change-Id: Icc94635cea738dcbd9051a3078f256661fcb5b8f
    Signed-off-by: Eric Miao <emiao@nvidia.com>
    Reviewed-on: http://git-master/r/669951
    Reviewed-by: Alexander Van Brunt <avanbrunt@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Dan Willemsen <dwillemsen@nvidia.com>
    Reviewed-on: http://git-master/r/755347
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Eric Chuang <echuang@nvidia.com>
    Tested-by: Eric Chuang <echuang@nvidia.com>

--- drivers/crypto/tegra-se.c
+++ drivers/crypto/tegra-se.c
@@ -2274,7 +2274,7 @@ static struct crypto_alg aes_algs[] = {
 	{
 		.cra_name = "cbc(aes)",
 		.cra_driver_name = "cbc-aes-tegra",
-		.cra_priority = 300,
+		.cra_priority = 100,
 		.cra_flags = CRYPTO_ALG_TYPE_ABLKCIPHER | CRYPTO_ALG_ASYNC,
 		.cra_blocksize = TEGRA_SE_AES_BLOCK_SIZE,
 		.cra_ctxsize  = sizeof(struct tegra_se_aes_context),
@@ -2294,7 +2294,7 @@ static struct crypto_alg aes_algs[] = {
 	}, {
 		.cra_name = "ecb(aes)",
 		.cra_driver_name = "ecb-aes-tegra",
-		.cra_priority = 300,
+		.cra_priority = 100,
 		.cra_flags = CRYPTO_ALG_TYPE_ABLKCIPHER | CRYPTO_ALG_ASYNC,
 		.cra_blocksize = TEGRA_SE_AES_BLOCK_SIZE,
 		.cra_ctxsize  = sizeof(struct tegra_se_aes_context),
@@ -2314,7 +2314,7 @@ static struct crypto_alg aes_algs[] = {
 	}, {
 		.cra_name = "ctr(aes)",
 		.cra_driver_name = "ctr-aes-tegra",
-		.cra_priority = 300,
+		.cra_priority = 100,
 		.cra_flags = CRYPTO_ALG_TYPE_ABLKCIPHER | CRYPTO_ALG_ASYNC,
 		.cra_blocksize = TEGRA_SE_AES_BLOCK_SIZE,
 		.cra_ctxsize  = sizeof(struct tegra_se_aes_context),
@@ -2335,7 +2335,7 @@ static struct crypto_alg aes_algs[] = {
 	}, {
 		.cra_name = "ofb(aes)",
 		.cra_driver_name = "ofb-aes-tegra",
-		.cra_priority = 300,
+		.cra_priority = 100,
 		.cra_flags = CRYPTO_ALG_TYPE_ABLKCIPHER | CRYPTO_ALG_ASYNC,
 		.cra_blocksize = TEGRA_SE_AES_BLOCK_SIZE,
 		.cra_ctxsize  = sizeof(struct tegra_se_aes_context),
