2f10d2d9b37363bce320c7c616340bc42fcb257e
Alex Waterman <alexw@nvidia.com>
2015-01-14 10:25:48 -0800

    arm64: tegra: emc: Make parser support old tables
    When rev. 6 tables were added rev. 5 tables caused a problem since
    they were missing certain fields present in rev. 6 tables. This patch
    skips the fields present in rev. 6 fields if prior tables are detected
    during parsing.
    
    Change-Id: I2171650ab2a4f9557c0bcd312cbff9122fd1bd67
    Signed-off-by: Alex Waterman <alexw@nvidia.com>
    Reviewed-on: http://git-master/r/672483
    (cherry picked from commit 3089a0ee8ade3dc5fb5ad21cc102e8c248202d42)
    Reviewed-on: http://git-master/r/674044
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Yu-Huan Hsu <yhsu@nvidia.com>

--- drivers/platform/tegra/mc/tegra_emc_dt_parse.c
+++ drivers/platform/tegra/mc/tegra_emc_dt_parse.c
@@ -170,6 +170,9 @@ static void *tegra_emc_dt_parse_pdata_comp(const char *emc_mode,
 #elif defined(CONFIG_ARCH_TEGRA_21x_SOC)
 			PNE_U32(iter, "nvidia,needs-training", needs_training);
 			PNE_U32(iter, "nvidia,trained", trained);
+
+			if (tables[i].rev < 0x6)
+				goto skip_periodic_training_params;
 			PNE_U32(iter, "nvidia,periodic_training",
 				periodic_training);
 			PNE_U32(iter, "nvidia,trained_dram_clktree_c0d0u0",
@@ -206,6 +209,8 @@ static void *tegra_emc_dt_parse_pdata_comp(const char *emc_mode,
 				current_dram_clktree_c1d1u1);
 			PNE_U32(iter, "nvidia,run_clocks", run_clocks);
 			PNE_U32(iter, "nvidia,tree_margin", tree_margin);
+
+skip_periodic_training_params:
 			PNE_U32(iter, "nvidia,burst-regs-per-ch-num",
 				burst_regs_per_ch_num);
 			PNE_U32(iter, "nvidia,trim-regs-num", trim_regs_num);
