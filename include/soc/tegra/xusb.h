b66ba801ef5e6f6d4064876043bd9049de415dd8
JC Kuo <jckuo@nvidia.com>
2015-01-15 17:37:17 +0800

    mailbox: Add NVIDIA Tegra XUSB mailbox driver
    The Tegra xHCI controller's firmware communicates requests to the host
    processor through a mailbox interface.  While there is only a single
    physical channel, messages sent by the controller can be divided
    into two groups: those intended for the PHY driver and those intended
    for the host-controller driver.  The requesting driver is assigned
    one of two virtual channels when the single physical channel is
    requested.  All incoming messages are sent to both virtual channels.
    
    Signed-off-by: Andrew Bresticker <abrestic@chromium.org>
    Reviewed-by: Stephen Warren <swarren@nvidia.com>
    
    bug 200084946
    
    Change-Id: I116e4a3944afe7f137a7c62630640859bf840987
    Signed-off-by: JC Kuo <jckuo@nvidia.com>
    Reviewed-on: http://git-master/r/736744
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Ashutosh Jha <ajha@nvidia.com>

---
+++ include/soc/tegra/xusb.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2014 NVIDIA Corporation
+ * Copyright (C) 2014 Google, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ */
+
+#ifndef __SOC_TEGRA_XUSB_H__
+#define __SOC_TEGRA_XUSB_H__
+
+/* Command requests from the firmware */
+enum tegra_xusb_mbox_cmd {
+	MBOX_CMD_MSG_ENABLED = 1,
+	MBOX_CMD_INC_FALC_CLOCK,
+	MBOX_CMD_DEC_FALC_CLOCK,
+	MBOX_CMD_INC_SSPI_CLOCK,
+	MBOX_CMD_DEC_SSPI_CLOCK,
+	MBOX_CMD_SET_BW, /* no ACK/NAK required */
+	MBOX_CMD_SET_SS_PWR_GATING,
+	MBOX_CMD_SET_SS_PWR_UNGATING,
+	MBOX_CMD_SAVE_DFE_CTLE_CTX,
+	MBOX_CMD_AIRPLANE_MODE_ENABLED, /* unused */
+	MBOX_CMD_AIRPLANE_MODE_DISABLED, /* unused */
+	MBOX_CMD_START_HSIC_IDLE,
+	MBOX_CMD_STOP_HSIC_IDLE,
+	MBOX_CMD_DBC_WAKE_STACK, /* unused */
+	MBOX_CMD_HSIC_PRETEND_CONNECT,
+	MBOX_CMD_RESET_SSPI,
+	MBOX_CMD_DISABLE_SS_LFPS_DETECTION,
+	MBOX_CMD_ENABLE_SS_LFPS_DETECTION,
+	MBOX_CMD_SS_PORT_IN_POLLING,
+	MBOX_CMD_MAX,
+
+	/* Response message to above commands */
+	MBOX_CMD_ACK = 128,
+	MBOX_CMD_NAK,
+	MBOX_CMD_COMPL /* mailbox completed without sending ACK/NAK */
+};
+
+struct tegra_xusb_mbox_msg {
+	u32 cmd;
+	u32 data;
+};
+
+#endif /* __SOC_TEGRA_XUSB_H__ */
