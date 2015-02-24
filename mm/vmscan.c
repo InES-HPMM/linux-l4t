49cf4c647d8160a3ebba6e124375201d784fefdb
Krishna Reddy <vdumpa@nvidia.com>
2015-02-23 21:57:11 -0800

    mm: avoid kswapd for normal zone when dma32 zone exist
    Bug 200055047
    
    Change-Id: Ide4bbc16d2d927ea6f9fa3c26fdbe32cec0606f7
    Signed-off-by: Krishna Reddy <vdumpa@nvidia.com>
    Reviewed-on: http://git-master/r/710672
    Reviewed-on: http://git-master/r/710626

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -3150,6 +3150,12 @@ void wakeup_kswapd(struct zone *zone, int order, enum zone_type classzone_idx)
 {
 	pg_data_t *pgdat;
 
+#ifdef CONFIG_ZONE_DMA32
+	/* Avoid Normal zone balancing when DMA32 zone exist. */
+	if (classzone_idx == ZONE_NORMAL)
+		return;
+#endif
+
 	if (!populated_zone(zone))
 		return;
 
