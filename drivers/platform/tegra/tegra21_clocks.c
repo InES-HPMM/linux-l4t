1f6903e8f54a4cfc7bbfce4bae503c9ca4149652
Alex Frid <afrid@nvidia.com>
2015-03-10 23:18:09 -0700

    clock: tegra21: Set ADSP back-up frequency
    Specified ADSP back-up frequency below maximum characterized ADSP rate
    at minimum voltage. Calculated back-up divider accordingly instead of
    specifying back-up divider settings directly.
    
    Bug 1623029
    
    Change-Id: I3badbb7038876052d5872252eb44dd31e7ed27fb
    Signed-off-by: Alex Frid <afrid@nvidia.com>
    Reviewed-on: http://git-master/r/716657
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Yu-Huan Hsu <yhsu@nvidia.com>

--- drivers/platform/tegra/tegra21_clocks.c
+++ drivers/platform/tegra/tegra21_clocks.c
@@ -10050,6 +10050,7 @@ static void tegra21_pllp_init_dependencies(unsigned long pllp_rate)
 {
 	u32 div;
 	unsigned long backup_rate;
+	unsigned long adsp_backup_rate;
 
 	switch (pllp_rate) {
 	case 216000000:
@@ -10058,7 +10059,7 @@ static void tegra21_pllp_init_dependencies(unsigned long pllp_rate)
 		tegra_pll_p_out5.u.pll_div.default_rate = 108000000;
 		tegra_clk_sbus_cmplx.u.system.threshold = 108000000;
 		tegra_clk_host1x.u.periph.threshold = 108000000;
-		tegra_clk_aclk_adsp.u.cclk.div71 = 2; /* reg settings */
+		adsp_backup_rate = 108000000;
 		break;
 	case 408000000:
 		tegra_pll_p_out3.u.pll_div.default_rate = 102000000;
@@ -10066,7 +10067,7 @@ static void tegra21_pllp_init_dependencies(unsigned long pllp_rate)
 		tegra_pll_p_out5.u.pll_div.default_rate = 204000000;
 		tegra_clk_sbus_cmplx.u.system.threshold = 204000000;
 		tegra_clk_host1x.u.periph.threshold = 204000000;
-		tegra_clk_aclk_adsp.u.cclk.div71 = 2; /* reg settings */
+		adsp_backup_rate = 136000000;
 		break;
 	case 204000000:
 		tegra_pll_p_out3.u.pll_div.default_rate = 102000000;
@@ -10074,7 +10075,7 @@ static void tegra21_pllp_init_dependencies(unsigned long pllp_rate)
 		tegra_pll_p_out5.u.pll_div.default_rate = 204000000;
 		tegra_clk_sbus_cmplx.u.system.threshold = 204000000;
 		tegra_clk_host1x.u.periph.threshold = 204000000;
-		tegra_clk_aclk_adsp.u.cclk.div71 = 0; /* reg settings */
+		adsp_backup_rate = 102000000;
 		break;
 	default:
 		pr_err("tegra: PLLP rate: %lu is not supported\n", pllp_rate);
@@ -10089,6 +10090,11 @@ static void tegra21_pllp_init_dependencies(unsigned long pllp_rate)
 	div = pllp_rate / CPU_LP_BACKUP_RATE_TARGET;
 	backup_rate = pllp_rate / div;
 	tegra_clk_virtual_cpu_lp.u.cpu.backup_rate = backup_rate;
+
+	div = pllp_rate / adsp_backup_rate;
+	tegra_clk_aclk_adsp.u.cclk.div71 = 2 * div - 2; /* reg settings */
+	backup_rate = pllp_rate / div;
+	tegra_clk_virtual_adsp_cpu.u.cpu.backup_rate = adsp_backup_rate;
 }
 
 static void tegra21_init_one_clock(struct clk *c)
