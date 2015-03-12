9e1005b01be24f09a7ee66e3c9e5dacfa139c03a
Suresh Mangipudi <smangipudi@nvidia.com>
2015-03-12 19:14:38 +0530

    USB: phy: disable PD_TX after delay
    Delay after disabling PD_TX could cause a glitch on the USB bus
    
    Bug 1600315
    
    Change-Id: I89e9eec228201ecca27dd750eaf8116704dd4315
    Signed-off-by: Suresh Mangipudi <smangipudi@nvidia.com>
    Reviewed-on: http://git-master/r/716824
    (cherry picked from commit 62d9de2c0654f13eadebb77e7214e845de84b373)
    Reviewed-on: http://git-master/r/734251
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Venkat Moganty <vmoganty@nvidia.com>

--- drivers/usb/phy/tegra11x_usb_phy.c
+++ drivers/usb/phy/tegra11x_usb_phy.c
@@ -2115,19 +2115,19 @@ static void uhsic_phy_restore_end(struct tegra_usb_phy *phy)
 		phy->ctrlr_suspended = false;
 	}
 
-	val = tegra_usb_pmc_reg_read(PMC_UHSIC_SLEEP_CFG(phy->inst));
-	if (val & UHSIC_MASTER_ENABLE(phy->inst)) {
-		val = readl(base + UHSIC_PADS_CFG1);
-		val &= ~(UHSIC_PD_TX);
-		writel(val, base + UHSIC_PADS_CFG1);
-	}
-
 	if (irq_disabled) {
 		local_irq_restore(flags);
 		usleep_range(25000, 26000);
 		local_irq_save(flags);
 	} else
 		usleep_range(10000, 11000);
+
+	val = tegra_usb_pmc_reg_read(PMC_UHSIC_SLEEP_CFG(phy->inst));
+	if (val & UHSIC_MASTER_ENABLE(phy->inst)) {
+		val = readl(base + UHSIC_PADS_CFG1);
+		val &= ~(UHSIC_PD_TX);
+		writel(val, base + UHSIC_PADS_CFG1);
+	}
 
 	pmc->pmc_ops->disable_pmc_bus_ctrl(pmc, 1);
 	phy->pmc_remote_wakeup = false;
