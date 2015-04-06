068354121f6b43b283d455395a64ad2a727b0fbb
Naveen Kumar S <nkumars@nvidia.com>
2015-04-06 19:11:52 +0530

    fiq_debugger: register fiq mode only if FIQ glue is supported
    CONFIG_FIQ_GLUE option is required for fiq mode so as to register
    a fiq glue handler, which is called by the FIQ interrupt handler,
    and pass CPU context between the interrupt handler and the fiq
    glue handler.
    
    Driver probe should fail fast for fiq mode if FIQ glue is not
    supported, otherwise there can be some unexpected probe failures.
    
    Bug 200093033
    
    Change-Id: I5cdaef7ef00fe4edf44f4c66e4e8b42166bcab6a
    Signed-off-by: Naveen Kumar S <nkumars@nvidia.com>
    Signed-off-by: Allen Yu <alleny@nvidia.com>
    Reviewed-on: http://git-master/r/728037
    Reviewed-by: Varun Wadekar <vwadekar@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Hayden Du <haydend@nvidia.com>

--- drivers/staging/android/fiq_debugger/fiq_debugger.c
+++ drivers/staging/android/fiq_debugger/fiq_debugger.c
@@ -4,7 +4,7 @@
  * Serial Debugger Interface accessed through an FIQ interrupt.
  *
  * Copyright (C) 2008 Google, Inc.
- * Copyright (c) 2014, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2014-2015, NVIDIA CORPORATION.  All rights reserved.
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -1069,6 +1069,11 @@ static int fiq_debugger_probe(struct platform_device *pdev)
 	 * is required */
 	if ((uart_irq < 0 && fiq < 0) || (uart_irq >= 0 && fiq >= 0))
 		return -EINVAL;
+#ifndef CONFIG_FIQ_GLUE
+	/* FIQ glue is required for fiq mode */
+	if (fiq >= 0)
+		return -EINVAL;
+#endif
 	if (fiq >= 0 && !pdata->fiq_enable)
 		return -EINVAL;
 
