1dc3326266d007d19746d4d2684b957356b4d8a2
sreenivasulu velpula <svelpula@nvidia.com>
2015-04-07 11:07:55 +0530

    arm: tegra: p2360: Fix for cpu clock source switch
    Don't make cpu clock rate fixed.
    
    cpu clock source switch is done via clock_set_rate API.
    If fixed_rate, then clock_set_rate will simply return
    success instead of proceeding further for clock source
    switch based on dfll_data.range variable.
    
    Bug 200094383
    
    Change-Id: Ic2cd0c150353362dc7186c81c5a87eb0deb90881
    Signed-off-by: sreenivasulu velpula <svelpula@nvidia.com>
    Reviewed-on: http://git-master/r/728420
    (cherry picked from commit 80380911a2a715d828ffebf945e701776e1af632)
    Signed-off-by: sreenivasulu velpula <svelpula@nvidia.com>
    Reviewed-on: http://git-master/r/743784
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Sumeet Gupta <sumeetg@nvidia.com>

--- arch/arm/mach-tegra/board-p2360.c
+++ arch/arm/mach-tegra/board-p2360.c
@@ -102,7 +102,6 @@ static struct platform_device *p2360_devices[] __initdata = {
 
 static __initdata struct tegra_clk_init_table p2360_fixed_rate_clk_table[] = {
 	{ "gk20a.gbus",	NULL,	600000000,	false},
-	{ "cpu_g",	NULL,	1524000000,	true},
 	{ NULL,		NULL,	0,		0},
 };
 
