c6ac75295e7ea8f125c6d9824e0094da3569c87d
Nitin Kumbhar <nkumbhar@nvidia.com>
2015-04-08 15:01:57 +0530

    tegra: nvadsp: fix local header inclusion
    To avoid need of extra flag for include path of local header
    files, change the way headers are included by aram/dram memory
    manager.
    
    Bug 1619059
    
    Change-Id: Ib82980551a71bbdd002d53f3410682a46769f76b
    Signed-off-by: Nitin Kumbhar <nkumbhar@nvidia.com>
    Reviewed-on: http://git-master/r/728924
    (cherry picked from commit 0e87aa358d9a448a9c2e426faa4b1e947132fb8d)
    Reviewed-on: http://git-master/r/736301
    Reviewed-by: Ajay Nandakumar M <anandakumarm@nvidia.com>

--- drivers/platform/tegra/nvadsp/dram_app_mem_manager.h
+++ drivers/platform/tegra/nvadsp/dram_app_mem_manager.h
@@ -1,7 +1,7 @@
 /*
  * Header file for dram app memory manager
  *
- * Copyright (c) 2014, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2014-2015, NVIDIA CORPORATION.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
@@ -16,7 +16,7 @@
 #ifndef __TEGRA_NVADSP_DRAM_APP_MEM_MANAGER_H
 #define __TEGRA_NVADSP_DRAM_APP_MEM_MANAGER_H
 
-#include <mem_manager.h>
+#include "mem_manager.h"
 
 int dram_app_mem_init(unsigned long, unsigned long);
 void dram_app_mem_exit(void);
