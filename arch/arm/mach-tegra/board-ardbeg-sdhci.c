641c553b033988c57b042eb36b9f481477fe9604
Mohan Thadikamalla <mohant@nvidia.com>
2015-04-09 12:35:24 +0530

    arm: dts: Add comms dtsi files for ST8
    - Add dtsi file for ST8 and include this on GPIO dtsi file
    - Check machine compatible before registering the platform
    devices in ardbeg_wifi_init
    
    Bug 200062862
    Bug 200093702
    
    Change-Id: I7590f2e059bcb38b7934e5e6cb35c6a6e051ff2e
    Signed-off-by: Mohan Thadikamalla <mohant@nvidia.com>
    Reviewed-on: http://git-master/r/715639
    (cherry picked from commit ceaae33a631729deda650a28658723c8f2303653)
    Reviewed-on: http://git-master/r/729495
    Reviewed-by: Eric Chuang <echuang@nvidia.com>
    Tested-by: Eric Chuang <echuang@nvidia.com>

--- arch/arm/mach-tegra/board-ardbeg-sdhci.c
+++ arch/arm/mach-tegra/board-ardbeg-sdhci.c
@@ -387,7 +387,8 @@ static int __init ardbeg_wifi_init(void)
 	struct board_info board_info;
 
 	tegra_get_board_info(&board_info);
-	if (of_machine_is_compatible("nvidia,green-arrow")) {
+	if (of_machine_is_compatible("nvidia,green-arrow") ||
+		of_machine_is_compatible("nvidia,tn8")) {
 		pr_err("%s: DT populated for %d board return\n",
 			__func__, board_info.board_id);
 		return 0;
