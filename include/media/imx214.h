327d3c74455eac066d126ebab5d0adcc91638de2
David Wang <davidw@nvidia.com>
2015-04-27 11:13:34 -0700

    media: tegra_v4l2_camera: update v4l2_imx214
    Updating the v4l2_imx214 driver to add additional
    modes, and controls.
    
    Bug 1579861.
    
    Change-Id: I393e0b4b9425434604fb293400626e900aec0d5f
    Signed-off-by: David Wang <davidw@nvidia.com>
    Reviewed-on: http://git-master/r/744465
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Gary Fitzer <gfitzer@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>

--- include/media/imx214.h
+++ include/media/imx214.h
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2014, NVIDIA Corporation.  All rights reserved.
+ * Copyright (c) 2014-2015, NVIDIA Corporation.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
@@ -21,21 +21,55 @@
 #include <media/nvc.h>
 #include <media/nvc_image.h>
 
-#define IMX214_IOCTL_SET_MODE		_IOW('o', 1, struct imx214_mode)
+#define IMX214_IOCTL_SET_MODE			_IOW('o', 1, struct imx214_mode)
 #define IMX214_IOCTL_GET_STATUS		_IOR('o', 2, __u8)
-#define IMX214_IOCTL_SET_FRAME_LENGTH	_IOW('o', 3, __u32)
-#define IMX214_IOCTL_SET_COARSE_TIME	_IOW('o', 4, __u32)
-#define IMX214_IOCTL_SET_GAIN		_IOW('o', 5, __u16)
-#define IMX214_IOCTL_GET_SENSORDATA	_IOR('o', 6, struct imx214_sensordata)
-#define IMX214_IOCTL_SET_GROUP_HOLD	_IOW('o', 7, struct imx214_ae)
+#define IMX214_IOCTL_SET_FRAME_LENGTH		_IOW('o', 3, __u32)
+#define IMX214_IOCTL_SET_COARSE_TIME		_IOW('o', 4, __u32)
+#define IMX214_IOCTL_SET_GAIN			_IOW('o', 5, __u16)
+#define IMX214_IOCTL_GET_SENSORDATA		_IOR('o', 6, \
+	 struct imx214_sensordata)
+#define IMX214_IOCTL_SET_GROUP_HOLD		_IOW('o', 7, struct imx214_ae)
 #define IMX214_IOCTL_SET_HDR_COARSE_TIME	_IOW('o', 8, struct imx214_hdr)
-#define IMX214_IOCTL_SET_POWER		_IOW('o', 20, __u32)
+#define IMX214_IOCTL_SET_POWER			_IOW('o', 20, __u32)
 
-#define IMX214_EEPROM_ADDRESS	0x50
-#define IMX214_EEPROM_SIZE	1024
+#define IMX214_EEPROM_ADDRESS		0x50
+#define IMX214_EEPROM_SIZE		1024
+#define IMX214_EEPROM_STR_SIZE		(IMX214_EEPROM_SIZE * 2)
 #define IMX214_EEPROM_BLOCK_SIZE	(1 << 8)
 #define IMX214_EEPROM_NUM_BLOCKS \
-	(IMX214_EEPROM_SIZE / IMX214_EEPROM_BLOCK_SIZE)
+	 (IMX214_EEPROM_SIZE / IMX214_EEPROM_BLOCK_SIZE)
+
+#define IMX214_OTP_CTRL_ADDR		0x0A00
+#define IMX214_OTP_STATUS_ADDR		0x0A01
+#define IMX214_OTP_PAGE_NUM_ADDR	0x0A02
+#define IMX214_OTP_PAGE_START_ADDR	0x0A04
+#define IMX214_OTP_PAGE_END_ADDR	0x0A43
+#define IMX214_OTP_NUM_PAGES		(16)
+#define IMX214_OTP_PAGE_SIZE \
+	 (IMX214_OTP_PAGE_END_ADDR - IMX214_OTP_PAGE_START_ADDR + 1)
+#define IMX214_OTP_SIZE \
+	 (IMX214_OTP_PAGE_SIZE * IMX214_OTP_NUM_PAGES)
+#define IMX214_OTP_STR_SIZE (IMX214_OTP_SIZE * 2)
+#define IMX214_OTP_STATUS_IN_PROGRESS		0
+#define IMX214_OTP_STATUS_READ_COMPLETE	1
+#define IMX214_OTP_STATUS_READ_FAIL		5
+
+#define IMX214_FUSE_ID_OTP_ROW_ADDR	0x0A36
+#define IMX214_FUSE_ID_OTP_PAGE	19 /*0x13*/
+#define IMX214_FUSE_ID_SIZE		11
+#define IMX214_FUSE_ID_STR_SIZE	(IMX214_FUSE_ID_SIZE * 2)
+
+#define IMX214_FRAME_LENGTH_ADDR_MSB		0x0340
+#define IMX214_FRAME_LENGTH_ADDR_LSB		0x0341
+#define IMX214_COARSE_TIME_ADDR_MSB		0x0202
+#define IMX214_COARSE_TIME_ADDR_LSB		0x0203
+#define IMX214_COARSE_TIME_SHORT_ADDR_MSB	0x0224
+#define IMX214_COARSE_TIME_SHORT_ADDR_LSB	0x0225
+#define IMX214_GAIN_ADDR_MSB			0x0204
+#define IMX214_GAIN_ADDR_LSB			0x0205
+#define IMX214_GAIN_SHORT_ADDR_MSB		0x0216
+#define IMX214_GAIN_SHORT_ADDR_LSB		0x0217
+#define IMX214_GROUP_HOLD_ADDR			0x0104
 
 struct imx214_mode {
 	__u32 xres;
@@ -64,14 +98,7 @@ struct imx214_ae {
 
 struct imx214_sensordata {
 	__u32 fuse_id_size;
-	__u8  fuse_id[16];
-};
-
-struct imx214_eeprom_data {
-	struct i2c_client *i2c_client;
-	struct i2c_adapter *adap;
-	struct i2c_board_info brd;
-	struct regmap *regmap;
+	__u8  fuse_id[IMX214_FUSE_ID_SIZE];
 };
 
 #ifdef __KERNEL__
@@ -82,6 +109,7 @@ struct imx214_power_rail {
 	struct regulator *ext_reg1;
 	struct regulator *ext_reg2;
 	struct clk *mclk;
+	unsigned int pwdn_gpio;
 	unsigned int cam1_gpio;
 	unsigned int reset_gpio;
 	unsigned int af_gpio;
