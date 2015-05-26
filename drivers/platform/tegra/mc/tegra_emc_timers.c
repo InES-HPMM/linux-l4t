9f69145b68de2ab17febb770d7894d8f63bc6b2c
Alex Waterman <alexw@nvidia.com>
2015-05-26 15:32:17 -0700

    Revert "platform: tegra: Fix sparse warning"
    This reverts commit cdc959fe3748c9d6f09dbad96c91644fccaf060b.
    
    The function tegra_check_disp_latency_allowance() will be used. The code
    that will actually use this API is still being developed.
    
    The change to the EMC timers code was also an incorrect fix. Including
    chip specific header files in the emc_timers code is incorrect.
    
    Change-Id: I018b7d7dc8e7565fd5b23ccf2d3fe0a22d5f5c0e
    Signed-off-by: Alex Waterman <alexw@nvidia.com>
    Reviewed-on: http://git-master/r/747436
    Reviewed-by: Yu-Huan Hsu <yhsu@nvidia.com>

--- drivers/platform/tegra/mc/tegra_emc_timers.c
+++ drivers/platform/tegra/mc/tegra_emc_timers.c
@@ -35,7 +35,6 @@
 #include <linux/timer.h>
 #include <linux/atomic.h>
 
-#include <linux/platform/tegra/tegra21_emc.h>
 #include <linux/platform/tegra/tegra_emc.h>
 
 #define TEGRA_DRAM_THERM_MAX_STATE     1
