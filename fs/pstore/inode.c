86eb8adae3e0afa1d3f3555c959f794bd970c427
Ian Chang <ianc@nvidia.com>
2015-07-01 10:24:22 +0800

    pstore: Removed id from pmsg-ramoops
    We are maintaining only one session's 
    ramoops and so no need to have ID.
    
    bug 200104058
    
    Change-Id: Ib862c331e0bb737555e429cd18f75bb936a708b8
    Signed-off-by: Ian Chang <ianc@nvidia.com>
    Reviewed-on: http://git-master/r/764534
    Reviewed-by: Sachin Nikam <snikam@nvidia.com>
    Reviewed-by: Gagan Grover <ggrover@nvidia.com>
    Reviewed-by: Mitch Luban <mluban@nvidia.com>

--- fs/pstore/inode.c
+++ fs/pstore/inode.c
@@ -405,7 +405,7 @@ int pstore_mkfile(enum pstore_type_id type, char *psname, u64 id, int count,
 		scnprintf(name, sizeof(name), "mce-%s-%lld", psname, id);
 		break;
 	case PSTORE_TYPE_PMSG:
-		scnprintf(name, sizeof(name), "pmsg-%s-%lld", psname, id);
+		scnprintf(name, sizeof(name), "pmsg-%s", psname);
 		break;
 	case PSTORE_TYPE_UNKNOWN:
 		scnprintf(name, sizeof(name), "unknown-%s-%lld", psname, id);
