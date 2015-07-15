77c8e7bcdd268ecf17743e8858d518d9a2f4ec41
Ivan Raul Guadarrama <iguadarrama@nvidia.com>
2015-07-15 18:19:13 +0300

    video: tegra: Add support for YUV420 only flag
    As userspace is required to set the FB_VMODE_Y420_ONLY flag for setting modes
    which only support YUV420 packing, update the conditions to also handle these
    modes. In general, very similar logic is needed for Y420 and Y420_ONLY modes.
    
    Bug 200110896
    
    Change-Id: Iedcd8471a6067a0084c8b9ebe220692fca04783d
    Signed-off-by: Ivan Raul Guadarrama <iguadarrama@nvidia.com>
    Reviewed-on: http://git-master/r/770536
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/video/tegra/dc/window.c
+++ drivers/video/tegra/dc/window.c
@@ -674,7 +674,8 @@ int tegra_dc_update_windows(struct tegra_dc_win *windows[], int n,
 			tegra_dc_writel(dc, 0, DC_WIN_WIN_OPTIONS);
 			if (dc->yuv_bypass) {
 				if (dc->mode.vmode &
-					(FB_VMODE_Y420 | FB_VMODE_Y24))
+					(FB_VMODE_Y420 | FB_VMODE_Y420_ONLY |
+					 FB_VMODE_Y24))
 					tegra_dc_writel(dc,
 						RGB_TO_YUV420_8BPC_BLACK_PIX,
 						DC_DISP_BLEND_BACKGROUND_COLOR);
