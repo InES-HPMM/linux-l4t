af77b4cc9f89f6c7d54c4771e87da1072b5ef495
Anand Prasad <anprasad@nvidia.com>
2015-07-21 12:28:49 -0700

    video: tegra: dc: Remove DPAUX powerdown for T210
    Powering down DPAUX caused lockouts with the crc testing device midway
    through the test. Post power-down, all subsequent i2c transactions
    to/from the device were locked out.
    
    Skipping the powering down sequence of the DPAUX pads is needed only on
    T210 onwards, where the default behaviour is to have these pads powered off.
    
    Bug 200111886
    Bug 1659406
    Bug 200114600
    
    Change-Id: I2fdf226a76e1f3732b89a02cc244bf7c387725d9
    Signed-off-by: Anand Prasad <anprasad@nvidia.com>
    Reviewed-on: http://git-master/r/773093
    (cherry picked from commit 0087651d511ce0c2f851517abbcaf5706e4823c3)
    Reviewed-on: http://git-master/r/780528
    Reviewed-by: Mitch Luban <mluban@nvidia.com>
    Tested-by: Mitch Luban <mluban@nvidia.com>

--- drivers/video/tegra/dc/dc.c
+++ drivers/video/tegra/dc/dc.c
@@ -3621,7 +3621,10 @@ static bool _tegra_dc_controller_enable(struct tegra_dc *dc)
 {
 	int failed_init = 0;
 	int i;
+
+#if !defined(CONFIG_ARCH_TEGRA_21x_SOC)
 	struct device_node *np_dpaux;
+#endif
 
 	if (WARN_ON(!dc || !dc->out || !dc->out_ops))
 		return false;
@@ -3665,6 +3668,7 @@ static bool _tegra_dc_controller_enable(struct tegra_dc *dc)
 		return false;
 	}
 
+#if !defined(CONFIG_ARCH_TEGRA_21x_SOC)
 	if (dc->out->type != TEGRA_DC_OUT_DP) {
 		np_dpaux = of_find_node_by_path(
 				dc->ndev->id ? DPAUX1_NODE : DPAUX_NODE);
@@ -3674,6 +3678,7 @@ static bool _tegra_dc_controller_enable(struct tegra_dc *dc)
 			TEGRA_DPAUX_INSTANCE_0, false);
 		of_node_put(np_dpaux);
 	}
+#endif
 
 	if (dc->out_ops && dc->out_ops->enable)
 		dc->out_ops->enable(dc);
