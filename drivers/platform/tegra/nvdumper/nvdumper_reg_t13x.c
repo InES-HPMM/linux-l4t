095a5b865394467aeb4ba99b68f178f3850666b4
Amit Sharma (SW-TEGRA) <amisharma@nvidia.com>
2015-09-08 15:28:14 +0530

    platform: tegra: nvdumper: add device info
    API: nvdumper_regdump_{init/exit}() allocate/free a bounce buffer in/from DMA
    Memory. For this nvdumper driver calls dma_{alloc/free}_coherent() API's with
    first arg: device as NULL.
    
    However, in order to monitor the DMA'able memory in debugfs: "/d/dma/usage",
    we need the device name info of each such client which uses the DMA API's
    like: dma_{alloc/free}_coherent. Therefore, declare a device struct and
    pouplate it with missing device name info.
    
    Bug 200134203
    
    Change-Id: I1d13cf048241b584e5a78272062938cbe6e1e786
    Signed-off-by: Amit Sharma (SW-TEGRA) <amisharma@nvidia.com>
    Reviewed-on: http://git-master/r/795687
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Sri Krishna Chowdary <schowdary@nvidia.com>
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>

--- drivers/platform/tegra/nvdumper/nvdumper_reg_t13x.c
+++ drivers/platform/tegra/nvdumper/nvdumper_reg_t13x.c
@@ -93,6 +93,7 @@ struct nvdumper_cpu_data_t {
 	struct task_struct *current_task;
 };
 
+static struct device nvdumper_dev;
 static struct nvdumper_cpu_data_t *nvdumper_cpu_data;
 static int max_cpus;
 static dma_addr_t nvdumper_p;
@@ -289,14 +290,28 @@ int nvdumper_regdump_init(void)
 	int ret;
 
 	max_cpus = num_possible_cpus();
+	nvdumper_dev.parent = NULL;
+	dev_set_name(&nvdumper_dev, "nvdumper_dev");
+	ret = device_register(&nvdumper_dev);
+	if (ret) {
+		pr_err("ERROR: nvdumper_dev device creation failed: %d\n", ret);
+		return ret;
+	}
 
-	nvdumper_cpu_data = dma_alloc_coherent(NULL,
+	ret = dma_set_coherent_mask(&nvdumper_dev, DMA_BIT_MASK(32));
+	if (ret) {
+		dev_err(&nvdumper_dev,
+			"dma_set_coherent_mask fails with: %d\n", ret);
+		goto clean;
+	}
+
+	nvdumper_cpu_data = dma_alloc_coherent(&nvdumper_dev,
 		sizeof(struct nvdumper_cpu_data_t) * max_cpus,
 		&nvdumper_p, 0);
 	if (!nvdumper_cpu_data) {
 		pr_err("%s: can not allocate bounce buffer\n", __func__);
-
-		return -ENOMEM;
+		ret = -ENOMEM;
+		goto clean;
 	}
 
 	ret = register_die_notifier(&nvdumper_die_notifier);
@@ -321,18 +336,21 @@ err_out2:
 
 err_out1:
 	if (nvdumper_cpu_data)
-		dma_free_coherent(NULL,
+		dma_free_coherent(&nvdumper_dev,
 				sizeof(struct nvdumper_cpu_data_t) * max_cpus,
 				nvdumper_cpu_data, nvdumper_p);
-
+clean:
+	device_unregister(&nvdumper_dev);
 	return ret;
 }
 
 void nvdumper_regdump_exit(void)
 {
-	dma_free_coherent(NULL, sizeof(struct nvdumper_cpu_data_t) * max_cpus,
+	dma_free_coherent(&nvdumper_dev,
+			sizeof(struct nvdumper_cpu_data_t) * max_cpus,
 			nvdumper_cpu_data, nvdumper_p);
 	unregister_die_notifier(&nvdumper_die_notifier);
 	atomic_notifier_chain_unregister(&panic_notifier_list,
 			&nvdumper_panic_notifier);
+	device_unregister(&nvdumper_dev);
 }
