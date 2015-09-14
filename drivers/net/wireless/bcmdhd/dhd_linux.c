cdd719b7c16df2b4720170b8213ddc2bd8a6bfeb
kraghavender <kraghavender@nvidia.com>
2015-09-14 16:29:10 +0530

    net: wireless: bcmdhd: Ignore IOCTL during shutdown
    Ignore the IOCTL's based on shutdown process instead of
    additonal DHD_BUS and hang checks. Control hang logs
    during shutdown process.
    
    Bug 200135299
    
    Change-Id: Ibecaf08b81a9460d55f23af28893435a1999af48
    Signed-off-by: Kasturi Raghavender <kraghavender@nvidia.com>
    Reviewed-on: http://git-master/r/798695
    Reviewed-by: Ashutosh Jha <ajha@nvidia.com>
    Tested-by: Ashutosh Jha <ajha@nvidia.com>

--- drivers/net/wireless/bcmdhd/dhd_linux.c
+++ drivers/net/wireless/bcmdhd/dhd_linux.c
@@ -508,6 +508,10 @@ typedef struct dhd_info {
 
 #define DHDIF_FWDER(dhdif)      FALSE
 
+int dhd_skip_hang_event(void *dhd) {
+	return (((wifi_adapter_info_t *)(((dhd_info_t *)dhd)->adapter))->skip_hang_evt ? 1:0);
+}
+
 /* Flag to indicate if we should download firmware on driver load */
 uint dhd_download_fw_on_driverload = TRUE;
 
@@ -3917,6 +3921,10 @@ static bool dhd_check_hang(struct net_device *net, dhd_pub_t *dhdp, int error)
 {
 	dhd_info_t *dhd;
 	wifi_adapter_info_t *adapter;
+	char static skip_hang_evt;
+
+	if (skip_hang_evt)
+		return FALSE;
 
 	if (!dhdp) {
 		DHD_ERROR(("%s: dhdp is NULL\n", __FUNCTION__));
@@ -3930,7 +3938,8 @@ static bool dhd_check_hang(struct net_device *net, dhd_pub_t *dhdp, int error)
 	adapter = dhd->adapter;
 
 	if (adapter->skip_hang_evt) {
-		DHD_ERROR(("%s: skipping hang evt during shutdown\n", __FUNCTION__));
+		skip_hang_evt=1;
+		DHD_TRACE(("%s: skipping hang evt during shutdown\n", __FUNCTION__));
 		return FALSE;
 	}
 
