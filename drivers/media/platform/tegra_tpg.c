d65600fcf77a75244b53330520426ff1cf4ef196
Alban Bedel <alban.bedel@avionic-design.de>
2015-10-08 17:53:27 +0200

    tegra: Add a new driver for the tegra's VI2
    Second prototype of our new driver. It is now basically working and
    has been tested with RAW, YUV and YUV formats.
    
    TODO:
     * Test planar formats
     * Test with dual camera setup
     * Fix broadcating one source to both pipes
     * Start/stop clocks and regulators only when really needed
     * Support 8 lanes input
    
    Change-Id: I4b56a3baaa30d883f12ef192b8c6084cdc940938
    Signed-off-by: Alban Bedel <alban.bedel@avionic-design.de>
    #
    #WARNING: line over 80 characters
    ##728: FILE: drivers/media/i2c/tc358840.c:693:
    #+			if (bt->width == 1920 && bt->height == 1080 && fps(bt) == 60 &&
    #
    #WARNING: line over 80 characters
    ##729: FILE: drivers/media/i2c/tc358840.c:694:
    #+					state->mbus_fmt_code == V4L2_MBUS_FMT_UYVY8_1X16) {
    #
    #WARNING: line over 80 characters
    ##733: FILE: drivers/media/i2c/tc358840.c:698:
    #+				pllconf_new = SET_PLL_PRD(pll_prd) | SET_PLL_FBD(pll_fbd);
    #
    #WARNING: line over 80 characters
    ##999: FILE: drivers/media/i2c/tc358840.c:964:
    #+			/* CISCO HACK: Use all lanes and lower clock speed for 1080p60 to
    #
    #WARNING: line over 80 characters
    ##1001: FILE: drivers/media/i2c/tc358840.c:966:
    #+			if (bt->width == 1920 && bt->height == 1080 && fps(bt) == 60 &&
    #
    #WARNING: line over 80 characters
    ##1002: FILE: drivers/media/i2c/tc358840.c:967:
    #+					state->mbus_fmt_code == V4L2_MBUS_FMT_UYVY8_1X16) {
    #
    #WARNING: line over 80 characters
    ##1014: FILE: drivers/media/i2c/tc358840.c:979:
    #+				i2c_wr32(sd, TCLK_HEADERCNT, pdata->tclk_headercnt);
    #
    #WARNING: line over 80 characters
    ##1015: FILE: drivers/media/i2c/tc358840.c:980:
    #+				i2c_wr32(sd, TCLK_TRAILCNT, pdata->tclk_trailcnt);
    #
    #WARNING: line over 80 characters
    ##1016: FILE: drivers/media/i2c/tc358840.c:981:
    #+				i2c_wr32(sd, THS_HEADERCNT, pdata->ths_headercnt);
    #
    #WARNING: line over 80 characters
    ##1141: FILE: drivers/media/i2c/tc358840.c:1106:
    #+	static struct v4l2_dv_timings default_timing = V4L2_DV_BT_CEA_1920X1080P60;
    #
    #WARNING: line over 80 characters
    ##1416: FILE: drivers/media/i2c/tc358840.c:1381:
    #+		 * The video is received by the OMAP (omapconf read 0×52001070),
    #
    #WARNING: line over 80 characters
    ##2188: FILE: drivers/media/i2c/tc358840.c:2153:
    #+static int tc358840_probe(struct i2c_client *client, const struct i2c_device_id *id)
    #
    #WARNING: line over 80 characters
    ##2200: FILE: drivers/media/i2c/tc358840.c:2165:
    #+	state = devm_kzalloc(&client->dev, sizeof(struct tc358840_state), GFP_KERNEL);
    #
    #total: 0 errors, 13 warnings, 3118 lines checked
    #
    #Your patch has style problems, please review.
    #
    #If any of these errors are false positives, please report
    #them to the maintainer, see CHECKPATCH in MAINTAINERS.

---
+++ drivers/media/platform/tegra_tpg.c
@@ -0,0 +1,182 @@
+/*
+ * Copyright 2015 Alban Bedel <alban.bedel@avionic-design.de>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2.
+ *
+ */
+
+#include <linux/platform_device.h>
+#include <linux/io.h>
+
+#include <media/v4l2-common.h>
+#include <media/v4l2-subdev.h>
+
+#include "tegra_vi2.h"
+
+struct tegra_tpg_color_regs {
+	u32 freq;
+	u32 freq_rate;
+};
+
+struct tegra_tpg_regs {
+	u32 ctrl;
+	u32 blank;
+	u32 phase;
+	struct tegra_tpg_color_regs color[3];
+};
+
+struct tegra_tpg {
+	struct v4l2_subdev sd;
+	struct tegra_tpg_regs __iomem *regs;
+	struct v4l2_mbus_framefmt fmt;
+	unsigned mode;
+	bool auto_inc;
+};
+
+#if 0
+#define TPG_MAX_WIDTH 3840
+#define TPG_MAX_HEIGHT 2160
+#else
+#define TPG_MAX_WIDTH 1280
+#define TPG_MAX_HEIGHT 720
+#endif
+
+static int tegra_tpg_enum_mbus_fmt(struct v4l2_subdev *sd, unsigned int index,
+				enum v4l2_mbus_pixelcode *code)
+{
+	switch (index) {
+	case 0:
+		*code = V4L2_MBUS_FMT_SBGGR10_1X10;
+		return 0;
+	case 1:
+		*code = V4L2_MBUS_FMT_RGB888_1X24;
+		return 0;
+	default:
+		return -EINVAL;
+	}
+}
+
+static int tegra_tpg_g_mbus_fmt(
+	struct v4l2_subdev *sd, struct v4l2_mbus_framefmt *fmt)
+{
+	struct tegra_tpg *tpg = v4l2_get_subdevdata(sd);
+
+	*fmt = tpg->fmt;
+
+	return 0;
+}
+
+static int tegra_tpg_try_mbus_fmt(
+	struct v4l2_subdev *sd, struct v4l2_mbus_framefmt *fmt)
+{
+	switch (fmt->code) {
+	case V4L2_MBUS_FMT_RGB888_1X24:
+	case V4L2_MBUS_FMT_SBGGR10_1X10:
+		break;
+	default:
+		return -EINVAL;
+	}
+
+	v4l_bound_align_image(&fmt->width, 16, TPG_MAX_WIDTH, 4,
+				&fmt->height, 16, TPG_MAX_HEIGHT, 1, 16);
+
+	fmt->field = V4L2_FIELD_NONE;
+	fmt->colorspace = V4L2_COLORSPACE_SRGB;
+
+	return 0;
+}
+
+static int tegra_tpg_s_mbus_fmt(
+	struct v4l2_subdev *sd, struct v4l2_mbus_framefmt *fmt)
+{
+	struct tegra_tpg *tpg = v4l2_get_subdevdata(sd);
+	int err;
+
+	err = tegra_tpg_try_mbus_fmt(sd, fmt);
+	if (err)
+		return err;
+
+	tpg->fmt = *fmt;
+
+	return 0;
+}
+
+static int tegra_tpg_g_mbus_config(
+	struct v4l2_subdev *sd, struct v4l2_mbus_config *cfg)
+{
+	cfg->type = V4L2_MBUS_CSI2;
+	cfg->flags = V4L2_MBUS_CSI2_4_LANE |
+		V4L2_MBUS_CSI2_CHANNEL_0 |
+		V4L2_MBUS_CSI2_CONTINUOUS_CLOCK;
+
+	return 0;
+}
+
+static int tegra_tpg_s_stream(struct v4l2_subdev *sd, int enable)
+{
+	struct tegra_tpg *tpg = v4l2_get_subdevdata(sd);
+	u32 mode = (tpg->mode & 3) << 2;
+
+	if (tpg->auto_inc)
+		mode |= BIT(1);
+
+	if (enable)
+		mode |= BIT(0);
+
+	writel(0, &tpg->regs->phase);
+	writel(0x100010, &tpg->regs->color[0].freq);
+	writel(0, &tpg->regs->color[0].freq_rate);
+	writel(0x100010, &tpg->regs->color[1].freq);
+	writel(0, &tpg->regs->color[1].freq_rate);
+	writel(0x100010, &tpg->regs->color[2].freq);
+	writel(0, &tpg->regs->color[2].freq_rate);
+
+	/* Slows things down by setting VBLANK as high as possible */
+	writel(0xFFFF0008, &tpg->regs->blank);
+
+	writel(mode, &tpg->regs->ctrl);
+
+	return 0;
+}
+
+static const struct v4l2_subdev_video_ops tegra_tpg_video_ops = {
+	.enum_mbus_fmt = tegra_tpg_enum_mbus_fmt,
+	.try_mbus_fmt = tegra_tpg_try_mbus_fmt,
+	.g_mbus_fmt = tegra_tpg_g_mbus_fmt,
+	.s_mbus_fmt = tegra_tpg_s_mbus_fmt,
+	.g_mbus_config = tegra_tpg_g_mbus_config,
+	.s_stream = tegra_tpg_s_stream,
+};
+
+static const struct v4l2_subdev_ops tegra_tpg_ops = {
+	.video = &tegra_tpg_video_ops,
+};
+
+struct v4l2_subdev *tegra_tpg_init(
+	struct platform_device *pdev, void __iomem *base)
+{
+	struct tegra_tpg *tpg;
+
+	tpg = devm_kzalloc(&pdev->dev, sizeof(*tpg), GFP_KERNEL);
+	if (!tpg)
+		return ERR_PTR(-ENOMEM);
+
+	v4l2_subdev_init(&tpg->sd, &tegra_tpg_ops);
+	tpg->sd.owner = THIS_MODULE;
+	v4l2_set_subdevdata(&tpg->sd, tpg);
+	tpg->regs = base;
+
+	tpg->fmt.width = TPG_MAX_WIDTH;
+	tpg->fmt.height = TPG_MAX_HEIGHT;
+	tpg->fmt.code = V4L2_MBUS_FMT_RGB888_1X24;
+	tpg->fmt.field = V4L2_FIELD_NONE;
+	tpg->fmt.colorspace = V4L2_COLORSPACE_SRGB;
+
+	/* Set default settings */
+	tpg->mode = 1;
+	tpg->auto_inc = true;
+
+	return &tpg->sd;
+}
