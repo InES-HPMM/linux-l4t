200b30781d64819b5b456332c27bd1b8d8283006
Michael Hsu <mhsu@nvidia.com>
2016-01-08 16:16:08 -0800

    net: wireless: bcmdhd_88: Fix Android Bug 25306181
    Original patch source: https://android.googlesource.com/kernel/msm
    
    Original patch commit message:
    > commit 3fffc78f70dc101add8b82af878d53457713d005
    > Author: Patrick Tjin <pattjin@google.com>
    > Date:   Wed Dec 9 15:24:05 2015 -0800
    >
    >     net: wireless: bcmdhd: check packet length for event messages
    >
    >     Check the datalen field is less than the size of
    >     packet received from the network.
    >
    >     Bug: 25306181
    >
    >     Signed-off-by: Patrick Tjin <pattjin@google.com>
    >     Change-Id: I3b021d88a95bd7d4e6e0d745d2527d73487bcadc
    >     (cherry picked from commit 10b850b7e82873a14068d24dac4fc2080d46ff76)
    
    Bug 1716690
    
    Change-Id: Ib7c75be9f8c3b8cb7b94690abb8e983a9b9145e5
    Signed-off-by: Michael Hsu <mhsu@nvidia.com>
    Reviewed-on: http://git-master/r/932614
    (cherry picked from commit 76ac78628cd40d9aa2a9718d609349247e3e1511)
    Reviewed-on: http://git-master/r/933908
    Reviewed-by: Om Prakash Singh <omp@nvidia.com>
    Tested-by: Om Prakash Singh <omp@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>

--- drivers/net/wireless/bcmdhd_88/dhd_linux.c
+++ drivers/net/wireless/bcmdhd_88/dhd_linux.c
@@ -612,7 +612,7 @@ static int dhd_toe_get(dhd_info_t *dhd, int idx, uint32 *toe_ol);
 static int dhd_toe_set(dhd_info_t *dhd, int idx, uint32 toe_ol);
 #endif /* TOE */
 
-static int dhd_wl_host_event(dhd_info_t *dhd, int *ifidx, void *pktdata,
+static int dhd_wl_host_event(dhd_info_t *dhd, int *ifidx, void *pktdata, size_t pktlen,
                              wl_event_msg_t *event_ptr, void **data_ptr);
 void dhd_enable_packet_filter(int value, dhd_pub_t *dhd);
 
@@ -2183,6 +2183,7 @@ dhd_rx_frame(dhd_pub_t *dhdp, int ifidx, void *pktbuf, int numpkt, uint8 chan)
 #else
 			skb->mac.raw,
 #endif
+			len - 2,
 			&event,
 			&data);
 
@@ -5672,13 +5673,13 @@ dhd_get_wireless_stats(struct net_device *dev)
 #endif /* defined(WL_WIRELESS_EXT) */
 
 static int
-dhd_wl_host_event(dhd_info_t *dhd, int *ifidx, void *pktdata,
+dhd_wl_host_event(dhd_info_t *dhd, int *ifidx, void *pktdata, size_t pktlen,
 	wl_event_msg_t *event, void **data)
 {
 	int bcmerror = 0;
 	ASSERT(dhd != NULL);
 
-	bcmerror = wl_host_event(&dhd->pub, ifidx, pktdata, event, data);
+	bcmerror = wl_host_event(&dhd->pub, ifidx, pktdata, pktlen, event, data);
 	if (bcmerror != BCME_OK)
 		return (bcmerror);
 
