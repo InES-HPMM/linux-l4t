bb4016a02053de05b16a035a05e95ac5d2d0cdb1
TW Chiu <twchiu@nvidia.com>
2016-01-27 14:56:46 +0800

    usb: cdc_ether: blacklist RTL8153
    Blacklist the following:
    
    REALTEK_VENDOR_ID, 0x8153
    NVIDIA_VENDOR_ID, 0x09ff
    
    Bug 200162110
    
    Change-Id: Iddf24eab87d1c7e7d48dda43cbc394983b7dd711
    Signed-off-by: TW Chiu <twchiu@nvidia.com>
    Reviewed-on: http://git-master/r/1001466
    (cherry picked from commit 354e13f54b7ad744ca76938c533cfaeb7bb418fc)
    Reviewed-on: http://git-master/r/1021833
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Bibek Basu <bbasu@nvidia.com>

--- drivers/net/usb/cdc_ether.c
+++ drivers/net/usb/cdc_ether.c
@@ -490,6 +490,7 @@ static const struct driver_info rmnet_info = {
 #define DELL_VENDOR_ID		0x413C
 #define REALTEK_VENDOR_ID	0x0bda
 #define SAMSUNG_VENDOR_ID       0x04e8
+#define NVIDIA_VENDOR_ID	0x0955
 
 static const struct usb_device_id	products [] = {
 /*
@@ -697,6 +698,20 @@ static const struct usb_device_id	products [] = {
 /* Samsung USB Ethernet Adapters */
 {
 	USB_DEVICE_AND_INTERFACE_INFO(SAMSUNG_VENDOR_ID, 0xa101, USB_CLASS_COMM,
+			USB_CDC_SUBCLASS_ETHERNET, USB_CDC_PROTO_NONE),
+	.driver_info = 0,
+},
+
+/* RTL8153 */
+{
+	USB_DEVICE_AND_INTERFACE_INFO(REALTEK_VENDOR_ID, 0x8153, USB_CLASS_COMM,
+			USB_CDC_SUBCLASS_ETHERNET, USB_CDC_PROTO_NONE),
+	.driver_info = 0,
+},
+
+/* Nvidia USB Ethernet */
+{
+	USB_DEVICE_AND_INTERFACE_INFO(NVIDIA_VENDOR_ID, 0x09ff, USB_CLASS_COMM,
 			USB_CDC_SUBCLASS_ETHERNET, USB_CDC_PROTO_NONE),
 	.driver_info = 0,
 },
