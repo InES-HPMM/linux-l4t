145a9a7edb14e4101983e0cb721c744621c11672
Wenjia Zhou <wenjiaz@nvidia.com>
2016-02-26 18:43:02 -0800

    drivers:media:platform: Add common mipical
    Add common mipical driver for CSI and DSI
    
    bug 1686313
    
    Change-Id: I4bac768c17805c8d71d7071ceac29d17d83416db
    Signed-off-by: Wenjia Zhou <wenjiaz@nvidia.com>
    (cherry picked from commit c2c51629c2d1dab55c8d0be8496cf01373ce19cc)
    Reviewed-on: http://git-master/r/1122125
    Signed-off-by: Bryan Wu <pengw@nvidia.com>
    Reviewed-on: http://git-master/r/1141915
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

---
+++ drivers/media/platform/tegra/mipical/mipi_cal.h
@@ -0,0 +1,43 @@
+/*
+ * drivers/misc/mipi_cal.h
+ *
+ * Copyright (c) 2016, NVIDIA CORPORATION, All rights reserved.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef MIPI_CAL_H
+#define MIPI_CAL_H
+
+#define DSID	(1 << 31)
+#define DSIC	(1 << 30)
+#define DSIB	(1 << 29)
+#define DSIA	(1 << 28)
+#define CSIF	(1 << 25)
+#define CSIE	(1 << 24)
+#define CSID	(1 << 23)
+#define CSIC	(1 << 22)
+#define CSIB	(1 << 21)
+#define CSIA	(1 << 20)
+
+#if defined(CONFIG_ARCH_TEGRA_21x_SOC)
+extern int tegra_mipi_bias_pad_enable(void);
+extern int tegra_mipi_bias_pad_disable(void);
+extern int tegra_mipi_calibration(int lanes);
+extern int tegra_mipi_select_mode(int mode);
+#else
+static int tegra_mipi_bias_pad_enable(void) {return -ENOSYS; }
+static int tegra_mipi_bias_pad_disable(void) {return -ENOSYS; }
+static int tegra_mipi_calibration(int lanes) {return -ENOSYS; }
+static int tegra_mipi_select_mode(int mode) {return -ENOSYS; }
+#endif
+
+#endif
