03fc0762c40e5ab068abeb6d6d97f9825d5f8392
Ken Chang <kenc@nvidia.com>
2016-04-06 16:40:28 +0800

    media: i2c: ov5693: add g_input_status
    Add g_input_status video op and return the status
    based on the pw->state value.
    
    Bug 200184502
    
    Change-Id: I89f7e07fb193ec62ecb0851a364dadb1ce6bf35c
    Signed-off-by: Ken Chang <kenc@nvidia.com>
    (cherry picked from commit 66525a54bab974421fcd17aef2f96a9fe98e2187)
    Reviewed-on: http://git-master/r/1120974
    Signed-off-by: Bryan Wu <pengw@nvidia.com>
    Reviewed-on: http://git-master/r/1141891
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Jihoon Bang <jbang@nvidia.com>

--- drivers/media/i2c/ov5693.c
+++ drivers/media/i2c/ov5693.c
@@ -529,6 +529,17 @@ exit:
 	return err;
 }
 
+static int ov5693_g_input_status(struct v4l2_subdev *sd, u32 *status)
+{
+	struct i2c_client *client = v4l2_get_subdevdata(sd);
+	struct camera_common_data *s_data = to_camera_common_data(client);
+	struct ov5693 *priv = (struct ov5693 *)s_data->priv;
+	struct camera_common_power_rail *pw = &priv->power;
+
+	*status = pw->state == SWITCH_ON;
+	return 0;
+}
+
 static struct v4l2_subdev_video_ops ov5693_subdev_video_ops = {
 	.s_stream	= ov5693_s_stream,
 	.s_mbus_fmt	= camera_common_s_fmt,
@@ -536,6 +547,7 @@ static struct v4l2_subdev_video_ops ov5693_subdev_video_ops = {
 	.try_mbus_fmt	= camera_common_try_fmt,
 	.enum_mbus_fmt	= camera_common_enum_fmt,
 	.g_mbus_config	= camera_common_g_mbus_config,
+	.g_input_status = ov5693_g_input_status,
 };
 
 static struct v4l2_subdev_core_ops ov5693_subdev_core_ops = {
