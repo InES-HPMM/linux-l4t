2dc638ea6eb9cea6f93e66bad23e9f2066463315
Shubham Chandra <shubhamc@nvidia.com>
2016-04-07 22:03:43 +0530

    media: i2c: sensor cropping feature
    Implementation of sensor cropping feature
    in new kernel driver for imx214/imx230 sensors
    using s_crop & g_crop ioctls.
    
    Bug 1687120
    
    Change-Id: Ib7ecf09a2f6b321530e91dd56fa27e07aa7a2feb
    Signed-off-by: Shubham Chandra <shubhamc@nvidia.com>
    Reviewed-on: http://git-master/r/1121968
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Ajith Kumar <ajithk@nvidia.com>
    Tested-by: Ajith Kumar <ajithk@nvidia.com>
    Reviewed-by: Shantanu Nath <snath@nvidia.com>

--- drivers/media/i2c/imx2xx.c
+++ drivers/media/i2c/imx2xx.c
@@ -96,7 +96,6 @@ const struct imx_sensor_data imx230_sensor_data = {
 #define IMX2XX_EEPROM_NUM_BLOCKS \
 	 (IMX2XX_EEPROM_SIZE / IMX2XX_EEPROM_BLOCK_SIZE)
 
-
 struct imx2xx {
 	struct camera_common_power_rail	power;
 	int				numctrls;
@@ -286,6 +285,37 @@ static inline void imx2xx_get_gain_short_reg(imx2xx_reg *regs,
 	regs->val = (gain >> 8) & 0xff;
 	(regs + 1)->addr = IMX2XX_GAIN_SHORT_ADDR_LSB;
 	(regs + 1)->val = (gain) & 0xff;
+}
+
+static void imx2xx_get_crop_regs(imx2xx_reg *regs,
+				struct v4l2_rect *rect)
+{
+	u32 x_start, y_start;
+	u32 x_end, y_end;
+	x_start = rect->left;
+	y_start = rect->top;
+	x_end = x_start + rect->width - 1;
+	y_end = y_start + rect->height - 1;
+
+	regs->addr = IMX2XX_CROP_X_START_ADDR_MSB;
+	regs->val = (x_start >> 8) & 0xff;
+	(regs + 1)->addr = IMX2XX_CROP_X_START_ADDR_LSB;
+	(regs + 1)->val = (x_start) & 0xff;
+
+	(regs + 2)->addr = IMX2XX_CROP_Y_START_ADDR_MSB;
+	(regs + 2)->val = (y_start >> 8) & 0xff;
+	(regs + 3)->addr = IMX2XX_CROP_Y_START_ADDR_LSB;
+	(regs + 3)->val = (y_start) & 0xff;
+
+	(regs + 4)->addr = IMX2XX_CROP_X_END_ADDR_MSB;
+	(regs + 4)->val = (x_end >> 8) & 0xff;
+	(regs + 5)->addr = IMX2XX_CROP_X_END_ADDR_LSB;
+	(regs + 5)->val = (x_end) & 0xff;
+
+	(regs + 6)->addr = IMX2XX_CROP_Y_END_ADDR_MSB;
+	(regs + 6)->val = (y_end >> 8) & 0xff;
+	(regs + 7)->addr = IMX2XX_CROP_Y_END_ADDR_LSB;
+	(regs + 7)->val = (y_end) & 0xff;
 }
 
 static int test_mode;
@@ -487,6 +517,8 @@ static int imx2xx_set_gain(struct imx2xx *priv, s32 val);
 static int imx2xx_set_frame_length(struct imx2xx *priv, s32 val);
 static int imx2xx_set_coarse_time(struct imx2xx *priv, s32 val);
 static int imx2xx_set_coarse_time_short(struct imx2xx *priv, s32 val);
+static int imx2xx_set_crop_data(struct imx2xx *priv, struct v4l2_rect *rect);
+static int imx2xx_get_crop_data(struct imx2xx *priv, struct v4l2_rect *rect);
 
 static int imx2xx_s_stream(struct v4l2_subdev *sd, int enable)
 {
@@ -559,8 +591,69 @@ exit:
 	return err;
 }
 
+static int imx2xx_s_crop(struct v4l2_subdev *sd, const struct v4l2_crop *crop)
+{
+	struct i2c_client *client = v4l2_get_subdevdata(sd);
+	struct camera_common_data *s_data = to_camera_common_data(client);
+	struct imx2xx *priv = (struct imx2xx *)s_data->priv;
+	struct v4l2_rect *rect = &crop->c;
+	int err;
+
+	u32 width, height;
+	u32 bottom, right;
+
+	width = s_data->fmt_width;
+	height = s_data->fmt_height;
+
+	if (crop->type != V4L2_BUF_TYPE_VIDEO_CAPTURE)
+		return -EINVAL;
+
+	if ((width != rect->width) || (height != rect->height)) {
+		dev_err(&client->dev,
+			"%s: CROP Resolution Mismatch: %dx%d\n",
+			__func__, rect->width, rect->height);
+		return -EINVAL;
+	}
+	if (rect->top < 0 || rect->left < 0) {
+		dev_err(&client->dev,
+			"%s: CROP Bound Error: left:%d, top:%d\n",
+			__func__, rect->left, rect->top);
+		return -EINVAL;
+	}
+	right = rect->left + width - 1;
+	bottom = rect->top + height - 1;
+	if ((right > s_data->def_width) ||
+		(bottom > s_data->def_height)) {
+		dev_err(&client->dev,
+			"%s: CROP Bound Error: right:%d, bottom:%d)\n",
+			__func__, right, bottom);
+		return -EINVAL;
+	}
+	err = imx2xx_set_crop_data(priv, rect);
+
+	return err;
+}
+
+static int imx2xx_g_crop(struct v4l2_subdev *sd, struct v4l2_crop *crop)
+{
+	struct i2c_client *client = v4l2_get_subdevdata(sd);
+	struct camera_common_data *s_data = to_camera_common_data(client);
+	struct imx2xx *priv = (struct imx2xx *)s_data->priv;
+	struct v4l2_rect *rect = &crop->c;
+	int err;
+
+	if (crop->type != V4L2_BUF_TYPE_VIDEO_CAPTURE)
+		return -EINVAL;
+
+	err = imx2xx_get_crop_data(priv, rect);
+
+	return err;
+}
+
 static struct v4l2_subdev_video_ops imx2xx_subdev_video_ops = {
 	.s_stream	= imx2xx_s_stream,
+	.s_crop		= imx2xx_s_crop,
+	.g_crop		= imx2xx_g_crop,
 	.s_mbus_fmt	= camera_common_s_fmt,
 	.g_mbus_fmt	= camera_common_g_fmt,
 	.try_mbus_fmt	= camera_common_try_fmt,
@@ -792,6 +885,71 @@ fail:
 	dev_dbg(&priv->i2c_client->dev,
 		 "%s: COARSE_TIME_SHORT control error\n", __func__);
 	return err;
+}
+
+static int imx2xx_set_crop_data(struct imx2xx *priv, struct v4l2_rect *rect)
+{
+	imx2xx_reg reg_list_crop[IMX2XX_NUM_CROP_REGS];
+	int err;
+	int i = 0;
+
+	dev_dbg(&priv->i2c_client->dev,
+		 "%s:  crop->left:%d, crop->top:%d, crop->res: %dx%d\n",
+		 __func__, rect->left, rect->top, rect->width, rect->height);
+
+	imx2xx_get_crop_regs(reg_list_crop, rect);
+	imx2xx_set_group_hold(priv);
+
+	for (i = 0; i < IMX2XX_NUM_CROP_REGS; i++) {
+		err = imx2xx_write_reg(priv->s_data, reg_list_crop[i].addr,
+			 reg_list_crop[i].val);
+		if (err) {
+			dev_dbg(&priv->i2c_client->dev,
+				"%s: SENSOR_CROP control error\n", __func__);
+			return err;
+		}
+	}
+
+	return 0;
+}
+
+static int imx2xx_get_crop_data(struct imx2xx *priv, struct v4l2_rect *rect)
+{
+	imx2xx_reg reg_list_crop[IMX2XX_NUM_CROP_REGS];
+	int i, err;
+	int a, b;
+	int right, bottom;
+
+	for (i = 0; i < IMX2XX_NUM_CROP_REGS; i++) {
+		reg_list_crop[i].addr = (IMX2XX_CROP_X_START_ADDR_MSB + i);
+		err = imx2xx_read_reg(priv->s_data, reg_list_crop[i].addr,
+			&reg_list_crop[i].val);
+		if (err) {
+			dev_dbg(&priv->i2c_client->dev,
+				"%s: SENSOR_CROP control error\n", __func__);
+			return err;
+		}
+	}
+
+	a = reg_list_crop[0].val & 0x00ff;
+	b = reg_list_crop[1].val & 0x00ff;
+	rect->left = (a << 8) | b;
+
+	a = reg_list_crop[2].val & 0x00ff;
+	b = reg_list_crop[3].val & 0x00ff;
+	rect->top = (a << 8) | b;
+
+	a = reg_list_crop[4].val & 0x00ff;
+	b = reg_list_crop[5].val & 0x00ff;
+	right = (a << 8) | b;
+	rect->width = right - rect->left + 1;
+
+	a = reg_list_crop[6].val & 0x00ff;
+	b = reg_list_crop[7].val & 0x00ff;
+	bottom = (a << 8) | b;
+	rect->height = bottom - rect->top + 1;
+
+	return 0;
 }
 
 static int imx2xx_eeprom_device_release(struct imx2xx *priv)
