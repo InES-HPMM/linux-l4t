9998120b3b6eb71ee58421ff66735d0eda788b88
Robert Collins <rcollins@nvidia.com>
2016-05-12 10:29:13 -0700

    Revert "iio: meter: Coverity: Use secure snprintf to prevent buffer overrun."
    This reverts commit deabbcecc70a29363d8750fdd3a970b8952c8b6a.
    
    Bug 1765352
    Bug 200199302
    Bug 200198106
    
    Signed-off-by: Robert Collins <rcollins@nvidia.com>
    Change-Id: I9576984c730625e13fcb2e33d897f2e18afa3941
    Reviewed-on: http://git-master/r/1146898
    (cherry picked from commit a0b285d279bbf401df76747d900c6cfa4e0f7169)
    Reviewed-on: http://git-master/r/1156595

--- drivers/staging/iio/meter/ina230.c
+++ drivers/staging/iio/meter/ina230.c
@@ -579,7 +579,7 @@ static int ina230_show_alert_flag(struct ina230_chip *chip, char *buf)
 	mutex_unlock(&chip->mutex);
 
 	alert_flag = (alert_flag >> 4) & 0x1;
-	return snprintf(buf, PAGE_SIZE, "%d\n", alert_flag);
+	return sprintf(buf, "%d\n", alert_flag);
 }
 
 static int ina230_hotplug_notify(struct notifier_block *nb,
@@ -667,12 +667,10 @@ static ssize_t ina230_show_channel(struct device *dev,
 
 	switch (this_attr->address) {
 	case CHANNEL_NAME:
-		return snprintf(buf, PAGE_SIZE, "%s\n",
-				chip->pdata->rail_name);
+		return sprintf(buf, "%s\n", chip->pdata->rail_name);
 
 	case CURRENT_THRESHOLD:
-		return snprintf(buf, PAGE_SIZE, "%d mA\n",
-				chip->pdata->current_threshold);
+		return sprintf(buf, "%d mA\n", chip->pdata->current_threshold);
 
 	case ALERT_FLAG:
 		return ina230_show_alert_flag(chip, buf);
@@ -681,8 +679,7 @@ static ssize_t ina230_show_channel(struct device *dev,
 		ret = ina230_get_vbus_voltage_current(chip, &current_ma,
 						      &voltage_mv);
 		if (!ret)
-			return snprintf(buf, PAGE_SIZE, "%d %d\n",
-					voltage_mv, current_ma);
+			return sprintf(buf, "%d %d\n", voltage_mv, current_ma);
 		return ret;
 
 	default:
