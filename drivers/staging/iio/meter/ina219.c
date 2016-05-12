2bb713418e6c9cab42e2ddaba574d487ae3ca429
Robert Collins <rcollins@nvidia.com>
2016-05-12 10:29:54 -0700

    Revert "iio: meter: Coverity: Use secure snprintf to prevent buffer overrun."
    This reverts commit 8b64f7f401ee53341a35f2187262d059f37407db.
    
    Bug 1765352
    Bug 200199302
    Bug 200198106
    
    Signed-off-by: Robert Collins <rcollins@nvidia.com>
    Change-Id: I5bce97b92f44f197a155bf6ca22b445a7e31da24
    Reviewed-on: http://git-master/r/1146899
    (cherry picked from commit bc6623967cdc27b0ded3362bb7ce3389da69d857)
    Reviewed-on: http://git-master/r/1156596

--- drivers/staging/iio/meter/ina219.c
+++ drivers/staging/iio/meter/ina219.c
@@ -515,12 +515,11 @@ static ssize_t ina219_show_channel(struct device *dev,
 
 	switch (this_attr->address) {
 	case CHANNEL_NAME:
-		return snprintf(buf, PAGE_SIZE, "%s\n",
-				chip->pdata->rail_name);
+		return sprintf(buf, "%s\n", chip->pdata->rail_name);
 
 	case CHANNEL_STATE:
 		mutex_lock(&chip->mutex);
-		count = snprintf(buf, PAGE_SIZE, "%d\n", chip->state);
+		count = sprintf(buf, "%d\n", chip->state);
 		mutex_unlock(&chip->mutex);
 		return count;
 
