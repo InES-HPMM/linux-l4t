d855b56128a75d5409bb3b5ba9976e603fb2ef72
Robert Collins <rcollins@nvidia.com>
2016-05-19 16:45:28 -0700

    Revert "Revert "iio: meter: Coverity: Use secure snprintf to prevent buffer overrun.""
    This reverts commit 11eb58677582c320c043e60f2ef942b64c29ad0a.
    
    Bug 1768847
    Bug 200199302
    
    Change-Id: I9b52a3a34d4b997e52fbcf8c93f6c2b973a70a3d
    Signed-off-by: Robert Collins <rcollins@nvidia.com>
    Reviewed-on: http://git-master/r/1150887
    (cherry picked from commit 6e99b208ff68d8accc327eff21bd93c2914888a8)
    Reviewed-on: http://git-master/r/1165228
    GVS: Gerrit_Virtual_Submit

--- drivers/staging/iio/meter/ina3221.c
+++ drivers/staging/iio/meter/ina3221.c
@@ -320,7 +320,7 @@ static int ina3221_get_mode(struct ina3221_chip *chip, char *buf)
 
 	v = (IS_TRIGGERED(chip->mode)) ? 0 : 1;
 	mutex_unlock(&chip->mutex);
-	return sprintf(buf, "%d\n", v);
+	return snprintf(buf, PAGE_SIZE, "%d\n", v);
 }
 
 static int ina3221_set_mode_val(struct ina3221_chip *chip, long val)
@@ -826,19 +826,19 @@ static ssize_t ina3221_show_channel(struct device *dev,
 
 	switch (mode) {
 	case CHANNEL_NAME:
-		return sprintf(buf, "%s\n",
-			chip->pdata->cpdata[channel].rail_name);
+		return snprintf(buf, PAGE_SIZE, "%s\n",
+				chip->pdata->cpdata[channel].rail_name);
 
 	case CRIT_CURRENT_LIMIT:
 		ret = ina3221_get_channel_critical(chip, channel, &current_ma);
 		if (!ret)
-			return sprintf(buf, "%d ma\n", current_ma);
+			return snprintf(buf, PAGE_SIZE, "%d ma\n", current_ma);
 		return ret;
 
 	case WARN_CURRENT_LIMIT:
 		ret = ina3221_get_channel_warning(chip, channel, &current_ma);
 		if (!ret)
-			return sprintf(buf, "%d ma\n", current_ma);
+			return snprintf(buf, PAGE_SIZE, "%d ma\n", current_ma);
 
 		return ret;
 
@@ -849,7 +849,8 @@ static ssize_t ina3221_show_channel(struct device *dev,
 		ret = ina3221_get_channel_vbus_voltage_current(chip,
 					channel, &current_ma, &voltage_mv);
 		if (!ret)
-			return sprintf(buf, "%d %d\n", voltage_mv, current_ma);
+			return snprintf(buf, PAGE_SIZE, "%d %d\n",
+					voltage_mv, current_ma);
 		return ret;
 
 	default:
