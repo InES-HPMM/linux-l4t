b6caf52061b26d676a592745c76317f19bb33cca
Arun Kannan <akannan@nvidia.com>
2016-06-01 17:50:52 -0700

    media: tegra_camera: update bypass driver name
    Update soc_camera bypass driver name
    
    Bug 200191959
    
    Change-Id: I520d9fd593bcec12fabc69473180262aabe3b18e
    Signed-off-by: Arun Kannan <akannan@nvidia.com>
    Reviewed-on: http://git-master/r/1161594
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Bhanu Murthy V <bmurthyv@nvidia.com>
    Reviewed-by: Kamal Balagopalan <kbalagopalan@nvidia.com>

--- drivers/media/platform/soc_camera/tegra_camera/vi_bypass.c
+++ drivers/media/platform/soc_camera/tegra_camera/vi_bypass.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2015, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2013-2016, NVIDIA CORPORATION.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
@@ -28,7 +28,7 @@
 #include "common.h"
 
 #define VI_BYPASS_CAM_DRV_NAME				"vi-bypass"
-#define VI_BYPASS_CAM_CARD_NAME				"vi-bypass"
+#define VI_BYPASS_CAM_CARD_NAME				"vi-output"
 #define VI_BYPASS_CAM_VERSION				KERNEL_VERSION(0, 0, 5)
 
 struct vi_bypass_camera_clk {
